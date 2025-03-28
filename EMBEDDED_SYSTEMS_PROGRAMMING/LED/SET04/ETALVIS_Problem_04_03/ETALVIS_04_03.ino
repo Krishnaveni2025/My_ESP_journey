a28a44fd86b7c4faee89c8701f82199a712640a3
Krishnaveni2025 <krishnavenipurne@gmail.com>
2025-03-28 20:38:00 +0530

    Add files via upload

---
+++ EMBEDDED_SYSTEMS_PROGRAMMING/LED/SET04/ETALVIS_Problem_04_03/ETALVIS_04_03.ino
@@ -0,0 +1,71 @@
+//EMBEDDED SYSTEMS PROGRAMMING - GPIO
+//LED Control Problems
+//SET 04 Problem 04
+//Connect 8 LEDs to Port A from bit0 to bit7
+//Connect 8 LEDs to Port B from bit0 to bit7
+//Problem: Blink one by one all the 16 leds from top  to the bottom
+void delay1();//delay function declaration
+void row1();
+void row2();
+void setup() {
+  // put your setup code here, to run once:
+  volatile char *ddrA, *ddrB;
+  ddrA = 0x21;
+  ddrB = 0x24;
+  *ddrA = 0xFF; //setting entire PORTA as OUTPUT
+  *ddrB = 0xFF;//setting entire PORTB as OUTPUT
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  row2();//2nd row leds glows
+  row1();//1st row leds glows
+
+}
+void row1()
+{
+  volatile char *outA, a;
+  outA = 0x22; a = 7;
+  unsigned char blinka[8] = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
+  for (a = 7; a > 0; a--)
+  {
+    *outA = blinka[a];
+    delay1();
+    *outA = 0x00;
+    delay1();
+    *outA = blinka[a];
+    delay1();
+  }
+  if(a==0)
+  {
+    *outA = blinka[a];
+    delay1();
+    *outA = 0x00;
+  }
+}
+void row2()
+{
+  volatile char *outB, b;
+  outB = 0x25; b = 7;
+  unsigned char blinkb[8] = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
+  for (b = 7; b > 0; b--)
+  {
+    *outB = blinkb[b];
+    delay1();
+    *outB = 0x00;
+    delay1();
+    *outB = blinkb[b];
+    delay1();
+  }
+   if(b==0)
+  {
+    *outB = blinkb[b];
+    delay1();
+    *outB = 0x00;
+  }
+}
+void delay1()
+{
+  volatile long d;
+  for (d = 0; d < 600000; d++);
+}
