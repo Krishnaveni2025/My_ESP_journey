4de63aa7946915cfba7a6cc644e00e9b3dc1c61d
Krishnaveni2025 <krishnavenipurne@gmail.com>
2025-03-28 18:09:39 +0530

    Add files via upload

---
+++ EMBEDDED_SYSTEMS_PROGRAMMING/LED/SET03/ETALVIS_Problem_03_01/ETALVIS_Problem_03_01.ino
@@ -0,0 +1,29 @@
+//EMBEDDED SYSTEMS PROGRAMMING - GPIO
+//LED Control Problems
+//SET 03 Problem 01
+//Connect 8 LEDs to Port A from bit0 to bit7
+//Problem: GLow the LEDs in the following order
+//3,7 delay - 2,6 delay - 1,5 delay - 0,4 delay
+void delay1();//delay function declaration
+void setup() {
+  // put your setup code here, to run once:
+  volatile char *ddrA,*outA;
+  ddrA = 0x21;outA=0x22;
+  *ddrA=0xFF;//setting entire PORTA as OUTPUT
+  unsigned char glowLeds[4]={0x88,0x44,0x22,0x11};
+  volatile int g ;
+  for(g=0;g<4;g++)
+  {
+    *outA = glowLeds[g];
+    delay1();//calling delay function
+  }
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+void delay1()//delay function defination
+{
+  volatile long d;
+  for(d=0;d<=600000;d++);
+}
