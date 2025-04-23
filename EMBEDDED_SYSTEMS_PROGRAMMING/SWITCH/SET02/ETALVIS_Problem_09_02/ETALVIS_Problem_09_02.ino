8b09e5928623429897d47e5f7143118293d6f89f
Krishnaveni2025 <krishnavenipurne@gmail.com>
2025-04-23 21:29:50 +0530

    Add files via upload

---
+++ EMBEDDED_SYSTEMS_PROGRAMMING/SWITCH/SET02/ETALVIS_Problem_09_02/ETALVIS_Problem_09_02.ino
@@ -0,0 +1,34 @@
+//EMBEDDED SYSTEMS PROGRAMMING - GPIO
+//SWITCH MONITOR  PROBLEMS
+//SET 09 PROBLEM 02
+//Connect 8 LEDs on PORT A and connect 8 push button switches on PORT B
+//Press  3rd switch first glow 3rd LED delay, 1st LED delay,7th LED delay,4th LED delay.
+void delay1(void);
+void setup() {
+  // put your setup code here, to run once:
+  volatile char *ddra, *ddrb;
+  ddra = 0x21; ddrb = 0x24;
+  *ddra = 0xFF; // Set Port A as output
+  *ddrb = 0x00; // Set Port B as output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  volatile char *outA, *inB, x;
+  outA = 0x22; inB = 0x23;
+  int count = 0;
+  unsigned char led[4] = {0x04, 0x02, 0x80, 0x10};
+  while((*inB & 0x08)==0x08)
+  {
+    for(count = 0;count<4;count++)
+    {
+      *outA = led[count];
+      delay1();
+    }
+  }
+}
+void delay1(void)
+{
+  volatile long d;
+  for (d = 0; d < 1000000; d++);
+}
