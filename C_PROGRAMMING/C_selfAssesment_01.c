9b5e6629f3128dbf9795ecce2673518bd935e0b7
Krishnaveni2025 <krishnavenipurne@gmail.com>
2025-04-24 23:13:45 +0530

    Add files via upload

---
+++ C_PROGRAMMING/C_selfAssesment_01.c
@@ -0,0 +1,113 @@
+/*C LEVEL 1 - PROBLEM 1
+Question: Get anumber from thr user , add 2 to that number, and print the result.
+Example:
+Input:45     Output:47
+Input:56789  Output:56791
+
+#include<stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter a Number: ");
+    scanf("%d",&x);
+    //your code here
+    y = x+2;
+    printf("Result = %d",y);
+}
+    */
+/*C LEVEL 1 - PROBLEM 2
+Question: Get anumber from thr user , subtract 5 to that number, and print the result.
+Example:
+Input:45     Output:40
+Input:56789  Output:56784
+
+#include<stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter a Number: ");
+    scanf("%d",&x);
+    //your code here
+    y = x-5;
+    printf("Result = %d",y);
+}*/
+/*C LEVEL 1 - PROBLEM 3
+Question: Get anumber from thr user , multiply 3 by that number, and print the result.
+Example:
+Input:45     Output:135
+Input:1200  Output:3600
+
+#include<stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter a Number: ");
+    scanf("%d",&x);
+    //your code here
+    y = x*3;
+    printf("Result = %d",y);
+}*/
+/*C LEVEL 1 - PROBLEM 4
+Question: Get anumber from thr user , divide the number by 6, and print the result.
+Example:
+Input:45     Output:7
+Input:143  Output:23
+
+#include<stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter a Number: ");
+    scanf("%d",&x);
+    //your code here
+    y = x/6;
+    printf("Result = %d",y);
+}*/
+/*C LEVEL 1 - PROBLEM 5
+Question: Get anumber from thr user , divide the number by 8, and print the reminder.
+Example:
+Input:45     Output:5
+Input:143  Output:7
+
+#include<stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter a Number: ");
+    scanf("%d",&x);
+    //your code here
+    y = x%8;
+    printf("Result = %d",y);
+}*/
+/*C LEVEL 1 - PROBLEM 6
+Question: Get a two digitnumber from thr user and print the one's digit.
+Example:
+Input:45  Output:5
+Input:56  Output:6
+
+#include<stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter a two-digt Number: ");
+    scanf("%d",&x);
+    //your code here
+    y = x%10;
+    printf("Result = %d",y);
+}*/
+/*C LEVEL 1 - PROBLEM 7
+Question: Get a two digit number from thr user and print the ten's digit.
+Example:
+Input:45  Output:4
+Input:56  Output:5
+
+#include<stdio.h>
+int main()
+{
+    int x, y;
+    printf("Enter a two-digt Number: ");
+    scanf("%d",&x);
+    //your code here
+    y = x/10;
+    printf("Result = %d",y);
+}*/
