f3a6b31b51637420e5bec911da9c4e8830bf1a08
Krishnaveni2025 <krishnavenipurne@gmail.com>
2025-05-06 18:08:03 +0530

    Add files via upload

---
+++ C_PROGRAMMING/C_selfAssesment_13.c
@@ -0,0 +1,100 @@
+/*  C PROGRAMMING SELF ASSESSMENT 13*/
+/*1. Create a linnk list for the follwing structure and get
+user input of id,Maths Mark. Science Mark.
+Once the user inputs id as -1 exit the input mode 
+and display the entries.
+*****Hint***** 
+struct student
+{
+int id;
+int maths;
+int science;
+struct student *next;
+};
+*/
+/*2. Create a sample link list with about 5 entries
+using the following  Structure. 
+Insert a new entry before or after a given id.
+
+Menu Items
+1.Insert Entry
+2.Display List
+3.Exit
+*****Hint*****
+struct student
+{
+int id;
+int maths;
+int science;
+struct student *next;
+};
+*/
+/*3.
+Create a sample link list with about 5 entries
+using the following  Structure. 
+Insert a new entry before or after a given id.
+
+Menu Items
+1.Insert Entry
+2.Delete Entry
+3.Display List
+4.Exit
+struct student
+{
+int id;
+int maths;
+int science;
+struct student *next;
+};*/
+/*4. Create a dpubly linked list with about 5 entries
+using the following  Structure.
+Insert a new entry before or after a given id.
+Menu Items
+1.Insert Entry
+2.Delete Entry
+3.Display List
+4.Exit
+struct student
+{
+int id;
+int maths;
+int science;
+struct student *next;
+struct student *prev;
+};*/
+/*5. Write a program to create a stack using linked list. 
+Use pus & Pop . 
+Push wiill insert the entry in top of the list. 
+Pop will get top of thew list and delete it.
+Display will show from top to bottom.
+Menu Items
+1.Push
+2.Pop
+3.Display
+4.Exit
+struct student
+{
+int id;
+int maths;
+int science;
+struct student *next;
+};*/
+/*6. Write a program to create a queue using linked list.
+Use add and remove . 
+Add will insert the entry in top of the list. 
+Remove will get top of the list and delete it.
+Display will show from top to bottom.
+Menu Items
+1.Add
+2.Remove
+3.Display
+4.Exit
+struct student
+{
+int id;
+int maths;
+int science;
+struct student *next;
+};*/
+
+
