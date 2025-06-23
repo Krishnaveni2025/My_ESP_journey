d9e528e61801bcfc27acf4e11d0df45a9fb5b1c8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-06-23 19:40:26 +0530

    Update DAY02.c

--- C_PROGRAMMING/Daily_C_Programming/DAY02.c
+++ C_PROGRAMMING/Daily_C_Programming/DAY02.c
@@ -1,1 +1,29 @@
-
+/* Write a loop program 
+to print 1 to 5 one by one. 
+Write a funnction and print the result in a main  function. 
+Function NAme : arrange_ascend 
+Answer: 
+1
+2
+3
+4
+5
+*/
+#include<stdio.h>
+void arrange_ascend(int *ptr);
+int main()
+{
+    int num,*p;
+    p=&num;
+    arrange_ascend(p);
+    return 0;
+}
+void arrange_ascend(int *ptr)
+{
+int i;
+for(i=1;i<=5;i++)
+{
+*ptr=i;
+printf("%d\n",*ptr);
+}
+}
