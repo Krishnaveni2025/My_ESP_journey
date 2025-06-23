3f04a9b834400e099f17579fdb09b7b07e5e69d0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-06-23 16:39:41 +0530

    Add files via upload

---
+++ EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET01/ETALVIS_Problem_11_09/ETALVIS_Problem_11_09.ino
@@ -0,0 +1,32 @@
+//Embedded Systems programming - GPIO
+//Dot Matrix Control Problems
+//use PORT A for ROWS and PORTB for COLUMNs
+//self Assessment set -11 Problem 09
+//Display diagonal LEDs connecting left top LED to Right bottom LED
+
+void setup() {
+  // put your setup code here, to run once:
+  volatile char *ddra, *ddrb; //both port A and B as output
+  ddra = 0x21; *ddra = 0xFF;
+  ddrb = 0x24; *ddrb = 0xFF;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  volatile char *porta, *portb, row, col;
+  porta = 0x22; portb = 0x25;
+  row = *porta;
+  col = *portb;
+
+  for (row = 0; row < 8; row++)
+  {
+    *porta = (1 << row);           // Set only one row HIGH
+    *portb = ~(1 << row);          // Clear only the corresponding column (active LOW)
+    for ( i = 0; i < 500000; i++); // small delay to make LED visible
+    *porta = 0x00;
+    *portb = 0xFF;
+    for ( i = 0; i < 500000; i++);
+  }
+
+}
