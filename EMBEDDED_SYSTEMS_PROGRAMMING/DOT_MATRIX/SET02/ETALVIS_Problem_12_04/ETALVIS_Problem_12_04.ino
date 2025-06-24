134cae39186a9be985274b6930409a4fb81667d3
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-06-24 14:09:19 +0530

    Add files via upload

---
+++ EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET02/ETALVIS_Problem_12_04/ETALVIS_Problem_12_04.ino
@@ -0,0 +1,42 @@
+//Embedded Systems programming - GPIO
+//Dot Matrix Control Problems
+//use PORT A for ROWS and PORTB for COLUMNs
+//self Assessment set -12 Problem 04
+//Display Number 2
+
+void setup() {
+  volatile char *ddra = 0x21;
+  volatile char *ddrb = 0x24;
+  *ddra = 0xFF;  // Set PORTA as output (Rows)
+  *ddrb = 0xFF;  // Set PORTB as output (Columns)
+}
+
+void loop() {
+  volatile char *porta = 0x22;
+  volatile char *portb = 0x25;
+  char display[8] = {0xC7, 0x99, 0x99, 0xF3,0xE7,0xCF, 0x81, 0x01}; // '1'
+  while (1)
+  {
+
+    for (int row = 0 ; row < 8; row++)
+    {
+      *porta = (1 << row);
+      *portb = display[row];
+      displayoff();
+      delay1();
+    }
+
+  }
+}
+void displayoff(void)
+{
+  volatile char *porta = 0x22;
+  volatile char *portb = 0x25;
+  *porta = 0x00;
+  *portb = 0xff;
+}
+void delay1(void)
+{
+  volatile long d;
+  for (d = 0; d <= 100; d++);
+}
