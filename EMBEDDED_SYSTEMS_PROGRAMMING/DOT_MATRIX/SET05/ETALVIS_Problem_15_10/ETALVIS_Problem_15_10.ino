7e173ff11914739576e82e67baed7eac0e5c29b3
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-06-28 15:40:43 +0530

    Update ETALVIS_Problem_15_10.ino

--- EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET05/ETALVIS_Problem_15_10/ETALVIS_Problem_15_10.ino
+++ EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET05/ETALVIS_Problem_15_10/ETALVIS_Problem_15_10.ino
@@ -2,55 +2,12 @@
 //Dot Matrix Control Problems
 //use max 7219-matrix DOTMATRIX
 //use PORT A
-//self Assessment set -15 Problem 09
-//DISPLAY "ETAL" by entering from top
+//self Assessment set -15 Problem 10
+//DISPLAY "1234" by entering from bottom
+
 
 #include "max7219.h"
-
-// ETAL Letter Bitmaps (top to bottom animation)
-unsigned char letterE[8] = {
-    0b00000000,
-    0b01111110,
-    0b00000010,
-    0b00000010,
-    0b01111110,
-    0b00000010,
-    0b00000010,
-    0b01111110
-};
-
-unsigned char letterT[8] = {
-    0b00000000,
-    0b00010000,
-    0b00010000,
-    0b00010000,
-    0b00010000,
-    0b00010000,
-    0b00010000,
-    0b01111100
-};
-
-unsigned char letterA[8] = {
-    0b00000000,
-    0b01000010,
-    0b01000010,
-    0b01000010,
-    0b01111110,
-    0b01000010,
-    0b01000010,
-    0b00111100
-};
-
-unsigned char letterL[8] = {
-    0b00000000,
-    0b01111110,
-    0b00000010,
-    0b00000010,
-    0b00000010,
-    0b00000010,
-    0b00000010,
-    0b00000010
-};
+#include "font_digits.h"
 
 void delay_ms(unsigned int ms) {
     for (unsigned int i = 0; i < ms; i++)
@@ -60,19 +17,18 @@ void delay_ms(unsigned int ms) {
 void setup(void) {
     max7219_init();
 
-    // Animate ETAL entering from the top row by row
     for (char shift = -7; shift <= 0; shift++) {
         for (unsigned char row = 1; row <= 8; row++) {
-            signed char index = shift + row - 1;
+            char index = shift + row - 1;
 
-            unsigned char e = (index >= 0 && index < 8) ? letterE[index] : 0x00;
-            unsigned char t = (index >= 0 && index < 8) ? letterT[index] : 0x00;
-            unsigned char a = (index >= 0 && index < 8) ? letterA[index] : 0x00;
-            unsigned char l = (index >= 0 && index < 8) ? letterL[index] : 0x00;
+            unsigned char d1 = (index >= 0 && index < 8) ? digit1[index] : 0x00;
+            unsigned char d2 = (index >= 0 && index < 8) ? digit2[index] : 0x00;
+            unsigned char d3 = (index >= 0 && index < 8) ? digit3[index] : 0x00;
+            unsigned char d4 = (index >= 0 && index < 8) ? digit4[index] : 0x00;
 
-            max7219_send_row4(9 - row, l, a, t, e);  // Send from top to bottom
+            max7219_send_row4(row, d4, d3, d2, d1);
         }
-        delay_ms(100);  // Adjust animation speed
+        delay_ms(100); // Tune animation speed
     }
 }
 
