d70dc6e06f2c63b8857313f8ff8d040f1d633a88
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-06-28 16:25:34 +0530

    Create max7219.h

---
+++ EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET06/ETALVIS_Problem_16_02/max7219.h
@@ -0,0 +1,17 @@
+#ifndef MAX7219_H
+#define MAX7219_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void max7219_init(void);
+void max7219_clear_all(void);
+void max7219_send_row4(unsigned char row, unsigned char d1, unsigned char d2,
+                       unsigned char d3, unsigned char d4);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
