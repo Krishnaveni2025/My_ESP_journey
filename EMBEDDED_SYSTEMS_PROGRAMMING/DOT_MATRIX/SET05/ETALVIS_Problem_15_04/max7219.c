5e7c546d5648861dcebd7c0555115de851d80a5e
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-06-28 14:17:12 +0530

    Create max7219.c

---
+++ EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET05/ETALVIS_Problem_15_04/max7219.c
@@ -0,0 +1,53 @@
+#include "max7219.h"
+
+volatile char *porta = (volatile char *)0x22;
+volatile char *ddra  = (volatile char *)0x21;
+
+#define DIN_BIT 0
+#define CLK_BIT 1
+#define CS_BIT  2
+
+void max7219_send_all(unsigned char addr, unsigned char d1, unsigned char d2,
+                      unsigned char d3, unsigned char d4) {
+    unsigned char data[4] = {d1, d2, d3, d4};
+
+    *porta &= ~(1 << CS_BIT);
+
+    for (char m = 3; m >= 0; m--) {
+        unsigned int packet = (addr << 8) | data[m];
+        for (char i = 0; i < 16; i++) {
+            if (packet & 0x8000)
+                *porta |= (1 << DIN_BIT);
+            else
+                *porta &= ~(1 << DIN_BIT);
+
+            *porta &= ~(1 << CLK_BIT);
+            for (volatile int d = 0; d < 10; d++);
+            *porta |= (1 << CLK_BIT);
+            packet <<= 1;
+        }
+    }
+
+    *porta |= (1 << CS_BIT);
+}
+
+void max7219_init(void) {
+    *ddra = 0xFF;
+    *porta = (1 << CS_BIT);
+
+    for (char i = 0; i < 4; i++) {
+        max7219_send_all(0x0C, 0x01, 0x01, 0x01, 0x01);
+        max7219_send_all(0x09, 0x00, 0x00, 0x00, 0x00);
+        max7219_send_all(0x0A, 0x08, 0x08, 0x08, 0x08);
+        max7219_send_all(0x0B, 0x07, 0x07, 0x07, 0x07);
+        max7219_send_all(0x0F, 0x00, 0x00, 0x00, 0x00);
+    }
+
+    max7219_clear();
+}
+
+void max7219_clear(void) {
+    for (unsigned char row = 1; row <= 8; row++) {
+        max7219_send_all(row, 0x00, 0x00, 0x00, 0x00);
+    }
+}
