776bc55e462e1fca779c040c38f1c6e4dc8e0aa8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-06-28 15:42:04 +0530

    Update max7219.h

--- EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET05/ETALVIS_Problem_15_10/max7219.h
+++ EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET05/ETALVIS_Problem_15_10/max7219.h
@@ -6,9 +6,9 @@ extern "C" {
 #endif
 
 void max7219_init(void);
+void max7219_clear_all(void);
 void max7219_send_row4(unsigned char row, unsigned char d1, unsigned char d2,
                        unsigned char d3, unsigned char d4);
-void max7219_clear_all(void);
 
 #ifdef __cplusplus
 }
