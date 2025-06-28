52076f6b45144737d172d556245dffb4cca3de13
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-06-28 13:15:21 +0530

    Update ETALVIS_Problem_14_04.ino

--- EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET04/ETALVIS_Problem_14_04/ETALVIS_Problem_14_04.ino
+++ EMBEDDED_SYSTEMS_PROGRAMMING/DOT_MATRIX/SET04/ETALVIS_Problem_14_04/ETALVIS_Problem_14_04.ino
@@ -3,13 +3,13 @@
 //use max 7219-matrix DOTMATRIX
 //use PORT A 
 //self Assessment set -14 Problem 04
-//Display RIGHT most BOTTOM LED.
+//Display LEFT most BOTTOM LED.
 
 #include "max7219.h"
 
 void setup(void) {
     max7219_init();
-    max7219_display_pixel(8, 1);  // RIGHT most BOTTOM LED (row 8, column 8)
+    max7219_display_pixel(8, 8);  // LEFT most BOTTOM LED (row 8, column 8)
 }
 
 void loop(void) {
