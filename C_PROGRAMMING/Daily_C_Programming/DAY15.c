1d75bff5f4cc042d216437ab3112953cc4301f68
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-07-24 19:39:23 +0530

    Update DAY15.c

--- C_PROGRAMMING/Daily_C_Programming/DAY15.c
+++ C_PROGRAMMING/Daily_C_Programming/DAY15.c
@@ -1,1 +1,20 @@
+//c program for converting
+//binary to decimal
+#include <stdio.h>
 
+int main()
+{
+    int binary, decimal = 0, base = 1, rem;
+    printf("Enter a binary number: ");
+    scanf("%d", &binary);// Input binary number
+    // Convert binary to decimal
+    while (binary > 0)// Loop until binary is 0
+    {
+        rem = binary % 10;// Get the last digit
+        decimal = decimal + rem * base;// Add the digit to the decimal
+        binary = binary / 10;// Remove the last digit
+        base = base * 2;// Multiply the base by 2
+    }
+    printf("Decimal equivalent: %d\n", decimal);// Print the decimal equivalent
+    return 0;
+}
