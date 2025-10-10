e83c04b2500caaf5c3e0b781e4f0aebb1887efb3
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-10-10 22:52:12 +0530

    Create Day047.c

---
+++ C_PROGRAMMING/Daily_C_Programming/Day047.c
@@ -0,0 +1,595 @@
+/*=================================== C LEVEL 1 ======================================================
+  1. Get a number from the user and add 2 to that number and print the result.
+  2. Get a number from the user and subtract 5 from that number and print the result
+  3. Get a number from the user and multiply 3 by the number and print the result
+  4. Get a number from the user and divide the number by 6 and print the result
+  5. Get a number from the user and divide the number by 8 and print the remainder
+  6. Get a two-digit number from the user and print it's one's digit
+  7. Get a two-digit number from the user and print it's ten's digit
+  8. Get a three-digit number from the user and print it's one's digit
+  9. Get a three-digit number from the user and print it's hundred's digit
+  10. Get a three-digit number from the user and print it's ten's digit
+  11. Get a two-digit number from the user and print the sum of the digits of the number.
+  12. Get a three-digit number from the user and print it's sum of digits
+  13. Get a two-digit number froom the user and print reverse of  number
+  14. Get a three-digit number from the user and print reverse of the number
+  15. Get a four-digit number from the user and reverse only first two digits (one's & ten's)of the number then print the number
+  16. Get a four-digit number from the user and reverse only last two digits (1000's & 100's digits) of the number, then print the number
+  17. Get a two-digit number from the user and make the one's digit 0, then print it
+  18. Get a two-digit number from the user and make the ten's digit 1, then print it
+  19. Get a three-digit number from the user and make the one's digit 2, then print it
+  20. Get a three-digit number from the user and make ten's digit as 0, then print it.
+  21. Get a number from the user and subtract 5 from that number if the number is odd, then print the result. Do not use 'if'
+  22. Get a number from the user and subtract 5 from that number if the number's ten's digit is odd, then print the result. Do not use 'if'
+  23. Get a two-digit number from the user and subtract 5 from that number if the sum of the digits of the number is odd, then print the result. Do not use 'if'
+  24. Get a three-digit number from the user and subtract 5 from the number if one's digit and 100's digit are odd, then print the result. Do not use 'if'
+  25. Get a three-digit number from the user, if the sum of the digits is less than 10 then print the sum, otherwise print the digits of the sum. If the sum of the digits is less than 10 print the sum, otherwise add the digits of the sum and print the sum.
+        Note: The result should always be a single digit only
+        =========================C LEVEL 2====================================
+1) use only int variables
+2) use only decimals
+3) use the following operations only
+    +,-,*,/,%,&, ==,>,<,!=,<=,=>
+4) use only if, for, while, arrays, pointers, strings etc
+1. Get a number from the user and check if the number is equals to 50. If yes, then print 1, otherwise print 0
+2. Get a number from the user and check if the number is not equal to 50. If yes, then print 1, otherwise print 0
+3. Get a number from the user and check if the number is less than 50. If yes, then print 1, otherwise print 0
+4. Get a number from the user and check if the number is greater than 50. If yes, then print 1, otherwise print 0
+5. Get a number from the user and check if the number is less than or equal to 50. If yes then print 1, otherwise print 0
+6. Get a number from the user and check if the number is greater than or equals to 50. If yes then print 1, otherwise print 0
+7. Get a two-digit number from the user and check if the digit 0 and digit 1 are identical. If yes, then print 1, otherwise print 0
+8. Get a two-digit number from the user and check if the digit 0 and digit 1 are not identical. If yes, then print 1, otherwise print 0
+9. Get a two-digit number from the user and check if the digit 0 is less than the digit 1. If yes print 1; otherwise print 0
+10. Get a two digit number from the user and check if the digit 0 is greater than digit 1. If yes print 1: otherwise print 0
+11. Get a two-digit number from the user and check if the digit 1 is less than or equals to digit 0. If yes print 1; otherwise print 0
+12. Get a two-digit number from the user and check if the digit 1 is greater than or equal to digit 0. If yes print 1; otherwise print 0
+13. Get a three-digit number from the user and check if the digit 1 and digit 0 are same and digit 1 , digit 2 are different. If yes print 1; otherwise print 0
+14. Get a four-digit number from the user and check if the first two digits and last two digits are same. If yes print 1; otherwise print 0
+
+
+1) Use int variables
+2) Use only decimals
+3) Use if/else statement.
+=======================C LEVEL 3==========================================
+1. Get a number from the user and check if the number equals 50. If yes, then print "SUCCESS", otherwise print "Failure"
+2. Get a number from the user and check if the number is not equals to 50. If yes, then print "Success", otherwise print "Failure"
+3. Get a number from the user and check if the number is less than 50. If yes, then print "Success", otherwise print "Failure"
+4. Get a number from the user and check if the number is greater than 50. If yes, then print "Success", otherwise print "Failure"
+5. Get a number from the user and check if the number is less than or equal to 50. IF yes, then print "Success", otherwise print "Failure"
+6. Get a number from the user and check if the number is greater than or equal to 50. If yes print "Success", otherwise print "Failure"
+7. Get a two-digit number from the user and check if the digit 0 and  digit 1 are identical. If yes, print "Success", otherwise print "Failure"
+8. Get a two-digit number from the user and check if the digit 0 and  digit 1 are not identical. If yes, print "Success", otherwise print "Failure"
+9. Get a two-digit number from the user and check if the digit 0 is less than digit 1. If yes, print "Success", otherwise print "Failure"
+10. Get a two-digit number from the user and check if the digit 0 is greater than digit 1. If yes, print "Success", otherwise print "Failure"
+11. Get a two-digit number from the user and check if the digit 1 is less than or equal to the digit 0. If yes, print "Success", otherwise print "Failure"
+12. Get a two-digit number from the user and check if the digit 1 is greater than or equal to the digit 0. If yes, print "Success", otherwise print "Failure"
+13. Get a three-digit number from the user and check if the digit 0 and digit 1 are same and if digit 1 and digit 2 are different. If yes, print "Success", otherwise print "Failure"
+14. Get a four-digit number from the user and check if the first 2 digits and last two digits are same. If yes, print "Success", otherwise print "Failure"
+
+
+1) Use only int variables
+2) Use only decimals
+3) Use only "if/else ", goto statements. Do not use "for" or "while"
+4) Don't use pointers,arrays,strings,etc.
+==========================C LEVEL 4=========================================
+1. Write a loop program to print 1 to 5 on one by one
+2. Write a loop program to print 5 to 1 on one by one
+3. write a loop program to print sum of 1 to 5
+4. Write a loop program to print sum of 6 to 1
+5. write a loop program to print odd numbers, between 1 and 9
+6. Write a loop program to print odd numbers, below 20
+7. Write a loop program to print two digit odd numbers whose sum of digits are 7
+8. Write a loop program to print two digit even numbers whose sum of digits are 6
+9. same as 8
+10. Write a loop program to print the sum of two digit odd numbers, whose ten's digit is 7
+11. Write a program to get a number from the user and print the total number of digits in that number
+12. Write a program to get a number from the user and print sum of all digits
+13. Write a program to get a number from the user and print the reverse of that number
+14. Write a program to get a number from the user and interchange the first and last digits and print the result
+15. Write a program to get a number from the user and if the last digit of the number is even print the same number. If the last digit of the number is odd then subtract 1 from the last digit and print the number
+16. Write a program to get a number from the user and print whether that number is prime or not
+17. same as 16*/
+#include <stdio.h>
+
+int main()
+{
+  #if 0
+#if 0
+    int x, y;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+// your code here
+// C LEVEL 1
+//  y = x+2;//1
+// y = x-5;//2
+// y = x*3;//3
+// y = x/6;//4
+// y = x%8;//5
+// y = x%10;//6
+// y = x/10;//7
+// y = x%10;//8
+// y = (x/100);//9
+// y = (x/10)%10;//10
+// y = ((x/10)+(x%10));//11
+// y = ((x/100)+(x%10)+((x/10)%10));//12
+// y = (((x%10)*10)+(x/10));//13
+// y = (((x%10)*100)+((x/10)%10)+(x/100));//14
+// y = ((x/100)*100)+((x%10)*10)+((x/10)%10);//15
+// y = (((x/100)%10)*1000)+((x/1000)*100)+(((x/10)%10)*10)+(x%10);//16
+// y = ((x/10)*10);//17
+// y = (10*(x%10));//18
+// y = ((x/10)*10)+2;//19
+// y = ((x/100)*100)+(x%10);//20
+#if 0
+    y = x%2; 
+    while(y!=0)
+    {
+            y = x-5;
+            break;
+    }
+    while (y==0)
+    {
+            y = x;
+            break;
+    }
+#endif // 21
+#if 0
+ while((x/10)%10)
+ {
+    y = x-5;
+    break;
+ }
+ while (!((x/10)%10))
+ {
+    y = x;
+    break;
+ }
+#endif // 22
+#if 0
+ while((((x%10)+(x/10))%2))
+{
+    y = x-5;
+    break;
+}
+while(!(((x%10)+(x/10))%2))
+{
+    y = x;
+    break;
+}
+#endif // 23
+#if 0
+while((x/100)%2)
+{
+    while ((x%10)%2)
+    {
+        y = x-5;
+        break;
+    }
+    break;
+}
+#endif // 24
+#if 0
+y = ((x/100)+(x%10)+((x/10)%10));
+while(y>10)
+{
+    y = (y/10)+(y%10);
+    while (y==10)
+    {
+        y = 1;
+        break;
+    }
+    break;
+}
+#endif // 25
+    // C LEVEL 2
+    // y = x==50;//1
+    // y = x!=50;//2
+    // y = x<50;//3
+    // y = x>50;//4
+    // y = x<=50;//5
+    // y = x>=50;//6
+    // y = ((x%10)==(x/10));//7
+    // y = (!((x%10)==(x/10)));//8
+    // y = ((x%10)<(x/10));//9
+    // y = ((x%10)>(x/10));//10
+    // y = ((x/10)<=(x%10));//11
+    // y = ((x/10)>=(x%10));//12
+    // y = (((x%10)==((x/10)%10))&((x/100)!=((x/10)%10)));//13
+    // y = (x/100)==(x%100);//14
+    printf("Result = %d", y);
+// C LEVEL 3
+#if 0
+if(x==50)
+    printf("Success");
+else
+        printf("Failure");
+#endif // 1
+#if 0 
+if(x!=50)
+        printf("Success");
+else
+        printf("Failure");
+#endif // 2
+#if 0 
+if(x<50)
+        printf("Success");
+else
+        printf("Failure");
+#endif // 3
+#if 0 
+if(x>50)
+        printf("Success");
+else
+        printf("Failure");
+#endif // 4
+#if 0 
+if(x<=50)
+        printf("Success");
+else
+        printf("Failure");
+#endif // 5
+#if 0 
+if(x>=50)
+        printf("Success");
+else
+        printf("Failure");
+#endif // 6
+#if 0
+if((x%10)==(x/10))
+        printf("Success");
+else
+        printf("Failure");
+#endif // 7
+#if 0
+if((x%10)!=(x/10))
+        printf("Success");
+else
+        printf("Failure");
+#endif // 8
+#if 0
+if((x%10)<(x/10))
+        printf("Success");
+else
+        printf("Failure");
+#endif // 9
+#if 0
+if((x%10)>(x/10))
+        printf("Success");
+else
+        printf("Failure");
+#endif // 10
+#if 0
+if((x/10)<=(x%10))
+        printf("Success");
+else
+        printf("Failure");
+#endif // 11
+#if 0
+if((x/10)>=(x%10))
+        printf("Success");
+else
+        printf("Failure");
+#endif // 12
+#if 0
+if((x%10)==((x/10)%10))
+{
+if((x/100)!=((x/10)%10))
+        printf("Success");
+}
+else
+{
+printf("Failure");
+}
+#endif // 13
+#if 0
+if((x%100)==(x/100))
+     printf("Success");
+else
+     printf("Failure");
+#endif // 14
+//C LEVEL 4
+#endif
+    int x;
+#if 0
+x = 0;
+loop:if(x<6)
+{
+    printf("%d\n",x);
+    x++;
+    goto loop;
+}
+#endif // 1
+#if 0
+x = 5;
+loop:if(x>0)
+{
+    printf("%d\n",x);
+    x--;
+    goto loop;
+}
+#endif // 2
+#if 0
+x = 1;
+sum = 0;
+loop:if(x<6)
+{
+    sum += x;
+    x++;
+    goto loop;
+}
+printf("%d\n",sum);
+#endif // 3
+#if 0
+x = 6;
+sum = 0;
+loop:if(x>0)
+{
+    sum += x;
+    x--;
+    goto loop;
+}
+printf("%d\n",sum);
+#endif // 4
+#if 0
+x = 1;
+loop:if(x<10)
+{
+    if(x%2)
+    {
+        printf("%d\n",x);
+    }
+    x++;
+    goto loop;
+}
+#endif // 5
+#if 0
+x = 11;
+loop:if(x<20)
+{
+    if(x%2)
+    {
+        printf("%d\n",x);
+    }
+    x++;
+    goto loop;
+}
+#endif // 6
+#if 0
+x = 11;
+loop:if(x<100)
+{
+    if(x%2)
+    {
+        if(((x%10)+(x/10))==7){
+        printf("%d\n",x);
+        }
+    }
+    x++;
+    goto loop;
+}
+#endif // 7
+#if 0
+x = 11;
+loop:if(x<100)
+{
+    if(!(x%2))
+    {
+        if(((x%10)+(x/10))==6){
+        printf("%d\n",x);
+        }
+    }
+    x++;
+    goto loop;
+}
+#endif // 8
+// 9 same as 8
+#if 0
+    int sum = 0;
+    x = 11;
+loop:
+    if (x < 100)
+    {
+        if (x % 2)
+        {
+            if ((x / 10) == 7)
+            {
+                sum += x;
+            }
+        }
+        x++;
+        goto loop;
+    }
+    printf("%d", sum);
+#endif // 10
+#if 0
+int digit = 0;
+loop: 
+if(x)
+{
+    x=x/10;
+    digit++;
+    goto loop;
+}
+    printf("%d",digits);
+#endif // 11
+#if 0
+int sum = 0;
+loop: 
+if(x)
+{
+    sum += (x%10);
+    x=x/10;
+    goto loop;
+}
+    printf("%d",sum);
+#endif // 12
+#if 0
+int reverse = 0;
+loop: 
+if(x)
+{
+    reverse = (reverse*10)+(x%10);
+    x=x/10;
+    goto loop;
+}
+    printf("%d",reverse);
+#endif // 13
+#if 0
+scanf("%d",&x);
+int first,last,temp,power=1;
+first = x%10;
+temp = x;
+loop: if(temp>10)
+{
+    temp = temp/10;
+    power = power*10;
+    goto loop;
+}
+else{
+    last = temp;
+}
+int middle = (x%power)/10;
+int new_num = (first*power)+(middle*10)+last;
+printf("%d",new_num);
+#endif // 14
+#if 0
+scanf("%d",&x);
+int first,last,temp,power=1;
+first = x%10;
+temp = x;
+loop: if(temp>10)
+{
+    temp = temp/10;
+    power = power*10;
+    goto loop;
+}
+else{
+    last = temp;
+}
+if(last%2)
+{
+    last = last -1;
+    int tail = (x%power);
+    int new_num = last*power + tail;
+    printf("%d",new_num); 
+}
+else if(!(last%2))
+{
+    printf("%d",x);
+}
+#endif // 15
+    scanf("%d", &x);
+    int i = 2, is_prime = 1;
+    if (x <= 1)
+    {
+        is_prime = 0;
+    }
+    else
+    {
+    loop:
+        if (i * i <= x)
+        {
+            if (x % i == 0)
+            {
+                is_prime = 0;
+            }
+            else
+            {
+                i++;
+                goto loop;
+            }
+        }
+    }
+    if (is_prime)
+    {
+        printf("Prime");
+    }
+    else
+    {
+        printf("Not Prime");
+    } // 16 and 17
+    return 0;
+}#endif
+/*
+ Control Register Using Nested Bitfields
+You are given a control register represented using nested struct bitfields. The register is 8-bit wide and divided into the following layout:
+
+Bits	Field	Description
+0	enable	1 = ON, 0 = OFF
+1	mode	0 = Normal, 1 = Sleep
+2–3	priority	2-bit value (0–3)
+4–7	reserved	Reserved (must be 0)
+
+Your task is to:
+
+Simulate this register using nested struct and bitfields
+Implement a function that takes a pointer to the register and validates:
+enable must be 1
+priority must be less than or equal to 2
+reserved must be all 0s
+Return 1 if valid, else return 0.
+
+Example-1
+
+Input: 0x05 → 00000101
+Output: 1
+(enable=1, mode=0, priority=1, reserved=0)
+
+
+Example-2
+
+Input: 0x0F → 00001111
+Output: 0
+
+(priority=3, reserved=0)
+
+
+Example-3
+
+Input: 0x95 → 10010101
+Output: 0
+
+(reserved ≠ 0)
+
+*/
+Code
+
+#include <stdio.h>
+
+typedef union {
+    unsigned char reg;
+    struct {
+        unsigned char enable : 1;
+        unsigned char mode : 1;
+        unsigned char priority : 2;
+        unsigned char reserved : 4;
+    } bits;
+} ControlRegister;
+
+// Write your logic here
+int validate_register(ControlRegister *ctrl) {
+    if(ctrl->bits.enable!=1)
+            return 0;
+    if(ctrl->bits.priority>2)
+            return 0;
+    if(ctrl->bits.reserved!=0)
+                return 0;
+    return 1;
+}
+
+int main() {
+    ControlRegister ctrl;
+    scanf("%hhx", &ctrl.reg);
+
+    int result = validate_register(&ctrl);
+    printf("%d", result);
+
+    return 0;
+}
+/*
+Solving Approach
+
+access through ARROW OPERATOR '->' and check conditions
+*/
+
+
