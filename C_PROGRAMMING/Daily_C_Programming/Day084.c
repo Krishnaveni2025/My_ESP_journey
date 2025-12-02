6587ef1c07918811d045693e0020975b90a26666
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-02 22:01:29 +0530

    Document programming tasks and constraints in Day084.c
    Added comments outlining programming tasks and constraints.

--- C_PROGRAMMING/Daily_C_Programming/Day084.c
+++ C_PROGRAMMING/Daily_C_Programming/Day084.c
@@ -1,1 +1,21 @@
+/*
+1)Use only 'int' variables
+2)Use only Decimals
+3)Use 'for' statements to solve problems, 'if' also  can be used to decision making but not for loop
+4)Don't use arrays, pointers, strings, etc.
 
+16. Write a program to get a number from the user print whether that number is prime or not
+17. Write a program to get a number from the user print whether that number is prime and sum of digits is equal to 14
+18. Write a program to get a number from the user print whether the first two digits (1's, 10's) is prime
+19. Write a program to get a four-digit number fromt the user and print whether it's middle two digits (100's, 10's) is prime
+20. Write a program to print single digit prime numbers
+21. Write a program to get a number from the user and print the total number of digits which are odd in the number
+22. Write a program to get a number from the user and print the total number of two-digit odd numbers in that number
+23. Write a program to get a number from the user and print the total number of single digit perfect square numbers in the number
+24. Write a program to get a number from the user and print the total number of two digit perfect square numbers in the number
+25. Write a program to get a number from the user and print the total number of single digit prime numbers in the number
+26. Write a program to print the biggest four-digit number which is divisible by 7 and 9
+27. Write a program to print the total count of numbers which are less than 100000 and whose sum of digits is 14
+28. Write a program to get two numbers from the user and print LCM of those numbers
+29. Write a program to get three numbers from the user and print LCM of those numbers
+30. Write a program to get two numbers from the user and print HCF of those numbers*/
