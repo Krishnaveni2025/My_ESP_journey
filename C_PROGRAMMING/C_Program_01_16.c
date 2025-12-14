2f57a80d890e74eb8efa289c3dad33af245d5742
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 04:58:57 +0530

    Create C_Program_01_16.c

---
+++ C_PROGRAMMING/C_Program_01_16.c
@@ -0,0 +1,17 @@
+/* C LEVEL 01 Program 16
+  Question: Get a four digit number from the user and only reverse last two digits of the number then print the number
+Example:
+Input:9561 Output:5961
+Input:3859 Output:8359
+*/
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  //your code here
+  printf("Enter a four digit number: ");
+  scanf("%d",&x);
+  y = (((x/100)%100)*100)+((x/1000)*10)+(((x/100)%10)*10)+(x%10);
+  printf("%d",y);
+  return  0;
+}
