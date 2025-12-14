8ccbece532981525ce51a3497f370f5ad1a200c8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-14 21:43:22 +0530

    Implement subtraction of 5 from user input
    Added a program that subtracts 5 from user input.

--- C_PROGRAMMING/C_Program_01_02.c
+++ C_PROGRAMMING/C_Program_01_02.c
@@ -1,1 +1,15 @@
-
+/*
+Question: Get a number from the user and  subtract 5 from that number and  print the result
+Example: 
+Input: 45, Output: 40
+Input:56789, Output: 56784
+*/
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  scanf("%d",&x);
+  y = x-5;
+  printf("%d",y);
+  return 0;
+}
