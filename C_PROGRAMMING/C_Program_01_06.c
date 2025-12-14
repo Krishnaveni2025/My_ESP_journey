0f9c12a0ecef1fb69d7f5b3219cab643d3cfaf83
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 03:40:54 +0530

    Implement program to print ones digit of a number
    Added a program to extract and print the ones digit of a two-digit number.

--- C_PROGRAMMING/C_Program_01_06.c
+++ C_PROGRAMMING/C_Program_01_06.c
@@ -1,1 +1,14 @@
-
+/*    C LEVEL 01 Problem 06
+Question: Get a two-digit number from the user ans print the ones digit
+Example: Input: 45  Output: 5
+         Input: 56  Output: 6
+  */
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  scanf("%d",&x);
+  y = x%10;
+  printf("%d",y);
+  return 0;
+}
