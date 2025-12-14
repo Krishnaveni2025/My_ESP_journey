7e1ebf0ba56b5c20bdb786f2769b8917f44bc2b1
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-14 21:48:33 +0530

    Fix multiplication factor in user input program
    Corrected multiplication factor from 5 to 3 in the program.

--- C_PROGRAMMING/C_Program_01_03.c
+++ C_PROGRAMMING/C_Program_01_03.c
@@ -1,1 +1,15 @@
-
+/* C LEVEL 01 Problem 03
+Question: Get a number from the user and  multiply 3 to that number and  print the result
+Example: 
+Input: 45, Output: 135
+Input: 1200, Output: 3600
+*/
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  scanf("%d",&x);
+  y = x*5;
+  printf("%d",y);
+  return 0;
+}
