340f9e494073f603f5f7b1877f9459e9a664d8de
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-14 21:40:26 +0530

    Implement user input and addition program
    Added a program that reads a number from the user, adds 2 to it, and prints the result.

--- C_PROGRAMMING/C_Program_01_01.c
+++ C_PROGRAMMING/C_Program_01_01.c
@@ -1,1 +1,15 @@
-
+/*
+Question: Get a number from the user and add 2 to that number and  print the result
+Example: 
+Input: 45, Output: 47
+Input:56789, Output: 56791
+*/
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d",&x);
+    y = x+2;
+    printf("%d\n",y);
+    return 0;
+}
