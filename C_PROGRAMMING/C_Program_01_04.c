1e9e91606a31052d25b2f743527b9567bbbceb30
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-14 21:51:33 +0530

    Add user input division by 6 program
    Implement program to divide user input by 6 and print result.

--- C_PROGRAMMING/C_Program_01_04.c
+++ C_PROGRAMMING/C_Program_01_04.c
@@ -1,1 +1,16 @@
+/* C LEVEL 01 Problem 04
+Question: Get a number from the user and the number by 6 and  print the result
+Example: 
+Input: 45, Output: 7
+Input: 143, Output: 23
+*/
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  scanf("%d",&x);
+  y = x/6;
+  printf("%d",y);
+  return 0;
+}
 
