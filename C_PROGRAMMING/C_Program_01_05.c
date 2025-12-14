e92f99453049a55d9aca515da756e1ca7c3c49a4
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-14 21:54:15 +0530

    Add C program to compute remainder of input by 8
    Implement program to calculate remainder when divided by 8

--- C_PROGRAMMING/C_Program_01_05.c
+++ C_PROGRAMMING/C_Program_01_05.c
@@ -1,1 +1,17 @@
+/* C LEVEL 01 Problem 05
+Question: Get a number from the user and the number by 8 and  print the remainder
+Example: 
+Input: 45, Output: 5
+Input: 143, Output: 7
+*/
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  scanf("%d",&x);
+  y = x%8;
+  printf("%d",y);
+  return 0;
+}
+
 
