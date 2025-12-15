45ad3b9a2698c4a6f80eb4e0de2b0bd02bb18413
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 14:58:58 +0530

    Create C_Program_02_06.c

---
+++ C_PROGRAMMING/C_Program_02_06.c
@@ -0,0 +1,17 @@
+/* C LEVEL 02 Program 06
+  Question: Get a number from the user and check if the number is greater than or equal to 50. If yes then print 1, Otherwise print 0.
+Example:
+Input: 45    Output: 0
+Input: 56789 Output: 1
+  */
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  printf("Enter a number: ");
+  scanf("%d",&x);
+  //your code here
+  y = x>=50;
+  printf("Result = %d",y);
+  return 0;
+}
