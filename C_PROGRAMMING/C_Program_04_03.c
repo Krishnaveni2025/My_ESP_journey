5a46b1cbf3342b38b0605b435d8a1b1a85ebc51e
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 16:27:13 +0530

    Update C_Program_04_03.c

--- C_PROGRAMMING/C_Program_04_03.c
+++ C_PROGRAMMING/C_Program_04_03.c
@@ -1,1 +1,21 @@
-
+/* C LEVEL 04 Program 03
+  Question: Write a loop program to print sum of 1 to 5
+  Answer:
+  15
+  */
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  //your code here
+  x = 1;
+  y = 0;
+  loop:if(x<6)
+  {
+    y = x+y;
+    x++;
+    goto loop;
+  }
+  printf("Sum = %d",y);
+  return 0;
+}
