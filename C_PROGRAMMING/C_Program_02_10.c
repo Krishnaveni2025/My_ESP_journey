63c03e8c78b93b9df04ba5b42cf74708757badac
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 15:07:33 +0530

    Create C_Program_02_10.c

---
+++ C_PROGRAMMING/C_Program_02_10.c
@@ -0,0 +1,17 @@
+/* C LEVEL 02 Program 10
+  Question: Get a two digit number from the user and check if the digit 0 is greater than digit 1. If yes then print 1, Otherwise print 0.
+Example:
+Input: 45    Output: 1
+Input: 76    Output: 0
+  */
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  printf("Enter a two digit number: ");
+  scanf("%d",&x);
+  //your code here
+  y = ((x%10)>(x/10));
+  printf("Result = %d",y);
+  return 0;
+}
