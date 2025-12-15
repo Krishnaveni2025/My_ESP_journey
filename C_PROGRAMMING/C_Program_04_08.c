9b24cfa4196c19141bc1ebf44300227a84f5bee8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 16:42:38 +0530

    Update C_Program_04_08.c

--- C_PROGRAMMING/C_Program_04_08.c
+++ C_PROGRAMMING/C_Program_04_08.c
@@ -1,1 +1,27 @@
+/* C LEVEL 04 Program 08
+Question: Write a loop program to print two digit even numbers whose sum of digits are 6
+Answer:
+24
+42
+60
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x = 11;
+  loop:if(x<100)
+  {
+    if(!(x%2)){
+    if((x%10)+(x/10)==6)
+    {
+      printf("%d\n",x);
+    }
+  }
+    x++;
+    goto loop;
+  }
+  return 0;
+}
 
