7f25424a0237736ea3103dae5725d792a30da2a9
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 16:37:02 +0530

    Update C_Program_04_06.c

--- C_PROGRAMMING/C_Program_04_06.c
+++ C_PROGRAMMING/C_Program_04_06.c
@@ -1,1 +1,21 @@
+/* C LEVEL 04 Program 06
+Question: Write a loop program to print two digit odd numbers below 20
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x = 1;
+  loop:if(x<20)
+  {
+    if(x%2){
+    printf("%d\n",x);
+  }
+    x++;
+    goto loop;
+  }
+  return 0;
+}
+
 
