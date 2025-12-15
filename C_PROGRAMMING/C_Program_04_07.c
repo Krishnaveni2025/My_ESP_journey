34196ccfe923356719263d1514fa036615fc4299
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 16:40:49 +0530

    Update C_Program_04_07.c

--- C_PROGRAMMING/C_Program_04_07.c
+++ C_PROGRAMMING/C_Program_04_07.c
@@ -1,1 +1,26 @@
-
+/* C LEVEL 04 Program 07
+Question: Write a loop program to print two digit odd numbers whose sum of digits are 7
+Answer:
+25
+43
+61
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x = 11;
+  loop:if(x<100)
+  {
+    if(x%2){
+    if((x%10)+(x/10)==7)
+    {
+      printf("%d\n",x);
+    }
+  }
+    x++;
+    goto loop;
+  }
+  return 0;
+}
