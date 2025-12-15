a1d1747de1c6f3d1cae717f16a1f5180c4140fe6
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 16:53:03 +0530

    Update C_Program_04_11.c

--- C_PROGRAMMING/C_Program_04_11.c
+++ C_PROGRAMMING/C_Program_04_11.c
@@ -1,1 +1,26 @@
+/* C LEVEL 04 Program 11
+Question: Write a program to get a number from the user and print the total number of digits in that number 
+Answer:
+Input:123456   Output:6
+Input:76895439 Output:8
+Input:675      Output:3
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  printf("Enter a number: ");
+  scanf("%d",&x);
+  int y = 0;
+  loop:if(x)
+  {
+    y++;
+    x = x/10;
+     goto loop;
+  }
+   printf("Result = %d",y);
+  return 0;
+}
+
 
