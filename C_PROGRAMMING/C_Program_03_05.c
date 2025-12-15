3b3aba609826d9b3de75089fa9650981d1fa4ae7
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 15:38:58 +0530

    Create C_Program_03_05.c

---
+++ C_PROGRAMMING/C_Program_03_05.c
@@ -0,0 +1,23 @@
+/* C LEVEL 03 Program 05
+  Question: Get a number from the user and check if the number is less than or equal to 50. If yes then print "Success", otherwise print "Failure".
+Example:
+Input:56789    Output: Failure
+Input:45       Output: Success
+*/
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  printf("Enter a number: ");
+  scanf("%d",&x);
+  //your code here
+  if(x<=50)
+  {
+    printf("Success");
+  }
+  else
+  {
+    printf("Failure");
+  }
+  return 0;
+}
