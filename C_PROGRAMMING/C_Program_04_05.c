53e569b29fe6eec0b934da972ad6613373cce442
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 16:35:06 +0530

    Update C_Program_04_05.c

--- C_PROGRAMMING/C_Program_04_05.c
+++ C_PROGRAMMING/C_Program_04_05.c
@@ -1,1 +1,26 @@
+/* C LEVEL 04 Program 05
+Question: Write a loop program to print odd number between 1 and 9
+  Answer:
+  1
+  3 
+  5
+  7
+  9
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x = 1;
+  loop:if(x<10)
+  {
+    if(x%2){
+    printf("%d\n",x);
+  }
+    x++;
+    goto loop;
+  }
+  return 0;
+}
 
