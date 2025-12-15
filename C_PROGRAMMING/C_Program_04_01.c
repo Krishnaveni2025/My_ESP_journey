09e7174e30ae2fd30cd7dac4c133d003be63e20a
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-15 16:22:30 +0530

    Update C_Program_04_01.c

--- C_PROGRAMMING/C_Program_04_01.c
+++ C_PROGRAMMING/C_Program_04_01.c
@@ -1,1 +1,23 @@
-
+/* C LEVEL 04 Program 01
+  Question: Write a loop program to print 1 to 5 on one by one
+  Answer:
+  1
+  2
+  3
+  4
+  5
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x = 1;
+  loop:if(x<6)
+  {
+    printf("%d\n",x);
+    x++;
+    goto loop;
+  }
+  return 0;
+}
