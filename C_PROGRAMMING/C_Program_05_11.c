939df988ff1e889d782660abe895aed739007075
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-22 00:42:31 +0530

    Create C_Program_05_11.c

---
+++ C_PROGRAMMING/C_Program_05_11.c
@@ -0,0 +1,18 @@
+/* C LEVEL 05 Problem 11
+  Question: Get a number from the user print the total number of digits in that number.
+  Answer:
+  Input:123456   Output:6
+  Input:76895439 Output:8
+  Input:675      Output:3
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  for(;;)
+    {
+      //your code here
+      goto loop;
+    }
+}
