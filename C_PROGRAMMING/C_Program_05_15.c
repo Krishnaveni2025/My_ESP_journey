26db69282df6ee1bab3e5d5c670590a665d3fe83
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-22 01:03:38 +0530

    Create C_Program_05_15.c

---
+++ C_PROGRAMMING/C_Program_05_15.c
@@ -0,0 +1,19 @@
+/* C LEVEL 05 Problem 15
+  Question: Write a program to get a number from the user and if the last digit of the number is even print the same number. 
+  If the last digit of the number is odd subtract 5 from the last digit and print the number.(Note: Last Digit - MSB)
+  Answer:
+  Input:654324   Output:654324
+  Input:76895439 Output:66895439
+  Input:675      Output:675
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  for(;;)
+    {
+      //your code here
+      goto loop;
+    }
+}
