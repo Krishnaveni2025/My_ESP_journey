5e52f7353193c1c733a1000e607feda91f855bcc
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-22 00:43:57 +0530

    Create C_Program_05_12.c

---
+++ C_PROGRAMMING/C_Program_05_12.c
@@ -0,0 +1,18 @@
+/* C LEVEL 05 Problem 12
+  Question: Get a number from the user print the sum of all digits.
+  Answer:
+  Input:123456   Output:21
+  Input:76895439 Output:51
+  Input:675      Output:18
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  for(;;)
+    {
+      //your code here
+      goto loop;
+    }
+}
