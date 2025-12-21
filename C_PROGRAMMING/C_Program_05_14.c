ebf5bc2d15b9b9ada569e217d70164dbe1547e42
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-22 00:49:47 +0530

    Create C_Program_05_14.c

---
+++ C_PROGRAMMING/C_Program_05_14.c
@@ -0,0 +1,18 @@
+/* C LEVEL 05 Problem 14
+  Question: Write a program to get a number from the user and interchange the first and last digits and print the result .
+  Answer:
+  Input:123456   Output:623451
+  Input:76895439 Output:96895437
+  Input:675      Output:576
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  for(;;)
+    {
+      //your code here
+      goto loop;
+    }
+}
