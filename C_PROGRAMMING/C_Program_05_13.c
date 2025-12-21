0da2ee4a5ae9b709cc620451bc8d1efa0dbd5d70
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-22 00:47:29 +0530

    Create C_Program_05_13.c

---
+++ C_PROGRAMMING/C_Program_05_13.c
@@ -0,0 +1,18 @@
+/* C LEVEL 05 Problem 13
+  Question: Write a program to get a number from the user print the reverse of the number.
+  Answer:
+  Input:123456   Output:654321
+  Input:76895439 Output:93453867
+  Input:675      Output:576
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  for(;;)
+    {
+      //your code here
+      goto loop;
+    }
+}
