7338b21850fcca8546732d9bf662d0b41043dd15
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-26 04:41:33 +0530

    Add LCM calculation program in C
    Implemented a program to calculate the LCM of two user-input numbers.

---
+++ C_PROGRAMMING/C_Program_05_28.c
@@ -0,0 +1,36 @@
+/* C LEVEL 05 Problem 27
+  Question: Write a program to get ttwo numbers from the user and print the LCM of those numbers
+  Answer:
+  Input: 20 40 Output: 40
+  Input: 15 78 Output:390
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  int y,max;
+  printf("Enter two numbers: ");
+  scanf("%d %d",&x,&y);
+  if(x>y)
+      {
+        max = x;
+      }
+  else
+      {
+        max = y;
+      }
+  
+  for(;;)
+    {
+      //your code here
+      loop:
+      if(((max%x)==0)&&((max%y)==0))
+      {
+        printf("%d",max);
+        break;
+      }
+      ++max;
+      goto loop;
+    }
+}
