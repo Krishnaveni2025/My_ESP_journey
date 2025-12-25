44bf780cf23c11317c4e24a2779ab0a4551596a2
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-26 04:49:26 +0530

    Add LCM calculation program in C
    Implement LCM calculation for three user-input numbers.

---
+++ C_PROGRAMMING/C_Program_05_29.c
@@ -0,0 +1,40 @@
+/* C LEVEL 05 Problem 29
+  Question: Write a program to get three numbers from the user and print the LCM of those numbers
+  Answer:
+  Input: 20 40 80 Output: 40
+  Input: 15 78 63 Output: 8190
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  int y,z,max;
+  printf("Enter three numbers: ");
+  scanf("%d %d %d",&x,&y,&z);
+  if((x>y)&&(x>z))
+      {
+        max = x;
+      }
+  if((z>y)&&(z>x))
+      {
+        max = z;
+      }
+  else
+      {
+        max = y;
+      }
+  
+  for(;;)
+    {
+      //your code here
+      loop:
+      if(((max%x)==0)&&((max%y)==0)&&((max%z)==0))
+      {
+        printf("%d",max);
+        break;
+      }
+      ++max;
+      goto loop;
+    }
+}
