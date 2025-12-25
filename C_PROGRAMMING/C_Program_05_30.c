0734a04109dc24491618ccc027c4ef7f8ced3f9c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-26 05:04:02 +0530

    Add HCF calculation program in C
    Implemented a program to calculate the HCF of two numbers input by the user.

---
+++ C_PROGRAMMING/C_Program_05_30.c
@@ -0,0 +1,35 @@
+/* C LEVEL 05 Problem 30
+  Question: Write a program to get two numbers from the user and print the HCF of those numbers
+  Answer:
+  Input: 20 40  Output: 20
+  Input: 15 78  Output: 3
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  int y;
+  printf("Enter two numbers: ");
+  scanf("%d %d",&x,&y);
+  for(;;)
+    {
+      //your code here
+      loop:
+      if(x!=y)
+      {
+        if(x>y)
+        {
+        x -= y;
+        }
+        else
+        {
+        y-=x;
+        }
+        goto loop;
+      }
+      printf("%d",x);
+      break;
+    }
+}
+
