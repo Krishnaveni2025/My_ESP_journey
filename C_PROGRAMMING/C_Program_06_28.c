789db7684cf12bacd619bfd6882a6a070512989a
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:42:01 +0530

    Update C_Program_06_28.c

--- C_PROGRAMMING/C_Program_06_28.c
+++ C_PROGRAMMING/C_Program_06_28.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 28
+  Question: Write a program to get ttwo numbers from the user and print the LCM of those numbers
+  Answer:
+  Input: 20 40 Output: 40
+  Input: 15 78 Output:390
+  */
