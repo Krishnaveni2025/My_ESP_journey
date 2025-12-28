13d05e22fa4034abb5ea0e4d950f5931aa6d2d49
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:59:47 +0530

    Add digit counting functionality in C program
    Implement a program to count the digits of a user-input number.

--- C_PROGRAMMING/C_Program_06_11.c
+++ C_PROGRAMMING/C_Program_06_11.c
@@ -1,1 +1,29 @@
-
+/* C LEVEL 05 Problem 11
+  Question: Get a number from the user print the total number of digits in that number.
+  Answer:
+  Input:123456   Output:6
+  Input:76895439 Output:8
+  Input:675      Output:3
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  printf("Enter a number: ");
+  scanf("%d",&x);
+  int count = 0;
+  while(1)
+    {
+      //your code here
+      loop:
+      if(x)
+      {
+        count++;
+        x/=10;
+        goto loop;
+      }
+      printf("%d",count);
+      break;
+    }
+}
