b44b37e2a6f84f664d143a6a6ae6284b145ae308
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:48:11 +0530

    Add loop to sum two-digit numbers ending in 5
    Implemented a loop to calculate the sum of two-digit numbers with a ones digit of 5.

--- C_PROGRAMMING/C_Program_06_09.c
+++ C_PROGRAMMING/C_Program_06_09.c
@@ -1,1 +1,32 @@
+/* C LEVEL 06 Problem 09
+Question: write a loop program to print the sum of two digit numbers, who's 1's digits is 5
+Answer:
+495
+  */
+
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 10;
+  int sum = 0;
+  while(1)
+    {
+      //your code here
+     loop: 
+     if(x<100)
+     {
+         if((x%10)==5)
+         {
+             sum += x;
+         }
+         x++;
+         goto loop;
+     }
+      printf("%d",sum);
+     break;
+      
+    }
+}
 
