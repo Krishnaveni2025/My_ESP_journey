5ebc16ecdac5307592ff4bf8cebef7e6033728e8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 19:05:32 +0530

    Add digit sum calculation in C program
    Implement a program to sum the digits of a user-input number.

--- C_PROGRAMMING/C_Program_06_12.c
+++ C_PROGRAMMING/C_Program_06_12.c
@@ -1,1 +1,29 @@
-
+/* C LEVEL 06 Problem 12
+  Question: Get a number from the user print the sum of all digits.
+  Answer:
+  Input:123456   Output:21
+  Input:76895439 Output:51
+  Input:675      Output:18
+  */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  printf("Enter a number: ");
+  scanf("%d",&x);
+  int sum = 0;
+  while(1)
+    {
+      //your code here
+      loop:
+      if(x)
+      {
+        sum += (x%10);
+        x /= 10;
+        goto loop;
+      }
+      printf("%d",sum);
+      break;
+    }
+}
