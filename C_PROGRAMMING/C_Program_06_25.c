0a3871343c6d349e1ac83ba583e92279794ee1d8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:40:20 +0530

    Update C_Program_06_25.c

--- C_PROGRAMMING/C_Program_06_25.c
+++ C_PROGRAMMING/C_Program_06_25.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 25
+  Question: Write a program to get a number from the user and print the total number of single digit prime numbers in the number
+  Answer:
+  Input: 163496481 Output: 1
+  Input: 364925    Output: 3
+  */
