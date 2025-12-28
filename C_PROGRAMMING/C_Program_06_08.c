95a5bce9cf2c51d33fc7f0ad18f0a600c1484a89
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:43:18 +0530

    Add loop to find two-digit even numbers summing to 6
    Implemented a loop to print two-digit even numbers whose digits sum to 6.

--- C_PROGRAMMING/C_Program_06_08.c
+++ C_PROGRAMMING/C_Program_06_08.c
@@ -1,1 +1,34 @@
+/* C LEVEL 06 Problem
+Question: write a loop program to print the two digit even numbers, who's sum of digits are 6
+Answer:
+24
+42
+60
+  */
 
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 10;
+  while(1)
+    {
+      //your code here
+     loop: 
+     if(x<100)
+     {
+         if(!(x%2))
+         {
+             if(((x%10)+(x/10))==6)
+             {
+               printf("%d\n",x);
+             }
+         }
+         x++;
+         goto loop;
+     }
+     break;
+      
+    }
+}
