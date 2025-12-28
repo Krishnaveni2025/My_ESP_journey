d92a6f9c8fe0d0775bbe7e0dcf5cb6ccfe3a1273
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:42:25 +0530

    Update C_Program_06_29.c

--- C_PROGRAMMING/C_Program_06_29.c
+++ C_PROGRAMMING/C_Program_06_29.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 29
+  Question: Write a program to get three numbers from the user and print the LCM of those numbers
+  Answer:
+  Input: 20 40 80 Output: 40
+  Input: 15 78 63 Output: 8190
+  */
