7f34920d9ee9ee13ebd9289a7fbb6a82a50fd38e
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:36:08 +0530

    Add comments for C program problem 18
    Added comments to explain the purpose of the program.

--- C_PROGRAMMING/C_Program_06_19.c
+++ C_PROGRAMMING/C_Program_06_19.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 18
+  Question: Write a program to get a number from the user and print whether the first two digits(1's & ten's) of that number is prime or not.
+  Answer:
+Input: 359 - Output: Prime
+Input: 3577 - Output: Not prime
+*/
