28ea87cb9f79b1cd91043066c208c82516df310c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:21:43 +0530

    Add prime number checking functionality
    Implemented a program to check if a number is prime.

--- C_PROGRAMMING/C_Program_06_16.c
+++ C_PROGRAMMING/C_Program_06_16.c
@@ -1,1 +1,41 @@
+/* C LEVEL 06 Problem 16
+  Question: Write a program to get a number from the user and print whether that number is prime or not.
+  Answer:
+Input: 31 - Output: Prime
+Input: 27 - Output: Not prime
+*/
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  printf("Enter a Number: ");
+  scanf("%d",&x);
+  int i = 2,is_prime = 1;
+  while(1)
+    {
+      //your code here
+      loop:
+      if(i<x)
+      {
+        if(!(x%i))
+        {
+          is_prime = 0;
+          break;
+        }
+        i++;
+        goto loop;
+      }
+      break;
+    }
+  if(is_prime)
+  {
+    printf("Prime");
+  }
+  else
+  {
+    printf("Not Prime");
+  }
+  return 0;
+}
 
