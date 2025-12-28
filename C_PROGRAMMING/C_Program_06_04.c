6b9712c3c1d2ed0a282bc7151227fb7ba39ebdb0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:24:15 +0530

    Add loop to calculate sum from 6 to 1
    Implemented a loop to calculate the sum from 6 to 1 using a goto statement.

--- C_PROGRAMMING/C_Program_06_04.c
+++ C_PROGRAMMING/C_Program_06_04.c
@@ -1,1 +1,28 @@
+/*C LEVEL 06 Problem 04
+  Question: Write a loop program to print sum from 6 to 1
+  Answer:
+  21
+
+*/
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 6;
+  int sum = 0;
+  while(1)
+    {
+      //your code here
+     loop: if(x>0)
+      {
+        sum += x;
+        x--;
+        goto loop;
+      }
+      printf("%d",sum);
+      break;
+    }
+}
+
 
