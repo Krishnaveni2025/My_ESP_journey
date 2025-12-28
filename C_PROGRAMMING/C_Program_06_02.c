cdc3bed0e3332bd5a177617dfba46c8cf050f559
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:19:15 +0530

    Add loop to print numbers from 5 to 1
    Implemented a loop to print numbers from 5 to 1 using goto.

--- C_PROGRAMMING/C_Program_06_02.c
+++ C_PROGRAMMING/C_Program_06_02.c
@@ -1,1 +1,28 @@
+/*C LEVEL 06 Problem 02
+  Question: Write a loop program to print 5 to 1 on one by one
+  Answer:
+  5
+  4
+  3
+  2
+  1
+*/
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 5;
+  while(1)
+    {
+      //your code here
+     loop: if(x>0)
+      {
+        printf("%d\n",x);
+        x--;
+        goto loop;
+      }
+      break;
+    }
+}
 
