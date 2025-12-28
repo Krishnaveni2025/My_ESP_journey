2efd43b2db84d28d5005686eb4890b665cfea9f5
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:17:46 +0530

    Implement loop to print numbers 1 to 5
    Added a loop program to print numbers from 1 to 5.

--- C_PROGRAMMING/C_Program_06_01.c
+++ C_PROGRAMMING/C_Program_06_01.c
@@ -1,1 +1,28 @@
-
+/*C LEVEL 06 Problem 01
+  Question: Write a loop program to print 1 to 5 on one by one
+  Answer:
+  1
+  2
+  3
+  4
+  5
+*/
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 1;
+  while(1)
+    {
+      //your code here
+     loop: 
+     if(x<6)
+      {
+        printf("%d\n",x);
+        x++;
+        goto loop;
+      }
+      break;
+    }
+}
