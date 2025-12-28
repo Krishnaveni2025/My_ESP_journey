c95f0892ad631f8e7c44fc20b6995e97f77af5bc
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:35:12 +0530

    Update C_Program_06_17.c

--- C_PROGRAMMING/C_Program_06_17.c
+++ C_PROGRAMMING/C_Program_06_17.c
@@ -1,1 +1,8 @@
-
+/* C LEVEL 06 Problem 17
+  Question: Write a program to get a number from the user and print whether that number is prime or not and sum of the digits equal to 14.
+  Answer:
+Input: 59 - Output: Prime & sum of the digits is 14
+Input: 77 - Output: Not prime but sum of digits is 14
+Input: 13 - Output: Prime & sum of digits is not 14
+Input: 27 - Output: Not prime & sum of digits is not 14
+*/
