6fb82063680aafec74c35db9b34e7589829d3c5d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:39:51 +0530

    Update C_Program_06_24.c

--- C_PROGRAMMING/C_Program_06_24.c
+++ C_PROGRAMMING/C_Program_06_24.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 24
+  Question: Write a program to get a number from the user and print the total number of two digit perfect square numbers in the number
+  Answer:
+  Input: 163496481 Output: 4
+  Input: 364925    Output: 4
+  */
