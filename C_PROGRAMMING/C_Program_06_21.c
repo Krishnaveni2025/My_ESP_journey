e6600c6b1a2dfcc7e2b36584c707a24a5b89b46c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:38:21 +0530

    Add comments for C program problem 21
    Added comments explaining the purpose and expected output of the program.

--- C_PROGRAMMING/C_Program_06_21.c
+++ C_PROGRAMMING/C_Program_06_21.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 21
+  Question: Write a program to get a number from the user and print the total number of digits which are odd in that number
+  Answer:
+  Input: 12345678 Output: 4
+  Input: 987531   Output:5
+  */
