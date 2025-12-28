ed52604dd8ae4fe46481779d52a8769cdfa23a71
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:38:55 +0530

    Add problem description and examples to C program
    Added comments explaining the problem and expected output.

--- C_PROGRAMMING/C_Program_06_22.c
+++ C_PROGRAMMING/C_Program_06_22.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 22
+  Question: Write a program to get a number from the user and print the total number of two-digit odd numbers in the number
+  Answer:
+  Input: 12345678 Output: 3
+  Input: 987531   Output: 4
+  */
