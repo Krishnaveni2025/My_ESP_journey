fe789bdeb08aa8f7fc56fb899744731b3d73bf12
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:22:11 +0530

    Add loop to calculate sum from 1 to 5
    Implemented a loop to calculate the sum from 1 to 5 using a goto statement.

--- C_PROGRAMMING/C_Program_06_03.c
+++ C_PROGRAMMING/C_Program_06_03.c
@@ -1,1 +1,28 @@
+/*C LEVEL 06 Problem 03
+  Question: Write a loop program to print sum from 1 to 5
+  Answer:
+  15
+
+*/
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 1;
+  int sum = 0;
+  while(1)
+    {
+      //your code here
+     loop: if(x<6)
+      {
+        sum += x;
+        x++;
+        goto loop;
+      }
+      printf("%d",sum);
+      break;
+    }
+}
+
 
