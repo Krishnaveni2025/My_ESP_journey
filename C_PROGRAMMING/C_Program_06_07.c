a1faa0b79e785fab9208ec4066ca393539f48bfe
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:37:46 +0530

    Add loop to find two-digit odd numbers with digit sum 7
    Implemented a loop to print two-digit odd numbers whose digits sum to 7.

--- C_PROGRAMMING/C_Program_06_07.c
+++ C_PROGRAMMING/C_Program_06_07.c
@@ -1,1 +1,38 @@
+/*C LEVEL 06 Problem 07
+  Question: Write a loop program to print to two digit odd numbers whose sum of digits are 7
+  Answer:
+  25
+  43
+  61
+
+*/
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 11;
+  while(1)
+    {
+      //your code here
+     loop: 
+     if(x<100)
+     {
+         if(x%2)
+         {
+             if(((x%10)+(x/10))==7)
+             {
+               printf("%d\n",x);
+             }
+         }
+         x++;
+         goto loop;
+     }
+     break;     
+    }
+}
+
+
+
+
 
