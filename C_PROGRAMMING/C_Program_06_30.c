d13bc5be1e5f0453dfe13f45f4ef4e400a48fb05
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:43:02 +0530

    Update C_Program_06_30.c

--- C_PROGRAMMING/C_Program_06_30.c
+++ C_PROGRAMMING/C_Program_06_30.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 30
+  Question: Write a program to get two numbers from the user and print the HCF of those numbers
+  Answer:
+  Input: 20 40  Output: 20
+  Input: 15 78  Output: 3
+  */
