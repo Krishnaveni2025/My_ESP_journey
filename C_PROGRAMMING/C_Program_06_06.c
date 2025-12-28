bbd8ac46e6d60b74d68a93ee15fba0aba33f09b7
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:31:38 +0530

    Add loop to print odd numbers below 20
    Implemented a loop to print two-digit odd numbers below 20.

--- C_PROGRAMMING/C_Program_06_06.c
+++ C_PROGRAMMING/C_Program_06_06.c
@@ -1,1 +1,36 @@
+/*C LEVEL 06 Problem 06
+  Question: Write a loop program to print to two digit odd numbers below 20
+  Answer:
+  11
+  13
+  15
+  17
+  19
+
+*/
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 11;
+  while(1)
+    {
+      //your code here
+     loop: 
+     if(x<20)
+     {
+         if(x%2)
+         {
+             printf("%d\n",x);
+         }
+         x++;
+         goto loop;
+     }
+      break;
+    }
+}
+
+
+
 
