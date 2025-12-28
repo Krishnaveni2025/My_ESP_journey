d89939f0afe591a5ccea5c989ca006d514cac2f1
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 18:50:40 +0530

    Update C_Program_06_10.c

--- C_PROGRAMMING/C_Program_06_10.c
+++ C_PROGRAMMING/C_Program_06_10.c
@@ -1,1 +1,36 @@
+/* C LEVEL 06 Problem 10
+Question: write a loop program to print the sum of two digit odd numbers, who's 10's digits is 7
+Answer:
+375
+  */
+
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  x= 10;
+  int sum = 0;
+  while(1)
+    {
+      //your code here
+     loop: 
+     if(x<100)
+     {
+         if(x%2)
+         {
+           if((x/10)==7)
+           {
+             sum += x;
+           }
+         }
+         x++;
+         goto loop;
+     }
+      printf("%d",sum);
+     break;
+      
+    }
+}
+
 
