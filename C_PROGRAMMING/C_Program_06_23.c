5d6a4a4ae3647d69c974fe7372a56713bde73add
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-28 23:39:21 +0530

    Add comments for problem description and examples
    Added comments to explain the program's purpose and examples.

--- C_PROGRAMMING/C_Program_06_23.c
+++ C_PROGRAMMING/C_Program_06_23.c
@@ -1,1 +1,6 @@
-
+/* C LEVEL 06 Problem 23
+  Question: Write a program to get a number from the user and print the total number of single digit perfect square numbers in the number
+  Answer:
+  Input: 123456789 Output: 3
+  Input: 987531    Output: 2
+  */
