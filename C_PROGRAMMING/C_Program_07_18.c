b5bb3fd8422727c8d898f4a4ceb8c72bfd53e51b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:05:07 +0530

    Create C_Program_07_18.c

---
+++ C_PROGRAMMING/C_Program_07_18.c
@@ -0,0 +1,19 @@
+/* C LEVEL 07 Problem 18
+  Question: Write a program to get a number from the user and print whether the first two digits(1's & ten's) of that number is prime or not.
+  Function Name: check_first_2digits_prime
+  Answer:
+Input: 359 - Output: Prime
+Input: 3577 - Output: Not prime
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
