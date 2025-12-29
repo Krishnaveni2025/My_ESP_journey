8e5d43b186f62d7aad866b25ce7a82fdc507cdc0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:59:00 +0530

    Create C_Program_07_15.c

---
+++ C_PROGRAMMING/C_Program_07_15.c
@@ -0,0 +1,21 @@
+/* C LEVEL 07 Problem 15
+  Question: Write a program to get a number from the user and if the last digit of the number is even print the same number. 
+  If the last digit of the number is odd subtract 1 from the last digit and print the number.(Note: Last Digit - MSB)
+  Function Name: check_last_digit_odd
+  Answer:
+  Input:654324   Output:654324 (last digit = 6)
+  Input:76895439 Output:66895439 (last digit = 7)
+  Input:675      Output:675 (last digit = 6)
+  */
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
