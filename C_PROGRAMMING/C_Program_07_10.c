40873d306c5302e3b935194946fc81bc66903fae
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:49:48 +0530

    Create C_Program_07_10.c

---
+++ C_PROGRAMMING/C_Program_07_10.c
@@ -0,0 +1,18 @@
+/* C LEVEL 07 Problem 10
+Question: Write a loop program to print the sum of two digit odd numbers, whose ten's digit is 7
+Function Name: disp_2digit_odd_sum_tens7
+Answer:
+375
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
