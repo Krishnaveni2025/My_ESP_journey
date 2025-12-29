3701fe40a024885a5ab308544094cf59b7805437
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:22:55 +0530

    Create C_Program_07_23.c

---
+++ C_PROGRAMMING/C_Program_07_23.c
@@ -0,0 +1,20 @@
+/* C LEVEL 07 Problem 23
+  Question: Write a program to get a number from the user and print the total number of single digit perfect square numbers in the number
+  Function Name: disp_single_digit_square
+  Answer:
+  Input: 123456789 Output: 3
+  Input: 987531    Output: 2
+  */
+
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
