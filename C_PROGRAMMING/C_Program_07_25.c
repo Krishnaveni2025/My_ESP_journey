79cfa89db9cbbd99a3461816e43caa7be6f97d90
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:25:38 +0530

    Create C_Program_07_25.c

---
+++ C_PROGRAMMING/C_Program_07_25.c
@@ -0,0 +1,20 @@
+/* C LEVEL 07 Problem 25
+  Question: Write a program to get a number from the user and print the total number of single digit prime numbers in the number
+  Function Name: disp_single_digit_prime
+  Answer:
+  Input: 163496481 Output: 1
+  Input: 364925    Output: 3
+  */
+
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
