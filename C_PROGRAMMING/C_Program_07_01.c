803076c4973fb5ad7cc1672e342a829ce28bb706
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:16:44 +0530

    Create C_Program_07_01.c

---
+++ C_PROGRAMMING/C_Program_07_01.c
@@ -0,0 +1,22 @@
+/* C LEVEL 07 Problem 01
+Question: Write a loop program to print 1 to 5 on one by one
+Function Name: disp_assend
+Answer:
+1
+2
+3
+4
+5
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
