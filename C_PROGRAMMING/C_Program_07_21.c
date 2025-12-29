a46f80aac666cf63c407df82e332a3e58fb1699d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:14:09 +0530

    Create C_Program_07_21.c

---
+++ C_PROGRAMMING/C_Program_07_21.c
@@ -0,0 +1,19 @@
+/* C LEVEL 07 Problem 21
+  Question: Write a program to get a number from the user and print the total number of digits which are odd in that number
+  Function Name: disp_total_odd_digits
+  Answer:
+  Input: 12345678 Output: 4
+  Input: 987531   Output:5
+  */
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
