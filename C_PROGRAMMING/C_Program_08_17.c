44546d3a46ca38e1c82f2652fe34a47bfa0f3b31
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 02:13:30 +0530

    Create C_Program_08_17.c

---
+++ C_PROGRAMMING/C_Program_08_17.c
@@ -0,0 +1,20 @@
+/* C LEVEL 08 Problem 17
+  Question: Write a program to print the total count of numbers which are less than 100000 and whose sum of digits is 14
+  Function Name: disp_count_sum14
+  Answer:
+  2710
+  */
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
