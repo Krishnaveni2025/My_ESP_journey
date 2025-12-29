48415c01bbdb844dc99e847fe7168a0c5ac06164
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:08:54 +0530

    Update C_Program_06_28.c

--- C_PROGRAMMING/C_Program_06_28.c
+++ C_PROGRAMMING/C_Program_06_28.c
@@ -4,3 +4,14 @@
   Input: 20 40 Output: 40
   Input: 15 78 Output:390
   */
+#include<stdio.h>
+int main()
+{
+  int x;
+  //your code here
+  while()
+    {
+      //your code here
+      goto loop;
+    }
+}
