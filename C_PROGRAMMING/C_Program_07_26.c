8fc9332e2d3e63842fcd13d82270313ea7c6ee41
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:27:22 +0530

    Create C_Program_07_26.c

---
+++ C_PROGRAMMING/C_Program_07_26.c
@@ -0,0 +1,18 @@
+/* C LEVEL 07 Problem 26
+  Question: Write a program to print the biggest 4-digit number which is divisible by 7 and 9
+  Function Name: disp_biggest_4digit_div7_9 
+  Answer:
+  9954
+  */
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
