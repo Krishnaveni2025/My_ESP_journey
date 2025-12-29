a5b4ecb2d6b0f9c4dd94e1322ec66208947744cb
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 01:57:53 +0530

    Create C_Program_08_10.c

---
+++ C_PROGRAMMING/C_Program_08_10.c
@@ -0,0 +1,21 @@
+/* C LEVEL 08 Problem 10
+  Question: Write a program to get a number from the user and print whether that number is prime or not.
+  Function Name: check_prime
+  Answer:
+Input: 31 - Output: Prime
+Input: 27 - Output: Not prime
+*/
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
