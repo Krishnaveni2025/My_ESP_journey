d651760ba20979819f10d21a033b96f9dd3e4726
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:20:30 +0530

    Create C_Program_07_03.c

---
+++ C_PROGRAMMING/C_Program_07_03.c
@@ -0,0 +1,18 @@
+/* C LEVEL 07 Problem 03
+Question: Write a loop program to print sum from 1 to 5
+Function Name: disp_sum
+Answer:
+15
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
