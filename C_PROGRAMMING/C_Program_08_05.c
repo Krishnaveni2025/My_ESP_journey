4cefb45129918970483a24b5d7c58c55028a0485
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 01:52:02 +0530

    Create C_Program_08_05.c

---
+++ C_PROGRAMMING/C_Program_08_05.c
@@ -0,0 +1,22 @@
+/* C LEVEL 08 Problem 05
+Question: Write a program to get a number from the user and print the total number of digits in that number
+Function Name: count_total_digits
+Answer:
+Input:123456   Output:6
+Input:76895439 Output:8
+Input:675      Output:3
+*/
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
