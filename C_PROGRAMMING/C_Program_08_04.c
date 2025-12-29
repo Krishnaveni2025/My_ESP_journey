e35501029a161adf2bfb53dec8cac3e9055343a0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 01:23:01 +0530

    Create C_Program_08_04.c

---
+++ C_PROGRAMMING/C_Program_08_04.c
@@ -0,0 +1,20 @@
+/* C LEVEL 08 Problem 04
+Question: Write a program to print the sum of the two digit odd numbers whose tens digit is 7
+Function Name: disp_2digit_odd_sum_tens7
+Answer:
+375
+*/
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
