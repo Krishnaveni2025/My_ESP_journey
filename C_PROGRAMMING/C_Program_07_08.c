b8953b7fbf6134b428b2f4ddb647b081bd1af341
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:42:55 +0530

    Create C_Program_07_08.c

---
+++ C_PROGRAMMING/C_Program_07_08.c
@@ -0,0 +1,20 @@
+/* C LEVEL 07 Problem 08
+Question: Write a loop program to print the two digit even numbers, whose sum of digits are 6
+Function Name: disp_2digit_even_sum6
+Answer:
+24
+42
+60
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
