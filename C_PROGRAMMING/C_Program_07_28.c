d487941c33983d9f8a5da94180707d48e14810a0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:30:25 +0530

    Create C_Program_07_28.c

---
+++ C_PROGRAMMING/C_Program_07_28.c
@@ -0,0 +1,19 @@
+/* C LEVEL 07 Problem 28
+  Question: Write a program to get ttwo numbers from the user and print the LCM of those numbers
+  Function Name: disp_LCM2
+  Answer:
+  Input: 20 40 Output: 40
+  Input: 15 78 Output:390
+  */
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}l
