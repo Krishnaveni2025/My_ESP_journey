c7ce8a5c3c932faac8385db7aa99aba50a2ae21a
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:12:09 +0530

    Create C_Program_07_20.c

---
+++ C_PROGRAMMING/C_Program_07_20.c
@@ -0,0 +1,18 @@
+/* C LEVEL 07 Problem 20
+  Question: Write a program to print total number single digit prime numbers.
+  Function Name: disp_count_single_digit_prime
+  Answer:
+  4
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
