7362a3ba56e93bad8fb8548c7b275777817a36b8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:36:26 +0530

    Create C_Program_07_06.c

---
+++ C_PROGRAMMING/C_Program_07_06.c
@@ -0,0 +1,22 @@
+/* C LEVEL 07 Problem 06
+Question: Write a loop program to print two digit odd numbers, below 20
+Function Name: disp_2digit_odd_below20
+Answer:
+11
+13
+15
+17
+19
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
