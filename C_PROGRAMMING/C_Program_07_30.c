49b50fe61aa018fcb67d94095d9f4b0635e5ce6f
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:33:22 +0530

    Create C_Program_07_30.c

---
+++ C_PROGRAMMING/C_Program_07_30.c
@@ -0,0 +1,20 @@
+/* C LEVEL 07 Problem 30
+  Question: Write a program to get two numbers from the user and print the HCF of those numbers
+  Function Name: disp_count_HCF2
+  Answer:
+  Input: 20 40  Output: 20
+  Input: 15 78  Output: 3
+  */
+
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
