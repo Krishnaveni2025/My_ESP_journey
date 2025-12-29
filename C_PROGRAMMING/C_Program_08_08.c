98791ef0652bdf5249a9077bbf316228233ff5a0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 01:55:43 +0530

    Create C_Program_08_08.c

---
+++ C_PROGRAMMING/C_Program_08_08.c
@@ -0,0 +1,22 @@
+/* C LEVEL 08 Problem 08
+  Question: Write a program to get a number from the user and interchange the first and last digits and print the result 
+  Function Name: disp_interchange_first_last_digit
+  Answer:
+  Input:123456   Output:623451
+  Input:76895439 Output:96895437
+  Input:675      Output:576
+  */
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
