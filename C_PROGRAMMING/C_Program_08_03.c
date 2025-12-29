76b7ac329742be16fd2f490293a52d07e88bfd27
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 01:19:28 +0530

    Create C_Program_08_03.c

---
+++ C_PROGRAMMING/C_Program_08_03.c
@@ -0,0 +1,20 @@
+/* C LEVEL 08 Problem 02
+Question: Write a loop program to print the sum of two digit numbers whose one's digits is 5
+Function Name: disp_2digit_ones5
+Answer:
+495
+*/
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
