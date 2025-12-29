0625d3118a5c41982db5b47538ac830b06ade6d3
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 02:01:46 +0530

    Create C_Program_08_12.c

---
+++ C_PROGRAMMING/C_Program_08_12.c
@@ -0,0 +1,20 @@
+/* C LEVEL 08 Problem 12
+  Question: Write a program to get a number from the user and print the total number of two-digit odd numbers in the number
+  Function Name: disp_total_2digit_odd
+  Answer:
+  Input: 12345678 Output: 3
+  Input: 987531   Output: 4
+  */#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
