de47aa1315c4ea4b482e4d2e9999171f0a7481e5
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:02:00 +0530

    Create C_Program_07_17.c

---
+++ C_PROGRAMMING/C_Program_07_17.c
@@ -0,0 +1,21 @@
+/* C LEVEL 07 Problem 17
+  Question: Write a program to get a number from the user and print whether that number is prime or not and sum of the digits equal to 14.
+  Function Name: check_prime_and_sum14
+  Answer:
+Input: 59 - Output: Prime & sum of the digits is 14
+Input: 77 - Output: Not prime but sum of digits is 14
+Input: 13 - Output: Prime & sum of digits is not 14
+Input: 27 - Output: Not prime & sum of digits is not 14
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
