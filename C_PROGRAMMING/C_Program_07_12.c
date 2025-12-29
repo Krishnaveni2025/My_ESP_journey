f0551cd916d07f893f933f99bbc7d904cd21d8bd
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:54:16 +0530

    Create C_Program_07_12.c

---
+++ C_PROGRAMMING/C_Program_07_12.c
@@ -0,0 +1,20 @@
+/* C LEVEL 07 Problem 12
+  Question: Get a number from the user print the sum of all digits.
+  Function Name: disp_sum_all_digits
+  Answer:
+  Input:123456   Output:21
+  Input:76895439 Output:51
+  Input:675      Output:18
+  */
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
