5d8657f38121c1f60c175ef69baee15461a46def
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:52:26 +0530

    Create C_Program_07_11.c

---
+++ C_PROGRAMMING/C_Program_07_11.c
@@ -0,0 +1,20 @@
+/* C LEVEL 07 Problem 11
+Question: Write a loop program to get a number from the user and print the total digits in the number
+Function Name: count_total_digits
+Answer:
+Input:123456   Output:6
+Input:76895439 Output:8
+Input:675      Output:3
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
