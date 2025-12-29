56b0c2124c71bfee4617e1801a15ed35cb7f1314
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 01:09:16 +0530

    Create C_Program_08_02.c

---
+++ C_PROGRAMMING/C_Program_08_02.c
@@ -0,0 +1,20 @@
+/* C LEVEL 08 Problem 02
+Question: Write a loop program to print sum of 6 to 1
+Function Name: disp_rsum
+Answer:
+21
+*/
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
