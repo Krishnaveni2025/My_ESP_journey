7c43e8d2c0eee9a7cd0de68479e949972271925d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:55:46 +0530

    Create C_Program_07_13.c

---
+++ C_PROGRAMMING/C_Program_07_13.c
@@ -0,0 +1,20 @@
+/* C LEVEL 07 Problem 13
+  Question: Write a program to get a number from the user print the reverse of the number.
+  Function Name: disp_reverse_number
+  Answer:
+  Input:123456   Output:654321
+  Input:76895439 Output:93453867
+  Input:675      Output:576
+  */
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
