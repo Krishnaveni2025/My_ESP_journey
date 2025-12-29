1a97a606ac113932215c46e7a92d6ee75b231768
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:09:21 +0530

    Create C_Program_07_19.c

---
+++ C_PROGRAMMING/C_Program_07_19.c
@@ -0,0 +1,19 @@
+/* C LEVEL 07 Problem 19
+  Question: Write a program to get a four digit number from the user and print whether the middle two digits(10's & 100's) of that number is prime or not.
+  Function Name: middle_2digits_prime
+  Answer:
+Input: 6359 - Output: Not Prime
+Input: 3537 - Output: Prime
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
