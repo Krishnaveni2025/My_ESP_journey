db5c2c7a8fb8b949bed4b099d0c2b0372fc31912
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 00:24:13 +0530

    Create C_Program_07_24.c

---
+++ C_PROGRAMMING/C_Program_07_24.c
@@ -0,0 +1,19 @@
+/* C LEVEL 07 Problem 24
+  Question: Write a program to get a number from the user and print the total number of two digit perfect square numbers in the number
+  Function Name: disp_two_digit_square
+  Answer:
+  Input: 163496481 Output: 4
+  Input: 364925    Output: 4
+  */
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
