95c1fe749e868d413d4b09d7c871bfb8ec97c8bb
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 02:17:15 +0530

    Create C_Program_08_19.c

---
+++ C_PROGRAMMING/C_Program_08_19.c
@@ -0,0 +1,21 @@
+/* C LEVEL 08 Problem 19
+  Question: Write a program to get three numbers from the user and print the LCM of those numbers
+  Function Name: disp_LCM3
+  Answer:
+  Input: 20 40 80 Output: 40
+  Input: 15 78 63 Output: 8190
+  */
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+y = function_name(x);
+}
+int function_name(int a)
+{
+int a;
+//your code here
+return (y);
+}
