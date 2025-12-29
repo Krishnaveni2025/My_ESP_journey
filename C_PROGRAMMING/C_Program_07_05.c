340a97cb627ab70dd6c6d9fce78def9bfa0402cf
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-29 23:26:09 +0530

    Create C_Program_07_05.c

---
+++ C_PROGRAMMING/C_Program_07_05.c
@@ -0,0 +1,22 @@
+/* C LEVEL 07 Problem 05
+Question: Write a loop program to print odd numbers between 1 and 9
+Function Name: disp_odd
+Answer:
+1
+3
+5
+7
+9
+*/
+#include<stdio.h>
+void function_name(int);
+int main()
+{
+int x,y;
+scanf("%d",&x);
+function_name(x);
+}
+void function_name(int x)
+{
+//your code here
+}
