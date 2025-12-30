cc744840a917baed30a50e67b089e5c9f12197dc
Krishnaveni Purne <krishnavenipurne@gmail.com>
2025-12-30 16:36:41 +0530

    Update C_Program_06_19.c

--- C_PROGRAMMING/C_Program_06_19.c
+++ C_PROGRAMMING/C_Program_06_19.c
@@ -9,9 +9,28 @@ int main()
 {
   int x;
   //your code here
-  while()
+  printf("Enter a Four digit number: ");
+  scanf("%d",&x);
+  int i = 2, is_prime = 1;
+  x = (x%1000)/10;
+  while(1)
     {
       //your code here
+      loop:
+      if((i>=x)||(is_prime == 0))
+      {
+        break;
+      }
+      if(!(x%i))
+      {
+        is_prime = 0;
+      }
+      i++;
       goto loop;
     }
+  if(is_prime)
+      printf("Prime");
+  else
+      printf("Not Prime");
+  return 0;
 }
