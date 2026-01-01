2f0bb60348c482af35f6f73b7007639824bbdb99
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-02 04:37:27 +0530

    Update C_Program_07_17.c

--- C_PROGRAMMING/C_Program_07_17.c
+++ C_PROGRAMMING/C_Program_07_17.c
@@ -8,14 +8,50 @@ Input: 13 - Output: Prime & sum of digits is not 14
 Input: 27 - Output: Not prime & sum of digits is not 14
 */
 #include<stdio.h>
-void function_name(int);
+void check_prime_and_sum14(int);
 int main()
 {
-int x,y;
+int x;
 scanf("%d",&x);
-function_name(x);
+check_prime_and_sum14(x);
 }
-void function_name(int x)
+void check_prime_and_sum14(int x)
 {
 //your code here
+  int i = 2, is_prime = 1, sum = 0;
+  for(i = 2; i<x; i++)
+    {
+      if((x%i)==0)
+      {
+        is_prime = 0;
+        break;
+      }
+    }
+  while(x)
+    {
+      sum += (x%10);
+      x /= 10;
+    }
+  if(is_prime)
+  {
+    if(sum==14)
+    {
+      printf("Prime & sum of the digits is 14");
+    }
+    else
+    {
+      printf("Prime & sum of digits is not 14");
+    }
+  }
+  else
+  {
+    if(sum == 14)
+    {
+      printf("Not prime but sum of digits is 14");
+    }
+    else
+    {
+      printf("Not prime & sum of digits is not 14");
+    }
+  }
 }
