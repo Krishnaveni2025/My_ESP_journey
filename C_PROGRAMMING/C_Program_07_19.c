8831429d05f14e8b0270c2964d73cd74df1540af
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-02 04:51:09 +0530

    Update C_Program_07_19.c

--- C_PROGRAMMING/C_Program_07_19.c
+++ C_PROGRAMMING/C_Program_07_19.c
@@ -1,4 +1,4 @@
-/* C LEVEL 07 Problem 19
+`/* C LEVEL 07 Problem 19
   Question: Write a program to get a four digit number from the user and print whether the middle two digits(10's & 100's) of that number is prime or not.
   Function Name: middle_2digits_prime
   Answer:
@@ -6,14 +6,33 @@ Input: 6359 - Output: Not Prime
 Input: 3537 - Output: Prime
 */
 #include<stdio.h>
-void function_name(int);
+void middle_2digits_prime(int);
 int main()
 {
-int x,y;
+int x;
 scanf("%d",&x);
-function_name(x);
+middle_2digits_prime(x);
 }
-void function_name(int x)
+void middle_2digits_prime(int x)
 {
 //your code here
+  x = (x/10)%100;
+  printf("%d\n",x);
+  int i = 2, is_prime = 1;
+  for(i = 2; i<x; i++)
+    {
+      if(!(x%i))
+      {
+        is_prime = 0;
+        break;
+      }
+    }
+  if(is_prime)
+  {
+    printf("Prime");
+  }
+  else
+  {
+    printf("Not Prime");
+  }
 }
