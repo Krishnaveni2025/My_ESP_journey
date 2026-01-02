4115b4653fbe9acc035fd0073619256dad4cc3e9
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-02 08:58:28 +0530

    Update C_Program_08_02.c

--- C_PROGRAMMING/C_Program_08_02.c
+++ C_PROGRAMMING/C_Program_08_02.c
@@ -5,16 +5,22 @@ Answer:
 21
 */
 #include<stdio.h>
-int function_name(int);
+int disp_rsum(int);
 int main()
 {
 int x,y;
-scanf("%d",&x);
-y = function_name(x);
+x = 6;
+y = disp_rsum(x);
+printf("%d",y);
 }
-int function_name(int a)
+int disp_rsum(int a)
 {
-int a;
+int y = 0;
 //your code here
+  while(a>0)
+    {
+      y += a;
+      a--;
+    }
 return (y);
 }
