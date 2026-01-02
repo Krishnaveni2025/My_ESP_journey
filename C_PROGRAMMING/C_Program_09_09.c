50490107309c1426b6fa66d170ecc47a35dd9247
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:19:30 +0530

    Create C_Program_09_09.c

---
+++ C_PROGRAMMING/C_Program_09_09.c
@@ -0,0 +1,6 @@
+/*
+  C LEVEL 09 Problem 09
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get 5 numbers from the user and remove the even  numbers and create a new array and then  print the same*/
