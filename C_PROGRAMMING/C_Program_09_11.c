91243c2d876bb010b1c49c0a0269b2e06bda2621
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:22:27 +0530

    Create C_Program_09_11.c

---
+++ C_PROGRAMMING/C_Program_09_11.c
@@ -0,0 +1,6 @@
+/*
+  C LEVEL 09 Problem 06
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get 5 numbers from the user and reverse each number and create a new array and then  print the sum of all numbers in the new array*/
