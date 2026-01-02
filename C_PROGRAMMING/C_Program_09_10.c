de95dbd56d192d00b2b749d5eb0aa7c9db964d31
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:20:41 +0530

    Create C_Program_09_10.c

---
+++ C_PROGRAMMING/C_Program_09_10.c
@@ -0,0 +1,6 @@
+/*
+  C LEVEL 09 Problem 10
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get 5 numbers from the user and remove the prime  numbers and create a new array and then  print the same*/
