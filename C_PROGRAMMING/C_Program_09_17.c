b8268a385a3567372c1e63a48e4bda49c78d4d78
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:35:19 +0530

    Create C_Program_09_17.c

---
+++ C_PROGRAMMING/C_Program_09_17.c
@@ -0,0 +1,7 @@
+/*
+  C LEVEL 09 Problem 17
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Adjust the carry in an  integer array (i.e convert the 2 digit number into single digit and add carry to the next number
+*/
