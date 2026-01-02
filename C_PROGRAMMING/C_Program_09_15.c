bd2443a02e0c11766ef5b69a5acb48bd5ef338cd
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:31:38 +0530

    Create C_Program_09_15.c

---
+++ C_PROGRAMMING/C_Program_09_15.c
@@ -0,0 +1,7 @@
+/*
+  C LEVEL 09 Problem 15
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. 
+Add each numbers digit and put them in an array and arrange the new array in the ascending order and print the same*/
