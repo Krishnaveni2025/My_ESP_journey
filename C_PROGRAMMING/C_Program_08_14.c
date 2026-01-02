fafd372e162bf3edfbf2d95479c78c1d90cfd1a7
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-02 09:44:32 +0530

    Update C_Program_08_14.c

--- C_PROGRAMMING/C_Program_08_14.c
+++ C_PROGRAMMING/C_Program_08_14.c
@@ -5,16 +5,27 @@
   Input: 163496481 Output: 4
   Input: 364925    Output: 4
   */
+
 #include<stdio.h>
-int function_name(int);
+int disp_two_digit_square(int);
 int main()
 {
 int x,y;
 scanf("%d",&x);
-y = function_name(x);
+y = disp_two_digit_square(x);
+printf("%d",y);
 }
-int function_name(int a)
+int disp_two_digit_square(int a)
 {
-int a;
+int y = 0;
 //your code here
-retu
+  while(a>10)
+    {
+  if(((a%100)==16)||((a%100)==49)||((a%100)==64)||((a%100)==81)||((a%100)==25)||((a%100)==36))
+  {
+    y++;
+  }
+      a /= 10;
+    }
+return(y);
+}
