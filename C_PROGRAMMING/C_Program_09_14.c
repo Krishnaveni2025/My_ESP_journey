01e0cf741a91e8c4f5b8a6a482b206b0a62b4340
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:29:42 +0530

    Create C_Program_09_14.c

---
+++ C_PROGRAMMING/C_Program_09_14.c
@@ -0,0 +1,7 @@
+/*
+  C LEVEL 09 Problem 14
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. 
+If total number is odd then print middle number, otherwise print the average of middle two numbers */
