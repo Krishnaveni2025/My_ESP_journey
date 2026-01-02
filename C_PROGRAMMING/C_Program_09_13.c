5aa9ad585e215a09797eb8f058f65817c04a7115
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:27:53 +0530

    Create C_Program_09_13.c

---
+++ C_PROGRAMMING/C_Program_09_13.c
@@ -0,0 +1,6 @@
+/*
+  C LEVEL 09 Problem 13
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. If  numbers entered  if first and last numbers are equal, print "Success" else "Failure"*/
