b71d73e5114f9e97cd47d4d76f9127b8a7355bc1
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:18:35 +0530

    Create C_Program_09_08.c

---
+++ C_PROGRAMMING/C_Program_09_08.c
@@ -0,0 +1,6 @@
+/*
+  C LEVEL 09 Problem 06
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get 5 numbers from the user and remove the odd  numbersand crea a new array and then  print the same*/
