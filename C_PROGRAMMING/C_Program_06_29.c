bfdbe33f2d0d4273309ab5ed68b807bb2aae7c98
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-02 05:57:07 +0530

    Update C_Program_06_29.c

--- C_PROGRAMMING/C_Program_06_29.c
+++ C_PROGRAMMING/C_Program_06_29.c
@@ -1,7 +1,7 @@
 /* C LEVEL 06 Problem 29
   Question: Write a program to get three numbers from the user and print the LCM of those numbers
   Answer:
-  Input: 20 40 80 Output: 40
+  Input: 20 40 80 Output: 80
   Input: 15 78 63 Output: 8190
   */
 #include<stdio.h>
