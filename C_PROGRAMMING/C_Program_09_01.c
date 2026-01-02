b79b999aa38799c5be2565e7176b6740eca333b8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:10:06 +0530

    Update C_Program_09_01.c

--- C_PROGRAMMING/C_Program_09_01.c
+++ C_PROGRAMMING/C_Program_09_01.c
@@ -1,4 +1,7 @@
-//Use only int variables
-//Use ont decimals
-//use arrays
-//Question: Get 5 numbers from the user and print all the sum of all numbers
+/*
+  C LEVEL 09 Problem 01
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get 5 numbers from the user and print all the sum of all numbers
+*/
