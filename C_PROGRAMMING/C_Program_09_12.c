bb88791d01f7af95ec1223a7ded570070e265749
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 04:24:44 +0530

    Create C_Program_09_12.c

---
+++ C_PROGRAMMING/C_Program_09_12.c
@@ -0,0 +1,6 @@
+/*
+  C LEVEL 09 Problem 12
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. Print the total numbers entered by the user and print the sum of numbers*/
