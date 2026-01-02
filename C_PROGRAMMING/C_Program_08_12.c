c0f650624c138479a035cefa8a01680d737c7a9d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-02 09:37:36 +0530

    Update C_Program_08_12.c

--- C_PROGRAMMING/C_Program_08_12.c
+++ C_PROGRAMMING/C_Program_08_12.c
@@ -4,17 +4,28 @@
   Answer:
   Input: 12345678 Output: 3
   Input: 987531   Output: 4
-  */#include<stdio.h>
-int function_name(int);
+  */
+#include<stdio.h>
+int disp_total_2digit_odd(int);
 int main()
 {
 int x,y;
 scanf("%d",&x);
-y = function_name(x);
+y = disp_total_2digit_odd(x);
+printf("%d",y);
 }
-int function_name(int a)
+int disp_total_2digit_odd(int a)
 {
-int a;
+int y = 0;
 //your code here
+  while(a>10)
+    {
+      if((a%100)%2)
+      {
+        y++;
+      }
+      a /= 10;
+    }
 return (y);
 }
+
