77f8b16d5493b3f7368037aa7840fba0fe318937
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 08:56:28 +0530

    Update C_Program_11_08.c

--- C_PROGRAMMING/C_Program_11_08.c
+++ C_PROGRAMMING/C_Program_11_08.c
@@ -1,6 +1,5 @@
 /* C LEVEL 11 Problem 08
   Use Pointers
-Question: Write a function to find the two digit odd numbers whose sum of digits is 7. Print the result in the main function
-Function Name: find_2digit_odd_sum7
-Answer: 25,43,61
+Question: Write a function to compare to strings. Print success or failure
+Function Name: strcomp(src, dst)
 */
