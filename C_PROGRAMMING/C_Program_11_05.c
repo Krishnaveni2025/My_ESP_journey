24b26b886d3b8cd978579160effdb9da9bc7ca55
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 08:52:36 +0530

    Update C_Program_11_05.c

--- C_PROGRAMMING/C_Program_11_05.c
+++ C_PROGRAMMING/C_Program_11_05.c
@@ -1,6 +1,5 @@
 /* C LEVEL 11 Problem 05
   Use Pointers
-Question: Write a function to find the two digit odd numbers whose sum of digits is 7. Print the result in the main function
-Function Name: find_2digit_odd_sum7
-Answer: 25,43,61
+Question: Write a function to copy integers from one location to another location
+Function Name: memcopy(src,dst,size)
 */
