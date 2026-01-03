7c19710fe78858e60434377d1a2df94c303daa1c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 08:40:58 +0530

    Create C_Program_11_03.c

---
+++ C_PROGRAMMING/C_Program_11_03.c
@@ -0,0 +1,11 @@
+/* C LEVEL 11 Problem 03
+  Use Pointers
+Question: Write a loop program to print 1 to 5 one by one. Write a function and print the result in the main function
+Function Name: arrange_ascend
+Answer:
+1
+2
+3
+4
+5
+*/
