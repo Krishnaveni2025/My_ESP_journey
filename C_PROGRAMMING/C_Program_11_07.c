aa7b4bbc32daf538e537fcb7c139792038dcaa03
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 08:54:45 +0530

    Update C_Program_11_07.c

--- C_PROGRAMMING/C_Program_11_07.c
+++ C_PROGRAMMING/C_Program_11_07.c
@@ -1,5 +1,5 @@
 /* C LEVEL 11 Problem 07
   Use Pointers
-Question: Write a function to copy a string to another
-Function Name: strcopy(src,dst)
+Question: Write a function to compare a set of integers and print success or failure
+Function Name: strcomp(src,dst,size)
 */
