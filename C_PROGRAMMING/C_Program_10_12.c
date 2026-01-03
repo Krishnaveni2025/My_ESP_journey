91449087fbc989ace598cb6c9f77c590682128a4
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-03 08:33:05 +0530

    Create C_Program_10_12.c

---
+++ C_PROGRAMMING/C_Program_10_12.c
@@ -0,0 +1,7 @@
+/* C LEVEL 10 Problem 12
+  Don't use structures
+  Question: Convert an integer of upto 50 digits to an character array using "printf("%s,...");"
+  Example: 
+  Array-1 4 5 8 7 6 3
+  Answer:1458763
+*/
