500d8443e710a2fba7aaec65ca5f294de36a8368
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-04 15:10:35 +0530

    Update C_Program_09_04.c

--- C_PROGRAMMING/C_Program_09_04.c
+++ C_PROGRAMMING/C_Program_09_04.c
@@ -5,13 +5,6 @@
   use arrays
 Question: Get 5 numbers from the user and print the biggest number
 */
-/*
-  C LEVEL 09 Problem 03
-  Use only int variables
-  Use ont decimals
-  use arrays
-Question: Get 5 numbers from the user and print the smallest number
-*/
 #include <stdio.h>
 
 int main()
