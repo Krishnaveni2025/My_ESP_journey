7788c6a3fb46c27d93c5cb3db4975dcd587d2006
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-04 15:02:56 +0530

    Update C_Program_09_02.c

--- C_PROGRAMMING/C_Program_09_02.c
+++ C_PROGRAMMING/C_Program_09_02.c
@@ -5,3 +5,21 @@
   use arrays
 Question: Get 5 numbers from user and print average of all numbers
 */
+#include <stdio.h>
+
+int main()
+{
+    int i = 0,n;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int arr[n],avg = 0;
+    for(i = 0; i<n; i++)
+    {
+        printf("Enter %d Element\n",i);
+        scanf("%d",&arr[i]);
+        avg += arr[i];
+    }
+  avg /= n;
+    printf("%d",avg);
+    return 0;
+}
