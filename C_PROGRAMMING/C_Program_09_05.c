1825571ca6367ede8e9af3361c64ff02468159ed
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-04 23:54:27 +0530

    Update C_Program_09_05.c

--- C_PROGRAMMING/C_Program_09_05.c
+++ C_PROGRAMMING/C_Program_09_05.c
@@ -3,4 +3,33 @@
   Use only int variables
   Use ont decimals
   use arrays
-Question: Get 5 numbers from the user and arrange in ascending order and print the same*/
+Question: Get 5 numbers from the user and arrange in ascending order and print the same
+*/
+#include<stdio.h>
+int main()
+{
+  int n = 5,i = 0, arr[n], temp, j = 1;
+  for(i=0; i<n; i++)
+    {
+      printf("Enter %d Element:\n");
+      scanf("%d",&arr[i]);
+    }
+  i = 0;
+  for(i = 0; i<n; i++)
+    {
+      for(j=1; j<n; j++)
+        {
+          temp = arr[i];
+          if(temp>arr[j])
+          {
+            arr[j] = arr[i];
+            arr[j] = temp;
+          }
+        }
+    }
+  i = 0;
+  for(i = 0; i<n; i++)
+    {
+      printf("%d ",arr[i]);
+    }
+}
