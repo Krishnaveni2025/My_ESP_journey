51c662cdad24e6888bc7f49c18c7ef40584539cc
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-04 15:01:29 +0530

    Update C_Program_09_01.c

--- C_PROGRAMMING/C_Program_09_01.c
+++ C_PROGRAMMING/C_Program_09_01.c
@@ -5,3 +5,19 @@
   use arrays
 Question: Get 5 numbers from the user and print all the sum of all numbers
 */
+#include <stdio.h>
+int main()
+{
+    int i = 0,n;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int arr[n],sum = 0;
+    for(i = 0; i<n; i++)
+    {
+        printf("Enter %d Element\n",i);
+        scanf("%d",&arr[i]);
+        sum += arr[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
