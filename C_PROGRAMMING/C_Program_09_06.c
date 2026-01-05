2609300d4cde4aa46451b420f4d9f269de1edda3
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-05 18:32:56 +0530

    Update C_Program_09_06.c

--- C_PROGRAMMING/C_Program_09_06.c
+++ C_PROGRAMMING/C_Program_09_06.c
@@ -4,3 +4,36 @@
   Use ont decimals
   use arrays
 Question: Get 5 numbers from the user and arrange in ascending order and print the same*/
+#include<stdio.h>
+int main()
+{
+  int n = 5,i = 0, arr[n], temp, j = 1;
+  for(i=0; i<n; i++)
+    {
+      printf("Enter %d Element:\n",i);
+      scanf("%d",&arr[i]);
+    }
+  i = 0;
+  for(i = 0; i<n-1; i++)
+    {
+      int min = i;
+      for(j=i+1; j<n; j++)
+        {
+          temp = arr[i];
+          if(arr[j]<arr[min])
+          {
+             min = j;
+          }
+          if(min!=i){
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+          }
+        }
+    }
+  i = 0;
+  for(i = 0; i<n; i++)
+    {
+      printf("%d ",arr[i]);
+    }
+}
