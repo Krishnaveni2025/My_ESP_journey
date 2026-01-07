8ee4d7ad639a1ec2f2dea60655bf6b6559554651
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-01-07 06:14:42 +0530

    Update C_Program_09_08.c

--- C_PROGRAMMING/C_Program_09_08.c
+++ C_PROGRAMMING/C_Program_09_08.c
@@ -3,5 +3,24 @@
   Use only int variables
   Use ont decimals
   use arrays
-Question: Get 5 numbers from the user and remove the odd  numbersand crea a new array and then  print the same*/
+Question: Get 5 numbers from the user and remove the odd  numbers and create a new array and then  print the same*/
 #include<stdio.h>
+int main()
+{
+  int n = 5, arr[5], i = 0, j = 0, even[j], k =0;
+  for(i = 0; i<n; i++)
+    {
+      printf("Enter %d Element: ",i);
+      scanf("%d",&arr[i]);
+      if(!(arr[i]%2))
+      {
+          even[j] = arr[i];
+          j++;
+      }
+    }
+    for(k = 0; k<j; k++)
+    {
+        printf("%d ",even[k]);
+    }
+  
+}
