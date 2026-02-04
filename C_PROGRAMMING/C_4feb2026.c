5093fa650298d876d0ffa1ea5e34b630bd549c76
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-02-04 08:54:25 +0530

    Add files via upload

---
+++ C_PROGRAMMING/C_4feb2026.c
@@ -0,0 +1,87 @@
+/*=================================== C LEVEL 1 ======================================================
+  1. Get a number from the user and add 2 to that number and print the result.
+  2. Get a number from the user and subtract 5 from that number and print the result
+  3. Get a number from the user and multiply 3 by the number and print the result
+  4. Get a number from the user and divide the number by 6 and print the result
+  5. Get a number from the user and divide the number by 8 and print the remainder
+  6. Get a two-digit number from the user and print it's one's digit
+  7. Get a two-digit number from the user and print it's ten's digit
+  8. Get a three-digit number from the user and print it's one's digit
+  9. Get a three-digit number from the user and print it's hundred's digit
+  10. Get a three-digit number from the user and print it's ten's digit
+  11. Get a two-digit number from the user and print the sum of the digits of the number.
+  12. Get a three-digit number from the user and print it's sum of digits
+  13. Get a two-digit number froom the user and print reverse of  number
+  14. Get a three-digit number from the user and print reverse of the number
+  15. Get a four-digit number from the user and reverse only first two digits (one's & ten's)of the number then print the number
+  16. Get a four-digit number from the user and reverse only last two digits (1000's & 100's digits) of the number, then print the number
+  17. Get a two-digit number from the user and make the one's digit 0, then print it
+  18. Get a two-digit number from the user and make the ten's digit 1, then print it
+  19. Get a three-digit number from the user and make the one's digit 2, then print it
+  20. Get a three-digit number from the user and make ten's digit as 0, then print it.
+  21. Get a number from the user and subtract 5 from that number if the number is odd, then print the result. Do not use 'if'
+  22. Get a number from the user and subtract 5 from that number if the number's ten's digit is odd, then print the result. Do not use 'if'
+  23. Get a two-digit number from the user and subtract 5 from that number if the sum of the digits of the number is odd, then print the result. Do not use 'if'
+  24. Get a three-digit number from the user and subtract 5 from the number if one's digit and 100's digit are odd, then print the result. Do not use 'if'
+  25. Get a three-digit number from the user, if the sum of the digits is less than 10 then print the sum, otherwise print the digits of the sum. If the sum of the digits is less than 10 print the sum, otherwise add the digits of the sum and print the sum.
+        Note: The result should always be a single digit only
+  */
+#include <stdio.h>
+int main()
+{
+    int x, y;
+    /*printf("Enter a Number: ");
+    scanf("%d",&x);*/
+    // y = x+2; Problem 01
+    // y = x-5; Problem 02
+    // y= x*3;  Problem 03
+    // y = x/6; Problem 04
+    // y = x%8; Problem 05
+    /*printf("Enter a Two-digit Number: ");
+    scanf("%d",&x);*/
+    // y = x%10; Problem 06
+    // y = x/10; Problem 07
+    /*printf("Enter a Three-digit Number: ");
+    scanf("%d",&x);*/
+    // y = x%10;       Problem 08
+    // y = x/100;      Problem 09
+    // y = (x/10)%10;  Problem 10
+    /*printf("Enter a Two-digit Number: ");
+    scanf("%d",&x);*/
+    // y = ((x%10)+(x/10));      Problem 11
+    /*printf("Enter a Three-digit Number: ");
+    scanf("%d",&x);*/
+    // y = ((x%10)+(x/100)+((x/10)%10));  Problem 12
+    /*printf("Enter a Two-digit Number: ");
+    scanf("%d",&x);*/
+    // y = ((x%10)*10)+(x/10);  Problem 13
+    /*printf("Enter a Three-digit Number: ");
+    scanf("%d",&x);
+    y = ((x%10)*100)+(((x/10)%10)*10)+(x/100); Problem 14*/
+    /*printf("Enter a Four-digit Number: ");
+    scanf("%d",&x);*/
+    // y = ((x/100)*100)+((x%10)*10)+((x%100)/10);      Problem 15
+    // y = (((x/100)%10)*1000)+((x/1000)*100)+(x%100);  Problem 16
+    /*printf("Enter a Two-digit Number: ");
+    scanf("%d",&x);*/
+    // y = ((x/10)*10);       Problem 17
+    // y = 10+(x%10);         Problem 18
+    /*printf("Enter a Three-digit Number: ");
+    scanf("%d",&x);*/
+    // y  = ((x/10)*10)+2;           Problem 19
+    // y = ((x/100)*100)+(x%10);     Problem 20
+    /*printf("Enter a Number: ");
+    scanf("%d",&x);*/
+    // y = x - 5*(x%2);              Problem 21
+    // y = x - 5*(((x/10)%10)%2);    Problem 22
+    /*printf("Enter a Two-digit Number: ");
+    scanf("%d",&x);
+    y  =  x - 5*(((x/10)+(x%10))%2);    Problem 23*/
+    printf("Enter a Three-digit Number: ");
+    scanf("%d", &x);
+    // y = x - 5*(((x/100)%2)*((x%10)%2));   Problem 24
+    int s = (x % 10) + (x / 100) + ((x / 10) % 10);
+    y = (s * (1 - (s / 10))) + ((s / 10) * ((s / 10) + (s % 10)));
+    y = (y / 10) + (y % 10); // Problem 25
+    printf("Result = %d", y);
+}
