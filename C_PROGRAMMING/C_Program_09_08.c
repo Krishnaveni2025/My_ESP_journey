e3521fe8fe43981e062166dba0f56f87492bd7b4
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-01 15:41:06 +0530

    Update C_Program_09_08.c

--- C_PROGRAMMING/C_Program_09_08.c
+++ C_PROGRAMMING/C_Program_09_08.c
@@ -4,23 +4,54 @@
   Use ont decimals
   use arrays
 Question: Get 5 numbers from the user and remove the odd  numbers and create a new array and then  print the same*/
-#include<stdio.h>
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+/*
+  C LEVEL 09 Problem 07
+  Use only int variables
+  Use ont decimals
+  use arrays
+Question: Get 5 numbers from the user and arrange in descending order and print the same*/
+#include <stdio.h>
+
 int main()
 {
-  int n = 5, arr[5], i = 0, j = 0, even[j], k =0;
-  for(i = 0; i<n; i++)
+    int size = 5, input[size],output[size],i,evencount=0;
+    for(i = 0; i<size; i++)
     {
-      printf("Enter %d Element: ",i);
-      scanf("%d",&arr[i]);
-      if(!(arr[i]%2))
-      {
-          even[j] = arr[i];
-          j++;
-      }
+        printf("Enter Array %d Element: ",i);
+        scanf("%d",&input[i]);
     }
-    for(k = 0; k<j; k++)
+    printf("Array Before Removing Odd Numbers\n");
+    for(i = 0; i<size; i++)
     {
-        printf("%d ",even[k]);
+        printf("%d ",input[i]);
     }
-  
+    for(i = 0; i<size; i++)
+    {
+        if(!(input[i]%2))
+        {
+            output[evencount] = input[i];
+            evencount++;
+        }
+    }
+    if(!(evencount))
+    {
+        printf("All are ODD Numbers");
+    }
+    else
+    {
+        printf("\nArray After Removing Odd Numbers\n");
+        for(i = 0; i<evencount; i++)
+        {
+            printf("%d ",output[i]);
+        }
+    }
+    
 }
