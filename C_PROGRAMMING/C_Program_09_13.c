942036e7a83b7bad989affe7291c387bb428effa
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-06 22:51:36 +0530

    Add array input and success/failure check
    Implemented functionality to read multiple integers into an array until 0 is entered. Added success/failure message based on the first and last entered numbers.

--- C_PROGRAMMING/C_Program_09_13.c
+++ C_PROGRAMMING/C_Program_09_13.c
@@ -4,3 +4,31 @@
   Use ont decimals
   use arrays
 Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. If  numbers entered  if first and last numbers are equal, print "Success" else "Failure"*/
+#include<stdio.h>
+int main()
+{
+    int input[200],i=0,number,count=0;
+    while(1)
+    {
+        printf("Enter %d Element: ",i);
+        scanf("%d",&number);
+        input[i] = number;
+        i++;
+        count++;
+        if(!(number))
+            break;
+    }
+    printf("Entered Array :\n");
+    for(i=0; i<count; i++)
+    {
+        printf("%d ",input[i]);
+    }
+    if(input[0]==input[count-2])
+    {
+        printf("\nSuccess");
+    }
+    else
+    {
+        printf("\nFailure");
+    }
+}
