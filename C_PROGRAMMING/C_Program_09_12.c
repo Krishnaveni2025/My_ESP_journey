34498a1ca02345d4fa9c77b92ae6cfc47bdc1123
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-06 22:28:16 +0530

    Implement user input and sum calculation for array
    Modified the program to accept multiple integers from the user until 0 is entered. Added functionality to store the numbers in an array and calculate their sum.

--- C_PROGRAMMING/C_Program_09_12.c
+++ C_PROGRAMMING/C_Program_09_12.c
@@ -1,13 +1,29 @@
 /*
   C LEVEL 09 Problem 12
   Use only int variables
-  Use ont decimals
+  Use only decimals
   use arrays
 Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. Print the total numbers entered by the user and print the sum of numbers*/
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-  int i = 0, arr[];
-  scanf("%d",&arr[i]);
-  while(!(arr[i]));
+    int input[200],num,i=0,count=0,sum = 0;
+    while(1)
+    {
+        printf("Enter %d Element: ",i);
+        scanf("%d",&num);
+        input[i]=num;
+        i++;
+        count++;
+        if(num==0)
+            break;
+    }
+    printf("Entered Array Elements are:\n");
+    for(i=0; i<count; i++)
+    {
+        printf("%d ",input[i]);
+       sum += input[i];
+    }
+    printf("\nSum of array Elements: %d",sum);
+    return 0;
 }
