dfe23daec70b4ac70c111b620989d14be5929a9a
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-06 23:11:05 +0530

    Implement array input and middle number calculation
    The program now collects multiple numbers from the user until 0 is entered, stores them in an array, and calculates either the middle number or the average of the middle two numbers based on the count of inputs.

--- C_PROGRAMMING/C_Program_09_14.c
+++ C_PROGRAMMING/C_Program_09_14.c
@@ -5,3 +5,36 @@
   use arrays
 Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. 
 If total number is odd then print middle number, otherwise print the average of middle two numbers */
+
+#include<stdio.h>
+int main()
+{
+    int input[300],i=0,number,count=0,avg=0;
+    while(1)
+    {
+        printf("Enter %d Element: ",i);
+        scanf("%d",&number);
+        input[i] = number;
+        i++;
+        count++;
+        if(!number)
+            break;
+    }
+    printf("Entered Array:\n");
+    for(i=0; i<count; i++)
+    {
+        printf("%d ",input[i]);
+        avg += input[i];
+    }
+    count--;
+    if(count%2)
+    {
+        int middle = count/2;
+        printf("\nMiddle = %d",input[middle]);
+    }
+    else
+    {
+        avg /= count;
+        printf("\nAverage = %d",avg);
+    }
+}
