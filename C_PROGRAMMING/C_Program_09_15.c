b6db3e0135e857093c99fd4e69c8ea0f3766905e
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-07 23:15:43 +0530

    Add digit sum calculation and sorting to program

--- C_PROGRAMMING/C_Program_09_15.c
+++ C_PROGRAMMING/C_Program_09_15.c
@@ -1,14 +1,15 @@
 /*
   C LEVEL 09 Problem 15
   Use only int variables
-  Use ont decimals
+  Use only decimals
   use arrays
 Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. 
 Add each numbers digit and put them in an array and arrange the new array in the ascending order and print the same*/
 #include<stdio.h>
+int sumofdigits(int num);
 int main()
 {
-    int input[200], i = 0, count = 0, number,j=0, temp;
+    int input[200], output[200], i = 0, count = 0, number,j=0, temp;
     while(1)
     {
         printf("Enter %d Element: ",i);
@@ -16,6 +17,7 @@ int main()
         if(number)
         {
             input[i] = number;
+            output[i] = sumofdigits(number);
             i++;
             count++;
         }
@@ -28,23 +30,33 @@ int main()
     printf("\nNew Array: ");
     for(i=0; i<count; i++)
     {
-        printf("%d ",input[i]);
+        printf("%d ",output[i]);
     }
     for(i=0; i<count; i++)
     {
         for(j=i+1; j<count; j++)
         {
-            if(input[i]>input[j])
+            if(output[i]>output[j])
             {
-                temp = input[j];
-                input[j] = input[i];
-                input[i] = temp;
+                temp = output[j];
+                output[j] = output[i];
+                output[i] = temp;
             }
         }
     }
     printf("\nArranged Array in ascending order: ");
     for(i=0; i<count; i++)
     {
-        printf("%d ",input[i]);
+        printf("%d ",output[i]);
     }
+}
+int sumofdigits(int num)
+{
+    int sum = 0;
+    while(num)
+    {
+        sum+=num%10;
+        num/=10;
+    }
+    return sum;
 }
