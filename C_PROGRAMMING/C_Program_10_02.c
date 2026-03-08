78c0c042c3ac3931cc9734d7c5417fe1e303f81d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-08 20:59:25 +0530

    Implement ASCII value printing for a single number

--- C_PROGRAMMING/C_Program_10_02.c
+++ C_PROGRAMMING/C_Program_10_02.c
@@ -2,3 +2,12 @@
   Don't use structures
   Question: Get a single number and print it's ASCII value
 */
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter Single Number: ");
+    scanf("%d",&number);
+    printf("ASCII Value: %d",number+48);//ACII in decimal of numbers start with 0->48
+    return 0;
+}
