5638f71219c35d1652cd64b423eea0173f30c45b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-08 20:45:30 +0530

    Fix ASCII value printing for input alphabet

--- C_PROGRAMMING/C_Program_10_01.c
+++ C_PROGRAMMING/C_Program_10_01.c
@@ -2,3 +2,13 @@
   Don't use structures
   Question: Get an alphabet number and print it's ASCII value
 */
+#include <stdio.h>
+
+int main()
+{
+    char alphabet;
+    printf("Enter an Alphabet: ");
+    scanf("%c",&alphabet);
+    printf("ASCII Value: %d",alphabet);
+    return 0;
+}
