71a3fd197b0d6e321561a0cabb867673d03f50d0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-10 23:32:55 +0530

    Update C_Program_10_03.c

--- C_PROGRAMMING/C_Program_10_03.c
+++ C_PROGRAMMING/C_Program_10_03.c
@@ -2,3 +2,13 @@
   Don't use structures
   Question: Get a String and print the same
 */
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    printf("Enter String: ");
+    scanf("%s",str);
+    printf("%s",str);
+
+    return 0;
+}
