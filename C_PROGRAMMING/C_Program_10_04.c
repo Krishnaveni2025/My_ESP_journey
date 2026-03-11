321d5d4bc206e1f7dc291172b7680e87ea14cea3
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-11 14:28:55 +0530

    Correct string handling and output in C program
    Fix uninitialized size variable and print integer values of characters in the string.

--- C_PROGRAMMING/C_Program_10_04.c
+++ C_PROGRAMMING/C_Program_10_04.c
@@ -2,3 +2,18 @@
   Don't use structures
   Question: Get a number as string and print the integer value of the string
 */
+#include <stdio.h>
+int main()
+{
+    int size,i;
+    char numstr[size];
+    printf("Enter Size of String: ");
+    scanf("%d",&size);
+    printf("Enter Number String: ");
+    scanf("%s",numstr);
+    for(i = 0; i<size; i++)
+    {
+      printf("%d ",numstr[i]);   
+    }
+    return 0;
+}
