bdc4854b54eff24d43224456ed1cd8f7d77b650e
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-12 14:04:03 +0530

    Update C_Program_11_01.c

--- C_PROGRAMMING/C_Program_11_01.c
+++ C_PROGRAMMING/C_Program_11_01.c
@@ -2,3 +2,16 @@
   Use Pointers
 Question: Get a number from the user and print the same. Change the the value without directly changing it and print the same
 */
+#include <stdio.h>
+
+int main()
+{
+    int *number1, *number2, temp;
+    printf("Enter Two Numbers:");
+    scanf("%d %d",number1,number2);
+    temp = *number1;
+    number1 = number2;
+    number2 = &temp;
+    printf("number1 = %d number2 = %d",*number1,*number2);
+    return 0;
+}
