73d396799347e574101ff0f404ab67aa1b774c19
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-13 22:46:30 +0530

    Fix string input and output formatting

--- C_PROGRAMMING/C_Program_10_03.c
+++ C_PROGRAMMING/C_Program_10_03.c
@@ -2,13 +2,13 @@
   Don't use structures
   Question: Get a String and print the same
 */
+
 #include <stdio.h>
 int main()
 {
     char str[100];
     printf("Enter String: ");
-    scanf("%s",str);
-    printf("%s",str);
-
+    scanf("%99s",str);
+    printf("You Entered %s",str);
     return 0;
 }
