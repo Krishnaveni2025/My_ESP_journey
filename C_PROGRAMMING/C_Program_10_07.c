282f0c7707ca0e370c2a572b05b5423d1a315100
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-14 01:15:01 +0530

    Implement string length calculation in C program

--- C_PROGRAMMING/C_Program_10_07.c
+++ C_PROGRAMMING/C_Program_10_07.c
@@ -2,3 +2,19 @@
   Don't use structures
   Question: Get a string and find the length of the string
 */
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int length = 0,i=0;
+    printf("Enter String: ");
+    scanf("%99s",str);
+    while(str[i]!='\0')
+    {
+        length++;
+        i++;
+    }
+    printf("Length = %d",length);
+
+    return 0;
+}
