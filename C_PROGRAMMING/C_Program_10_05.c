4f8a61bbddb9cb9ace6eff33b26aad61af2e176b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-14 00:51:09 +0530

    Refactor integer to string conversion in C program

--- C_PROGRAMMING/C_Program_10_05.c
+++ C_PROGRAMMING/C_Program_10_05.c
@@ -3,27 +3,27 @@
   Question: Get an integer and print it as a string
 */
 #include <stdio.h>
-
-int main() {
-    int number,j,i = 0;
-    char str[100];   // array to hold string output
-    printf("Enter a Number: ");
-    scanf("%d",&number);
-    while(number)
+int main()
+{
+    char str[100];
+    int i=0,j,temp, value;
+    printf("Enter Number: ");
+    scanf("%d",&value);//12345
+    while(value)//t t t t t
     {
-        str[i] = (number%10)+'0';
-        number /= 10;
-        i++;
+        str[i] = (value%10)+'0';//5 54 543 5432 54321
+        value /= 10;//1234 123 12 1 0
+        i++;//1 2 3 4 5
     }
-    str[i] = '\0';  // null terminate
-
-    // Reverse the string (since digits were added backwards)
-    for (j = 0; j < i / 2; j++) {
-        char temp = str[j];
-        str[j] = str[i - j - 1];
-        str[i - j - 1] = temp;
+    str[i]='\0';// 54321\0
+    for(j=0; j<i/2; j++)//0 1
+    {
+        temp = str[j];//5 4
+        str[j] = str[i-j-1];//14321 12325
+        str[i-j-1] = temp;//  14325 12345  
+        i--;//4 3
     }
-    printf("String = %s",str);
+    printf("%s",str);//12345
 
     return 0;
 }
