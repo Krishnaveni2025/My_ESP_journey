e8b606f8dea9258dfdaf2ea00503ffe95b1104fd
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-14 01:11:59 +0530

    Correct digit extraction and printing from integer
    Fix digit printing logic and improve output format.

--- C_PROGRAMMING/C_Program_10_06.c
+++ C_PROGRAMMING/C_Program_10_06.c
@@ -2,3 +2,25 @@
   Don't use structures
   Question: Get an intger and print each digit as a character. Print one character on one line
 */
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i=0,j,temp, value;
+    printf("Enter Number: ");
+    scanf("%d",&value);//12345
+    while(value)//t t t t t
+    {
+        str[i] = (value%10)+'0';//5 54 543 5432 54321
+        value /= 10;//1234 123 12 1 0
+        i++;//1 2 3 4 5
+    }
+    str[i]='\0';
+    for(j=i;j>=0;j--)
+    {
+        printf("%c\n",str[i]);//1 2 3 4 5 
+        i--;
+    }
+
+    return 0;
+}
