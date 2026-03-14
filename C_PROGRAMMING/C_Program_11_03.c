181f5faf406e8a70da8a16e1b39d332f36eae344
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 01:07:54 +0530

    Update C_Program_11_03.c

--- C_PROGRAMMING/C_Program_11_03.c
+++ C_PROGRAMMING/C_Program_11_03.c
@@ -1,6 +1,7 @@
 /* C LEVEL 11 Problem 03
   Use Pointers
-Question: Write a loop program to print 1 to 5 one by one. Write a function and print the result in the main function
+Question: Write a loop program to print 1 to 5 one by one.
+Write a function and print the result in the main function
 Function Name: arrange_ascend
 Answer:
 1
@@ -9,3 +10,21 @@ Answer:
 4
 5
 */
+int print_1to5(int *p);
+#include<stdio.h>
+int main()
+{
+    int number;
+    number = 1;
+    while(number<6)
+    {
+        if(print_1to5(&number))
+            printf("%d\n",number);
+        number++;
+    }
+}
+int print_1to5(int *p)
+{
+    if(*p<6)
+         return 1;
+}
