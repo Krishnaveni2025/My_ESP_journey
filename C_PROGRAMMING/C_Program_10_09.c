d382dad6420a8b869b832ffd95e6a17a73dabcc1
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-14 17:27:23 +0530

    Add function to remove leading zeros from input string
    Implement logic to remove leading zeros from a string of numbers.

--- C_PROGRAMMING/C_Program_10_09.c
+++ C_PROGRAMMING/C_Program_10_09.c
@@ -2,3 +2,26 @@
   Don't use structures
   Question: Get a string of numbers upto 50 digits and remove all leading zeros
 */
+#include<stdio.h>
+int main()
+{
+    char numbstr[51];
+    printf("Enter a Number(50 digits):");
+    scanf("%s",numbstr);
+    int i = 0, j = 0;
+    while((numbstr[i]!='\0')&&(numbstr[i]=='0'))
+    {
+        i++;
+    }
+    if(numbstr[i]=='\0'){
+        numbstr[i] = '0';
+        numbstr[1] = '\0';
+    }
+    else{
+        while(numbstr[i]!='\0'){
+            numbstr[j++] = numbstr[i++];
+        }
+        numbstr[j] = '\0';
+    }
+    printf("%s",numbstr);
+}
