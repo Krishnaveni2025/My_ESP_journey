a453038a6d920f913af4e75bedd80d2a5ad92bf8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-14 16:45:10 +0530

    Add number validation for up to 50 digits

--- C_PROGRAMMING/C_Program_10_08.c
+++ C_PROGRAMMING/C_Program_10_08.c
@@ -2,3 +2,27 @@
   Don't use structures
   Question: Get a string of numbers upto 50 digits and validate the number
 */
+#include<stdio.h>
+int main()
+{
+    char numbstr[51];
+    printf("Enter a Number(50 digits): ");
+    scanf("%s",numbstr);
+    int valid = 1,i=0;
+    while(numbstr[i]!='\0')
+    {
+        if(numbstr[i]<'0' || numbstr[i]>'9')
+        {
+            valid = 0;
+            break;
+        }
+        i++;
+    }
+    if(valid)
+    {
+        printf("Your number is valid");
+    }
+    else{
+        printf("Pleease Enter a valid number");
+    }
+}
