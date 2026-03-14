cf516786bf2685e4c8fa9c4ea34855bd7dd735f6
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 01:15:47 +0530

    Update C_Program_11_04.c

--- C_PROGRAMMING/C_Program_11_04.c
+++ C_PROGRAMMING/C_Program_11_04.c
@@ -1,6 +1,28 @@
 /* C LEVEL 11 Problem 04
   Use Pointers
-Question: Write a function to find the two digit odd numbers whose sum of digits is 7. Print the result in the main function
+Question: Write a function to find the two digit odd numbers whose sum of digits is 7. 
+Print the result in the main function
 Function Name: find_2digit_odd_sum7
 Answer: 25,43,61
 */
+#include<stdio.h>
+int find_2digit_odd_sum7(int *p);
+int main()
+{
+    int number = 10;
+    while(number<100)
+    {
+        if(find_2digit_odd_sum7(&number))
+            printf("%d\n",number);
+        number++;
+    }
+}
+int find_2digit_odd_sum7(int *p)
+{
+    if((*p)%2)
+    {
+        if((((*p)/10)+((*p)%10))==7)
+            return 1;
+    }
+    return 0;
+}
