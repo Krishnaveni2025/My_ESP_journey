045dc82038bb101bb5451b6da2a1a9f3fac26caf
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-14 17:35:41 +0530

    Implement number reversal for up to 50 digits

--- C_PROGRAMMING/C_Program_10_10.c
+++ C_PROGRAMMING/C_Program_10_10.c
@@ -2,3 +2,23 @@
   Don't use structures
   Question: Get a number upto 50 digits and reverse it
 */
+#include<stdio.h>
+int main()
+{
+    char numbstr[51];
+    printf("Enter a Number(50 digits):");
+    scanf("%s",numbstr);
+    int i = 0, j = 0, temp;
+    while(numbstr[i]!='\0')
+    {
+        j++;
+        i++;
+    }
+    for(i=0; i<j/2; i++)
+    {
+        temp = numbstr[i];
+        numbstr[i] = numbstr[j-i-1];
+        numbstr[j-i-1] = temp;
+    }
+    printf("%s",numbstr);
+}
