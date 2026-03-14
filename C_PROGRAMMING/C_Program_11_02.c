7165d8e89d568fcf2b5b9cc07781387ebe58f9a3
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 00:35:43 +0530

    Correct array size handling and increment function
    Fix array initialization and increment logic.

--- C_PROGRAMMING/C_Program_11_02.c
+++ C_PROGRAMMING/C_Program_11_02.c
@@ -2,3 +2,35 @@
   Use Pointers
 Question: Pass an integer array to a function and increment each array element in the function
 */
+/* C LEVEL 11 Problem 02
+  Use Pointers
+Question: Pass an integer array to a function and increment each array element in the function
+*/
+#include<stdio.h>
+void inc_array_element(int arr[],int n);
+int main()
+{
+    int size, array[size], i;
+    printf("Enter Array Size: ");
+    scanf("%d",&size);
+    for(i=0; i<size; i++)
+    {
+        printf("Enter %d Element: ",i);
+        scanf("%d",&array[i]);
+    }
+    inc_array_element(array,size);
+    printf("After Increment:\n");
+    for(i=0; i<size; i++)
+    {
+        printf("%d ",array[i]);
+    }
+}
+void inc_array_element(int arr[], int n)
+{
+    int *element,i;
+    for(i=0;i<n;i++)
+    {
+        element = arr+i;
+        *element = *element+1;
+    }
+}
