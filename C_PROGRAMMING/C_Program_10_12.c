337eb40b657bf7c391ed2628503809e89da4b906
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-14 18:03:41 +0530

    Implement array input and character conversion

--- C_PROGRAMMING/C_Program_10_12.c
+++ C_PROGRAMMING/C_Program_10_12.c
@@ -5,3 +5,20 @@
   Array-1 4 5 8 7 6 3
   Answer:1458763
 */
+#include<stdio.h>
+int main()
+{
+    char numbstr[51];
+    int number[50],i = 0,j = 0, size;
+    printf("Enter the size of Array:");
+    scanf("%d",&size);
+    printf("Integer array:\n");
+    for(j=0;j<size;j++)
+    {
+        printf("%d Element:",j);
+        scanf("%d",&number[j]);
+        numbstr[j] = number[j]+48;
+    }
+    numbstr[j] = '\0';
+    printf("character array: %s",numbstr);
+}
