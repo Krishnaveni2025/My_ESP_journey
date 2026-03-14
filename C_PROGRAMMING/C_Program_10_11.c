80e46f7b39037907ce5496264d101232a05adde4
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-14 17:52:17 +0530

    Implement number string to integer array conversion
    Added functionality to convert a number string up to 50 digits into an integer array.

--- C_PROGRAMMING/C_Program_10_11.c
+++ C_PROGRAMMING/C_Program_10_11.c
@@ -2,3 +2,21 @@
   Don't use structures
   Question: Get a number string upto 50 digits and convert it to an integer array
 */
+#include<stdio.h>
+int main()
+{
+    char numbstr[51];
+    int number[50],i = 0,j = 0, digit;
+    printf("Enter Number string:");
+    scanf("%s",numbstr);
+    while(numbstr[i]!='\0')
+    {
+        number[i]= (numbstr[i])-48;
+        i++;
+    }
+    printf("Integer array:\n");
+    for(j=0;j<i;j++)
+    {
+        printf("%d",number[j]);
+    }
+}
