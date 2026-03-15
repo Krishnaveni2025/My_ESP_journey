795e644eebc423bbf4265cc3d7a908e980550faa
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 15:57:56 +0530

    Update C_Program_12_01.c

--- C_PROGRAMMING/C_Program_12_01.c
+++ C_PROGRAMMING/C_Program_12_01.c
@@ -1,1 +1,11 @@
-
+/* C LEVEL 12 - Problem 01
+Use Pointers
+  Question: Get two numbers upto 50 digits and perform addition and print the result.
+  #include<stdio.h>
+  int main()
+  {
+    getnumbers(number1, number2);
+    addnumbers(number1, number2, result);
+    print(result);
+  }
+*/
