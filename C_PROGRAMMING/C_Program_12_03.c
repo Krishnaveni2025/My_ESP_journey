73125fe7f3a086bbaba0185509117ec21774a1ed
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 16:03:30 +0530

    Update C_Program_12_03.c

--- C_PROGRAMMING/C_Program_12_03.c
+++ C_PROGRAMMING/C_Program_12_03.c
@@ -1,1 +1,9 @@
+/* C LEVEL 12 - Problem 03
+Use Pointers
+  Question: Get a check string and substring. check the substring in the main string and print the position.
+  Example:
+  string: helloetalvislearning
+  sub-striing: etal
+  Answer: 6
+*/
 
