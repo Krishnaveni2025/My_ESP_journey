49e89eacf795f2aa2f2dd03608eac95f75ab4f05
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 14:39:08 +0530

    Update C_Program_11_05.c

--- C_PROGRAMMING/C_Program_11_05.c
+++ C_PROGRAMMING/C_Program_11_05.c
@@ -3,3 +3,29 @@
 Question: Write a function to copy integers from one location to another location
 Function Name: memcopy(src,dst,size)
 */
+#include<stdio.h>
+void memcopy(int *src,int *dst, int size);
+int main()
+{
+    int size, array[size], copy[size] ,i;
+    printf("Enter the size of array: ");
+    scanf("%d",&size);
+    for(i=0; i<size; i++)
+    {
+        printf("Enter %d Element: ",i);
+        scanf("%d",&array[i]);
+    }
+    memcopy(array, copy, size);
+    printf("Copied array:\n");
+    for(i=0; i<size; i++)
+    {
+        printf("%d ",copy[i]);
+    }
+}
+void memcopy(int *src, int *dst, int size)
+{
+    for(int i =0 ; i<size; i++)
+    {
+        *(dst++) = *(src++);
+    }
+}
