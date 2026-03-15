f633254df763498716d01f56057ab86af603f1b5
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 16:00:47 +0530

    Update C_Program_12_02.c

--- C_PROGRAMMING/C_Program_12_02.c
+++ C_PROGRAMMING/C_Program_12_02.c
@@ -1,1 +1,9 @@
+/* C LEVEL 12 - Problem 02
+Use Pointers
+  Question: Get a string and a character from the user, find the all the positions where the character is present, and print it.
+  Example:
+  string: helloetalvislearning
+  character: e
+  Answer: 2,6,14
+*/
 
