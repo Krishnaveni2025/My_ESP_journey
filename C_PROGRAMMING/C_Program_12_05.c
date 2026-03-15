d4d1b6c6d15b07671734def9ca27e4b61737a3fa
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 16:07:21 +0530

    Update C_Program_12_05.c

--- C_PROGRAMMING/C_Program_12_05.c
+++ C_PROGRAMMING/C_Program_12_05.c
@@ -1,1 +1,11 @@
-
+/* C LEVEL 12 - Problem 05
+Use Pointers
+  Question: Write a program to multiply upto 50 digit numbers.
+  #include<stdio.h>
+  int main()
+  {
+    getnumbers(number1, number2);
+    multiplynumbers(number1, number2, result);
+    print(result);
+  }
+*/
