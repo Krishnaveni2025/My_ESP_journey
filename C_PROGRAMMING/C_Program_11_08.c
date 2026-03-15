4a12d418ff492d1d974a1135a1442cf6421b0a3c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 15:13:33 +0530

    Update C_Program_11_08.c

--- C_PROGRAMMING/C_Program_11_08.c
+++ C_PROGRAMMING/C_Program_11_08.c
@@ -3,3 +3,32 @@
 Question: Write a function to compare to strings. Print success or failure
 Function Name: strcomp(src, dst)
 */
+#include<stdio.h>
+int strcomp(char *src, char *dst);
+int main()
+{
+    char input1[100],input2[100];
+    printf("Enter First String:\n");
+    scanf("%s",input1);
+    printf("Enter Second String:\n");
+    scanf("%s",input2);
+    if(strcomp(input1,input2))
+            printf("Success");
+    else
+            printf("Failure");
+}
+int strcomp(char *src, char *dst)
+{
+    int result = 1;
+    while(*src != '\0')
+    {
+        if(*src != *dst)
+        {
+            result = 0;
+            break;
+        }
+        src++;
+        dst++;
+    }
+    return result;
+}
