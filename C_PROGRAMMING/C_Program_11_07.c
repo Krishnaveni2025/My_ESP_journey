c3a4ac3b98a5b3a31fd60bc048ec10a492c44ba7
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 15:05:28 +0530

    Update C_Program_11_07.c

--- C_PROGRAMMING/C_Program_11_07.c
+++ C_PROGRAMMING/C_Program_11_07.c
@@ -3,3 +3,35 @@
 Question: Write a function to compare a set of integers and print success or failure
 Function Name: strcomp(src,dst,size)
 */
+#include<stdio.h>
+int strcomp(int *src, int *dst, int size);
+int main()
+{
+    int size, array1[size], array2[size], i;
+    printf("Enter Arrays Size: ");
+    scanf("%d",&size);
+    printf("First Array:\n");
+    for(i=0; i<size; i++)
+    {
+        printf("Element %d: ",i);
+        scanf("%d",&array1[i]);
+    }
+    printf("Second Array:\n");
+    for(i=0; i<size; i++)
+    {
+        printf("Element %d: ",i);
+        scanf("%d",&array2[i]);
+    }
+    if(strcomp(array1,array2,size))
+            printf("Success");
+    else
+            printf("Failure");
+}
+int strcomp(int *src, int *dst, int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        if(src[i] != dst[i])
+            return 0;
+    }
+}
