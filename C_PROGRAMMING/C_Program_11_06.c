2bb25492a7b50d774f1be92de642fcc5d3f231af
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 14:50:54 +0530

    Update C_Program_11_06.c

--- C_PROGRAMMING/C_Program_11_06.c
+++ C_PROGRAMMING/C_Program_11_06.c
@@ -3,3 +3,21 @@
 Question: Write a function to copy a string to another
 Function Name: strcopy(src,dst)
 */
+#include<stdio.h>
+void strcopy(char *src, char *dst);
+int main()
+{
+    char input[100], copy[100];
+    printf("Enter a string: ");
+    scanf("%s",input);
+    strcopy(input, copy);
+    printf("Copied String: %s",copy);
+}
+void strcopy(char *src, char *dst)
+{
+    while(*src!='\0')
+    {
+        *(dst++) = *(src++);
+    }
+    *(dst) = '\0';
+}
