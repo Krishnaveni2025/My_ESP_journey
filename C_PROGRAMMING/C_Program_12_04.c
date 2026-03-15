cb313c1e473d577f55bc552181be568281acaebf
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-15 16:05:25 +0530

    Update C_Program_12_04.c

--- C_PROGRAMMING/C_Program_12_04.c
+++ C_PROGRAMMING/C_Program_12_04.c
@@ -1,1 +1,7 @@
-
+/* C LEVEL 12 - Problem 04
+Use Pointers
+  Question: Get a string using gets function and count all the words in it.
+  Example:
+  string: welcome to etalvis learning
+  Answer: 4
+*/
