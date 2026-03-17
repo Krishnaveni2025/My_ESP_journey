b1425e6e7c2aaca17d6a927a909aca1e5eae1867
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-17 07:04:09 +0530

    Update C_Program_12_06.c

--- C_PROGRAMMING/C_Program_12_06.c
+++ C_PROGRAMMING/C_Program_12_06.c
@@ -21,3 +21,170 @@ Each number can be of a different digit.
 Make sure that all the Input conditions are taken care.
 Try to minimize the execution speed.
 */
+#include <stdio.h>
+
+// Manual string length (no string.h)
+int mystrlen(char *s) {
+    int len = 0;
+    while (*s != '\0') {
+        len++;
+        s++;
+    }
+    return len;
+}
+
+// Addition of big integers
+void addnumbers(char *n1, char *n2, char *result) {
+    int len1 = mystrlen(n1);
+    int len2 = mystrlen(n2);
+    int carry = 0, sum;
+    int i = len1 - 1, j = len2 - 1, k = 0;
+    char temp[102];
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        int d1 = (i >= 0) ? n1[i] - '0' : 0;
+        int d2 = (j >= 0) ? n2[j] - '0' : 0;
+        sum = d1 + d2 + carry;
+        temp[k++] = (sum % 10) + '0';
+        carry = sum / 10;
+        i--; j--;
+    }
+
+    // reverse into result
+    for (int m = 0; m < k; m++)
+        result[m] = temp[k - m - 1];
+    result[k] = '\0';
+}
+
+// Subtraction (assume n1 >= n2)
+void subnumbers(char *n1, char *n2, char *result) {
+    int len1 = mystrlen(n1);
+    int len2 = mystrlen(n2);
+    int borrow = 0, diff;
+    int i = len1 - 1, j = len2 - 1, k = 0;
+    char temp[102];
+
+    while (i >= 0) {
+        int d1 = n1[i] - '0' - borrow;
+        int d2 = (j >= 0) ? n2[j] - '0' : 0;
+        if (d1 < d2) {
+            d1 += 10;
+            borrow = 1;
+        } else {
+            borrow = 0;
+        }
+        diff = d1 - d2;
+        temp[k++] = diff + '0';
+        i--; j--;
+    }
+
+    // remove leading zeros
+    while (k > 1 && temp[k - 1] == '0') k--;
+
+    // reverse into result
+    for (int m = 0; m < k; m++)
+        result[m] = temp[k - m - 1];
+    result[k] = '\0';
+}
+
+// Multiplication
+void mulnumbers(char *n1, char *n2, char *result) {
+    int len1 = mystrlen(n1);
+    int len2 = mystrlen(n2);
+    int temp[102] = {0};
+
+    for (int i = len1 - 1; i >= 0; i--) {
+        for (int j = len2 - 1; j >= 0; j--) {
+            int d1 = n1[i] - '0';
+            int d2 = n2[j] - '0';
+            temp[i + j + 1] += d1 * d2;
+        }
+    }
+
+    for (int k = len1 + len2 - 1; k > 0; k--) {
+        if (temp[k] >= 10) {
+            temp[k - 1] += temp[k] / 10;
+            temp[k] %= 10;
+        }
+    }
+
+    int start = 0;
+    while (start < len1 + len2 - 1 && temp[start] == 0) start++;
+
+    int pos = 0;
+    for (int m = start; m < len1 + len2; m++)
+        result[pos++] = temp[m] + '0';
+    result[pos] = '\0';
+}
+
+// Division (quotient and remainder)
+void divnumbers(char *n1, char *n2, char *quotient, char *remainder) {
+    // For simplicity, we’ll implement division only for small divisor (fits in int)
+    // Full big-integer division would require long division algorithm.
+    int divisor = 0;
+    for (int i = 0; n2[i] != '\0'; i++)
+        divisor = divisor * 10 + (n2[i] - '0');
+
+    int len1 = mystrlen(n1);
+    int rem = 0, pos = 0;
+
+    for (int i = 0; i < len1; i++) {
+        rem = rem * 10 + (n1[i] - '0');
+        quotient[pos++] = (rem / divisor) + '0';
+        rem = rem % divisor;
+    }
+    quotient[pos] = '\0';
+
+    // remove leading zeros
+    int start = 0;
+    while (quotient[start] == '0' && start < pos - 1) start++;
+    for (int i = start; i <= pos; i++)
+        quotient[i - start] = quotient[i];
+
+    // remainder
+    int rpos = 0;
+    if (rem == 0) {
+        remainder[rpos++] = '0';
+    } else {
+        int digits[20], dcount = 0;
+        while (rem > 0) {
+            digits[dcount++] = rem % 10;
+            rem /= 10;
+        }
+        for (int i = dcount - 1; i >= 0; i--)
+            remainder[rpos++] = digits[i] + '0';
+    }
+    remainder[rpos] = '\0';
+}
+
+int main() {
+    char num1[51], num2[51], result[102], quotient[102], remainder[51];
+    char op;
+
+    while (1) {
+        printf("Calc> ");
+        if (scanf("%50s", num1) != 1) break;
+        if (num1[0] == 'E' && num1[1] == 'x' && num1[2] == 'i' && num1[3] == 't')
+            break;
+        scanf(" %c", &op);
+        scanf("%50s", num2);
+
+        if (op == '+') {
+            addnumbers(num1, num2, result);
+            printf("%s\n", result);
+        } else if (op == '-') {
+            subnumbers(num1, num2, result);
+            printf("%s\n", result);
+        } else if (op == '*') {
+            mulnumbers(num1, num2, result);
+            printf("%s\n", result);
+        } else if (op == '/') {
+            divnumbers(num1, num2, quotient, remainder);
+            printf("Quotient: %s\n", quotient);
+            printf("Remainder: %s\n", remainder);
+        } else {
+            printf("Invalid operator\n");
+        }
+    }
+    return 0;
+}
