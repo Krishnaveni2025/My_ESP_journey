7492a871546e9ffae0951e7545aefd759dc83445
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-17 06:56:40 +0530

    Update C_Program_12_03.c

--- C_PROGRAMMING/C_Program_12_03.c
+++ C_PROGRAMMING/C_Program_12_03.c
@@ -7,3 +7,56 @@ Use Pointers
   Answer: 6
 */
 
+#include <stdio.h>
+
+// Function prototypes
+void get_strings(char *src, char *sub);
+void find_substring(char *src, char *sub);
+
+int main() {
+    char mainstr[100], substr[50];
+    get_strings(mainstr, substr);
+    find_substring(mainstr, substr);
+    return 0;
+}
+
+void get_strings(char *src, char *sub) {
+    printf("Enter Main String: ");
+    scanf("%99s", src);
+    printf("Enter Substring: ");
+    scanf("%49s", sub);
+    printf("Main String: %s\n", src);
+    printf("Substring: %s\n", sub);
+    printf("____________________________________________________________\n");
+}
+
+// Manual length function
+int mystrlen(char *s) {
+    int len = 0;
+    while (*s != '\0') {
+        len++;
+        s++;
+    }
+    return len;
+}
+
+void find_substring(char *src, char *sub) {
+    int len1 = mystrlen(src);
+    int len2 = mystrlen(sub);
+    int i, j, found;
+
+    for (i = 0; i <= len1 - len2; i++) {
+        found = 1;
+        for (j = 0; j < len2; j++) {
+            if (src[i + j] != sub[j]) {
+                found = 0;
+                break;
+            }
+        }
+        if (found) {
+            printf("Substring found at position: %d\n", i + 1); // 1-based index
+            return;
+        }
+    }
+    printf("Substring not found\n");
+}
