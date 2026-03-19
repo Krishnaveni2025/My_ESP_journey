fde44a55c4a5aeba38c1777573ff6021ece320c5
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-19 16:10:02 +0530

    Update C_Program_09_14.c

--- C_PROGRAMMING/C_Program_09_14.c
+++ C_PROGRAMMING/C_Program_09_14.c
@@ -1,40 +1,36 @@
 /*
   C LEVEL 09 Problem 14
   Use only int variables
-  Use ont decimals
+  Use only decimals
   use arrays
-Question: Get multiple numbers from the user and store them in an array . Stop when the number is 0. 
-If total number is odd then print middle number, otherwise print the average of middle two numbers */
-
+Question: 
+Get multiple numbers from the user and store them in an array . 
+Stop when the number is 0. 
+If total number is odd then print middle number, 
+otherwise print the average of middle two numbers */
 #include<stdio.h>
 int main()
 {
-    int input[300],i=0,number,count=0,avg=0;
+    int numbers[100], i = 0, number, total = 0,middle, average = 0;
     while(1)
     {
-        printf("Enter %d Element: ",i);
+        printf("Enter %d Number: ",i+1);
         scanf("%d",&number);
-        input[i] = number;
-        i++;
-        count++;
         if(!number)
             break;
+        numbers[i] = number;
+        i++;
+        total++;
     }
-    printf("Entered Array:\n");
-    for(i=0; i<count; i++)
+    middle = total/2;
+    if(total%2)
     {
-        printf("%d ",input[i]);
-        avg += input[i];
-    }
-    count--;
-    if(count%2)
-    {
-        int middle = count/2;
-        printf("\nMiddle = %d",input[middle]);
+        printf("Middle = %d",numbers[middle]);
     }
     else
     {
-        avg /= count;
-        printf("\nAverage = %d",avg);
+        average = ((numbers[middle])+(numbers[middle-1]));
+        average /= 2;
+        printf("Average = %d",average);
     }
 }
