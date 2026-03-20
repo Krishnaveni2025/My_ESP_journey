9ddc2e195cc9eb6ed198d9602eb720a3fbf26476
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-20 05:57:48 +0530

    Update C_Program_01_05.c

--- C_PROGRAMMING/C_Program_01_05.c
+++ C_PROGRAMMING/C_Program_01_05.c
@@ -1,17 +1,18 @@
 /* C LEVEL 01 Problem 05
-Question: Get a number from the user and the number by 8 and  print the remainder
+Question: 
+    Get a number from the user and 
+    the number by 8 and  
+    print the remainder
 Example: 
-Input: 45, Output: 5
-Input: 143, Output: 7
+    Input: 45, Output: 5
+    Input: 143, Output: 7
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
-  scanf("%d",&x);
-  y = x%8;
-  printf("%d",y);
+  int number;
+  printf("Input: ");
+  scanf("%d",&number);
+  printf("Output: %d",number%8);
   return 0;
 }
-
-
