b87e6947e47ac8a478a5c90a5d5eabc8c6292356
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:18:21 +0530

    Update C_Program_02_07.c

--- C_PROGRAMMING/C_Program_02_07.c
+++ C_PROGRAMMING/C_Program_02_07.c
@@ -1,4 +1,9 @@
 /* C LEVEL 02 Program 07
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +, -, /, *, %, ==, !=, <, >, <=, >=
+        Do not use if, for, while, arrays, pointers, strings, etc.
   Question: Get a two digit number from the user and check if the digit 1 and digit 0 are identical . If yes then print 1, Otherwise print 0.
 Example:
 Input: 55    Output: 1
