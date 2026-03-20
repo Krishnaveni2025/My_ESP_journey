f863fcd9feb40a859d11daa2c7f4f078f4155936
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:50:12 +0530

    Update C_Program_02_13.c

--- C_PROGRAMMING/C_Program_02_13.c
+++ C_PROGRAMMING/C_Program_02_13.c
@@ -2,21 +2,24 @@
         Use only int variables
         Use only Decimals
         Use the following operations only
-        +, -, /, *, %, ==, !=, <, >, <=, >=
+        +, -, /, *, %, &, ==, !=, <, >, <=, >=
         Do not use if, for, while, arrays, pointers, strings, etc.
-  Question: Get a three digit number from the user and check if the digit 0 and digit 1 are same and if digit 1 and 2 are different. If yes then print 1, Otherwise print 0.
-Example:
-Input: 988    Output: 1
-Input: 567    Output: 0
+   Question: 
+        Get a three digit number from the user and 
+        check if the digit 0 and digit 1 are same and 
+        if digit 1 and 2 are different. 
+        If yes then print 1, Otherwise print 0.
+   Example:
+        Input: 988    Output: 1
+        Input: 567    Output: 0
   */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   //your code here
-  y = (((x%10)==((x/10)%10))&((x/100)!=((x/10)%10)));
-  printf("Result = %d",y);
+  printf("Result = %d", ( ( (number%10) == ((number/10)%10) ) & ( ((number/10)%10) != (number/100) ) ));
   return 0;
 }
