26cc07a1a8ca0ce37c2c6e84c579f550d70b5176
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:48:39 +0530

    Update C_Program_01_20.c

--- C_PROGRAMMING/C_Program_01_20.c
+++ C_PROGRAMMING/C_Program_01_20.c
@@ -4,18 +4,19 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Get a three digit number from the user, make the ten's digit 0, then print it
-  Example:
-Input:695 Output:605
-Input:182 Output:102
+   Question: 
+        Get a three digit number from the user, 
+        make the ten's digit 0, then print it
+   Example:
+        Input:695 Output:605
+        Input:182 Output:102
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
-  y = (x/100)*100+(x%10);
-  printf("Result = %d",y);
+  scanf("%d",&number);
+  printf("Result = %d",(((number/100)*100)+(number%10)));
   return 0;
 }
