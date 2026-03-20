56ba8c93134ca525bfbec2cee69d61394e965fb2
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:14:40 +0530

    Update C_Program_01_08.c

--- C_PROGRAMMING/C_Program_01_08.c
+++ C_PROGRAMMING/C_Program_01_08.c
@@ -4,17 +4,19 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Write a program to get a three digit number from the user and print the one's digit
-  Example: Input:456 Output:6
-           Input:569 Output:9
+    Question: 
+        Write a program to get a three digit number from the user and 
+        print the one's digit
+    Example: 
+        Input:456 Output:6
+        Input:569 Output:9
   */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
-  y = x%10;
-  printf("%d",y);
+  scanf("%d",&number);
+  printf("Output: %d",number%10);
   return 0;
 }
