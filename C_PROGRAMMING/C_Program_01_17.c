a69c3bec089fffe14723bb0f1c83b9b23f776006
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:08:39 +0530

    Update C_Program_01_17.c

--- C_PROGRAMMING/C_Program_01_17.c
+++ C_PROGRAMMING/C_Program_01_17.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 17
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
 Question: Get a two digit number from the user, make the one's digit 0 then print it.
 Example:
 Input:95 Output:90
