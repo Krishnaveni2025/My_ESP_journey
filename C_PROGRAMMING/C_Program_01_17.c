410759b3ba89c122d1a3ebaacbc50f0576170085
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:41:50 +0530

    Update C_Program_01_17.c

--- C_PROGRAMMING/C_Program_01_17.c
+++ C_PROGRAMMING/C_Program_01_17.c
@@ -4,18 +4,19 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-Question: Get a two digit number from the user, make the one's digit 0 then print it.
-Example:
-Input:95 Output:90
-Input:18 Output:10
+   Question: 
+        Get a two digit number from the user, 
+        make the one's digit 0 then print it.
+   Example:
+        Input:95 Output:90
+        Input:18 Output:10
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a two digit number: ");
-  scanf("%d",&x);
-  y = (x/10)*10;
-  printf("Result = %d",y);
+  scanf("%d",&number);
+  printf("Result = %d",(number/10)*10);
   return 0;
 }
