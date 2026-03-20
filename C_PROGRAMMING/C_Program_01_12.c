892df4b2a10395a09ef3e8b092f2cd7a177dd648
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:07:19 +0530

    Update C_Program_01_12.c

--- C_PROGRAMMING/C_Program_01_12.c
+++ C_PROGRAMMING/C_Program_01_12.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 12
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
   Question: Write a program to get a three digit number from the user and print it's sum of digits
   Example: 
 Input:562 Output:13
