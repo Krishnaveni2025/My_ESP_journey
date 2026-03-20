fa6ab696bdfb1bdadd4f1d7901ef97c919f958dc
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:06:31 +0530

    Update C_Program_01_09.c

--- C_PROGRAMMING/C_Program_01_09.c
+++ C_PROGRAMMING/C_Program_01_09.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 09
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
   Question: Write a program to get a three digit number from the user and print the hundred's digit of the number.
   Example:
   Input:456 Output:4
