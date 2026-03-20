9bf3c25b13e8a305c0761c36058083a20da419b1
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-20 05:55:53 +0530

    Update C_Program_01_04.c

--- C_PROGRAMMING/C_Program_01_04.c
+++ C_PROGRAMMING/C_Program_01_04.c
@@ -1,16 +1,18 @@
 /* C LEVEL 01 Problem 04
-Question: Get a number from the user and the number by 6 and  print the result
+Question: 
+    Get a number from the user and 
+    the number by 6 and  
+    print the result
 Example: 
-Input: 45, Output: 7
-Input: 143, Output: 23
+    Input: 45, Output: 7
+    Input: 143, Output: 23
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
-  scanf("%d",&x);
-  y = x/6;
-  printf("%d",y);
+  int number;
+  printf("Input: ");
+  scanf("%d",&number);
+  printf("Output: %d",number/6);
   return 0;
 }
-
