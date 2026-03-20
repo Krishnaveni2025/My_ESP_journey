8b6f257d4a476830376b0ebbc6378ccb928a69f9
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:09:59 +0530

    Update C_Program_01_22.c

--- C_PROGRAMMING/C_Program_01_22.c
+++ C_PROGRAMMING/C_Program_01_22.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 22
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
   Question: Get a number from the user and subtract 5 from that number if the number's ten's digit position is odd then print the result. Do not use "if"
 Example:
 Input:685   Output:685
