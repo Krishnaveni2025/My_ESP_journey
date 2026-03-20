bd2c90567c6939bdf9259ca07515eb46d57c3694
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-20 06:04:32 +0530

    Update C_Program_01_03.c

--- C_PROGRAMMING/C_Program_01_03.c
+++ C_PROGRAMMING/C_Program_01_03.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Problem 03
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
 Question: 
     Get a number from the user and  
     multiply 3 to that number and  
