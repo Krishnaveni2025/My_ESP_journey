95589f5fec7bad885e745dc1bfc42121cc1d1ab4
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:07:33 +0530

    Update C_Program_01_13.c

--- C_PROGRAMMING/C_Program_01_13.c
+++ C_PROGRAMMING/C_Program_01_13.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 13
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
   Question: Write a program to get a two-digit number from the user and print the reverse of the number
 Example:
 Input:56 Output:65
