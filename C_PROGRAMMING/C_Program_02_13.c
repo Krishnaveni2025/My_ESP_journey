5b8dd080cd7df0c2dfdfe477d1461f8325113c9b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:20:02 +0530

    Update C_Program_02_13.c

--- C_PROGRAMMING/C_Program_02_13.c
+++ C_PROGRAMMING/C_Program_02_13.c
@@ -1,4 +1,9 @@
 /* C LEVEL 02 Program 13
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +, -, /, *, %, ==, !=, <, >, <=, >=
+        Do not use if, for, while, arrays, pointers, strings, etc.
   Question: Get a three digit number from the user and check if the digit 0 and digit 1 are same and if digit 1 and 2 are different. If yes then print 1, Otherwise print 0.
 Example:
 Input: 988    Output: 1
