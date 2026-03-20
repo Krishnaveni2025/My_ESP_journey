7a9ccbd72b4883b59e9c10607d20cef664b398fb
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:52:30 +0530

    Update C_Program_02_14.c

--- C_PROGRAMMING/C_Program_02_14.c
+++ C_PROGRAMMING/C_Program_02_14.c
@@ -4,19 +4,21 @@
         Use the following operations only
         +, -, /, *, %, ==, !=, <, >, <=, >=
         Do not use if, for, while, arrays, pointers, strings, etc.
-  Question: Get a four digit number from the user and check if first two digits and last two digits are same. If yes then print 1, Otherwise print 0.
-Example:
-Input: 1919    Output: 1
-Input: 6567    Output: 0
+  Question: 
+        Get a four digit number from the user and 
+        check if first two digits and last two digits are same. 
+        If yes then print 1, Otherwise print 0.
+  Example:
+        Input: 1919    Output: 1
+        Input: 6567    Output: 0
   */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a four digit number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   //your code here
-  y = ((x/100)==(x%100));
-  printf("Result = %d",y);
+  printf("Result = %d",((number%100) == (number/100)));
   return 0;
 }
