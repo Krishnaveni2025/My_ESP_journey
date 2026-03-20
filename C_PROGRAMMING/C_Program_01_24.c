1840c28eab6dccda0a4861303ffee3c07bd5a7a6
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:01:32 +0530

    Update C_Program_01_24.c

--- C_PROGRAMMING/C_Program_01_24.c
+++ C_PROGRAMMING/C_Program_01_24.c
@@ -4,19 +4,23 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Get a three digit number from the user and subtract 5 from that number if one's digit number and 100's digit number are odd, then print the result. Do not use "if"
-Example:
-Input:795  Output:790
-Input:372  Output:372
+   Question: 
+        Get a three digit number from the user and 
+        subtract 5 from that number 
+        if one's digit number and 100's digit number are odd, 
+        then print the result. 
+        Do not use "if"
+   Example:
+        Input:795  Output:790
+        Input:372  Output:372
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   //your code here
-  y = x - (5*((x%10)%2)*((x/100)%2));
-  printf("Result = %d",y);
+  printf("Result = %d",(number - 5*( ((number/100)%2) * ((number%10)%2) )));
   return 0;
 }
