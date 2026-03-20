5a550777e88e6d161c5d61966aef6ad7840e398b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:09:45 +0530

    Update C_Program_01_21.c

--- C_PROGRAMMING/C_Program_01_21.c
+++ C_PROGRAMMING/C_Program_01_21.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 21
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
 Question: Get a number from the user and subtract 5 from that number if the number is odd, then print the result. Do not use "if".
 Example:
 Input:695 Output:690
