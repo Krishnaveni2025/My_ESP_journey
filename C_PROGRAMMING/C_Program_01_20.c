8431224c5e4e0008593be63ad6d608bc0c13be99
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:09:30 +0530

    Update C_Program_01_20.c

--- C_PROGRAMMING/C_Program_01_20.c
+++ C_PROGRAMMING/C_Program_01_20.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 20
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
   Question: Get a three digit number from the user, make the ten's digit 0, then print it
   Example:
 Input:695 Output:605
