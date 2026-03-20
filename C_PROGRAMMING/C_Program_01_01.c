79d77dd13548f6776ca431cf311379dac5ac570a
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-20 06:05:39 +0530

    Update C_Program_01_01.c

--- C_PROGRAMMING/C_Program_01_01.c
+++ C_PROGRAMMING/C_Program_01_01.c
@@ -1,11 +1,16 @@
 /* C LEVEL 01 Problem 01
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
 Question: 
-Get a number from the user and 
-add 2 to that number and  
-print the result
+    Get a number from the user and 
+    add 2 to that number and  
+    print the result
 Example: 
-Input: 45, Output: 47
-Input:56789, Output: 56791
+    Input: 45, Output: 47
+    Input:56789, Output: 56791
 */
 #include<stdio.h>
 int main()
