0730f1c8e0fecacbd675c09709160fb932d0eb13
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:26:15 +0530

    Update C_Program_01_13.c

--- C_PROGRAMMING/C_Program_01_13.c
+++ C_PROGRAMMING/C_Program_01_13.c
@@ -4,19 +4,20 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Write a program to get a two-digit number from the user and print the reverse of the number
-Example:
-Input:56 Output:65
-Input:59 Output:95
+   Question: 
+        Write a program to get a two-digit number from the user and 
+        print the reverse of the number
+   Example:
+        Input:56 Output:65
+        Input:59 Output:95
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   //your code here
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
-  y = ((x%10)*10)+(x/10);
-  printf("Result = %d",y);
+  scanf("%d",&number);
+  printf("Result = %d",(((number%10)*10)+(number/10)));
   return 0;
 }
