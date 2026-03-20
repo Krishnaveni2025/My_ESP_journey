bcdb0fbce4a05da03a70c860cacdc7065cf63f3b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:20:12 +0530

    Update C_Program_02_14.c

--- C_PROGRAMMING/C_Program_02_14.c
+++ C_PROGRAMMING/C_Program_02_14.c
@@ -1,4 +1,9 @@
 /* C LEVEL 02 Program 14
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +, -, /, *, %, ==, !=, <, >, <=, >=
+        Do not use if, for, while, arrays, pointers, strings, etc.
   Question: Get a four digit number from the user and check if first two digits and last two digits are same. If yes then print 1, Otherwise print 0.
 Example:
 Input: 1919    Output: 1
