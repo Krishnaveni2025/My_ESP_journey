12bdbe71ca7db4e944c22314b5ed32677b7473cb
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:10:34 +0530

    Update C_Program_01_24.c

--- C_PROGRAMMING/C_Program_01_24.c
+++ C_PROGRAMMING/C_Program_01_24.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 24
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
   Question: Get a three digit number from the user and subtract 5 from that number if one's digit number and 100's digit number are odd, then print the result. Do not use "if"
 Example:
 Input:795  Output:790
