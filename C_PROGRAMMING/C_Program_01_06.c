9051fb2a7586a2dcd2eceae2b99ba868248e9846
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-20 06:03:34 +0530

    Update C_Program_01_06.c

--- C_PROGRAMMING/C_Program_01_06.c
+++ C_PROGRAMMING/C_Program_01_06.c
@@ -1,14 +1,20 @@
 /*    C LEVEL 01 Problem 06
-Question: Get a two-digit number from the user ans print the ones digit
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
+Question: 
+    Get a two-digit number from the user and print the ones digit
 Example: Input: 45  Output: 5
          Input: 56  Output: 6
   */
 #include<stdio.h>
 int main()
 {
-  int x,y;
-  scanf("%d",&x);
-  y = x%10;
-  printf("%d",y);
+  int number;
+  printf("Input: ");
+  scanf("%d",&number);
+  printf("Output: %d",number%10);
   return 0;
 }
