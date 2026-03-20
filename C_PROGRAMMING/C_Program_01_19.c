ca3921e18ba9ee1b17f58cb55e671a080df68055
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:46:40 +0530

    Update C_Program_01_19.c

--- C_PROGRAMMING/C_Program_01_19.c
+++ C_PROGRAMMING/C_Program_01_19.c
@@ -4,18 +4,19 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-Question: Get a three digit number from the user, make the one's digit 2 and then print it
-Example:
-Input:695 Output:692
-Input:181 Output:182
+   Question: 
+        Get a three digit number from the user, 
+        make the one's digit 2 and then print it
+   Example:
+        Input:695 Output:692
+        Input:181 Output:182
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
-  y = (x/10)*10+2;
-  printf("Result = %d",y);
+  scanf("%d",&number);
+  printf("Result = %d",((number/10)*10)+2);
   return 0;
 }
