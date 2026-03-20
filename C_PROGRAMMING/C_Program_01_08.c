dba117cea1179b8a7ca72fd7981a91458cea4222
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:06:09 +0530

    Update C_Program_01_08.c

--- C_PROGRAMMING/C_Program_01_08.c
+++ C_PROGRAMMING/C_Program_01_08.c
@@ -1,4 +1,9 @@
 /*  C LEVEL 01 Program 08
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
   Question: Write a program to get a three digit number from the user and print the one's digit
   Example: Input:456 Output:6
            Input:569 Output:9
