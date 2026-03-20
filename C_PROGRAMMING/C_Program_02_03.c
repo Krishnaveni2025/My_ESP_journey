fdd338cabfa594bad0c1131a46171e3d61e0d250
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:26:53 +0530

    Update C_Program_02_03.c

--- C_PROGRAMMING/C_Program_02_03.c
+++ C_PROGRAMMING/C_Program_02_03.c
@@ -4,19 +4,22 @@
         Use the following operations only
         +, -, /, *, %, ==, !=, <, >, <=, >=
         Do not use if, for, while, arrays, pointers, strings, etc.
-Question: Get a number from the user and chceck if the number is less than 50. If yes then print 1, otherwise print 0
-Example: 
-Input: 45    Output:1
-Input: 56789 Output:0
+   Question: 
+        Get a number from the user and 
+        check if the number is less than 50. 
+        If yes then print 1, 
+        otherwise print 0
+   Example: 
+        Input: 45    Output:1
+        Input: 56789 Output:0
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   //your code here
-  y = x<50;
-  printf("Result = %d",y);
+  printf("Result = %d",number<50);
   return 0;
 }
