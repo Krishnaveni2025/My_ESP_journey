3e84457593a382659bcae9537a188ce983581bc0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:41:16 +0530

    Update C_Program_02_10.c

--- C_PROGRAMMING/C_Program_02_10.c
+++ C_PROGRAMMING/C_Program_02_10.c
@@ -4,19 +4,21 @@
         Use the following operations only
         +, -, /, *, %, ==, !=, <, >, <=, >=
         Do not use if, for, while, arrays, pointers, strings, etc.
-  Question: Get a two digit number from the user and check if the digit 0 is greater than digit 1. If yes then print 1, Otherwise print 0.
-Example:
-Input: 45    Output: 1
-Input: 76    Output: 0
+   Question: 
+        Get a two digit number from the user and 
+        check if the digit 0 is greater than digit 1. 
+        If yes then print 1, Otherwise print 0.
+   Example:
+        Input: 45    Output: 1
+        Input: 76    Output: 0
   */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a two digit number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   //your code here
-  y = ((x%10)>(x/10));
-  printf("Result = %d",y);
+  printf("Result = %d",( (number%10)>(number/10) ));
   return 0;
 }
