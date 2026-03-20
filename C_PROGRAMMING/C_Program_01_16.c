374a09ced75154e71e9e766e815fe32d675faa97
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:39:39 +0530

    Update C_Program_01_16.c

--- C_PROGRAMMING/C_Program_01_16.c
+++ C_PROGRAMMING/C_Program_01_16.c
@@ -4,19 +4,21 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Get a four digit number from the user and only reverse last two digits of the number then print the number
-Example:
-Input:9561 Output:5961
-Input:3859 Output:8359
+   Question: 
+        Get a four digit number from the user and 
+        only reverse last two digits of the number (100's and 1000's)
+        then print the number
+   Example:
+        Input:9561 Output:5961
+        Input:3859 Output:8359
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   //your code here
   printf("Enter a four digit number: ");
-  scanf("%d",&x);
-  y = (((x/100)%10)*1000)+((x/1000)*100)+(x%100);
-  printf("%d",y);
+  scanf("%d",&number);
+  printf("%d",((((number/100)%10)*1000)+((number/1000)*100)+(number%100)));
   return  0;
 }
