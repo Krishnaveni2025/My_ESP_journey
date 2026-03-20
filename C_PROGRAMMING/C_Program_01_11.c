da739a65514fd279ec68abd9eaeaba2670527c89
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:21:03 +0530

    Update C_Program_01_11.c

--- C_PROGRAMMING/C_Program_01_11.c
+++ C_PROGRAMMING/C_Program_01_11.c
@@ -4,19 +4,20 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Write a program to get a two digit number from the user and print it's sum of digits
-Example:
-Input:56 Output:11
-Input:69 output:15
+   Question: 
+        Write a program to get a two digit number from the user and 
+        print it's sum of digits
+   Example:
+        Input:56 Output:11
+        Input:69 output:15
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   //your code here
   printf("Enter a two - digit number: ");
-  scanf("%d",&x);
-  y = (x%10)+(x/10);
-  printd("Result = %d",y);
+  scanf("%d",&number);
+  printf("Result = %d",((number%10)+(number/10)));
   return 0;
 }
