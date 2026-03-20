4630d02f158adf76d154d3e387042b8ad16cf1ec
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:23:48 +0530

    Update C_Program_01_12.c

--- C_PROGRAMMING/C_Program_01_12.c
+++ C_PROGRAMMING/C_Program_01_12.c
@@ -4,19 +4,20 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Write a program to get a three digit number from the user and print it's sum of digits
-  Example: 
-Input:562 Output:13
-Input:469 Output:19
+   Question: 
+        Write a program to get a three digit number from the user and 
+        print it's sum of digits
+   Example: 
+        Input:562 Output:13
+        Input:469 Output:19
 */
 #include<stdio.h>
 int main()
 {
-  int x, y;
+  int number;
   //your code here
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
-  y = ((x/10)%10)+(x/10)+(x%10);
-  printf("Result = %d", y);
+  scanf("%d",&number);
+  printf("Result = %d",((number%10)+(number/100)+((number/10)%10)));
   return 0;
 }
