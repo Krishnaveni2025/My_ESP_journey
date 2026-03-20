f6df7b447e09f8d9b41fa788baae651cf7826dd9
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:16:54 +0530

    Update C_Program_01_09.c

--- C_PROGRAMMING/C_Program_01_09.c
+++ C_PROGRAMMING/C_Program_01_09.c
@@ -4,19 +4,20 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Write a program to get a three digit number from the user and print the hundred's digit of the number.
+  Question: 
+        Write a program to get a three digit number from the user and 
+        print the hundred's digit of the number.
   Example:
-  Input:456 Output:4
-  Input:569 Output:5
+        Input:456 Output:4
+        Input:569 Output:5
   */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   //your code here
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
-  y = x/100;
-  printf("%d",y);
+  scanf("%d",&number);
+  printf("Output: %d",number/100);
   return 0;
 }
