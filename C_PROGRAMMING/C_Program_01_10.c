14bce3bae3bfde1f9771918185c8979edd3e47d6
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:18:48 +0530

    Update C_Program_01_10.c

--- C_PROGRAMMING/C_Program_01_10.c
+++ C_PROGRAMMING/C_Program_01_10.c
@@ -4,19 +4,20 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Write a c program to get a three digit number from the user and print the ten's digit
+  Question: 
+        Write a c program to get a three digit number from the user and 
+        print the ten's digit
   Example:
-Input:456 Output:5
-Input:569 Output:6
+        Input:456 Output:5
+        Input:569 Output:6
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   //your code here
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
-  y = (x/10)%10;
-  printf("Result = %d",y);
+  scanf("%d",&number);
+  printf("Result = %d",(number/10)%10);
   return 0;
 }
