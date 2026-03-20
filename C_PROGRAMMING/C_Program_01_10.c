21b8aa57f00b6c3bc50bd94897c3db10c82132c1
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 03:06:50 +0530

    Update C_Program_01_10.c

--- C_PROGRAMMING/C_Program_01_10.c
+++ C_PROGRAMMING/C_Program_01_10.c
@@ -1,4 +1,9 @@
 /* C LEVEL 01 Program 10
+        Use only int variables
+        Use only Decimals
+        Use the following operations only
+        +,-,/,*,%
+        Do not use any other operations
   Question: Write a c program to get a three digit number from the user and print the ten's digit
   Example:
 Input:456 Output:5
