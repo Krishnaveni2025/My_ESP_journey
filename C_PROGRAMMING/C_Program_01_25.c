25fcfbc3761e8150d38a73a7581da634b2d37860
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 04:12:29 +0530

    Update C_Program_01_25.c

--- C_PROGRAMMING/C_Program_01_25.c
+++ C_PROGRAMMING/C_Program_01_25.c
@@ -4,23 +4,33 @@
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
-  Question: Get a three digit number from the user. If the sum of the digits is less than 10, print the same, otherwise add the digits of sum. 
-  If the sum of the digits is less than 10 then print the sum, otherwise add the digits of the sum, and print the sum.
-  Note: the result of the sum should always be single digit only
-Example:
-Input:123 Output:6
-Input:149 Output:5(14,5)
-Input:991 Output:1(19;10;1)
+   Question: 
+        Get a three digit number from the user. 
+        If the sum of the digits is less than 10, 
+            print the same, 
+        otherwise 
+            add the digits of sum. 
+        If the sum of the digits is less than 10 
+            then print the sum, 
+        otherwise 
+            add the digits of the sum, 
+        and print the sum.
+   Note: The result of the sum should always be single digit only
+   Example:
+        Input:123 Output:6
+        Input:149 Output:5(14,5)
+        Input:991 Output:1(19;10;1)
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   //your code here
-  int sum = ((x/100)+(x%10)+((x/10)%10));
-  y = sum * (1 - sum/10) + (sum/10) * (sum/10 + sum%10);
-  printf("%d",y);
+  number = ((number/100)+(number%10)+((number/10)%10));
+  number = (number/10 + number%10);
+  number = (number/10 + number%10);
+  printf("%d",number);
   return 0;
 }
