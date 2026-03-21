548f0bd00c7d780627985e719c5719b1ae7d8a32
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 00:21:54 +0530

    Update C_Program_04_17.c

--- C_PROGRAMMING/C_Program_04_17.c
+++ C_PROGRAMMING/C_Program_04_17.c
@@ -3,10 +3,12 @@
         Use only Decimals
         Use only "if/else", goto statements. Don't use  "for/while"
         Don't use arrays, pointers, strings, etc.
-Question: Write a program to get a number from the user and print whether the number is prime or not
-Answer:
-Input:31   Output:prime
-Input:27 Output:not prime
+   Question: 
+        Write a program to get a number from the user and 
+        print whether the number is prime or not
+   Answer:
+        Input:31   Output:prime
+        Input:27  Output:not prime
   */
 #include<stdio.h>
 int main()
