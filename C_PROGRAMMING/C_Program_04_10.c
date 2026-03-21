1fac9ea1235ff0669e86314f2541aabeac175fa5
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 23:42:43 +0530

    Update C_Program_04_10.c

--- C_PROGRAMMING/C_Program_04_10.c
+++ C_PROGRAMMING/C_Program_04_10.c
@@ -1,4 +1,8 @@
 /* C LEVEL 04 Program 10
+        Use only int variables
+        Use only Decimals
+        Use only "if/else", goto statements. Don't use  "for/while"
+        Don't use arrays, pointers, strings, etc.
 Question: Write a loop program to print sum of two digit odd numbers whose ten's digit is 7
 Answer:
 375
