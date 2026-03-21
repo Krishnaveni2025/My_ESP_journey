a09af05644167acd6a22fdbbbec092cc80179d56
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:55:16 +0530

    Update C_Program_05_10.c

--- C_PROGRAMMING/C_Program_05_10.c
+++ C_PROGRAMMING/C_Program_05_10.c
@@ -1,35 +1,35 @@
 /* C LEVEL 05 Problem 10
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a loop program to print sum of two digit odd numbers, who's ten's digit is 7.
-  Answer:
-  375
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a loop program to print sum of two digit odd numbers, who's ten's digit is 7.
+   Answer:
+        375
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   //your code here
-  x  = 11;
+  number  = 11;
   int sum = 0;
   for(;;)
     {
       //your code here
       loop:
-      if(x<100)
+      if(number<100)
       {
-        if(x%2)
+        if(number%2)
         {
-          if((x/10)==7)
-            sum = sum + x;
+          if((number/10)==7)
+            sum = sum + number;
         }
-        x++;
+        number++;
         goto loop;
       }
       break;
     }
   printf("%d",sum);
 }
-
