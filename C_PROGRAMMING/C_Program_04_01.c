910aa25a655055b9beeff675dcb87a875df52bb8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 23:40:41 +0530

    Update C_Program_04_01.c

--- C_PROGRAMMING/C_Program_04_01.c
+++ C_PROGRAMMING/C_Program_04_01.c
@@ -1,22 +1,27 @@
 /* C LEVEL 04 Program 01
-  Question: Write a loop program to print 1 to 5 on one by one
-  Answer:
-  1
-  2
-  3
-  4
-  5
+        Use only int variables
+        Use only Decimals
+        Use only "if/else", goto statements. Don't use  "for/while"
+        Don't use arrays, pointers, strings, etc.
+   Question: 
+        Write a loop program to print 1 to 5 on one by one
+   Answer:
+        1
+        2
+        3
+        4
+        5
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   //your code here
-  x = 1;
-  loop:if(x<6)
+  number = 1;
+  loop:if(number<6)
   {
-    printf("%d\n",x);
-    x++;
+    printf("%d\n",number);
+    number++;
     goto loop;
   }
   return 0;
