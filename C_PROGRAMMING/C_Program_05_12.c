9152a31e73659b499a1f3a73d3f8b062a343b96a
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:59:32 +0530

    Update C_Program_05_12.c

--- C_PROGRAMMING/C_Program_05_12.c
+++ C_PROGRAMMING/C_Program_05_12.c
@@ -1,33 +1,34 @@
 /* C LEVEL 05 Problem 12
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Get a number from the user print the sum of all digits.
-  Answer:
-  Input:123456   Output:21
-  Input:76895439 Output:51
-  Input:675      Output:18
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Get a number from the user print the sum of all digits.
+   Answer:
+        Input:123456   Output:21
+        Input:76895439 Output:51
+        Input:675      Output:18
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   //your code here
   printf("Enter a Number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   int sum = 0;
   for(;;)
     {
       //your code here
       loop:
-      if(x)
+      if(number)
       {
-        sum = sum + (x%10);
-        x = x/10;
+        sum = sum + (number%10);
+        number /= 10;
         goto loop;
       }
-      printf("%d",sum);
       break;
     }
+    printf("%d",sum);
 }
