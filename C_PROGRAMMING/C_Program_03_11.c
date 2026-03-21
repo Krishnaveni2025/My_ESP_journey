3d72592c6a62ad79ef191b7271a3936d1bf48ec0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 22:33:03 +0530

    Update C_Program_03_11.c

--- C_PROGRAMMING/C_Program_03_11.c
+++ C_PROGRAMMING/C_Program_03_11.c
@@ -1,4 +1,7 @@
 /* C LEVEL 03 Program 11
+      Use int variables
+      Use only Decimals
+      Use "if/else" statements 
   Question: Get a two digit number from the user and check if the digit 1 is less than equals to digit 0. If yes then print "Success", otherwise "Failure".
   Example:
   Input:66 Output:"Success"
