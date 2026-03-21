1a3e118d460aa1055c4ecf61f560f8c6c343f140
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 23:41:32 +0530

    Update C_Program_04_04.c

--- C_PROGRAMMING/C_Program_04_04.c
+++ C_PROGRAMMING/C_Program_04_04.c
@@ -1,4 +1,8 @@
 /* C LEVEL 04 Program 04
+        Use only int variables
+        Use only Decimals
+        Use only "if/else", goto statements. Don't use  "for/while"
+        Don't use arrays, pointers, strings, etc.
   Question: Write a loop program to print sum of 6 to 1
   Answer:
   21
