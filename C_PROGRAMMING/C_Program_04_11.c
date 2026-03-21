0d423a1e61c3aa174571470be193c08f60604e5a
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 00:07:03 +0530

    Update C_Program_04_11.c

--- C_PROGRAMMING/C_Program_04_11.c
+++ C_PROGRAMMING/C_Program_04_11.c
@@ -3,27 +3,29 @@
         Use only Decimals
         Use only "if/else", goto statements. Don't use  "for/while"
         Don't use arrays, pointers, strings, etc.
-Question: Write a program to get a number from the user and print the total number of digits in that number 
-Answer:
-Input:123456   Output:6
-Input:76895439 Output:8
-Input:675      Output:3
+   Question: 
+        Write a program to get a number from the user and 
+        print the total number of digits in that number 
+   Answer:
+        Input:123456   Output:6
+        Input:76895439 Output:8
+        Input:675      Output:3
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   //your code here
   printf("Enter a number: ");
-  scanf("%d",&x);
-  int y = 0;
-  loop:if(x)
-  {
-    y++;
-    x = x/10;
+  scanf("%d",&number);
+  int digits = 0;
+  loop:if(number)
+  {     
+    digits++;
+    number /= 10;
      goto loop;
   }
-   printf("Result = %d",y);
+   printf("Result = %d",digits);
   return 0;
 }
 
