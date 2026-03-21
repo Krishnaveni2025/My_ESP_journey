09f178d90ebfd236f52bcc0ae667152c3e1b145b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:20:37 +0530

    Update C_Program_05_03.c

--- C_PROGRAMMING/C_Program_05_03.c
+++ C_PROGRAMMING/C_Program_05_03.c
@@ -1,4 +1,8 @@
 /* C LEVEL 05 Problem 03
+    Use only int variables
+    Use only Decimals
+    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+    Don't use Arrays, pointers, strings,etc.
   Question: Write a loop program to print sum of 1 to 5.
   */
 #include<stdio.h>
