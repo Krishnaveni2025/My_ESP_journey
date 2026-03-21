5d3b8912d85232d1b0ccc36ce5d52dfc96798df9
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:24:57 +0530

    Update C_Program_05_23.c

--- C_PROGRAMMING/C_Program_05_23.c
+++ C_PROGRAMMING/C_Program_05_23.c
@@ -1,4 +1,8 @@
 /* C LEVEL 05 Problem 23
+    Use only int variables
+    Use only Decimals
+    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+    Don't use Arrays, pointers, strings,etc.
   Question: Write a program to get a number from the user and print the total number of single digit perfect square numbers in the number
   Answer:
   Input: 123456789 Output: 3
