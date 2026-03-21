67d89dfe026d90c778c842ab2228b33d9450ab11
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 23:24:27 +0530

    Update C_Program_03_14.c

--- C_PROGRAMMING/C_Program_03_14.c
+++ C_PROGRAMMING/C_Program_03_14.c
@@ -2,19 +2,22 @@
       Use int variables
       Use only Decimals
       Use "if/else" statements 
-  Question: Get a four digit number from the user and check if the first two digits and last two digits are same. If yes then print "Success", otherwise "Failure".
-  Example:
-  Input:1919 Output:"Success"
-  Input:6567 Output:"Failure"
+   Question: 
+      Get a four digit number from the user and 
+      check if the first two digits and last two digits are same. 
+      If yes then print "Success", otherwise "Failure".
+   Example:
+      Input:1919 Output:"Success"
+      Input:6567 Output:"Failure"
 */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   printf("Enter a three digit number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   //your code here
-  if((x/100)==(x%100))
+  if((number/100)==(number%100))
   {
     printf("\"Success\"");
   }
