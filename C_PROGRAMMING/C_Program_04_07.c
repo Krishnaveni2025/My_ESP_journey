6707d742e5dd614e35e2999f422b8acd4de39599
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 23:58:00 +0530

    Update C_Program_04_07.c

--- C_PROGRAMMING/C_Program_04_07.c
+++ C_PROGRAMMING/C_Program_04_07.c
@@ -3,27 +3,29 @@
         Use only Decimals
         Use only "if/else", goto statements. Don't use  "for/while"
         Don't use arrays, pointers, strings, etc.
-Question: Write a loop program to print two digit odd numbers whose sum of digits are 7
-Answer:
-25
-43
-61
+   Question: 
+        Write a loop program to print two digit odd numbers whose sum of digits are 7
+   Answer:
+        25
+        43
+        61
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   //your code here
-  x = 11;
-  loop:if(x<100)
+  number = 11;
+  loop:if(number<100)
   {
-    if(x%2){
-    if((x%10)+(x/10)==7)
+    if(number%2)
     {
-      printf("%d\n",x);
+        if((number%10)+(number/10)==7)
+        {
+            printf("%d\n",number);
+        }
     }
-  }
-    x++;
+    number++;
     goto loop;
   }
   return 0;
