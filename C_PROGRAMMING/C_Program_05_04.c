302e76407fcd3ce2b00ddcca9270dfa88bf12b3e
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:36:52 +0530

    Update C_Program_05_04.c

--- C_PROGRAMMING/C_Program_05_04.c
+++ C_PROGRAMMING/C_Program_05_04.c
@@ -1,29 +1,31 @@
 /* C LEVEL 05 Problem 04
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a loop program to print sum of 6 to 1.
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a loop program to print sum of 6 to 1.
+   Answer:
+        21
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   //your code here
-  x  = 6;
+  number  = 6;
   int sum = 0;
   for(;;)
     {
       //your code here
       loop:
-      if(x>0)
+      if(number>0)
       {
-        sum = sum + x;
-        x--;
+        sum = sum + number;
+        number--;
         goto loop;
       }
       break;
     }
-    printf("%d",sum);
+    printf("Sum = %d",sum);
 }
-
