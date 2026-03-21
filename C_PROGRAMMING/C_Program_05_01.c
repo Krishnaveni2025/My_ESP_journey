09d2abd1120c030186882d54c79c201c22beb54c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:29:44 +0530

    Update C_Program_05_01.c

--- C_PROGRAMMING/C_Program_05_01.c
+++ C_PROGRAMMING/C_Program_05_01.c
@@ -1,32 +1,33 @@
 /* C LEVEL 05 Problem 01
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a loop program to print 1 to 5 on one by one.
-  Answer: 
-  1
-  2
-  3
-  4
-  5
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a loop program to print 1 to 5 on one by one.
+   Answer: 
+        1
+        2
+        3
+        4
+        5
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   //your code here
-  x  = 1;
+  number  = 1;
   for(;;)
     {
       //your code here
       loop:
-      if(x<6)
+      if(number<6)
       {
-        printf("%d\n",x);
-        x++;
+        printf("%d\n",number);
+        number++;
         goto loop;
       }
+      break;
     }
 }
-
