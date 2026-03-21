3ca82ef001e55b418a0365d1c0f455e18df1f4d4
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:19:57 +0530

    Update C_Program_05_01.c

--- C_PROGRAMMING/C_Program_05_01.c
+++ C_PROGRAMMING/C_Program_05_01.c
@@ -1,4 +1,8 @@
 /* C LEVEL 05 Problem 01
+    Use only int variables
+    Use only Decimals
+    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+    Don't use Arrays, pointers, strings,etc.
   Question: Write a loop program to print 1 to 5 on one by one.
   Answer: 
   1
