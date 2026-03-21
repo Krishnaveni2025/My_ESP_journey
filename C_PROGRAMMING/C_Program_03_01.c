398c1f9a695bdef7bd6802b6000d892551cf7b8c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 22:35:35 +0530

    Update C_Program_03_01.c

--- C_PROGRAMMING/C_Program_03_01.c
+++ C_PROGRAMMING/C_Program_03_01.c
@@ -2,19 +2,22 @@
       Use int variables
       Use only Decimals
       Use "if/else" statements 
-  Question: Get a number from the user and check if the number is equals to 50. If yes then print "Success", otherwise print "Failure".
-Example:
-Input:50    Output: Success
-Input:56789 Output: Failure
+   Question: 
+      Get a number from the user and 
+      check if the number is equals to 50. 
+      If yes then print "Success", otherwise print "Failure".
+   Example:
+      Input:50    Output: Success
+      Input:56789 Output: Failure
 */
 #include<stdio.h>
 int main()
 {
-  int x,y;
+  int number;
   printf("Enter a number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   //your code here
-  if(x==50)
+  if(number==50)
   {
     printf("Success");
   }
