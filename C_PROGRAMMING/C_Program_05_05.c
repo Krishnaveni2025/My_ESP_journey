320aeefa6618cb2fb3a6b50ec5d6c695088ce785
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:40:35 +0530

    Update C_Program_05_05.c

--- C_PROGRAMMING/C_Program_05_05.c
+++ C_PROGRAMMING/C_Program_05_05.c
@@ -1,30 +1,36 @@
 /* C LEVEL 05 Problem 05
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a loop program to print odd number between 1 and 9.
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a loop program to print odd number between 1 and 9.
+   Answer:
+        1
+        3
+        5
+        7
+        9
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number;
   //your code here
-  x  = 1;
+  number  = 1;
   for(;;)
     {
       //your code here
       loop:
-      if(x<10)
+      if(number<10)
       {
-        if(x%2)
+        if(number%2)
         {
-         printf("%d\n",x);
+         printf("%d\n",number);
         }
-        x++;
+        number++;
         goto loop;
       }
       break;
     }  
 }
-
