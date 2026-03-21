87e65524b60f8b9be3aec6ce845cebf55a2a199d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 04:23:51 +0530

    Update C_Program_05_18.c

--- C_PROGRAMMING/C_Program_05_18.c
+++ C_PROGRAMMING/C_Program_05_18.c
@@ -1,4 +1,8 @@
 /* C LEVEL 05 Problem 18
+    Use only int variables
+    Use only Decimals
+    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+    Don't use Arrays, pointers, strings,etc.
   Question: Write a program to get a number from the user and print whether the first two digits(1's & ten's) of that number is prime or not.
   Answer:
 Input: 359 - Output: Prime
