c2b10819bae4f796fb111320398018d5643d0a11
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-21 23:42:33 +0530

    Update C_Program_04_09.c

--- C_PROGRAMMING/C_Program_04_09.c
+++ C_PROGRAMMING/C_Program_04_09.c
@@ -1,4 +1,8 @@
 /* C LEVEL 04 Program 09
+        Use only int variables
+        Use only Decimals
+        Use only "if/else", goto statements. Don't use  "for/while"
+        Don't use arrays, pointers, strings, etc.
 Question: Write a loop program to print two digit even numbers whose sum of digits are 6
 Answer:
 24
