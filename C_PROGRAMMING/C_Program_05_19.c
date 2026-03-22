f3fc324564f757eecfec984d45d080fa04087784
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 22:30:07 +0530

    Update C_Program_05_19.c

--- C_PROGRAMMING/C_Program_05_19.c
+++ C_PROGRAMMING/C_Program_05_19.c
@@ -1,9 +1,11 @@
 /* C LEVEL 05 Problem 19
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a four digit number from the user and print whether the middle two digits(10's & 100's) of that number is prime or not.
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a program to get a four digit number from the user and 
+        print whether the middle two digits(10's & 100's) of that number is prime or not.
   Answer:
 Input: 6359 - Output: Not Prime
 Input: 3537 - Output: Prime
@@ -11,39 +13,28 @@ Input: 3537 - Output: Prime
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a 4-digit Number: ");
-  scanf("%d",&x);
-  int is_prime = 1,i = 2;
-  x = (x%1000)/10;
-  if(x<2)
-  {
-    is_prime = 0;
-  }
-  for(;;)
+    int number, is_prime = 1, i = 2;
+    printf("Enter 4 - digit Number: ");
+    scanf("%d",&number);
+    number = (number / 10)%100;
+    for(;;)
     {
-       Ploop:
-      if((i>=x)||(is_prime==0))
-        {
-          break;
-        }
-     
-        if((x%i)==0)
-        {
-          is_prime = 0;
-        }
-      
-      i++;
-      goto Ploop;
-    } 
-  if(is_prime)
-  {
-    printf("Prime");
-  }
-  else
-  {
-    printf("Not Prime");
-  }
+        loop:
+            if(i<number)
+            {
+                if(!(number%i))
+                {
+                    is_prime = 0;
+                    break;
+                }
+                i++;
+                goto loop;
+            }
+            break;
+    }
+    if(is_prime)
+            printf("Prime");
+    else
+            printf("Not Prime");
   return 0;
 }
