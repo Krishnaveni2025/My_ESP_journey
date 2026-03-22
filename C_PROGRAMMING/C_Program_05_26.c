75bde7e115b418872040ecaaee4512ad643135a9
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 01:14:22 +0530

    Update C_Program_05_26.c

--- C_PROGRAMMING/C_Program_05_26.c
+++ C_PROGRAMMING/C_Program_05_26.c
@@ -1,33 +1,31 @@
 /* C LEVEL 05 Problem 26
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to print the biggest 4-digit number which is divisible by 7 and 9
-  Answer:
-  9954
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a program to print the biggest 4-digit number which is divisible by 7 and 9
+   Answer:
+        9954
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  x = 1000;
-  int big;
-  for(;;)
+    int number = 1000, biggest;
+    for(;;)
     {
-      //your code here
-      loop:
-      if(x<10000)
-      {
-        if(((x%7)==0)&&((x%9==0)))
+        loop:
+        if(number<10000)
         {
-          big = x;
+            if(!(number%7))
+            {
+                if(!(number%9))
+                    biggest = number;
+            }
+            number++;
+            goto loop;
         }
-        x++;
-        goto loop;
-      }
-      printf("%d",big);
-      break;
+        break;
     }
+    printf("Output : %d", biggest);
 }
