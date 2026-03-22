ebbf04cab8f760fe3b244156845e4395ea6c42e8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 00:06:33 +0530

    Update C_Program_05_20.c

--- C_PROGRAMMING/C_Program_05_20.c
+++ C_PROGRAMMING/C_Program_05_20.c
@@ -1,46 +1,41 @@
 /* C LEVEL 05 Problem 20
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to print total number single digit prime numbers.
-  Answer:
-  4
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a program to print total number single digit prime numbers.
+   Answer:
+        4
 */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int single_digit;
   //your code here
-  x = 2;
-  int count = 0;
+  single_digit = 2;
+  int total_count = 0, is_prime, i;
 
   for(;;)
     {
-      if(x>=10)
-      {
-        break;
-      }
-      int i = 2,is_prime = 1;
-      pcheck:
-      if(i<x)
-      {
-        if((x%i)==0)
-        {
-          is_prime = 0; 
-        }
-        else
-        {
-            i++;
-            goto pcheck;
-        }
-      }
-      if(is_prime==1)
-      {
-        count++;
-      }
-      x++;
+        i = 2, is_prime = 1;
+        if(single_digit>=10)
+           { break;}
+        pcheck:
+            if(i<single_digit)
+            {
+                if(!(single_digit%i))
+                {
+                    is_prime = 0;
+                }
+                i++;
+                goto pcheck;
+            }
+            if(is_prime){
+                total_count++;
+            }
+        single_digit++;
     }
-  printf("%d",count);
+  printf("%d",total_count);
   return 0;
 }
