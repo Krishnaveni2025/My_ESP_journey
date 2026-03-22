261a383d597081ce3ce8da5a088034bf59434896
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-22 22:05:38 +0530

    Update C_Program_05_17.c

--- C_PROGRAMMING/C_Program_05_17.c
+++ C_PROGRAMMING/C_Program_05_17.c
@@ -1,66 +1,58 @@
 /* C LEVEL 05 Problem 17
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print whether that number is prime or not and sum of the digits equal to 14.
-  Answer:
-Input: 59 - Output: Prime & sum of the digits is 14
-Input: 77 - Output: Not prime but sum of digits is 14
-Input: 13 - Output: Prime & sum of digits is not 14
-Input: 27 - Output: Not prime & sum of digits is not 14
+         Use only int variables
+         Use only Decimals
+         Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
+   Question: 
+         Write a program to get a number from the user and print whether that number is prime or not and sum of the digits equal to 14.
+   Answer:
+         Input: 59 - Output: Prime & sum of the digits is 14
+         Input: 77 - Output: Not prime but sum of digits is 14
+         Input: 13 - Output: Prime & sum of digits is not 14
+         Input: 27 - Output: Not prime & sum of digits is not 14
 */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a Number: ");
-  scanf("%d",&x);
-  int is_prime = 1,i = 2, sum = 0,temp = x;
-  if(x<2)
-  {
-    is_prime = 0;
-  }
+  int number, sum = 0, temp, is_prime = 1, i =2;
+  printf("Enter Number: ");
+  scanf("%d",&number);
+  temp = number;
   for(;;)
-    {
-       Ploop:
-      if((i>=x)||(is_prime==0))
-        {
-          break;
-        }
-     
-        if((x%i)==0)
-        {
-          is_prime = 0;
-        }
-      
-      i++;
-      goto Ploop;
-    }
-    Sloop:
-      if(temp!=0)
-      {
-        sum = sum + (temp%10);
-        temp = temp/10;
-        goto Sloop;
-      }
-      
-  if((is_prime == 1)&&(sum==14))
   {
-    printf("Prime & sum of digits is 14");
+      sloop:
+            if(temp)
+            {
+                sum += temp %10;
+                temp /= 10;
+                goto sloop;
+            }
+      ploop: 
+            if(i<number)
+            {
+                if(!(number%i))
+                {
+                    is_prime = 0;
+                    break;
+                }
+                i++;
+                goto ploop;
+            }
+            break;
   }
-  else if((is_prime == 1)&&(sum != 14))
+  if(is_prime)
   {
-    printf("Prime & sum of digits is not 14");
-  }
-  else if((is_prime == 0) && (sum == 14))
-  {
-    printf("Not Prime but sum of digits is 14");
+      if(sum==14)
+            printf("Prime & sum of the digits is 14");
+      else
+            printf("Prime & sum of digits is not 14");
   }
   else
   {
-    printf("Not Prime & sum of digits is not 14");
+      if(sum==14)
+            printf("Not prime but sum of digits is 14");
+      else
+            printf("Not prime & sum of digits is not 14");
   }
   return 0;
 }
