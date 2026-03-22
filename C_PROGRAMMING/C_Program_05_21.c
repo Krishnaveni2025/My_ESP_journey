b29137aba75dca4ab207632f33e216a866a9561c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 00:12:13 +0530

    Update C_Program_05_21.c

--- C_PROGRAMMING/C_Program_05_21.c
+++ C_PROGRAMMING/C_Program_05_21.c
@@ -1,35 +1,34 @@
 /* C LEVEL 05 Problem 21
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print the total number of digits which are odd in that number
-  Answer:
-  Input: 12345678 Output: 4
-  Input: 987531   Output:5
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a program to get a number from the user and 
+        print the total number of digits which are odd in that number
+   Answer:
+        Input: 12345678 Output: 4
+        Input: 987531   Output:5
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&x);
+  int number;
+  printf("Enter Number: ");
+  scanf("%d",&number);
   int odd_count = 0;
   for(;;)
-    {
-      //your code her
+  {
       loop:
-      if(x!=0)
-      {
-        if((x%10)%2)
-        {
-          odd_count++;
-        }
-        x= x/10;
-        goto loop;
-      }
-      printf("%d",odd_count);
-      break;
-    }
+            if(number)
+            {
+                if((number%10)%2)
+                        odd_count++;
+                number/=10;
+                goto loop;
+            }
+        break;
+  }
+  printf("Ouput: %d",odd_count);
+  return 0;
 }
