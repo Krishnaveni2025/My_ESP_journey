7c63f9f17faa754af8e1d0f971f3611d690d2783
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 02:05:50 +0530

    Update C_Program_05_30.c

--- C_PROGRAMMING/C_Program_05_30.c
+++ C_PROGRAMMING/C_Program_05_30.c
@@ -1,39 +1,35 @@
 /* C LEVEL 05 Problem 30
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get two numbers from the user and print the HCF of those numbers
-  Answer:
-  Input: 20 40  Output: 20
-  Input: 15 78  Output: 3
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: Write a program to get two numbers from the user and print the HCF of those numbers
+   Answer:
+        Input: 20 40  Output: 20
+        Input: 15 78  Output: 3
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  int y;
-  printf("Enter two numbers: ");
-  scanf("%d %d",&x,&y);
-  for(;;)
+    int number1, number2, hcf;
+    printf("Enter Two Numbers: ");
+    scanf("%d %d",&number1, &number2);
+    for(;;)
     {
-      //your code here
-      loop:
-      if(x!=y)
-      {
-        if(x>y)
+        loop:
+        if(number2!=number1)
         {
-        x -= y;
+            if(number1>number2)
+            {
+                number1 -= number2;
+            }
+            else
+            {
+                number2 -= number1;
+            }
+            goto loop;
         }
-        else
-        {
-        y-=x;
-        }
-        goto loop;
-      }
-      printf("%d",x);
-      break;
+        break;
     }
+    printf("Output: %d",number1);
 }
-
