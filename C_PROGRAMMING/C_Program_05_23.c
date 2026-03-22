2517a5c49ba3f07a91e394fda11fbe8054b56d31
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 00:21:51 +0530

    Update C_Program_05_23.c

--- C_PROGRAMMING/C_Program_05_23.c
+++ C_PROGRAMMING/C_Program_05_23.c
@@ -1,35 +1,43 @@
 /* C LEVEL 05 Problem 23
-    Use only int variables
-    Use only Decimals
-    Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-    Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print the total number of single digit perfect square numbers in the number
-  Answer:
-  Input: 123456789 Output: 3
-  Input: 987531    Output: 2
+        Use only int variables
+        Use only Decimals
+        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+        Don't use Arrays, pointers, strings,etc.
+   Question: 
+        Write a program to get a number from the user and 
+        print the total number of single digit perfect square numbers in the number
+   Answer:
+        Input: 123456789 Output: 3
+        Input: 987531    Output: 2
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number,i = 1;
   //your code here
   printf("Enter a number: ");
-  scanf("%d",&x);
+  scanf("%d",&number);
   int ps_count = 0;
   for(;;)
-    {
-      //your code her
+  {
       loop:
-      if(x!=0)
+      if(number)
       {
-        if(((x%10)==1)||((x%10)==4)||((x%10)==9))
-        {
-          ps_count++;
-        }
-        x= x/10;
-        goto loop;
+          i = 1;
+          loop1:
+            if(i<4)
+            {
+                if((number%10)==(i*i))
+                {
+                    ps_count++;
+                }
+                i++;
+                goto loop1;
+            }
+            number /= 10;
+            goto loop;
       }
-      printf("%d",ps_count);
       break;
-    }
+  }
+  printf("Output: %d",ps_count);
 }
