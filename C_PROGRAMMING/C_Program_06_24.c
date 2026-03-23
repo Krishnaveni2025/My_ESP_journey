94699a461c1bafd25c1dc5037668ccc597090918
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 19:41:08 +0530

    Update C_Program_06_24.c

--- C_PROGRAMMING/C_Program_06_24.c
+++ C_PROGRAMMING/C_Program_06_24.c
@@ -3,34 +3,39 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print the total number of two digit perfect square numbers in the number
-  Answer:
-  Input: 163496481 Output: 4
-  Input: 364925    Output: 4
+   Question: 
+         Write a program to get a number from the user and 
+         print the total number of two digit perfect square numbers in the number
+   Answer:
+         Input: 163496481 Output: 4
+         Input: 364925    Output: 4
   */
 #include<stdio.h>
 int main()
 {
-  int x;
+  int number, twops_count = 0, i;
   //your code here
   printf("Enter a number: ");
-  scanf("%d",&x);
-  int count = 0;
+  scanf("%d",&number);
   while(1)
-    {
-      //your code here
+  {
       loop:
-      if(x)
-      {
-        if(((x%100)==16)||((x%100)==25)||((x%100)==36)||((x%100)==49)||((x%100)==64)||((x%100)==81))
-        {
-          count++;
-        }
-        x /= 10;
-        goto loop;
-      }
-      printf("%d",count);
-      break;
-    }
+            if(number>9)
+            {
+                i =4;
+                ploop:
+                    if(i<10)
+                    {
+                        if((number%100)==(i*i))
+                                twops_count++;
+                    i++;
+                    goto ploop;
+                    }
+                number /= 10;
+                goto loop;
+            }
+        break;
+  }
+  printf("Output: %d",twops_count);
   return 0;
 }
