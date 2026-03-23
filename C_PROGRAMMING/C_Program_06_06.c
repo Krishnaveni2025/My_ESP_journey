508c1b6c66f0a922eb566209eead9bc5f45859ba
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:21:51 +0530

    Update C_Program_06_06.c

--- C_PROGRAMMING/C_Program_06_06.c
+++ C_PROGRAMMING/C_Program_06_06.c
@@ -3,38 +3,32 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a loop program to print to two digit odd numbers below 20
+  Question: 
+         Write a loop program to print to two digit odd numbers below 20
   Answer:
-  11
-  13
-  15
-  17
-  19
+         11
+         13
+         15
+         17
+         19
 
 */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  x= 11;
-  while(1)
+    int number = 11;
+    while(1)
     {
-      //your code here
-     loop: 
-     if(x<20)
-     {
-         if(x%2)
-         {
-             printf("%d\n",x);
-         }
-         x++;
-         goto loop;
-     }
-      break;
+        loop:
+                if(number<20)
+                {
+                    if(number%2)
+                    {
+                        printf("%d\n",number);
+                    }
+                    number++;
+                    goto loop;
+                }
+        break;
     }
 }
-
-
-
-
