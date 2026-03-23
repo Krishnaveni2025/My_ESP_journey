ed9ed83968b4fe2ee03a6a90c35f3451b4b6bdc5
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:00:34 +0530

    Update C_Program_06_17.c

--- C_PROGRAMMING/C_Program_06_17.c
+++ C_PROGRAMMING/C_Program_06_17.c
@@ -1,4 +1,8 @@
 /* C LEVEL 06 Problem 17
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to get a number from the user and print whether that number is prime or not and sum of the digits equal to 14.
   Answer:
 Input: 59 - Output: Prime & sum of the digits is 14
