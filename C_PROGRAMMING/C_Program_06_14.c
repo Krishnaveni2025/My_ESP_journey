d603bc4b07ae4a04b48e1939812eb85f46219081
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:58:15 +0530

    Update C_Program_06_14.c

--- C_PROGRAMMING/C_Program_06_14.c
+++ C_PROGRAMMING/C_Program_06_14.c
@@ -3,38 +3,36 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and interchange the first and last digits and print the result .
-  Answer:
-  Input:123456   Output:623451
-  Input:76895439 Output:96895437
-  Input:675      Output:576
+   Question: 
+         Write a program to get a number from the user and 
+         interchange the first and last digits and 
+         print the result .
+   Answer:
+         Input:123456   Output:623451
+         Input:76895439 Output:96895437
+         Input:675      Output:576
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&x);
-  int num, first, last, power = 1, middle, result = 0;
-  first = x%10;
-  num = x;
-  while(1)
+    int number, first, last, multiplier = 1, temp, middle, result;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    first = number%10;
+    temp = number;
+    while(1)
     {
-      //your code here
-      loop:
-      if(num)
-      {
-        power *= 10;
-        num /=10;
-        goto loop;
-      }
-      power = power /10; 
-      last = (x/power);
-      middle = (x%power)/10;
-      result = (first*power) + (middle*10)+ last;
-      printf("%d",result);
-      break;
-   }
+        loop:
+            if(temp>9)
+            {
+                multiplier *= 10;
+                temp /= 10;
+                goto loop;
+            }
+            break;
+    }
+    last = temp;
+    middle = (number%multiplier)/10;
+    result = (first*multiplier)+(middle*10)+last;
+    printf("Output: %d",result);
 }
-
