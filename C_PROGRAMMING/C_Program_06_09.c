87e5165443ef0d233a98fc958c6638fb4c31a73e
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:35:10 +0530

    Update C_Program_06_09.c

--- C_PROGRAMMING/C_Program_06_09.c
+++ C_PROGRAMMING/C_Program_06_09.c
@@ -3,34 +3,27 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-Question: write a loop program to print the sum of two digit numbers, who's 1's digits is 5
-Answer:
-495
+   Question: 
+         write a loop program to print the sum of two digit numbers, who's 1's digits is 5
+   Answer:
+         495
   */
 
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  x= 10;
-  int sum = 0;
-  while(1)
+    int number = 15, sum = 0;
+    while(1)
     {
-      //your code here
-     loop: 
-     if(x<100)
-     {
-         if((x%10)==5)
-         {
-             sum += x;
-         }
-         x++;
-         goto loop;
-     }
-      printf("%d",sum);
-     break;
-      
+        loop:
+        if(number<100)
+        {
+            if((number%10)==5)
+                    sum+= number;
+            number++;
+            goto loop;
+        }
+        break;
     }
+    printf("Answer: %d",sum);
 }
-
