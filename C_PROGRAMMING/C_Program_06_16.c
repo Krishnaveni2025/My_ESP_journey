7ecffb2d7e6f482f3af75fc562a42b42ac194ef5
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 10:12:48 +0530

    Update C_Program_06_16.c

--- C_PROGRAMMING/C_Program_06_16.c
+++ C_PROGRAMMING/C_Program_06_16.c
@@ -11,35 +11,27 @@ Input: 27 - Output: Not prime
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a Number: ");
-  scanf("%d",&x);
-  int i = 2,is_prime = 1;
-  while(1)
+    int number, is_prime = 1, divider = 2;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    while(1)
     {
-      //your code here
-      loop:
-      if(i<x)
-      {
-        if(!(x%i))
-        {
-          is_prime = 0;
-          break;
-        }
-        i++;
-        goto loop;
-      }
-      break;
+        loop:
+                if(divider<number)
+                {
+                    if(!(number%divider))
+                    {
+                        is_prime = 0;
+                        break;
+                    }
+                    divider++;
+                    goto loop;
+                }
+            break;
     }
-  if(is_prime)
-  {
-    printf("Prime");
-  }
-  else
-  {
-    printf("Not Prime");
-  }
-  return 0;
+    if(is_prime)
+            printf("Prime");
+    else
+            printf("Not Prime");
+    return 0;
 }
-
