b5df680e5433cfdcd0d088d3ee9f6468ac8ec0a6
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:03:38 +0530

    Update C_Program_06_27.c

--- C_PROGRAMMING/C_Program_06_27.c
+++ C_PROGRAMMING/C_Program_06_27.c
@@ -1,4 +1,8 @@
 /* C LEVEL 06 Problem 27
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to print the total count of numbers which are less than 100000 and whose sum of digits is 14
   Answer:
   2710
