a7440f11420033af016fa2fe1e193847490b3c5b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:01:50 +0530

    Update C_Program_06_20.c

--- C_PROGRAMMING/C_Program_06_20.c
+++ C_PROGRAMMING/C_Program_06_20.c
@@ -1,4 +1,8 @@
 /* C LEVEL 06 Problem 20
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to print total number single digit prime numbers.
   Answer:
   4
