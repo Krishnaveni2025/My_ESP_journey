8bf6583a6bcc193685aeb70c49fc8bd65cca5385
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 22:28:34 +0530

    Update C_Program_06_25.c

--- C_PROGRAMMING/C_Program_06_25.c
+++ C_PROGRAMMING/C_Program_06_25.c
@@ -3,33 +3,73 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print the total number of single digit prime numbers in the number
-  Answer:
-  Input: 163496481 Output: 1
-  Input: 364925    Output: 3
+   Question: 
+         Write a program to get a number from the user and 
+         print the total number of single digit prime numbers in the number
+   Answer:
+         Input: 163496481 Output: 1
+         Input: 364925    Output: 3
   */
 #include<stdio.h>
+
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&x);
-  int count = 0;
-  while(1)
+    int number, sprime_count = 0, divider, is_prime, digit;
+    
+    printf("Enter Number: ");
+    scanf("%d", &number);
+
+    while(number > 0)
     {
-      //your code here
-      loop:
-      if(x)
-      {
-        if(((x%10)==2)||((x%10)==3)||((x%10)==5)||((x%10)==7))
-        {
-          count++;
-        }
-        x /= 10;
-        goto loop;
-      }
-      printf("%d",count);
-      break;
+        loop:
+            // 1. Extract the specific digit we want to check
+            digit = number % 10;
+            divider = 2;
+            is_prime = 1;
+
+            // 2. Prime check logic for the single digit
+            if(digit < 2) 
+            {
+                is_prime = 0; // 0 and 1 are not prime
+            }
+            else 
+            {
+                pcheck:
+                    if(divider < digit)
+                    {
+                        if(digit % divider == 0)
+                        {
+                            is_prime = 0;
+                            // We found a factor, no need to check more for this digit
+                        }
+                        else 
+                        {
+                            divider++;
+                            goto pcheck;
+                        }
+                    }
+            }
+
+            // 3. Increment count if the digit was prime
+            if(is_prime)
+            {
+                sprime_count++;
+            }
+
+            // 4. Move to the next digit
+            number /= 10;
+            
+            // If there are more digits, go back to the top
+            if(number > 0)
+            {
+                goto loop;
+            }
+            else
+            {
+                break; 
+            }
     }
+
+    printf("Output: %d", sprime_count);
+    return 0;
 }
