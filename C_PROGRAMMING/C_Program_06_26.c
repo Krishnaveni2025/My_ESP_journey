d775375d6748a08e3d0286d619c4344b89c22df2
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:03:09 +0530

    Update C_Program_06_26.c

--- C_PROGRAMMING/C_Program_06_26.c
+++ C_PROGRAMMING/C_Program_06_26.c
@@ -1,4 +1,8 @@
 /* C LEVEL 06 Problem 26
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to print the biggest 4-digit number which is divisible by 7 and 9
   Answer:
   9954
