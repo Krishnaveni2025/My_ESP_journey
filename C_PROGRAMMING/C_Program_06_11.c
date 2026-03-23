f49ac446b7b1761649f870cda6b0088ff02b9047
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:42:52 +0530

    Update C_Program_06_11.c

--- C_PROGRAMMING/C_Program_06_11.c
+++ C_PROGRAMMING/C_Program_06_11.c
@@ -3,31 +3,29 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Get a number from the user print the total number of digits in that number.
-  Answer:
-  Input:123456   Output:6
-  Input:76895439 Output:8
-  Input:675      Output:3
+   Question: 
+         Get a number from the user print the total number of digits in that number.
+   Answer:
+         Input:123456   Output:6
+         Input:76895439 Output:8
+         Input:675      Output:3
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&x);
-  int count = 0;
-  while(1)
+    int number, total_digits = 0;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    while(1)
     {
-      //your code here
-      loop:
-      if(x)
-      {
-        count++;
-        x/=10;
-        goto loop;
-      }
-      printf("%d",count);
-      break;
+        loop:
+                if(number)
+                {
+                    total_digits++;
+                    number /= 10;
+                    goto loop;
+                }
+            break;
     }
+    printf("Output: %d",total_digits);
 }
