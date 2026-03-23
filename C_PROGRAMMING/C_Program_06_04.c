aacd7e411da9b1f9f3fd783b147948d8c057544f
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 08:57:34 +0530

    Update C_Program_06_04.c

--- C_PROGRAMMING/C_Program_06_04.c
+++ C_PROGRAMMING/C_Program_06_04.c
@@ -1,4 +1,8 @@
 /*C LEVEL 06 Problem 04
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a loop program to print sum from 6 to 1
   Answer:
   21
