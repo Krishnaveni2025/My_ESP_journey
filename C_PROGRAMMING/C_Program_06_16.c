8cd6973c3a771a6e69d43d55fdea4911af2b3f87
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:00:15 +0530

    Update C_Program_06_16.c

--- C_PROGRAMMING/C_Program_06_16.c
+++ C_PROGRAMMING/C_Program_06_16.c
@@ -1,4 +1,8 @@
 /* C LEVEL 06 Problem 16
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to get a number from the user and print whether that number is prime or not.
   Answer:
 Input: 31 - Output: Prime
