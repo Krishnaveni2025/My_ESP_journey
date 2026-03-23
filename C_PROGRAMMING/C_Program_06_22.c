a8e6d69cef2131332f2c9fbb1f24e5d29b66738c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 19:23:44 +0530

    Update C_Program_06_22.c

--- C_PROGRAMMING/C_Program_06_22.c
+++ C_PROGRAMMING/C_Program_06_22.c
@@ -3,37 +3,31 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print the total number of two-digit odd numbers in the number
-  Answer:
-  Input: 12345678 Output: 3
-  Input: 987531   Output: 4
+   Question: 
+         Write a program to get a number from the user and 
+         print the total number of two-digit odd numbers in the number
+   Answer:
+         Input: 12345678 Output: 3
+         Input: 987531   Output: 4
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&x);
-  int count = 0;
-  while(1)
+    int number, odd2d_count = 0;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    while(1)
     {
-      //your code here
-      if(x<10)
-      {
+        loop:
+                if(number>9)
+                {
+                    if((number%100)%2)
+                         odd2d_count++;
+                    number/=10;
+                    goto loop;
+                }
         break;
-      }
-      loop:
-      if(x>10)
-      {
-          if((x%100)%2)
-          {
-              count++;
-          }
-      x /= 10;
-      goto loop;
-      }
     }
-    printf("%d",count);
+    printf("Output: %d",odd2d_count);
     return 0;
 }
