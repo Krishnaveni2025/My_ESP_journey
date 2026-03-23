da33427b179f5020300547addcf912da142960b3
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:13:42 +0530

    Update C_Program_06_03.c

--- C_PROGRAMMING/C_Program_06_03.c
+++ C_PROGRAMMING/C_Program_06_03.c
@@ -6,27 +6,21 @@
   Question: Write a loop program to print sum from 1 to 5
   Answer:
   15
-
 */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  x= 1;
-  int sum = 0;
-  while(1)
+    int number = 1, sum = 0;
+    while(1)
     {
-      //your code here
-     loop: if(x<6)
-      {
-        sum += x;
-        x++;
-        goto loop;
-      }
-      printf("%d",sum);
-      break;
+        loop:
+        if(number<6)
+        {
+            sum += number;
+            number++;
+            goto loop;
+        }
+        break;
     }
+    printf("Answer: %d",sum);
 }
-
-
