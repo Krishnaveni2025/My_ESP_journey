6a870b93cea6e97e92f1e0c1262d067c3a0dbf7b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:16:28 +0530

    Update C_Program_06_04.c

--- C_PROGRAMMING/C_Program_06_04.c
+++ C_PROGRAMMING/C_Program_06_04.c
@@ -3,30 +3,26 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a loop program to print sum from 6 to 1
+  Question: 
+         Write a loop program to print sum from 6 to 1
   Answer:
-  21
+         21
 
 */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  x= 6;
-  int sum = 0;
-  while(1)
+    int number = 6, sum = 0;
+    while(1)
     {
-      //your code here
-     loop: if(x>0)
-      {
-        sum += x;
-        x--;
-        goto loop;
-      }
-      printf("%d",sum);
-      break;
+        loop:
+        if(number>0)
+        {
+            sum += number;
+            number--;
+            goto loop;
+        }
+        break;
     }
+    printf("Answer: %d",sum);
 }
-
-
