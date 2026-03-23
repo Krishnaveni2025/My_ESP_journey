56326375ce5d1ad55600a0dd9a6a09d9ed768986
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 08:59:03 +0530

    Update C_Program_06_11.c

--- C_PROGRAMMING/C_Program_06_11.c
+++ C_PROGRAMMING/C_Program_06_11.c
@@ -1,4 +1,8 @@
 /* C LEVEL 06 Problem 11
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
   Question: Get a number from the user print the total number of digits in that number.
   Answer:
   Input:123456   Output:6
