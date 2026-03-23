54bc5a1836df622dfeed28405f0408bc8afe3174
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:19:02 +0530

    Update C_Program_06_05.c

--- C_PROGRAMMING/C_Program_06_05.c
+++ C_PROGRAMMING/C_Program_06_05.c
@@ -3,37 +3,32 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a loop program to print to odd numbers between 1 and 9
+  Question: 
+         Write a loop program to print to odd numbers between 1 and 9
   Answer:
-  1
-  3
-  5
-  7
-  9
+         1
+         3
+         5
+         7
+         9
 
 */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  x= 1;
+  int number = 1;
   while(1)
-    {
-      //your code here
-     loop: 
-     if(x<10)
-     {
-         if(x%2)
-         {
-             printf("%d\n",x);
-         }
-         x++;
-         goto loop;
-     }
+  {
+      loop:
+            if(number<10)
+            {
+                if(number%2)
+                {
+                    printf("%d\n",number);
+                }
+                number++;
+                goto loop;
+            }
       break;
-    }
+  }
 }
-
-
-
