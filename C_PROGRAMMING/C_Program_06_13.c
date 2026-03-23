d15f814a1c7f40e4f756000f0958c754b0723d38
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:50:52 +0530

    Update C_Program_06_13.c

--- C_PROGRAMMING/C_Program_06_13.c
+++ C_PROGRAMMING/C_Program_06_13.c
@@ -3,32 +3,30 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user print the reverse of the number.
-  Answer:
-  Input:123456   Output:654321
-  Input:76895439 Output:93453867
-  Input:675      Output:576
+   Question: 
+         Write a program to get a number from the user 
+         print the reverse of the number.
+   Answer:
+         Input:123456   Output:654321
+         Input:76895439 Output:93453867
+         Input:675      Output:576
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a Number: ");
-  scanf("%d",&x);
-  int rev = 0;
+  int number, reverse = 0;
+  printf("Enter number: ");
+  scanf("%d",&number);
   while(1)
-    {
-      //your code here
+  {
       loop:
-      if(x)
-      {
-        rev = (rev*10)+(x%10);
-        x = x/10;
-        goto loop;
-      }
-      printf("%d",rev);
-      break;
-    }
+            if(number)
+            {
+                reverse = (reverse*10)+(number%10);
+                number /= 10;
+                goto loop;
+            }
+        break;
+  }
+  printf("Output: %d",reverse);
 }
-
