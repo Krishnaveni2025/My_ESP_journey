cd0189d5c2b97191dae9d45cd601dd25948b8465
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 08:58:38 +0530

    Update C_Program_06_09.c

--- C_PROGRAMMING/C_Program_06_09.c
+++ C_PROGRAMMING/C_Program_06_09.c
@@ -1,4 +1,8 @@
 /* C LEVEL 06 Problem 09
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
 Question: write a loop program to print the sum of two digit numbers, who's 1's digits is 5
 Answer:
 495
