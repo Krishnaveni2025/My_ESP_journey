836f22a457e88ce088ec8fc990a3ecd7be2b0301
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 10:05:31 +0530

    Update C_Program_06_15.c

--- C_PROGRAMMING/C_Program_06_15.c
+++ C_PROGRAMMING/C_Program_06_15.c
@@ -3,46 +3,38 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and if the last digit of the number is even print the same number. 
-  If the last digit of the number is odd subtract 1 from the last digit and print the number.(Note: Last Digit - MSB)
+   Question: 
+         Write a program to get a number from the user and 
+         if the last digit of the number is even print the same number. 
+         If the last digit of the number is odd subtract 1 from the last digit and 
+         print the number.(Note: Last Digit - MSB)
   Answer:
-  Input:654324   Output:654324 (last digit = 6)
-  Input:76895439 Output:66895439 (last digit = 7)
-  Input:675      Output:675 (last digit = 6)
+         Input:654324   Output:654324 (last digit = 6)
+         Input:76895439 Output:66895439 (last digit = 7)
+         Input:675      Output:675 (last digit = 6)
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&x);
-  int num, last, power = 1,result = 0;
-  num = x;
-  while(1)
+    int number, last, multiplier = 1, temp, result;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    temp = number;
+    while(1)
     {
-      //your code here
-      loop:
-      if(num)
-      {
-        power *= 10;
-        num /= 10;
-        goto loop;
-      }
-      power /= 10;
-      last = x/power;
-      if(last%2)
-      {
-        last--;
-        result = (last*power)+(x%power);
-        break;
-      }
-      else
-      {
-        result = x;
-        break;
-      }
+        loop:
+                if(temp>9)
+                {
+                    temp /= 10;
+                    multiplier *= 10;
+                    goto loop;
+                }
+            break;
     }
-  printf("%d",result);
-  return 0;
+    last = temp;
+    if(last%2)
+            last = last-1;
+    result = (last*multiplier)+(number%multiplier);
+    printf("Output: %d",result);
+    return 0;
 }
