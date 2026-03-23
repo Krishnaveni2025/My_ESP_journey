f0855a6fb80b5c3d420582e1902b0aa82f82e979
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 19:32:09 +0530

    Update C_Program_06_23.c

--- C_PROGRAMMING/C_Program_06_23.c
+++ C_PROGRAMMING/C_Program_06_23.c
@@ -3,34 +3,38 @@
          Use only Decimals
          Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print the total number of single digit perfect square numbers in the number
-  Answer:
-  Input: 123456789 Output: 3
-  Input: 987531    Output: 2
+   Question: 
+         Write a program to get a number from the user and 
+         print the total number of single digit perfect square numbers in the number
+   Answer:
+         Input: 123456789 Output: 3
+         Input: 987531    Output: 2
   */
 #include<stdio.h>
 int main()
 {
-  int x;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&x);
-  int count = 0;
-  while(1)
+    int number, single_pscount = 0, i=1;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    while(1)
     {
-      //your code here
-      loop:
-      if(x)
-      {
-        if(((x%10)==1)||((x%10)==4)||((x%10)==9))
-        {
-          count++;
-        }
-        x /= 10;
-        goto loop;
-      }
-      printf("%d",count);
-      break;
+        loop:
+                if(number)
+                {
+                    i = 1;
+                    ploop:
+                            if(i<4)
+                            {
+                                if((number%10)==(i*i))
+                                        single_pscount++;
+                                i++;
+                                goto ploop;
+                            }
+                    number /= 10;
+                    goto loop;
+                }
+        break;
     }
+    printf("Output: %d",single_pscount);
   return 0;
 }
