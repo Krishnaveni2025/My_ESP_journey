c4a85d3862ae953d22650ec49c35f54fd020622b
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-23 09:04:09 +0530

    Update C_Program_06_29.c

--- C_PROGRAMMING/C_Program_06_29.c
+++ C_PROGRAMMING/C_Program_06_29.c
@@ -1,4 +1,8 @@
 /* C LEVEL 06 Problem 29
+         Use only int variables
+         Use only Decimals
+         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to get three numbers from the user and print the LCM of those numbers
   Answer:
   Input: 20 40 80 Output: 80
