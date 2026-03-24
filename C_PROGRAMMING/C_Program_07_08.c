f83762617e548d93588124dcc01faaea8b3b3c36
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 22:35:01 +0530

    Update C_Program_07_08.c

--- C_PROGRAMMING/C_Program_07_08.c
+++ C_PROGRAMMING/C_Program_07_08.c
@@ -3,32 +3,32 @@
          Use only Decimals
          Use functions with not return value.
          Don't use Arrays, pointers, strings,etc.
-Question: Write a loop program to print the two digit even numbers, whose sum of digits are 6
-Function Name: disp_2digit_even_sum6
-Answer:
-24
-42
-60
+   Question: 
+         Write a loop program to print the two digit even numbers, whose sum of digits are 6
+   Function Name: 
+         disp_2digit_even_sum6
+   Answer:
+         24
+         42
+         60
 */
 #include<stdio.h>
-void disp_2digit_even_sum6(int);
+void disp_2digit_even_sum6(int n);
 int main()
 {
-int x;
-x = 10;
-disp_2digit_even_sum6(x);
-}
-void disp_2digit_even_sum6(int x)
-{
-//your code here
-  for(x = 10; x<100; x++)
+    int number = 24;
+    while(number<=60)
     {
-      if(!(x%2))
-      {
-        if((x%10)+(x/10)==6)
-        {
-          printf("%d\n",x);
-        }
-      }
+        disp_2digit_even_sum6(number);
+        number++;
+    }
+}
+void disp_2digit_even_sum6(int n)
+{
+    if(!(n%2))
+    {
+        int sum = (n%10)+(n/10);
+        if(sum==6)
+                 printf("%d\n",n);
     }
 }
