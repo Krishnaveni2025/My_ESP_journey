2fc7a446696d638289a4626cc32e4f80fe1f4206
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 22:18:42 +0530

    Update C_Program_07_07.c

--- C_PROGRAMMING/C_Program_07_07.c
+++ C_PROGRAMMING/C_Program_07_07.c
@@ -3,12 +3,14 @@
          Use only Decimals
          Use functions with not return value.
          Don't use Arrays, pointers, strings,etc.
-Question: Write a loop program to print the two-digit odd numbers, who's sum of digits are 7
-Function Name: disp_2digit_odd_sum7
-Answer: 
-25
-43
-61
+   Question: 
+         Write a loop program to print the two-digit odd numbers, who's sum of digits are 7
+   Function Name: 
+         disp_2digit_odd_sum7
+   Answer: 
+         25
+         43
+         61
 */
 #include<stdio.h>
 void disp_2digit_odd_sum7(int n);
