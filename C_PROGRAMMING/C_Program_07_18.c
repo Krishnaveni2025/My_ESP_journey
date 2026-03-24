f70ccad1f37019af29253bf18bdf1d2a3c1637a6
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 21:30:05 +0530

    Update C_Program_07_18.c

--- C_PROGRAMMING/C_Program_07_18.c
+++ C_PROGRAMMING/C_Program_07_18.c
@@ -1,4 +1,8 @@
 /* C LEVEL 07 Problem 18
+         Use only int variables
+         Use only Decimals
+         Use functions with not return value.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to get a number from the user and print whether the first two digits(1's & ten's) of that number is prime or not.
   Function Name: check_first_2digits_prime
   Answer:
