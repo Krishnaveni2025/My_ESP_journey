88ecfef30b0ec2bd39c79f2f88d862066c6f4f3f
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 21:31:12 +0530

    Update C_Program_07_21.c

--- C_PROGRAMMING/C_Program_07_21.c
+++ C_PROGRAMMING/C_Program_07_21.c
@@ -1,4 +1,8 @@
 /* C LEVEL 07 Problem 21
+         Use only int variables
+         Use only Decimals
+         Use functions with not return value.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to get a number from the user and print the total number of digits which are odd in that number
   Function Name: disp_total_odd_digits
   Answer:
