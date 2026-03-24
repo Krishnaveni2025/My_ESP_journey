815cb880e04482d6266577fb0bfbc17265c153b0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 21:23:12 +0530

    Update C_Program_07_02.c

--- C_PROGRAMMING/C_Program_07_02.c
+++ C_PROGRAMMING/C_Program_07_02.c
@@ -1,4 +1,8 @@
 /* C LEVEL 07 Problem 02
+         Use only int variables
+         Use only Decimals
+         Use functions with not return value.
+         Don't use Arrays, pointers, strings,etc.
 Question: Write a loop program to print 5 to 1 on one by one
 Function Name: disp_descend
 Answer:
