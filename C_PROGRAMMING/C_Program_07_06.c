a48af889b84080ae2b858a78a925dc819fc2d195
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 22:10:28 +0530

    Update C_Program_07_06.c

--- C_PROGRAMMING/C_Program_07_06.c
+++ C_PROGRAMMING/C_Program_07_06.c
@@ -3,31 +3,30 @@
          Use only Decimals
          Use functions with not return value.
          Don't use Arrays, pointers, strings,etc.
-Question: Write a loop program to print two digit odd numbers, below 20
-Function Name: disp_2digit_odd_below20
-Answer:
-11
-13
-15
-17
-19
+   Question: 
+         Write a loop program to print two digit odd numbers, below 20
+   Function Name: 
+         disp_2digit_odd_below20
+   Answer:
+         11
+         13
+         15
+         17
+         19
 */
 #include<stdio.h>
-void disp_2digit_odd_below20(int);
+void disp_2digit_odd_below20(int n);
 int main()
 {
-int x;
-x = 11;
-disp_2digit_odd_below20(x);
-}
-void disp_2digit_odd_below20(int x)
-{
-//your code here
-  for(x = 11; x<20; x++)
+    int number = 11;
+    while(number<20)
     {
-      if(x%2)
-      {
-        printf("%d\n",x);
-      }
+        disp_2digit_odd_below20(number);
+        number++;
     }
+}
+void disp_2digit_odd_below20(int n)
+{
+    if(n%2)
+            printf("%d\n",n);
 }
