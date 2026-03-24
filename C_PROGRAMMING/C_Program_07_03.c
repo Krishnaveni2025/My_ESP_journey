30a8d4dc65a29bd925004c841e6edbed6c1c2f22
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 21:55:52 +0530

    Update C_Program_07_03.c

--- C_PROGRAMMING/C_Program_07_03.c
+++ C_PROGRAMMING/C_Program_07_03.c
@@ -3,26 +3,27 @@
          Use only Decimals
          Use functions with not return value.
          Don't use Arrays, pointers, strings,etc.
-Question: Write a loop program to print sum from 1 to 5
-Function Name: disp_sum
-Answer:
-15
+   Question: 
+         Write a loop program to print sum from 1 to 5
+   Function Name: 
+         disp_sum
+   Answer:
+         15
 */
 #include<stdio.h>
-void disp_sum(int);
+void disp_sum(int n);
 int main()
 {
-int x;
-x = 1;
-disp_sum(x);
+    int number = 1;
+    disp_sum(1);
 }
-void disp_sum(int x)
+void disp_sum(int n)
 {
-//your code here
-  int sum = 0;
-  for(x = 1; x<6; x++)
+    int sum = 0;
+    while(n<6)
     {
-      sum += x;
+        sum += n;
+        n++;
     }
-  printf("%d",sum);
+    printf("%d",sum);
 }
