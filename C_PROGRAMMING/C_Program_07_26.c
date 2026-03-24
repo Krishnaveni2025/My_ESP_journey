96b318cad0372db40377284c0ddef6937bb5b259
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 21:33:47 +0530

    Update C_Program_07_26.c

--- C_PROGRAMMING/C_Program_07_26.c
+++ C_PROGRAMMING/C_Program_07_26.c
@@ -1,4 +1,8 @@
 /* C LEVEL 07 Problem 26
+         Use only int variables
+         Use only Decimals
+         Use functions with not return value.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to print the biggest 4-digit number which is divisible by 7 and 9
   Function Name: disp_biggest_4digit_div7_9 
   Answer:
