300f28d5c6a7dbdbaa9bbc4e905ae7210d47f423
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 21:34:13 +0530

    Update C_Program_07_27.c

--- C_PROGRAMMING/C_Program_07_27.c
+++ C_PROGRAMMING/C_Program_07_27.c
@@ -1,4 +1,8 @@
 /* C LEVEL 07 Problem 27
+         Use only int variables
+         Use only Decimals
+         Use functions with not return value.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to print the total count of numbers which are less than 100000 and whose sum of digits is 14
   Function Name: disp_count_sum14
   Answer:
