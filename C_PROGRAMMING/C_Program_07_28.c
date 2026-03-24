97e93ce7b0e8d2be6df40ad5798ea6fc6b378de2
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-24 21:34:34 +0530

    Update C_Program_07_28.c

--- C_PROGRAMMING/C_Program_07_28.c
+++ C_PROGRAMMING/C_Program_07_28.c
@@ -1,4 +1,8 @@
 /* C LEVEL 07 Problem 28
+         Use only int variables
+         Use only Decimals
+         Use functions with not return value.
+         Don't use Arrays, pointers, strings,etc.
   Question: Write a program to get ttwo numbers from the user and print the LCM of those numbers
   Function Name: disp_LCM2
   Answer:
