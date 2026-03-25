eaed4d3663be5dccf33be584c4f5fed1b998a2a4
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-25 19:27:19 +0530

    Update C_Program_07_12.c

--- C_PROGRAMMING/C_Program_07_12.c
+++ C_PROGRAMMING/C_Program_07_12.c
@@ -3,29 +3,31 @@
          Use only Decimals
          Use functions with not return value.
          Don't use Arrays, pointers, strings,etc.
-  Question: Get a number from the user print the sum of all digits.
-  Function Name: disp_sum_all_digits
-  Answer:
-  Input:123456   Output:21
-  Input:76895439 Output:51
-  Input:675      Output:18
+   Question: 
+         Get a number from the user print the sum of all digits.
+   Function Name: 
+         disp_sum_all_digits
+   Answer:
+         Input:123456   Output:21
+         Input:76895439 Output:51
+         Input:675      Output:18
   */
 #include<stdio.h>
-void disp_sum_all_digits(int, int);
+void disp_sum_all_digits(int n);
 int main()
 {
-int x,y;
-scanf("%d",&x);
-y = 0;
-disp_sum_all_digits(x,y);
+    int number;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    disp_sum_all_digits(number);
 }
-void disp_sum_all_digits(int x, int sum)
+void disp_sum_all_digits(int n)
 {
-//your code here
-  while(x)
+    int sum_digits = 0;
+    while(n)
     {
-      sum += (x%10);
-      x /= 10;
+        sum_digits += n%10;
+        n /= 10;
     }
-  printf("%d",sum);
+    printf("%d",sum_digits);
 }
