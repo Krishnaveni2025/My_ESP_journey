6c87b9ab7851623bb44110e53783f3e4f6a3a460
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 04:28:18 +0530

    Update C_Program_08_06.c

--- C_PROGRAMMING/C_Program_08_06.c
+++ C_PROGRAMMING/C_Program_08_06.c
@@ -1,28 +1,33 @@
 /* C LEVEL 08 Problem 06
-  Question: Get a number from the user print the sum of all digits.
-  Function Name: disp_sum_all_digits
-  Answer:
-  Input:123456   Output:21
-  Input:76895439 Output:51
-  Input:675      Output:18
+        Use only int variables
+        Use only Decimals
+        Use Functions with a return value
+        Do not use arrays, strings, pointers, etc.
+   Question: 
+        Get a number from the user print the sum of all digits.
+   Function Name: 
+        disp_sum_all_digits
+   Answer:
+        Input:123456   Output:21
+        Input:76895439 Output:51
+        Input:675      Output:18
   */
 #include<stdio.h>
-int disp_sum_all_digits(int);
+int disp_sum_all_digits(int n);
 int main()
 {
-int x,y;
-scanf("%d",&x);
-y = disp_sum_all_digits(x);
-printf("%d",y);
+    int number;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    printf("%d",disp_sum_all_digits(number));
 }
-int disp_sum_all_digits(int a)
+int disp_sum_all_digits(int n)
 {
-int y = 0;
-//your code here
-  while(a)
+    int sum = 0;
+    while(n)
     {
-      y += (a%10);
-      a /= 10;
+        sum += (n%10);
+        n /= 10;
     }
-return (y);
+    return sum;
 }
