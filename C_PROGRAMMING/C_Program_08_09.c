80ee21d3b8a1c19a0161a68132042f29d29fb032
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 04:51:19 +0530

    Update C_Program_08_09.c

--- C_PROGRAMMING/C_Program_08_09.c
+++ C_PROGRAMMING/C_Program_08_09.c
@@ -1,39 +1,40 @@
 /* C LEVEL 08 Problem 09
-  Question: Write a program to get a number from the user and if the last digit of the number is even print the same number. 
-  If the last digit of the number is odd subtract 1 from the last digit and print the number.(Note: Last Digit - MSB)
-  Function Name: check_last_digit_odd
-  Answer:
-  Input:654324   Output:654324 (last digit = 6)
-  Input:76895439 Output:66895439 (last digit = 7)
-  Input:675      Output:675 (last digit = 6)
+        Use only int variables
+        Use only Decimals
+        Use Functions with a return value
+        Do not use arrays, strings, pointers, etc.
+   Question: 
+        Write a program to get a number from the user and 
+        if the last digit of the number is even print the same number. 
+        If the last digit of the number is odd subtract 1 from the last digit and 
+        print the number.(Note: Last Digit - MSB)
+   Function Name: 
+        check_last_digit_odd
+   Answer:
+        Input:654324   Output:654324 (last digit = 6)
+        Input:76895439 Output:66895439 (last digit = 7)
+        Input:675      Output:675 (last digit = 6)
   */
 #include<stdio.h>
-int check_last_digit_odd(int);
+int check_last_digit_odd(int n);
 int main()
 {
-int x,y;
-scanf("%d",&x);
-y = check_last_digit_odd(x);
-  printf("%d",y);
+    int number;
+    printf("Enter number: ");
+    scanf("%d",&number);
+    printf("%d",check_last_digit_odd(number));
 }
-int function_name(int a)
+int check_last_digit_odd(int n)
 {
-int y = 0;
-//your code here
-  int temp = a,last, power = 1;
-  while(temp>10)
+    int last, multiplier = 1;
+    last = n;
+    while(last>9)
     {
-      power *= 10;
-      temp /= 10;
+        multiplier *= 10;
+        last /= 10;
     }
-  last = temp;
-  if(last%2)
-  {
-    y = ((last-1)*power)+(a%power);
-  }
-  else
-  {
-    y = a;
-  }
-return (y);
+    if(last%2)
+        return (((last-1)*multiplier)+(n%multiplier));
+    else
+        return n;
 }
