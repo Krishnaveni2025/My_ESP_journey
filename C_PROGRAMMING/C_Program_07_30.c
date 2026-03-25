ef3e5a78c4cac9078f783b8bfe639ebd2f5466ab
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 02:25:28 +0530

    Update C_Program_07_30.c

--- C_PROGRAMMING/C_Program_07_30.c
+++ C_PROGRAMMING/C_Program_07_30.c
@@ -3,34 +3,40 @@
          Use only Decimals
          Use functions with not return value.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get two numbers from the user and print the HCF of those numbers
-  Function Name: disp_count_HCF2
-  Answer:
-  Input: 20 40  Output: 20
-  Input: 15 78  Output: 3
+   Question: 
+         Write a program to get two numbers from the user and 
+         print the HCF of those numbers
+   Function Name: 
+         disp_count_HCF2
+   Answer:
+         Input: 20 40  Output: 20
+         Input: 15 78  Output: 3
   */
 
 #include<stdio.h>
-void disp_count_HCF2(int, int);
+void disp_count_HCF2(int n1, int n2);
 int main()
 {
-int x,y;
-scanf("%d %d",&x, &y);
-disp_count_HCF2(x,y);
+    int number1, number2;
+    printf("Enter Number 1: ");
+    scanf("%d",&number1);
+    printf("Enter Number 2: ");
+    scanf("%d",&number2);
+    disp_count_HCF2(number1,number2);
+    return 0;
 }
-void disp_count_HCF2(int x, int y)
+void disp_count_HCF2(int n1, int n2)
 {
-//your code here
-  while(x!=y)
+    while(n2!=n1)
     {
-      if(x>y)
-      {
-        x -= y;
-      }
-      else
-      {
-        y -= x;
-      }
+        if(n1>n2)
+        {
+            n1 -= n2;
+        }
+        else
+        {
+            n2 -= n1;
+        }
     }
-  printf("%d",x);
+    printf("%d",n1);
 }
