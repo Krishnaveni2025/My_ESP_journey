5fad86775b8dc20263653a5ff212a30a6221d75a
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 01:41:55 +0530

    Update C_Program_07_22.c

--- C_PROGRAMMING/C_Program_07_22.c
+++ C_PROGRAMMING/C_Program_07_22.c
@@ -3,31 +3,34 @@
          Use only Decimals
          Use functions with not return value.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print the total number of two-digit odd numbers in the number
-  Function Name: disp_total_2digit_odd
-  Answer:
-  Input: 12345678 Output: 3
-  Input: 987531   Output: 4
+   Question: 
+         Write a program to get a number from the user and 
+         print the total number of two-digit odd numbers in the number
+   Function Name: 
+         disp_total_2digit_odd
+   Answer:
+         Input: 12345678 Output: 3
+         Input: 987531   Output: 4
   */
 #include<stdio.h>
-void disp_total_2digit_odd(int);
+void disp_total_2digit_odd(int n);
 int main()
 {
-int x;
-scanf("%d",&x);
-disp_total_2digit_odd(x);
+    int number;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    disp_total_2digit_odd(number);
 }
-void disp_total_2digit_odd(int x)
+void disp_total_2digit_odd(int n)
 {
-//your code here
-  int count = 0;
-  while(x>10)
+    int odd2_count = 0;
+    while(n>9)
     {
-      if((x%100)%2)
-      {
-        count++;
-      }
-      x /= 10;
+        if((n%100)%2)
+        {
+            odd2_count++;
+        }
+        n /= 10;
     }
-  printf("%d",count);
+    printf("%d",odd2_count);
 }
