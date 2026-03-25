248206387d368bef46b4a44aea5294c76e9242de
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-25 21:01:53 +0530

    Update C_Program_07_18.c

--- C_PROGRAMMING/C_Program_07_18.c
+++ C_PROGRAMMING/C_Program_07_18.c
@@ -3,28 +3,30 @@
          Use only Decimals
          Use functions with not return value.
          Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get a number from the user and print whether the first two digits(1's & ten's) of that number is prime or not.
-  Function Name: check_first_2digits_prime
-  Answer:
-Input: 359 - Output: Prime
-Input: 3577 - Output: Not prime
+   Question: 
+         Write a program to get a number from the user and print whether the first two digits(1's & ten's) of that number is prime or not.
+   Function Name: 
+         check_first_2digits_prime
+   Answer:
+         Input: 359 - Output: Prime
+         Input: 3577 - Output: Not prime
 */
 #include<stdio.h>
-void check_first_2digits_prime(int);
+void check_first_2digits_prime(int n);
 int main()
 {
-int x;
-scanf("%d",&x);
-check_first_2digits_prime(x);
+    int number;
+    scanf("%d",&number);
+    check_first_2digits_prime(number);
 }
-void check_first_2digits_prime(int x)
+void check_first_2digits_prime(int n)
 {
 //your code here
-  x %= 100;
-  int i = 2, is_prime = 1;
-  for(i = 2; i<x; i++)
+  n %= 100;
+  int divider = 2, is_prime = 1;
+  for(divider = 2; divider<n; divider++)
     {
-      if((x%i)==0)
+      if((n%divider)==0)
       {
         is_prime = 0;
         break;
