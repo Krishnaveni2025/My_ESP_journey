fce61562e303c38a1ad40edec61de73650a9b78d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 06:59:09 +0530

    Update C_Program_08_20.c

--- C_PROGRAMMING/C_Program_08_20.c
+++ C_PROGRAMMING/C_Program_08_20.c
@@ -1,32 +1,36 @@
 /* C LEVEL 08 Problem 20
-  Question: Write a program to get two numbers from the user and print the HCF of those numbers
-  Function Name: disp_count_HCF2
+        Use only int variables
+        Use only Decimals
+        Use Functions with a return value
+        Do not use arrays, strings, pointers, etc.
+  Question: 
+        Write a program to get two numbers from the user and 
+        print the HCF of those numbers
+  Function Name: 
+        disp_count_HCF2
   Answer:
-  Input: 20 40  Output: 20
-  Input: 15 78  Output: 3
+        Input: 20 40  Output: 20
+        Input: 15 78  Output: 3
   */
 #include<stdio.h>
-int disp_count_HCF2(int, int);
+int disp_count_HCF2(int n1, int n2);
 int main()
 {
-int x,y,hcf;
-scanf("%d %d",&x, &y);
-hcf = disp_count_HCF2(x,y);
-  printf("%d",hcf);
+    int number1, number2;
+    printf("Enter Number: ");
+    scanf("%d",&number1);
+    printf("Enter Number: ");
+    scanf("%d",&number2);
+    printf("%d",disp_count_HCF2(number1,number2));
 }
-int disp_count_HCF2(int a, int b)
+int disp_count_HCF2(int n1, int n2)
 {
-//your code here
-  while(a!=b)
+    while(n1!=n2)
     {
-      if(a>b)
-      {
-        a -= b;
-      }
-      else
-      {
-        b -= a;
-      }
-    } 
-return (b);//a
+        if(n1>n2)
+                n1 -= n2;
+        else
+                n2 -= n1;
+    }
+    return n1;
 }
