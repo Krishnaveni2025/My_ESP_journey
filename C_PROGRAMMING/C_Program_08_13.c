75c189c7257b4f7ccd670c9980def1671a237d21
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 05:46:48 +0530

    Update C_Program_08_13.c

--- C_PROGRAMMING/C_Program_08_13.c
+++ C_PROGRAMMING/C_Program_08_13.c
@@ -1,30 +1,37 @@
 /* C LEVEL 08 Problem 13
-  Question: Write a program to get a number from the user and print the total number of single digit perfect square numbers in the number
-  Function Name: disp_single_digit_square
+        Use only int variables
+        Use only Decimals
+        Use Functions with a return value
+        Do not use arrays, strings, pointers, etc.
+   Question: 
+        Write a program to get a number from the user and 
+        print the total number of single digit perfect square numbers in the number
+   Function Name:  
+        disp_single_digit_square
   Answer:
-  Input: 123456789 Output: 3
-  Input: 987531    Output: 2
+        Input: 123456789 Output: 3
+        Input: 987531    Output: 2
   */
 #include<stdio.h>
-int disp_single_digit_square(int);
+int disp_single_digit_square(int n);
 int main()
 {
-int x,y;
-scanf("%d",&x);
-y = disp_single_digit_square(x);
-printf("%d",y);
+    int number;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    printf("%d",disp_single_digit_square(number));
 }
-int disp_single_digit_square(int a)
+int disp_single_digit_square(int n)
 {
-int y = 0;
-//your code here
-  while(a)
+    int count = 0;
+    while(n)
     {
-  if(((a%10)==1)||((a%10)==4)||((a%10)==9))
-  {
-    y++;
-  }
-      a /= 10;
+        for(int i = 1; i<4; i++)
+        {
+            if((n%10)==(i*i))
+                    count++;
+        }
+        n /= 10;
     }
-return(y);
+    return count;
 }
