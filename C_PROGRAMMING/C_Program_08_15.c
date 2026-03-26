0d61605fcf477da2b4658c868b4e98d398dd397c
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 06:26:04 +0530

    Update C_Program_08_15.c

--- C_PROGRAMMING/C_Program_08_15.c
+++ C_PROGRAMMING/C_Program_08_15.c
@@ -1,31 +1,46 @@
 /* C LEVEL 08 Problem 15
-  Question: Write a program to get a number from the user and print the total number of single digit prime numbers in the number
-  Function Name: disp_single_digit_prime
-  Answer:
-  Input: 163496481 Output: 1
-  Input: 364925    Output: 3
+        Use only int variables
+        Use only Decimals
+        Use Functions with a return value
+        Do not use arrays, strings, pointers, etc.
+   Question: 
+        Write a program to get a number from the user and 
+        print the total number of single digit prime numbers in the number
+   Function Name: 
+        disp_single_digit_prime
+   Answer:
+        Input: 163496481 Output: 1
+        Input: 364925    Output: 3
   */
 
 #include<stdio.h>
-int disp_single_digit_prime(int);
+int disp_single_digit_prime(int n);
 int main()
 {
-int x,y;
-scanf("%d",&x);
-y = disp_single_digit_prime(x);
-printf("%d",y);
+    int number;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    printf("%d",disp_single_digit_prime(number));
 }
-int disp_single_digit_prime(int a)
+int disp_single_digit_prime(int n)
 {
-int y = 0;
-//your code here
-  while(a)
+    int is_prime, divider, count = 0;
+    while(n)
     {
-  if(((a%10)==2)||((a%10)==3)||((a%10)==5)||((a%10)==7))
-  {
-    y++;
-  }
-      a /= 10;
+        is_prime = 1;
+        for(divider = 2; divider<(n%10); divider++)
+        {
+            if(!((n%10)%divider))
+            {
+                is_prime = 0;
+                break;
+            }
+        }
+        if((n%10)<2)
+             is_prime = 0;
+        if(is_prime)
+                count++;
+        n /= 10;
     }
-return(y);
+    return count;
 }
