a004e78122788f3524cedbc69483fc4efe2dee9f
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 10:24:36 +0530

    Update C_Program_09_03.c

--- C_PROGRAMMING/C_Program_09_03.c
+++ C_PROGRAMMING/C_Program_09_03.c
@@ -1,24 +1,31 @@
 /*
   C LEVEL 09 Problem 03
-  Use only int variables
-  Use ont decimals
-  use arrays
-Question: Get 5 numbers from the user and print the smallest number
+          Use only int variables
+          Use ont decimals
+          Use arrays
+  Question: 
+          Get 5 numbers from the user and print the smallest number
 */
 #include<stdio.h>
+void print_small_number(int a[]);
 int main()
 {
-    int numbers[5], i, small;
-    for(i = 0; i<5; i++)
+    int numbers[5];
+    print_small_number(numbers);
+}
+void print_small_number(int a[])
+{
+    int small,i;
+    for(i=0; i<5; i++)
     {
-        printf("Enter %d Element: ",i);
-        scanf("%d",&numbers[i]);
+        printf("Enter %d Number:",i);
+        scanf("%d",&a[i]);
     }
-    small = numbers[0];
-    for(i=1;i<5;i++)
+    small = a[0];
+    for(i=1; i<5; i++)
     {
-        if(small>numbers[i])
-            small = numbers[i];
+        if(small>a[i])
+                small = a[i];
     }
-    printf("Smallest Element: %d",small);
+    printf("%d",small);
 }
