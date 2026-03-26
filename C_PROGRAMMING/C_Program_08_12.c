dc07eb0a23e60329d1a33d9dbb8cbf2cd3f82356
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 05:40:51 +0530

    Update C_Program_08_12.c

--- C_PROGRAMMING/C_Program_08_12.c
+++ C_PROGRAMMING/C_Program_08_12.c
@@ -1,31 +1,34 @@
 /* C LEVEL 08 Problem 12
-  Question: Write a program to get a number from the user and print the total number of two-digit odd numbers in the number
-  Function Name: disp_total_2digit_odd
-  Answer:
-  Input: 12345678 Output: 3
-  Input: 987531   Output: 4
+        Use only int variables
+        Use only Decimals
+        Use Functions with a return value
+        Do not use arrays, strings, pointers, etc.
+   Question: 
+        Write a program to get a number from the user and 
+        print the total number of two-digit odd numbers in the number
+   Function Name: 
+        disp_total_2digit_odd
+   Answer:
+        Input: 12345678 Output: 3
+        Input: 987531   Output: 4
   */
 #include<stdio.h>
-int disp_total_2digit_odd(int);
+int disp_total_2digit_odd(int n);
 int main()
 {
-int x,y;
-scanf("%d",&x);
-y = disp_total_2digit_odd(x);
-printf("%d",y);
+    int number;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    printf("%d",disp_total_2digit_odd(number));
 }
-int disp_total_2digit_odd(int a)
+int disp_total_2digit_odd(int n)
 {
-int y = 0;
-//your code here
-  while(a>10)
+    int count = 0;
+    while(n>10)
     {
-      if((a%100)%2)
-      {
-        y++;
-      }
-      a /= 10;
+        if((n%100)%2)
+                count++;
+        n /= 10;
     }
-return (y);
+    return count;
 }
-
