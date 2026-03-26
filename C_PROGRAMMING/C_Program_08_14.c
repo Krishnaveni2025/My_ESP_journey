fa9a730de46b1c457a84f3be436f45e60ef5ed07
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 05:52:14 +0530

    Update C_Program_08_14.c

--- C_PROGRAMMING/C_Program_08_14.c
+++ C_PROGRAMMING/C_Program_08_14.c
@@ -1,31 +1,37 @@
 /* C LEVEL 08 Problem 14
-  Question: Write a program to get a number from the user and print the total number of two digit perfect square numbers in the number
-  Function Name: disp_two_digit_square
-  Answer:
-  Input: 163496481 Output: 4
-  Input: 364925    Output: 4
+        Use only int variables
+        Use only Decimals
+        Use Functions with a return value
+        Do not use arrays, strings, pointers, etc.
+   Question: 
+        Write a program to get a number from the user and 
+        print the total number of two digit perfect square numbers in the number
+   Function Name: 
+        disp_two_digit_square
+   Answer:
+        Input: 163496481 Output: 4
+        Input: 364925    Output: 4
   */
-
 #include<stdio.h>
-int disp_two_digit_square(int);
+int disp_two_digit_square(int n);
 int main()
 {
-int x,y;
-scanf("%d",&x);
-y = disp_two_digit_square(x);
-printf("%d",y);
+    int number;
+    printf("Enter Number: ");
+    scanf("%d",&number);
+    printf("%d",disp_two_digit_square(number));
 }
-int disp_two_digit_square(int a)
+int disp_two_digit_square(int n)
 {
-int y = 0;
-//your code here
-  while(a>10)
+    int count = 0;
+    while(n>10)
     {
-  if(((a%100)==16)||((a%100)==49)||((a%100)==64)||((a%100)==81)||((a%100)==25)||((a%100)==36))
-  {
-    y++;
-  }
-      a /= 10;
+        for(int i=4; i<10; i++)
+        {
+            if((n%100)==(i*i))
+                    count++;
+        }
+        n /= 10;
     }
-return(y);
+    return count;
 }
