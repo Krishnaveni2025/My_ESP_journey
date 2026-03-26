f916f0366594524c7504436771f0bc627b619e01
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 10:43:03 +0530

    Update C_Program_09_07.c

--- C_PROGRAMMING/C_Program_09_07.c
+++ C_PROGRAMMING/C_Program_09_07.c
@@ -1,35 +1,52 @@
 /*
   C LEVEL 09 Problem 07
-  Use only int variables
-  Use only decimals
-  use arrays
-Question: Get 5 numbers from the user and arrange in descending order and print the same
+        Use only int variables
+        Use only decimals
+        Use arrays
+  Question: 
+        Get 5 numbers from the user and arrange in descending order and print the same
 */
 #include<stdio.h>
-
+void get5numbers(int a[]);
+void arrange_descend(int a[]);
+void print_descend(int a[]);
 int main()
 {
-    int numbers[5], i = 0, j = 0,temp;
+    int numbers[5];
+    get5numbers(numbers);
+    arrange_descend(numbers);
+    print_descend(numbers);
+}
+void print_descend(int a[])
+{
+    int i = 0;
+    for(i = 0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+void arrange_descend(int a[])
+{
+    int i, j,temp;
     for(i=0; i<5; i++)
     {
-        printf("Enter Number %d: ",i+1);
-        scanf("%d",&numbers[i]);
-    }
-    for(i = 0; i<5; i++)
-    {
-        for(j=i+1; j<5; j++)
+        for(j = i+1; j< 5; j++)
         {
-            if(numbers[i]<numbers[j])
+            if(a[j]>a[i])
             {
-                temp = numbers[j];
-                numbers[j] = numbers[i];
-                numbers[i] = temp;
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
             }
         }
     }
+}
+void get5numbers(int a[])
+{
+    int i =0;
     for(i=0; i<5; i++)
     {
-        printf("%d ",numbers[i]);
+        printf("Enter %d Element: ",i);
+        scanf("%d",&a[i]);
     }
-    return 0;
 }
