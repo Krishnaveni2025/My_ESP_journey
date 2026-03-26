d0db4f2befa9631a8748c71463f54f54a47c543d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-26 06:43:43 +0530

    Update C_Program_08_18.c

--- C_PROGRAMMING/C_Program_08_18.c
+++ C_PROGRAMMING/C_Program_08_18.c
@@ -1,42 +1,47 @@
 /* C LEVEL 08 Problem 18
-  Question: Write a program to get ttwo numbers from the user and print the LCM of those numbers
-  Function Name: disp_LCM2
-  Answer:
-  Input: 20 40 Output: 40
-  Input: 15 78 Output:390
+        Use only int variables
+        Use only Decimals
+        Use Functions with a return value
+        Do not use arrays, strings, pointers, etc.
+   Question: 
+        Write a program to get two numbers from the user and 
+        print the LCM of those numbers
+   Function Name: 
+        disp_LCM2
+   Answer:
+        Input: 20 40 Output: 40
+        Input: 15 78 Output:390
   */
 #include<stdio.h>
-int disp_LCM2(int, int);
+int disp_LCM2(int n1, int n2);
 int main()
 {
-int x,y,lcm;
-scanf("%d %d",&x,&y);
-lcm = disp_LCM2(x,y);
-  printf("%d",lcm);
+    int number1, number2;
+    printf("Enter Number: ");
+    scanf("%d",&number1);
+    printf("Enter Number: ");
+    scanf("%d",&number2);
+    printf("%d",disp_LCM2(number1, number2));
 }
-int disp_LCM2(int a, int b)
+int disp_LCM2(int n1, int n2)
 {
-int lcm;
-//your code here
-  if(a>b)
-  {
-    lcm = a;
-  }
+    int lcm;
+    if(n1>n2)
+    {
+        lcm = n1;
+    }
     else
     {
-      lcm = b;
+        lcm = n2;
     }
     while(1)
-      {
-        if(!(lcm%a))
+    {
+        if(!(lcm%n1))
         {
-          if(!(lcm%b))
-          {
-            return (lcm);
-            break;
-          }
+            if(!(lcm%n2))
+                break;
         }
         lcm++;
-      }
-  }
-
+    }
+    return lcm;
+}
