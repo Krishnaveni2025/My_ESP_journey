0e0eabe5b5b180671d0731f89c618e626e9e8cbb
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-27 05:42:43 +0530

    Update C_Program_09_09.c

--- C_PROGRAMMING/C_Program_09_09.c
+++ C_PROGRAMMING/C_Program_09_09.c
@@ -1,29 +1,41 @@
 /*
   C LEVEL 09 Problem 09
-  Use only int variables
-  Use only decimals
-  use arrays
-Question: Get 5 numbers from the user and 
-remove the even numbers and 
-create a new array and 
-then  print the same*/
+    Use only int variables
+    Use only decimals
+    Use arrays
+  Question: Get 5 numbers from the user and 
+            remove the even  numbers and 
+            create a new array and 
+            then  print the same
+*/
 #include<stdio.h>
+int get5numbersremoveeven(int a[],int b[]);
+void printresult(int n[], int o);
 int main()
 {
-    int input[5], output[5], oddcount = 0, i;
+    int input[5], output[5], oddcount;
+    oddcount = get5numbersremoveeven(input, output);
+    printresult(output,oddcount);
+}
+void printresult(int n[],int o)
+{
+    for(int i = 0; i<o; i++)
+    {
+        printf("%d ",n[i]);
+    }
+}
+int get5numbersremoveeven(int a[],int b[])
+{
+    int i, oddcount = 0;
     for(i=0; i<5; i++)
     {
-        printf("Enter Number %d: ",i+1);
-        scanf("%d",&input[i]);
-        if(input[i]%2)
+        printf("Enter %d Element: ",i);
+        scanf("%d",&a[i]);
+        if(a[i]%2)
         {
-            output[oddcount] = input[i];
+            b[oddcount] = a[i];
             oddcount++;
         }
     }
-    for(i=0; i<oddcount; i++)
-    {
-        printf("%d ",output[i]);
-    }
-    
+    return oddcount;
 }
