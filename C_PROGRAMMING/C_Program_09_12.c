3e993782db24a651316b7dca9c1b3482d90b833d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-28 04:01:19 +0530

    Update C_Program_09_12.c

--- C_PROGRAMMING/C_Program_09_12.c
+++ C_PROGRAMMING/C_Program_09_12.c
@@ -1,28 +1,32 @@
 /*
   C LEVEL 09 Problem 12
-  Use only int variables
-  Use only decimals
-  use arrays
-Question: Get multiple numbers from the user and 
-store them in an array . 
-Stop when the number is 0. 
-Print the total numbers entered by the user and 
-print the sum of numbers*/
+    Use only int variables
+    Use only decimals
+    Use arrays
+  Question: 
+    Get multiple numbers from the user and 
+    store them in an array . 
+    Stop when the number is 0. 
+    Print the total numbers entered by the user and 
+    print the sum of numbers
+*/
 #include<stdio.h>
 int main()
 {
-    int numbers[100], i = 0, number, total = 0, sum = 0;
+    int number, numbers[100], total = 0, sum = 0, i = 0;
     while(1)
     {
         printf("Enter %d Number: ",i+1);
         scanf("%d",&number);
         if(!number)
+        {
             break;
+        }
         numbers[i] = number;
+        total++;
         sum += numbers[i];
         i++;
-        total++;
     }
-    printf("Total Numbers: %d",total);
-    printf("\nSum of Numbers: %d",sum);
+    printf("%d",total);
+    printf("\n%d",sum);
 }
