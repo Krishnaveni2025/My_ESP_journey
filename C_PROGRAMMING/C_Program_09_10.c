7fb865753e9893bd73f42b4a1fcee04fe4c7c460
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-28 01:16:51 +0530

    Update C_Program_09_10.c

--- C_PROGRAMMING/C_Program_09_10.c
+++ C_PROGRAMMING/C_Program_09_10.c
@@ -1,46 +1,60 @@
 /*
   C LEVEL 09 Problem 10
-  Use only int variables
-  Use only decimals
-  use arrays
-Question: Get 5 numbers from the user and 
-remove the prime numbers and 
-create a new array and 
-then  print the same*/
+        Use only int variables
+        Use only decimals
+        Use arrays
+  Question: 
+        Get 5 numbers from the user and 
+        remove the prime numbers and 
+        create a new array and 
+        then  print the same
+*/
 
 #include<stdio.h>
-int prime_check(int n);
+int removeprime(int a[],int b[], int s);
+void get5numbers(int a[], int s);
+void printresult(int b[],int c);
 int main()
 {
-    int input[5], output[5], compositecount = 0, i;
-    for(i=0; i<5; i++)
+    int s = 5,input[s], output[s], non_prime ;
+    get5numbers(input,s);
+    non_prime = removeprime(input, output, s);
+    printresult(output, non_prime);
+}
+void get5numbers(int a[], int s)
+{
+    for(int i = 0; i<s; i++)
     {
-        printf("Enter Number %d: ",i+1);
-        scanf("%d",&input[i]);
-        if(!(prime_check(input[i])))
-        {
-            output[compositecount] = input[i];
-            compositecount++;
-        }
-    }
-    for(i=0; i<compositecount; i++)
-    {
-        printf("%d ",output[i]);
+        printf("Enter %d Element: ",i);
+        scanf("%d",&a[i]);
     }
 }
-int prime_check(int n)
+int removeprime(int a[], int b[], int s)
 {
-    int i= 2, is_prime = 1;
-    for(i=2; i<n; i++)
+    int comp_count = 0, divider, is_prime;
+    for(int i = 0; i<s; i++)
     {
-        if(!(n%i))
+        is_prime = 1;
+        for(divider = 2; divider<a[i]; divider++)
         {
-            is_prime = 0;
-            break;
+            if(!(a[i]%divider))
+                {
+                    is_prime = 0;
+                    break;
+                }
         }
+        if(!(is_prime))
+            {
+                b[comp_count] = a[i];
+                comp_count++;
+            }
     }
-    if(is_prime)
-        return 1;
-    else
-        return 0;
+    return comp_count;
+}
+void printresult(int b[],int c)
+{
+    for(int i = 0; i<c; i++)
+    {
+        printf("%d ",b[i]);
+    }
 }
