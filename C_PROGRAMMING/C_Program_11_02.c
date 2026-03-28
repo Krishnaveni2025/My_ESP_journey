6f29342be77ddacecd7809469eecb38aa11dd3b6
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-29 04:50:27 +0530

    Refactor array increment functions and cleanup code

--- C_PROGRAMMING/C_Program_11_02.c
+++ C_PROGRAMMING/C_Program_11_02.c
@@ -1,36 +1,22 @@
 /* C LEVEL 11 Problem 02
-  Use Pointers
-Question: Pass an integer array to a function and increment each array element in the function
-*/
-/* C LEVEL 11 Problem 02
-  Use Pointers
-Question: Pass an integer array to a function and increment each array element in the function
+        Use Pointers
+   Question: 
+        Pass an integer array to a function and 
+        increment each array element in the function
 */
 #include<stdio.h>
-void inc_array_element(int arr[],int n);
+void incementby1(int *a);
+int integer[] = { 1, 2, 3, 4, 5};
+int size = *(&integer + 1)- integer;
 int main()
 {
-    int size, array[size], i;
-    printf("Enter Array Size: ");
-    scanf("%d",&size);
-    for(i=0; i<size; i++)
-    {
-        printf("Enter %d Element: ",i);
-        scanf("%d",&array[i]);
-    }
-    inc_array_element(array,size);
-    printf("After Increment:\n");
-    for(i=0; i<size; i++)
-    {
-        printf("%d ",array[i]);
-    }
+    incementby1(integer);
 }
-void inc_array_element(int arr[], int n)
+void incementby1(int *a)
 {
-    int *element,i;
-    for(i=0;i<n;i++)
+    for(int i=0; i< size; i++)
     {
-        element = arr+i;
-        *element = *element+1;
+        a[i] += 1;
+        printf("%d ",a[i]);
     }
 }
