cea48086fa9b5fbf60f78bb1e82ba77b1ed99117
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-29 05:07:56 +0530

    Refactor loop to use for statement and update function

--- C_PROGRAMMING/C_Program_11_03.c
+++ C_PROGRAMMING/C_Program_11_03.c
@@ -1,30 +1,30 @@
 /* C LEVEL 11 Problem 03
-  Use Pointers
-Question: Write a loop program to print 1 to 5 one by one.
-Write a function and print the result in the main function
-Function Name: arrange_ascend
-Answer:
-1
-2
-3
-4
-5
+            Use Pointers
+  Question: 
+            Write a loop program to print 1 to 5 one by one.
+            Write a function and print the result in the main function
+  Function Name: 
+            arrange_ascend
+  Answer:
+            1
+            2
+            3
+            4
+            5
 */
-int print_1to5(int *p);
 #include<stdio.h>
+int arrange_ascend(int *p);
 int main()
 {
     int number;
-    number = 1;
-    while(number<6)
+    for(number = 1; number<6; number++)
     {
-        if(print_1to5(&number))
-            printf("%d\n",number);
-        number++;
+        if(arrange_ascend(&number))
+                printf("%d\n",number);   
     }
 }
-int print_1to5(int *p)
+int arrange_ascend(int *p)
 {
     if(*p<6)
-         return 1;
+            return 1;
 }
