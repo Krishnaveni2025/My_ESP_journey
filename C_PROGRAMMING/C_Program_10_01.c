45bb2de124e7698aac2f59efb8f1f60d9d9f4589
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-29 00:03:02 +0530

    Refactor ASCII value program structure

--- C_PROGRAMMING/C_Program_10_01.c
+++ C_PROGRAMMING/C_Program_10_01.c
@@ -1,14 +1,17 @@
 /* C LEVEL 10 Problem 01
-  Don't use structures
-  Question: Get an alphabet number and print it's ASCII value
+    Don't use structures
+   Question: Get an alphabet  and print it's ASCII value
 */
-#include <stdio.h>
-
+#include<stdio.h>
+char alphabet;
+void getalphaprintASCII(char c);
 int main()
 {
-    char alphabet;
-    printf("Enter an Alphabet: ");
-    scanf("%c",&alphabet);
-    printf("ASCII Value: %d",alphabet);
-    return 0;
+    getalphaprintASCII(alphabet);
+}
+void getalphaprintASCII(char c)
+{
+    printf("Enter Alphabet: ");
+    scanf("%c",&c);
+    printf("ASCII: %d",c);
 }
