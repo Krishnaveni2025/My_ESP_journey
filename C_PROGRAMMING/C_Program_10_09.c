ce2dd300a6285c8f6743e7fd56cf43583bf6d9c9
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-29 02:53:26 +0530

    Refactor leading zero removal logic in C program
    Refactor the program to remove leading zeros from a string of numbers. Updated input handling and removed unnecessary code.

--- C_PROGRAMMING/C_Program_10_09.c
+++ C_PROGRAMMING/C_Program_10_09.c
@@ -1,27 +1,28 @@
 /* C LEVEL 10 Problem 09
-  Don't use structures
-  Question: Get a string of numbers upto 50 digits and remove all leading zeros
+        Don't use structures
+   Question: 
+        Get a string of numbers upto 50 digits and remove all leading zeros
 */
 #include<stdio.h>
+void removeleading0s(char *numstrng);
+char input[51], output[51];
+int i,j;
 int main()
 {
-    char numbstr[51];
-    printf("Enter a Number(50 digits):");
-    scanf("%s",numbstr);
-    int i = 0, j = 0;
-    while((numbstr[i]!='\0')&&(numbstr[i]=='0'))
+    printf("Enter 50 digits:");
+    scanf("%s",input);
+    removeleading0s(input);
+    printf("Result\n%s",output);
+}
+void removeleading0s(char *numstrng)
+{
+    while(numstrng[i]!='\0')
     {
+        if(numstrng[i]!='0')
+        {
+            output[j] = numstrng[i];
+            j++;
+        }
         i++;
     }
-    if(numbstr[i]=='\0'){
-        numbstr[i] = '0';
-        numbstr[1] = '\0';
-    }
-    else{
-        while(numbstr[i]!='\0'){
-            numbstr[j++] = numbstr[i++];
-        }
-        numbstr[j] = '\0';
-    }
-    printf("%s",numbstr);
 }
