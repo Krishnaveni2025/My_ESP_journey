96949008f7165bd3765b70ffea06cf6084f896c0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-29 02:26:19 +0530

    Improve number validation and input prompt
    Refactor number validation logic and update input handling.

--- C_PROGRAMMING/C_Program_10_08.c
+++ C_PROGRAMMING/C_Program_10_08.c
@@ -1,28 +1,30 @@
 /* C LEVEL 10 Problem 08
-  Don't use structures
-  Question: Get a string of numbers upto 50 digits and validate the number
+        Don't use structures
+   Question: 
+        Get a string of numbers upto 50 digits and validate the number
 */
+int valid(char *strng);
+char input[51];
 #include<stdio.h>
 int main()
 {
-    char numbstr[51];
-    printf("Enter a Number(50 digits): ");
-    scanf("%s",numbstr);
-    int valid = 1,i=0;
-    while(numbstr[i]!='\0')
+    printf("Enter number of 50 digits:\n");
+    scanf("%s",input);
+    if(valid(input))
+            printf("Valid");
+    else
+            printf("Not Valid");
+}
+int valid(char *strng)
+{
+    int valid = 1;
+    for(int i = 0; i<51; i++)
     {
-        if(numbstr[i]<'0' || numbstr[i]>'9')
+        if(strng[i]<'0' || strng[i]>'9')
         {
             valid = 0;
             break;
         }
-        i++;
     }
-    if(valid)
-    {
-        printf("Your number is valid");
-    }
-    else{
-        printf("Pleease Enter a valid number");
-    }
+    return valid;
 }
