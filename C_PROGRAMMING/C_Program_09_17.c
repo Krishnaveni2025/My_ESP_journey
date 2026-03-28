37dcc47be41d0f253d8e616dd625949c2db197d8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-28 23:27:38 +0530

    Refactor integer addition with carry handling

--- C_PROGRAMMING/C_Program_09_17.c
+++ C_PROGRAMMING/C_Program_09_17.c
@@ -1,48 +1,59 @@
 /*
   C LEVEL 09 Problem 17
         Use only int variables
-        Use ont decimals
+        Use only decimals
         Use arrays
   Question: 
         Adjust the carry in an  integer array 
         (i.e convert the 2 digit number into single digit and add carry to the next number)
 */
-#include <stdio.h>
-
-int input1[50], input2[50], output[51];
-
-int main() {
-    int i, carry = 0;
-
-    printf("Enter first Array (50 digits):\n");
-    for (i = 0; i < 50; i++) {
-        printf("Element %d: ",i);
-        scanf("%d", &input1[i]);
+#include<stdio.h>
+int integer1[50], integer2[50], result[51];
+int i,carry, sum, total = 50;
+void getinteger(int a[], int s);
+void printarray(int a[], int s);
+void addintegers(int a[], int b[]);
+int main()
+{
+    getinteger(integer1, total);
+    getinteger(integer2, total);
+    printarray(integer1, total);
+    printarray(integer2, total);
+    addintegers(integer1, integer2);
+    printarray(result, 51);
+}
+void addintegers(int a[], int b[])
+{
+    for(i=49; i>=0; i--)
+    {
+        a[i] += carry;
+        sum = a[i] + b[i];
+        if(sum>9)
+        {
+            result[i+1] = sum%10;
+            carry = sum/10;
+        }
+        else
+        {
+            carry = 0;
+            result[i+1] = sum;
+        }
     }
-
-    printf("Enter second Array (50 digits):\n");
-    for (i = 0; i < 50; i++) {
-        printf("Element %d: ",i);
-        scanf("%d", &input2[i]);
-    }
-
-    // Add from rightmost digit
-    for (i = 49; i >= 0; i--) {
-        int sum = input1[i] + input2[i] + carry;
-        output[i + 1] = sum % 10;   // store single digit
-        carry = sum / 10;           // update carry
-    }
-
-    // Handle final carry
-    output[0] = carry;
-
-    // Print result
-    printf("Result = ");
-    for (i = 0; i < 51; i++) {
-        if (i == 0 && output[i] == 0) continue; // skip leading zero
-        printf("%d", output[i]);
-    }
+}
+void getinteger(int a[], int s)
+{
     printf("\n");
-
-    return 0;
+    for(i=0; i<s; i++)
+    {
+        printf("Enter %d Number: ",i+1);
+        scanf("%d",&a[i]);
+    }
+}
+void printarray(int a[], int s)
+{
+    printf("\n");
+    for(i=0; i<s; i++)
+    {
+        printf("%d ",a[i]);
+    }
 }
