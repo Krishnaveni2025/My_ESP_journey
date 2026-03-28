aef8615f322e51b9cc2b8754254f00b3bf5e4b43
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-29 01:59:16 +0530

    Update string length calculation in C program
    Refactor string length calculation and input handling.

--- C_PROGRAMMING/C_Program_10_07.c
+++ C_PROGRAMMING/C_Program_10_07.c
@@ -1,20 +1,23 @@
 /* C LEVEL 10 Problem 07
-  Don't use structures
-  Question: Get a string and find the length of the string
+        Don't use structures
+   Question: 
+        Get a string and find the length of the string
 */
-#include <stdio.h>
+#include<stdio.h>
+char input[100];
+int length;
+int getlength(char *str);
 int main()
 {
-    char str[100];
-    int length = 0,i=0;
     printf("Enter String: ");
-    scanf("%99s",str);
-    while(str[i]!='\0')
+    scanf("%s",input);
+    printf("Length: %d",getlength(input));
+}
+int getlength(char *str)
+{
+    while(str[length]!='\0')
     {
         length++;
-        i++;
     }
-    printf("Length = %d",length);
-
-    return 0;
+    return length;
 }
