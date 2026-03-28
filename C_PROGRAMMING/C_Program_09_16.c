4b4e7e368d10dd4daa47281e9d7fc864317b516f
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-28 23:13:47 +0530

    Refactor integer array addition program

--- C_PROGRAMMING/C_Program_09_16.c
+++ C_PROGRAMMING/C_Program_09_16.c
@@ -6,38 +6,42 @@
   Question: 
     Add two integer arrays upto 50 digits and store the result in an 51 digit array
 */
-#include <stdio.h>
-
-int input1[50], input2[50], output[51];
-
-int main() {
-    int i;
-
-    printf("Enter first Array (50 digits):\n");
-    for (i = 0; i < 50; i++) {
-        printf("Element %d: ",i);
-        scanf("%d", &input1[i]);
+#include<stdio.h>
+int integer1[50], integer2[50], result[51];
+int i, j, total = 50;
+void getinteger(int a[], int s);
+void printarray(int a[], int s);
+void addintegers(int a[], int b[]);
+int main()
+{
+    getinteger(integer1, total);
+    getinteger(integer2, total);
+    printarray(integer1, total);
+    printarray(integer2, total);
+    addintegers(integer1, integer2);
+    printarray(result, 51);
+}
+void addintegers(int a[], int b[])
+{
+    for(i=49; i>=0; i--)
+    {
+        result[i+1] = a[i] + b[i];
     }
-
-    printf("Enter second Array (50 digits):\n");
-    for (i = 0; i < 50; i++) {
-        printf("Element %d: ",i);
-        scanf("%d", &input2[i]);
-    }
-
-    // Add from rightmost digit
-    for (i = 49; i >= 0; i--) {
-        output[i + 1]  = input1[i] + input2[i] ;
-    }
-    output[0] = 0;
-
-    // Print result
-    printf("Result = ");
-    for (i = 0; i < 51; i++) {
-        if (i == 0 && output[i] == 0) continue; // skip leading zero
-        printf("%d", output[i]);
-    }
+}
+void getinteger(int a[], int s)
+{
     printf("\n");
-
-    return 0;
+    for(i=0; i<s; i++)
+    {
+        printf("Enter %d Number: ",i+1);
+        scanf("%d",&a[i]);
+    }
+}
+void printarray(int a[], int s)
+{
+    printf("\n");
+    for(i=0; i<s; i++)
+    {
+        printf("%d ",a[i]);
+    }
 }
