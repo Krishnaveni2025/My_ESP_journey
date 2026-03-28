b27c670ba544111d4f2d67dfd74a4694145b1248
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-29 01:45:43 +0530

    Refactor digit printing logic in C program

--- C_PROGRAMMING/C_Program_10_06.c
+++ C_PROGRAMMING/C_Program_10_06.c
@@ -1,26 +1,37 @@
 /* C LEVEL 10 Problem 06
-  Don't use structures
-  Question: Get an intger and print each digit as a character. Print one character on one line
+        Don't use structures
+   Question: 
+        Get an intger and print each digit as a character. 
+        Print one character on one line
 */
-#include <stdio.h>
+#include<stdio.h>
+int number,i,j, temp;
+char charofnum[100];
+void getnum_printchar(void);
 int main()
 {
-    char str[100];
-    int i=0,j,temp, value;
+    getnum_printchar();
+}
+void getnum_printchar(void)
+{
     printf("Enter Number: ");
-    scanf("%d",&value);//12345
-    while(value)//t t t t t
+    scanf("%d",&number);
+    while(number)
     {
-        str[i] = (value%10)+'0';//5 54 543 5432 54321
-        value /= 10;//1234 123 12 1 0
-        i++;//1 2 3 4 5
+        charofnum[i] = (number%10) + '0';
+        number /= 10;
+        i++;
     }
-    str[i]='\0';
-    for(j=i;j>=0;j--)
+    charofnum[i] = '\0';
+    
+    for(j=0; j<i/2; j++)
     {
-        printf("%c\n",str[i]);//1 2 3 4 5 
-        i--;
+        temp = charofnum[j];
+        charofnum[j] = charofnum[i-j-1];
+        charofnum[i-j-1] = temp;
     }
-
-    return 0;
+    for(j=0; charofnum[j]!='\0';j++)
+    {
+        printf("%c\n",charofnum[j]);
+    }
 }
