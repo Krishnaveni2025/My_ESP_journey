42051bbefaaabaf9fcde9681d292f22059a1eda8
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-03-30 23:25:24 +0530

    Refactor C program for adding large numbers using pointers

--- C_PROGRAMMING/C_Program_12_01.c
+++ C_PROGRAMMING/C_Program_12_01.c
@@ -1,6 +1,6 @@
 /* C LEVEL 12 - Problem 01
-Use Pointers
-  Question: Get two numbers upto 50 digits and perform addition and print the result.
+        Use Pointers
+   Question: Get two numbers upto 50 digits and perform addition and print the result.
   #include<stdio.h>
   int main()
   {
@@ -8,69 +8,70 @@ Use Pointers
     addnumbers(number1, number2, result);
     print(result);
   }
+
 */
-
-#include <stdio.h>
-
+#include<stdio.h>
 void getnumbers(char *n1, char *n2);
-void addnumbers(char *n1, char *n2, char *result);
-void print(char *result);
+void addnumbers(char *n1, char *n2, char *r);
+void print(char *str);
+int getlength(char *str);
+void reversestring(char *str, int length);
 
-int main() {
-    char number1[51], number2[51], result[52]; // 50 digits + null, result may need 1 extra digit
+char number1[51], number2[51], result[52];
+int main()
+{
     getnumbers(number1, number2);
+    print(number1);
+    print(number2);
     addnumbers(number1, number2, result);
     print(result);
-    return 0;
 }
-
-void getnumbers(char *n1, char *n2) {
-    printf("Enter 1st Number (up to 50 digits): ");
-    scanf("%50s", n1);
-    printf("Enter 2nd Number (up to 50 digits): ");
-    scanf("%50s", n2);
-    printf("___________________________________________________________________\n");
+void getnumbers(char *n1, char *n2)
+{
+    printf("Enter First Number\n");
+    scanf("%s",n1);
+    printf("Enter Second Number\n");
+    scanf("%s",n2);
 }
-
-// Manual length function (no string.h)
-int mystrlen(char *s) {
-    int len = 0;
-    while (*s != '\0') {
-        len++;
-        s++;
+void addnumbers(char *n1, char *n2, char *r)
+{
+    int d1, d2, l1, l2, l = 0, i, j, sum , carry = 0;
+    l1 = getlength(n1);
+    l2 = getlength(n2);
+    i = l1-1;
+    j = l2-1;
+    while(n1[i]!='\0'|| n2[j]!='\0'||carry>0)
+    {
+        d1 = (i>=0) ? n1[i--]-'0' : 0;
+        d2 = (j>=0) ? n2[j--]-'0' : 0;
+        sum = d1+d2+carry;
+        carry = sum /10;
+        r[l] = sum + '0';
+        l++;
     }
-    return len;
+    r[l] = '\0';
+    reversestring(r,l);
 }
-
-void addnumbers(char *n1, char *n2, char *result) {
-    int len1 = mystrlen(n1);
-    int len2 = mystrlen(n2);
-    int carry = 0, sum;
-    int i = len1 - 1;
-    int j = len2 - 1;
-    int k = 0;
-
-    char temp[52]; // temporary result in reverse order
-
-    while (i >= 0 || j >= 0 || carry > 0) {
-        int d1 = (i >= 0) ? n1[i] - '0' : 0;
-        int d2 = (j >= 0) ? n2[j] - '0' : 0;
-
-        sum = d1 + d2 + carry;
-        temp[k++] = (sum % 10) + '0';
-        carry = sum / 10;
-
-        i--;
-        j--;
+void reversestring(char *str, int length)
+{
+    int temp, i;
+    for(i = 0; i<length/2; i++)
+    {
+        temp = str[i];
+        str[i] = str[length-i-1];
+        str[length-i-1] = temp;
     }
-
-    // Reverse temp into result
-    for (int m = 0; m < k; m++) {
-        result[m] = temp[k - m - 1];
-    }
-    result[k] = '\0';
 }
-
-void print(char *result) {
-    printf("Result\t\t\t\t : %s\n", result);
+void print(char *str)
+{
+    printf("%s\n",str);
+}
+int getlength(char *str)
+{
+    int length = 0;
+    while(str[length]!='\0')
+    {
+        length++;
+    }
+    return length;
 }
