53b53fa96f18eb8f35432c2473eb59f5c2657462
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-04-01 08:36:00 +0530

    Add double linked list implementation in C
    Implement a double linked list with insertion, deletion, and display functionalities.

---
+++ C_PROGRAMMING/C_Program_13_04.c
@@ -0,0 +1,19 @@
+/*
+        C LEVEL 13 - Problem 04
+        Question:
+                  Create a Double linked list about 5 entries using the following structure
+                  insert a new entry before or after given a given id.
+                  Delete an entry of a given id.
+                  Menu Items
+                  1. Insert Entry
+                  2. Delete Entry
+                  3. Display List
+                  3. Exit
+        ********************Hint**********************************
+        struct student{
+          int id;
+          int math;
+          int science;
+          struct student *next;
+        }
+*/
