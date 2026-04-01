c05904155a363baf02e245b95f116115fc4426fc
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-04-01 08:43:00 +0530

    Create stack implementation in C using linked list
    Implement stack using linked list with push, pop, and display functionality.

---
+++ C_PROGRAMMING/C_Program_13_05.c
@@ -0,0 +1,20 @@
+/*
+        C LEVEL 13 - Problem 05
+        Question:
+                  Write a program to Create a stack using linked list .
+                  Use Push & Pop .
+                  Push will insert the entry on the top of the list.
+                  Pop will delete top of the list and display will show from top to bottom.
+                  Menu Items
+                  1. Push
+                  2. Pop
+                  3. Display Stack
+                  3. Exit
+        ********************Hint**********************************
+        struct student{
+          int id;
+          int math;
+          int science;
+          struct student *next;
+        }
+*/
