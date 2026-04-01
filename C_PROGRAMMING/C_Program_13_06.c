d9fbaf127ceeb1b8bd130e765d79f22572a8d585
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-04-01 08:46:35 +0530

    Update program description for Queue implementation

---
+++ C_PROGRAMMING/C_Program_13_06.c
@@ -0,0 +1,21 @@
+/*
+        C LEVEL 13 - Problem 06
+        Question:
+                  Write a program to Create a Queue using linked list .
+                  Use Add & Remove .
+                  Add will insert the entry on the top of the list.
+                  Pop will get the bottom of the list and display .
+                  Display will show from top to bottom.
+                  Menu Items
+                  1. Add
+                  2. Remove
+                  3. Display Queue list
+                  3. Exit
+        ********************Hint**********************************
+        struct student{
+          int id;
+          int math;
+          int science;
+          struct student *next;
+        }
+*/
