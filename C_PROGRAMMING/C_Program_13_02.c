c2f314501c5c1d5093c57fe635c98f0431af77bb
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-04-01 08:32:19 +0530

    Create linked list program with menu for entries
    Added a sample linked list program with menu options.

---
+++ C_PROGRAMMING/C_Program_13_02.c
@@ -0,0 +1,17 @@
+/*
+        C LEVEL 13 - Problem 02
+        Question:
+                  Create a sample link list about five entries using the following structure
+                  insert a new entry before or after given a given id.
+                  Menu Items
+                  1. Insert Entry
+                  2. Display List
+                  3. Exit
+        ********************Hint**********************************
+        struct student{
+          int id;
+          int math;
+          int science;
+          struct student *next;
+        }
+*/
