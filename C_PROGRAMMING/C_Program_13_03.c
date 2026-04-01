049f280e9646d9cbec23933e0aec0a6b065d484d
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-04-01 08:34:57 +0530

    Implement linked list operations in C
    Added a C program for linked list operations including insertion, deletion, and display.

---
+++ C_PROGRAMMING/C_Program_13_03.c
@@ -0,0 +1,19 @@
+/*
+        C LEVEL 13 - Problem 03
+        Question:
+                  Create a sample link list about 5 entries using the following structure
+                  insert a new entry before or after given a given id.
+                  Delete an entry of a given id.
+                  Menu Items
+                  1. Insert Entry
+                  2. Delete Entry
+                  3. Display List
+                  3. Exit
+        ********************Hint**********************************
+        struct student{
+          int id;
+          int math;
+          int science;
+          struct student *next;
+        }
+*/
