7342aac79f83b482e75582e86070be6344e11ed0
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-04-02 04:23:12 +0530

    Refactor character search and input functions

--- C_PROGRAMMING/C_Program_12_02.c
+++ C_PROGRAMMING/C_Program_12_02.c
@@ -9,38 +9,37 @@
    Answer: 2,6,14
 */
 #include<stdio.h>
-
 char char_string[100], letter;
-void get_char_string(char *str, char *c);
+void get_string_char(char * str, char *c);
 void print_position(char *str, char c);
-
 int main()
 {
-    get_char_string(char_string,&letter);
+    get_string_char(char_string, &letter);
     print_position(char_string, letter);
 }
 void print_position(char *str, char c)
 {
-    int  i = 0, found = 0;
+    int i = 0, found = 0, count = 0;
     while(str[i]!='\0')
     {
         if(str[i] == c)
-        {
-            if(found)
-                printf(",");
-            printf("%d",i+1);
-            found = 1;
-        }
-        found = 0;
+            {
+                if(found)
+                        printf(",");
+                found = 0;
+                printf("%d",i+1);
+                found = 1;
+                count++;
+            }
         i++;
     }
-    if(!found)
-            printf("\ncharacter is found");
+    if(!count)
+            printf("character not found");
 }
-void get_char_string(char *str, char *c)
+void get_string_char(char *str, char *c)
 {
-    printf("Enter String: ");
+    printf("String: ");
     scanf("%s",str);
-    printf("Enter character: ");
+    printf("character: ");
     scanf(" %c",c);
 }
