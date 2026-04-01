87d379aa30479d6bac84c2a36c5da00c5651c494
Krishnaveni Purne <krishnavenipurne@gmail.com>
2026-04-01 08:25:28 +0530

    Create linked list for student data input
    Implement linked list structure for student records with user input functionality.

---
+++ C_PROGRAMMING/C_Program_13_01.c
@@ -0,0 +1,13 @@
+/*
+Create a linked list for the following structure
+and get user inputs id, math mark, science mark.
+once the user inputs the id as -1
+exit the input mmode and display all the entries.
+********************Hint**********************************
+struct student{
+    int id;
+    int math;
+    int science;
+    struct student *next;
+}
+*/
