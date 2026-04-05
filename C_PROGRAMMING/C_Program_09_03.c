3e03aa97761e36458ee3991b5b8018e36313c93f
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-05 18:12:53 +0530

    Update C_Program_09_03.c

--- C_PROGRAMMING/C_Program_09_03.c
+++ C_PROGRAMMING/C_Program_09_03.c
@@ -1,31 +1,43 @@
 /*
   C LEVEL 09 Problem 03
           Use only int variables
-          Use ont decimals
+          Use only decimals
           Use arrays
   Question: 
           Get 5 numbers from the user and print the smallest number
 */
 #include<stdio.h>
-void print_small_number(int a[]);
+
+void get5numbers(int a[]);
+int findsmall(int a[],int s);
+void print(int s);
+
 int main()
 {
-    int numbers[5];
-    print_small_number(numbers);
+    int number[5], small;
+    get5numbers(number);
+    small = findsmall(number, small);
+    print(small);
 }
-void print_small_number(int a[])
+int findsmall(int a[], int s)
 {
-    int small,i;
-    for(i=0; i<5; i++)
+    s = a[0];
+    for(int j = 1; j<5; j++)
     {
-        printf("Enter %d Number:",i);
+        if(s>a[j])
+                s = a[j];
+    }
+    return s;
+}
+void get5numbers(int a[])
+{
+    for(int i = 0; i<5; i++)
+    {
+        printf("Enter %d Number: ",i+1);
         scanf("%d",&a[i]);
     }
-    small = a[0];
-    for(i=1; i<5; i++)
-    {
-        if(small>a[i])
-                small = a[i];
-    }
-    printf("%d",small);
+}
+void print(int s)
+{
+    printf("Smallest Number: %d",s);
 }
