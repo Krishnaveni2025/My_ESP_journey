417ae3392276c7910727714fd4c1add41be6b32a
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-05 17:42:10 +0530

    Update C_Program_09_01.c

--- C_PROGRAMMING/C_Program_09_01.c
+++ C_PROGRAMMING/C_Program_09_01.c
@@ -1,26 +1,33 @@
 /*
   C LEVEL 09 Problem 01
         Use only int variables
-        Use ont decimals
+        Use only decimals
         Use arrays
 Question: 
-        Get 5 numbers from the user and print all the sum of all numbers
+        Get 5 numbers from the user and print the sum of all numbers
 */
+
 #include<stdio.h>
-void print_sum5(int a[]);
+
+int arr[5], result;
+void get5numbers(int a[]);
+void print(int sum);
 int main()
 {
-    int numbers[5];
-    print_sum5(numbers);
+    get5numbers(arr);
+    print(result);
 }
-void print_sum5(int a[])
+void get5numbers(int a[])
 {
-    int i = 0, sum = 0;
-    for(i=0;i<5;i++)
+    result = 0;
+    for(int i = 0 ; i<5; i++)
     {
-        printf("Enter %d Element: ",i);
+        printf("Enter %d Number: ",i+1);
         scanf("%d",&a[i]);
-        sum += a[i];
+        result += a[i];
     }
-    printf("%d",sum);
+}
+void print(int sum)
+{
+    printf("Sum = %d",sum);
 }
