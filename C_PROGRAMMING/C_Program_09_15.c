4a64b384b5f756d130a8bb8ebc6a7505e472ce6b
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-06 12:52:07 +0530

    Update C_Program_09_15.c

--- C_PROGRAMMING/C_Program_09_15.c
+++ C_PROGRAMMING/C_Program_09_15.c
@@ -1,72 +1,85 @@
 /*
   C LEVEL 09 Problem 15
-        Use only int variables
-        Use only decimals
-        Use arrays
+            Use only int variables
+            Use only decimals
+            Use arrays
   Question: 
-        Get multiple numbers from the user and store them in an array . 
-        Stop when the number is 0.
-        Add each numbers digit and put them in an array and 
-        arrange the new array in the ascending order and 
-        print the same
+            Get multiple numbers from the user and store them in an array . 
+            Stop when the number is 0.
+            Add each numbers digit and put them in an array and 
+            arrange the new array in the ascending order and 
+            print the same
 */
 #include<stdio.h>
+
 void getnumbers(int a[]);
-void printarray(int a[],int s);
-int sumofdigits(int n);
-void array_ascend(int a[], int total);
-int input[100], output[100], number,temp, total, i, j;
+void addDigits(int a[], int b[]);
+void arrange_ascend(int a[]);
+void print(int a[]);
+int sum(int n);
+int total;
+
 int main()
 {
-    getnumbers(input);
-    printarray(input, total);
-    printarray(output, total);
-    array_ascend(output, total);
-    printarray(output, total);
+    int numbers[50],result[50];
+    getnumbers(numbers);
+    print(numbers);
+    addDigits(numbers,result);
+    print(result);
+    arrange_ascend(result);
+    print(result);
 }
-void array_ascend(int a[], int total)
+void arrange_ascend(int a[])
 {
-    for(i=0; i<total; i++)
+    int temp;
+    for(int i = 0; i<total; i++)
     {
-        for(j = i+1; j<total; j++)
+        for(int j = i+1; j<total; j++)
         {
-            if(a[i]>a[j])
+            if(a[j]<a[i])
             {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
             }
         }
     }
 }
-int sumofdigits(int n)
-{
-    int sum = 0;
-    while(n)
-    {
-        sum += (n%10);
-        n/=10;
-    }
-    return sum;
-}
-void printarray(int a[], int s)
+void print(int a[])
 {
     printf("\n");
-    for(i = 0; i<total; i++)
+    for(int i = 0; i<total; i++)
     {
         printf("%d ",a[i]);
     }
 }
+int sum(int n)
+{
+    int sumofdigits = 0;
+    while(n)
+    {
+        sumofdigits += (n%10);
+        n /= 10;
+    }
+    return sumofdigits;
+}
+void addDigits(int a[], int b[])
+{
+    for(int i = 0; i<total; i++)
+    {
+        b[i] = sum(a[i]);
+    }
+}
 void getnumbers(int a[])
 {
+    int number;
     while(1)
     {
-        printf("Enter %d Element: ",total+1);
+        printf("Enter %d Number: ",total+1);
         scanf("%d",&number);
-        if(!number)
+        if(!(number))
                 break;
-        input[total] = number;
-        output[total] = sumofdigits(number);
+        a[total] = number;
         total++;
     }
 }
