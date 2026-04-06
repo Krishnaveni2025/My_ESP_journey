217b03b43ac046eab4c5fa07334a0652b43f8b79
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-06 10:09:44 +0530

    Update C_Program_09_07.c

--- C_PROGRAMMING/C_Program_09_07.c
+++ C_PROGRAMMING/C_Program_09_07.c
@@ -7,30 +7,34 @@
         Get 5 numbers from the user and arrange in descending order and print the same
 */
 #include<stdio.h>
-void get5numbers(int a[]);
-void arrange_descend(int a[]);
-void print_descend(int a[]);
+
+void get5numbers(int a[], int s);
+void arrange_descend(int a[], int s);
+void print_array(int a[], int s);
 int main()
 {
-    int numbers[5];
-    get5numbers(numbers);
-    arrange_descend(numbers);
-    print_descend(numbers);
+    int size = 5;
+    int numbers[size];
+    get5numbers(numbers, size);
+    print_array(numbers, size);
+    arrange_descend(numbers, size);
+    print_array(numbers, size);
 }
-void print_descend(int a[])
+void print_array(int a[], int s)
 {
+    printf("\n");
     int i = 0;
-    for(i = 0;i<5;i++)
+    for(i = 0;i<s;i++)
     {
         printf("%d ",a[i]);
     }
 }
-void arrange_descend(int a[])
+void arrange_descend(int a[], int s)
 {
     int i, j,temp;
-    for(i=0; i<5; i++)
+    for(i=0; i<s; i++)
     {
-        for(j = i+1; j< 5; j++)
+        for(j = i+1; j< s; j++)
         {
             if(a[j]>a[i])
             {
@@ -41,12 +45,12 @@ void arrange_descend(int a[])
         }
     }
 }
-void get5numbers(int a[])
+void get5numbers(int a[], int s)
 {
     int i =0;
-    for(i=0; i<5; i++)
+    for(i=0; i<s; i++)
     {
-        printf("Enter %d Element: ",i);
+        printf("Enter %d Element: ",i+1);
         scanf("%d",&a[i]);
     }
 }
