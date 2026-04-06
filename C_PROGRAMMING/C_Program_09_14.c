88eb4d9b4512b4504564d1d549c0d00b4260a6af
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-06 12:34:37 +0530

    Update C_Program_09_14.c

--- C_PROGRAMMING/C_Program_09_14.c
+++ C_PROGRAMMING/C_Program_09_14.c
@@ -1,34 +1,37 @@
 /*
   C LEVEL 09 Problem 14
-    Use only int variables
-    Use only decimals
-    Use arrays
+            Use only int variables
+            Use only decimals
+            Use arrays
   Question: 
-    Get multiple numbers from the user and store them in an array . 
-    Stop when the number is 0. 
-    If total number is odd then print middle number, 
-    otherwise print the average of middle two numbers 
+            Get multiple numbers from the user and store them in an array . 
+            Stop when the number is 0. 
+            If total number is odd then print middle number, 
+            otherwise print the average of middle two numbers 
 */
 #include<stdio.h>
-int number,total, input[500];
-int getnumbers(int a[]);
+
+void getnumbers(int a[]);
+int total;
 int main()
 {
-    printf("%d",getnumbers(input));
+    int numbers[50];
+    getnumbers(numbers);
+    if(total%2)
+            printf("Total number is odd\nMiddle = %d",numbers[total/2]);
+    else
+            printf("Total number is Even\nAverage = %d",((numbers[total/2])+(numbers[(total/2)-1]))/2);
 }
-int getnumbers(int a[])
+void getnumbers(int a[])
 {
+    int number;
     while(1)
     {
         printf("Enter %d Number: ",total+1);
         scanf("%d",&number);
         if(!number)
-             break;
-        input[total] = number;
+                break;
+        a[total] = number;
         total++;
     }
-    if(total%2)
-            return input[total/2];
-    else
-            return ((input[total/2]+(input[(total/2)-1]))/2);
 }
