010ccc3f68581196c0309cb2264ebd6719cc1de8
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-06 12:19:35 +0530

    Update C_Program_09_13.c

--- C_PROGRAMMING/C_Program_09_13.c
+++ C_PROGRAMMING/C_Program_09_13.c
@@ -1,35 +1,48 @@
 /*
   C LEVEL 09 Problem 13
-    Use only int variables
-    Use ont decimals
-    Use arrays
+            Use only int variables
+            Use ont decimals
+            Use arrays
   Question: 
-    Get multiple numbers from the user and 
-    store them in an array . 
-    Stop when the number is 0. 
-    If  numbers entered  if first and last numbers are equal, 
-    print "Success" else "Failure"
+            Get multiple numbers from the user and 
+            store them in an array . 
+            Stop when the number is 0. 
+            If  numbers entered  if first and last numbers are equal, 
+            print "Success" else "Failure"
 */
 #include<stdio.h>
+
 void getnumbers(int a[]);
-int input[], number, total;
+int equal(int f, int l);
+int total;
+
 int main()
 {
-    getnumbers(input);
-    if(input[0]==input[total-1])
+    int numbers[50];
+    getnumbers(numbers);
+    int first = numbers[0];
+    int last = numbers[total-1];
+    if(equal(first,last))
             printf("Success");
     else
             printf("Failure");
 }
+int equal(int f, int l)
+{
+    if(f==l)
+            return 1;
+    return 0;
+}
 void getnumbers(int a[])
 {
+    int number;
     while(1)
     {
-        printf("Enter %d Element: ",total+1);
+        printf("Enter %d Number: ",total+1);
         scanf("%d",&number);
         if(!number)
-            break;
-        input[total] = number;
+                break;
+        a[total] = number;
         total++;
     }
 }
