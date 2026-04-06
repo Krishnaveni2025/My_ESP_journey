41ac18f0c42894a29b67f5b4d6658e4adebf2639
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-06 18:43:19 +0530

    Update C_Program_10_03.c

--- C_PROGRAMMING/C_Program_10_03.c
+++ C_PROGRAMMING/C_Program_10_03.c
@@ -1,7 +1,7 @@
 /* C LEVEL 10 Problem 03
-        Don't use structures
+                Don't use structures
    Question: 
-        Get a String and print the same
+                Get a String and print the same
 */
 #include<stdio.h>
 void getstrprintstr(char *strng);
