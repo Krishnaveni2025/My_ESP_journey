32221076fd59549a8bcea30dc3a7deae8847c8d3
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-07 17:47:14 +0530

    Update C_Program_10_12.c

--- C_PROGRAMMING/C_Program_10_12.c
+++ C_PROGRAMMING/C_Program_10_12.c
@@ -1,10 +1,11 @@
-/* C LEVEL 10 Problem 12
-        Don't use structures
-   Question: 
-        Convert an integer of upto 50 digits to an character array using "printf("%s,...");"
-   Example: 
-        Array-1 4 5 8 7 6 3
-        Answer:1458763
+/* 
+        C LEVEL 10 Problem 12
+                        Don't use structures
+        Question: 
+                        Convert an integer of upto 50 digits to an character array using "printf("%s,...");"
+        Example: 
+                        Array-1 4 5 8 7 6 3
+                        Answer:1458763
 */
 #include<stdio.h>
 int integer[50],i;
