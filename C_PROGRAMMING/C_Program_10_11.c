a3256d51acb768365de8467c01385ee7407c848e
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-07 17:45:38 +0530

    Update C_Program_10_11.c

--- C_PROGRAMMING/C_Program_10_11.c
+++ C_PROGRAMMING/C_Program_10_11.c
@@ -1,7 +1,8 @@
-/* C LEVEL 10 Problem 11
-        Don't use structures
-   Question: 
-        Get a number string upto 50 digits and convert it to an integer array
+/* 
+    C LEVEL 10 Problem 11
+                Don't use structures
+    Question: 
+                Get a number string upto 50 digits and convert it to an integer array
 */
 #include<stdio.h>
 char input[51];
