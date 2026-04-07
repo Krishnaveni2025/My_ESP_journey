c1d7ebdab2d098b97728d1ecd07f152e79524006
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-07 17:48:28 +0530

    Update C_Program_11_01.c

--- C_PROGRAMMING/C_Program_11_01.c
+++ C_PROGRAMMING/C_Program_11_01.c
@@ -1,7 +1,7 @@
 /* C LEVEL 11 Problem 01
-        Use Pointers
+            Use Pointers
    Question: 
-        Get a number from the user and print the same. 
+            Get a number from the user and print the same. 
             Change the the value without directly 
             changing it and print the same
 */
