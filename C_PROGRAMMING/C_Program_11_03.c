9a539e0c8bf140a9ec0c09f97b37bb2659c1f8da
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-09 21:31:39 +0530

    Update C_Program_11_03.c

--- C_PROGRAMMING/C_Program_11_03.c
+++ C_PROGRAMMING/C_Program_11_03.c
@@ -1,16 +1,17 @@
-/* C LEVEL 11 Problem 03
-            Use Pointers
+/* 
+  C LEVEL 11 Problem 03
+                Use Pointers
   Question: 
-            Write a loop program to print 1 to 5 one by one.
-            Write a function and print the result in the main function
+                Write a loop program to print 1 to 5 one by one.
+                Write a function and print the result in the main function
   Function Name: 
-            arrange_ascend
+                arrange_ascend
   Answer:
-            1
-            2
-            3
-            4
-            5
+                1
+                2
+                3
+                4
+                5
 */
 #include<stdio.h>
 int arrange_ascend(int *p);
