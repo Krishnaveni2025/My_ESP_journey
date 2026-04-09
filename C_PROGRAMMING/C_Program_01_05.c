cf69fe0984ccaf52987255473b66876aa64033bd
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-09 19:31:13 +0530

    Update C_Program_01_05.c

--- C_PROGRAMMING/C_Program_01_05.c
+++ C_PROGRAMMING/C_Program_01_05.c
@@ -1,23 +1,27 @@
-/* C LEVEL 01 Problem 05
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
-Question: 
-    Get a number from the user and 
-    the number by 8 and  
-    print the remainder
-Example: 
-    Input: 45, Output: 5
-    Input: 143, Output: 7
+/* 
+ C LEVEL 01 Problem 05
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
+ Question: 
+            Get a number from the user and 
+            the divide number by 8 and  
+            print the remainder
+ Example: 
+           Input: 45, Output: 5
+           Input: 143, Output: 7
 */
 #include<stdio.h>
+void getDivby8PrintRem(void);
 int main()
+{
+    getDivby8PrintRem();
+}
+void getDivby8PrintRem(void)
 {
   int number;
   printf("Input: ");
-  scanf("%d",&number);
-  printf("Output: %d",number%8);
-  return 0;
+  scanf("%d",&number);    
 }
