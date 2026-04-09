edd854cd18f7ed14a4fc8c8eef78a45816babe58
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-09 19:27:57 +0530

    Update C_Program_01_04.c

--- C_PROGRAMMING/C_Program_01_04.c
+++ C_PROGRAMMING/C_Program_01_04.c
@@ -1,23 +1,28 @@
-/* C LEVEL 01 Problem 04
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
-Question: 
-    Get a number from the user and 
-    the number by 6 and  
-    print the result
-Example: 
-    Input: 45, Output: 7
-    Input: 143, Output: 23
+/* 
+    C LEVEL 01 Problem 04
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
+   Question: 
+            Get a number from the user and 
+            the number by 6 and  
+            print the result
+   Example: 
+            Input: 45, Output: 7
+            Input: 143, Output: 23
 */
 #include<stdio.h>
+void getnumDivby6(void);
 int main()
 {
-  int number;
-  printf("Input: ");
-  scanf("%d",&number);
-  printf("Output: %d",number/6);
-  return 0;
+    getnumDivby6();
+}
+void getnumDivby6(void)
+{
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    printf("Output: %d",number/6);
 }
