4bb5f0994ad545087b5c73e903740ade6855bf7b
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-09 19:25:03 +0530

    Update C_Program_01_03.c

--- C_PROGRAMMING/C_Program_01_03.c
+++ C_PROGRAMMING/C_Program_01_03.c
@@ -1,23 +1,28 @@
-/* C LEVEL 01 Problem 03
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
-Question: 
-    Get a number from the user and  
-    multiply 3 to that number and  
-    print the result
-Example: 
-    Input: 45, Output: 135
-    Input: 1200, Output: 3600
+/* 
+    C LEVEL 01 Problem 03
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
+   Question: 
+            Get a number from the user and  
+            multiply 3 to that number and  
+            print the result
+   Example: 
+            Input: 45, Output: 135
+            Input: 1200, Output: 3600
 */
 #include<stdio.h>
+void getnumMul3(void);
 int main()
+{
+    getnumMul3();
+}
+void getnumMul3(void)
 {
   int number;
   printf("Input: ");
   scanf("%d",&number);
-  printf("Output: %d",number*3);
-  return 0;
+  printf("Output: %d",number*3);   
 }
