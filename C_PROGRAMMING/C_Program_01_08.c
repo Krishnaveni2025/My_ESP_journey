f27d3eaf5d969aee2808b2bb850472acb5b3280f
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-09 20:46:38 +0530

    Update C_Program_01_08.c

--- C_PROGRAMMING/C_Program_01_08.c
+++ C_PROGRAMMING/C_Program_01_08.c
@@ -1,22 +1,27 @@
-/*  C LEVEL 01 Program 08
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
-    Question: 
-        Write a program to get a three digit number from the user and 
-        print the one's digit
-    Example: 
-        Input:456 Output:6
-        Input:569 Output:9
+/*  
+ C LEVEL 01 Program 08
+          Use only int variables
+          Use only Decimals
+          Use the following operations only
+          +,-,/,*,%
+          Do not use any other operations
+ Question: 
+          Write a program to get a three digit number from the user and 
+          print the one's digit
+ Example: 
+          Input:456 Output:6
+          Input:569 Output:9
   */
 #include<stdio.h>
+void get3d_print1sd(void);
 int main()
+{
+    get3d_print1sd();
+}
+void get3d_print1sd(void)
 {
   int number;
   printf("Enter a three digit number: ");
   scanf("%d",&number);
-  printf("Output: %d",number%10);
-  return 0;
+  printf("Output: %d",number%10);    
 }
