c40f93e3d7e2b5e428961067436d9036f4a0cc9d
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 21:58:17 +0530

    Update C_Program_11_07.c

--- C_PROGRAMMING/C_Program_11_07.c
+++ C_PROGRAMMING/C_Program_11_07.c
@@ -6,48 +6,58 @@
    Function Name: 
                   memcomp(src,dst,size)
 */
+void print(int *a, int s);
+int memcomp(int *source, int *destination, int length);
+void getint(int *set1, int *set2, int s);
+
 #include<stdio.h>
-int memcomp(int *src, int *dst, int size);
-void getinteger(int*a, int size);
-void printinteger(int *a, int size);
 int main()
 {
-    int size;
-    printf("Enter Size: ");
+    int size,equal;
+    printf("Enter size of the set: ");
     scanf("%d",&size);
-    int source[size], destination[size];
-    getinteger(source, size);
-    getinteger(destination, size);
-    printinteger(source, size);
-    printinteger(destination, size);
-    if(memcomp(source, destination, size))
+    int src[size],dst[size];
+    getint(src,dst,size);
+    equal = memcomp(src, dst,size);
+    print(src,size);
+    print(dst,size);
+    if(equal)
             printf("\nsuccess");
     else
             printf("\nfailure");
+    return 0;
+    
 }
-void printinteger(int *a, int size)
+void getint(int *set1, int *set2, int s)
 {
-    printf("\n");
-    for(int i =0; i<size; i++)
+    int i = 0, j = 0;
+    printf("First Set\n");
+    for( i = 0; i<s; i++)
     {
-        printf("%d ",a[i]);
+        printf("Enter %d Number: ",i+1);
+        scanf("%d",&set1[i]);
+    }
+    printf("Second Set\n");
+    for(j = 0; j<s; j++)
+    {
+        printf("Enter %d Number: ",j+1);
+        scanf("%d",&set2[j]);
     }
 }
-void getinteger(int*a, int size)
+int memcomp(int *source, int *destination, int length)
 {
-    printf("\n");
-    for(int i = 0; i<size; i++)
+    for(int i = 0; i<length; i++)
     {
-        printf("Enter %d Element: ",i+1);
-        scanf("%d",&a[i]);
-    }
-}
-int memcomp(int *src, int *dst, int size)
-{
-    for(int i = 0; i<size; i++)
-    {
-        if(src[i] != dst[i])
+        if(*(source+i)!= *(destination+i))
                 return 0;
     }
     return 1;
+}
+void print(int *a, int s)
+{
+    printf("\n");
+    for(int i = 0; i<s; i++)
+    {
+        printf("%d ",a[i]);
+    }
 }
