b3b417f291a1031bcc8932df22217cdc206ca377
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:28:23 +0530

    Update C_Program_01_11.c

--- C_PROGRAMMING/C_Program_01_11.c
+++ C_PROGRAMMING/C_Program_01_11.c
@@ -1,23 +1,27 @@
-/* C LEVEL 01 Program 11
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+    C LEVEL 01 Program 11
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Write a program to get a two digit number from the user and 
-        print it's sum of digits
+            Write a program to get a two digit number from the user and 
+            print it's sum of digits
    Example:
-        Input:56 Output:11
-        Input:69 output:15
+            Input:56 Output:11
+            Input:69 output:15
 */
 #include<stdio.h>
+void get2d_printsumd(void);
 int main()
 {
-  int number;
-  //your code here
-  printf("Enter a two - digit number: ");
-  scanf("%d",&number);
-  printf("Result = %d",((number%10)+(number/10)));
-  return 0;
+    get2d_printsumd();
+}
+void get2d_printsumd(void)
+{
+    int twodigitNumber;
+    printf("Enter Two Digit Number: ");
+    scanf("%d",&twodigitNumber);
+    printf("Sum: %d",((twodigitNumber%10)+(twodigitNumber/10)));
 }
