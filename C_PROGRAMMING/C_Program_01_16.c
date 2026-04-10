ef2bf9069daf7d2280f639fb3fe2c9097d82ff42
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:42:56 +0530

    Update C_Program_01_16.c

--- C_PROGRAMMING/C_Program_01_16.c
+++ C_PROGRAMMING/C_Program_01_16.c
@@ -1,24 +1,29 @@
-/* C LEVEL 01 Program 16
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+    C LEVEL 01 Program 16
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Get a four digit number from the user and 
-        only reverse last two digits of the number (100's and 1000's)
-        then print the number
+            Get a four digit number from the user and 
+            only reverse last two digits of the number (100's and 1000's)
+            then print the number
    Example:
-        Input:9561 Output:5961
-        Input:3859 Output:8359
+            Input:9561 Output:5961
+            Input:3859 Output:8359
 */
 #include<stdio.h>
+void get4d_rev100s1000s(void);
 int main()
+{
+    get4d_rev100s1000s();
+}
+void get4d_rev100s1000s(void)
 {
   int number;
   //your code here
   printf("Enter a four digit number: ");
   scanf("%d",&number);
-  printf("%d",((((number/100)%10)*1000)+((number/1000)*100)+(number%100)));
-  return  0;
+  printf("%d",((((number/100)%10)*1000)+((number/1000)*100)+(number%100)));    
 }
