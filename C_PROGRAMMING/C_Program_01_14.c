125b067c20eedad44324dc2da99472eb6226bed9
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:37:18 +0530

    Update C_Program_01_14.c

--- C_PROGRAMMING/C_Program_01_14.c
+++ C_PROGRAMMING/C_Program_01_14.c
@@ -1,23 +1,28 @@
-/* C LEVEL 01 Program 14
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+    C LEVEL 01 Program 14
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Write a program to get a number from the user and 
-        print the reverse of the number.
+            Write a program to get a number from the user and 
+            print the reverse of the number.
    Example:
-        Input:561 Output:165
-        Input:859 Output:958
+            Input:561 Output:165
+            Input:859 Output:958
 */
 #include<stdio.h>
+void get3d_printrev(void);
 int main()
+{
+    get3d_printrev();
+}
+void get3d_printrev(void)
 {
   int number;
   //your code here
   printf("Enter a three-digit number: ");
   scanf("%d",&number);
-  printf("Result = %d", (((number%10)*100)+(((number/10)%10)*10)+(number/100)));
-  return 0;
+  printf("Result = %d", (((number%10)*100)+(((number/10)%10)*10)+(number/100)));    
 }
