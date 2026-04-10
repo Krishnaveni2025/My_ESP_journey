fc8621c1c4cb0dd5e91f1c3e350fae8030639113
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 12:38:21 +0530

    Update C_Program_01_09.c

--- C_PROGRAMMING/C_Program_01_09.c
+++ C_PROGRAMMING/C_Program_01_09.c
@@ -1,23 +1,27 @@
-/* C LEVEL 01 Program 09
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+  C LEVEL 01 Program 09
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
   Question: 
-        Write a program to get a three digit number from the user and 
-        print the hundred's digit of the number.
+            Write a program to get a three digit number from the user and 
+            print the hundred's digit of the number.
   Example:
-        Input:456 Output:4
-        Input:569 Output:5
+            Input:456 Output:4
+            Input:569 Output:5
   */
-#include<stdio.h>
-int main()
-{
-  int number;
-  //your code here
-  printf("Enter a three digit number: ");
-  scanf("%d",&number);
-  printf("Output: %d",number/100);
-  return 0;
-}
+  #include<stdio.h>
+  void get3d_print100s(void);
+  int main()
+  {
+      get3d_print100s();
+  }
+  void get3d_print100s(void)
+  {
+      int threedigitNumber;
+      printf("Enter Three Digit Number: ");
+      scanf("%d",&threedigitNumber);
+      printf("%d",threedigitNumber/100);
+  }
