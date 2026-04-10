da3c4b4feb2683825076cbc0e02f7f9fb527d0ea
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:47:22 +0530

    Update C_Program_01_18.c

--- C_PROGRAMMING/C_Program_01_18.c
+++ C_PROGRAMMING/C_Program_01_18.c
@@ -1,22 +1,28 @@
-/* C LEVEL 01 Program 18
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+  C LEVEL 01 Program 18
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
   Question: 
-        Get a two - digit number from the user, 
-        make the ten's digit 1 and print the number
+            Get a two - digit number from the user, 
+            make the ten's digit 1 and 
+            print the number
   Example:
-        Input:95 Output:15
-        Input:82 Output:12
+            Input:95 Output:15
+            Input:82 Output:12
 */
 #include<stdio.h>
+void get2d_make10s1(void);
 int main()
+{
+    get2d_make10s1();
+}
+void get2d_make10s1(void)
 {
   int number;
   printf("Enter a two digit number: ");
   scanf("%d",&number);
   printf("Result = %d",10+(number%10));
-  return 0;
 }
