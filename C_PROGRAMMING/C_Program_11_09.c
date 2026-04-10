2a52670791be5b88a4da126a7b12f5261b8a83cf
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 22:37:54 +0530

    Update C_Program_11_09.c

--- C_PROGRAMMING/C_Program_11_09.c
+++ C_PROGRAMMING/C_Program_11_09.c
@@ -7,62 +7,57 @@
                  intcon(src1, size1, src2, size2, dst)
 */
 #include<stdio.h>
-void intcon(int *src1, int size1,int *src2,int size2, int *dst);
-void getarray(int *src, int s);
-void printarray(int *src, int s);
-int size1, size2, size;
 
+void intcon(int *source1, int size1, int *source2, int size2, int *destination);
+void getint(int *set1, int s1, int *set2, int s2);
+void print(int *a, int s);
 int main()
 {
-    printf("Enter Size of first array\n");
+    int size1, size2,size;
+    printf("Enter size of the first array: ");
     scanf("%d",&size1);
-    
-    printf("Enter Size of second array\n");
+    printf("Enter size of the second array: ");
     scanf("%d",&size2);
-    
-    int source1[size1], source2[size2];
-    
-    
-    printf("First Integer\n");
-    getarray(source1, size1);
-    printf("Second Integer\n");
-    getarray(source2, size2);
-    
-    printf("First Integer\n");
-    printarray(source1, size1);
-    printf("\nSecond Integer\n");
-    printarray(source2, size2);
-    
     size = size1+size2;
-    int destination[size];
-    intcon(source1, size1, source2, size2,destination);
-    
-    printf("\nSingle Integer\n");
-    printarray(destination, size);
+    int src1[size1], src2[size2],dst[size];
+    getint(src1,size1,src2,size2);
+    print(src1,size1);
+    print(src2,size2);
+    intcon(src1, size1, src2, size2, dst);
+    print(dst,size);
 }
-void intcon(int *src1, int s1, int *src2, int s2, int *dst)
+void print(int *a, int s)
 {
-    for(int i = 0;i<s1; i++)
-    {
-        *(dst+i) = *(src1+i);
-    }
-    for(int j = 0; j<s2; j++)
-    {
-        *(dst+s1+j) = *(src2+j); 
-    }
-}
-void printarray(int *src, int s)
-{
+    printf("\n");
     for(int i = 0; i<s; i++)
     {
-        printf("%d ",src[i]);
+        printf("%d ",a[i]);
     }
 }
-void getarray(int *src, int size)
+void getint(int *set1, int s1, int *set2, int s2)
 {
-    for(int i = 0; i<size; i++)
+    printf("First Set\n");
+    for(int i = 0; i<s1; i++)
     {
-        printf("Enter %d Element: ",i+1);
-        scanf("%d",&src[i]);
+        printf("Enter %d Number: ",i+1);
+        scanf("%d",&set1[i]);
+    }
+    printf("Second Set\n");
+    for(int j = 0; j<s2; j++)
+    {
+        printf("Enter %d Number: ",j+1);
+        scanf("%d",&set2[j]);
+    }
+}
+void intcon(int *source1, int size1, int *source2, int size2, int *destination)
+{
+    int i = 0, j = 0;
+    for(i = 0; i<size1; i++)
+    {
+        *(destination+i) = *(source1+i);
+    }
+    for(j = 0; j<size2; j++)
+    {
+       *(destination+i+j) = *(source2+j); 
     }
 }
