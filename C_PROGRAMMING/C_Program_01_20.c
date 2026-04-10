81efb2acd320b613ca0898e7613ff654416e7698
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:52:44 +0530

    Update C_Program_01_20.c

--- C_PROGRAMMING/C_Program_01_20.c
+++ C_PROGRAMMING/C_Program_01_20.c
@@ -1,22 +1,27 @@
-/* C LEVEL 01 Program 20
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+   C LEVEL 01 Program 20
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Get a three digit number from the user, 
-        make the ten's digit 0, then print it
+            Get a three digit number from the user, 
+            make the ten's digit 0, then print it
    Example:
-        Input:695 Output:605
-        Input:182 Output:102
+            Input:695 Output:605
+            Input:182 Output:102
 */
 #include<stdio.h>
+void get3d_make10s0(void);
 int main()
+{
+    get3d_make10s0();
+}
+void get3d_make10s0(void)
 {
   int number;
   printf("Enter a three digit number: ");
   scanf("%d",&number);
   printf("Result = %d",(((number/100)*100)+(number%10)));
-  return 0;
 }
