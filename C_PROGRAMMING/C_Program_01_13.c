601f9da79365da1624c374fb1c8d4c2c0002dcf6
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:35:05 +0530

    Update C_Program_01_13.c

--- C_PROGRAMMING/C_Program_01_13.c
+++ C_PROGRAMMING/C_Program_01_13.c
@@ -1,23 +1,28 @@
-/* C LEVEL 01 Program 13
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+    C LEVEL 01 Program 13
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Write a program to get a two-digit number from the user and 
-        print the reverse of the number
+            Write a program to get a two-digit number from the user and 
+            print the reverse of the number
    Example:
-        Input:56 Output:65
-        Input:59 Output:95
+            Input:56 Output:65
+            Input:59 Output:95
 */
 #include<stdio.h>
+void get2d_printrev(void);
 int main()
+{
+    get2d_printrev();
+}
+void get2d_printrev(void)
 {
   int number;
   //your code here
   printf("Enter a three digit number: ");
   scanf("%d",&number);
   printf("Result = %d",(((number%10)*10)+(number/10)));
-  return 0;
 }
