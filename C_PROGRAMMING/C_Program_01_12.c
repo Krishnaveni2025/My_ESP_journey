b35b04f4a0ac11d1ad038428286b9414030cd9b6
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:33:06 +0530

    Update C_Program_01_12.c

--- C_PROGRAMMING/C_Program_01_12.c
+++ C_PROGRAMMING/C_Program_01_12.c
@@ -1,23 +1,27 @@
-/* C LEVEL 01 Program 12
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+    C LEVEL 01 Program 12
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Write a program to get a three digit number from the user and 
-        print it's sum of digits
+            Write a program to get a three digit number from the user and 
+            print it's sum of digits
    Example: 
-        Input:562 Output:13
-        Input:469 Output:19
+            Input:562 Output:13
+            Input:469 Output:19
 */
 #include<stdio.h>
+void get3d_printsum(void);
 int main()
 {
-  int number;
-  //your code here
-  printf("Enter a three digit number: ");
-  scanf("%d",&number);
-  printf("Result = %d",((number%10)+(number/100)+((number/10)%10)));
-  return 0;
+    get3d_printsum();
+}
+void get3d_printsum(void)
+{
+    int threedNumber;
+    printf("Enter Three Digit Number: ");
+    scanf("%d",&threedNumber);
+    printf("Sum : %d",(threedNumber/100)+((threedNumber%100)/10)+(threedNumber%10));
 }
