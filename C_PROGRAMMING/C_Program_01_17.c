6962cb8dbac7a1ac06b2abb6619ca08f663afab0
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:45:09 +0530

    Update C_Program_01_17.c

--- C_PROGRAMMING/C_Program_01_17.c
+++ C_PROGRAMMING/C_Program_01_17.c
@@ -1,22 +1,27 @@
-/* C LEVEL 01 Program 17
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+   C LEVEL 01 Program 17
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Get a two digit number from the user, 
-        make the one's digit 0 then print it.
+            Get a two digit number from the user, 
+            make the one's digit 0 then print it.
    Example:
-        Input:95 Output:90
-        Input:18 Output:10
+            Input:95 Output:90
+            Input:18 Output:10
 */
 #include<stdio.h>
+void get2d_make1s0(void);
 int main()
+{
+    get2d_make1s0();
+}
+void get2d_make1s0(void)
 {
   int number;
   printf("Enter a two digit number: ");
   scanf("%d",&number);
   printf("Result = %d",(number/10)*10);
-  return 0;
 }
