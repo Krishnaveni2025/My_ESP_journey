bb287b473ab8f26bb1dce40026f4532196f6bdbe
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-10 13:49:23 +0530

    Update C_Program_01_19.c

--- C_PROGRAMMING/C_Program_01_19.c
+++ C_PROGRAMMING/C_Program_01_19.c
@@ -1,22 +1,28 @@
-/* C LEVEL 01 Program 19
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+   C LEVEL 01 Program 19
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Get a three digit number from the user, 
-        make the one's digit 2 and then print it
+            Get a three digit number from the user, 
+            make the one's digit 2 and 
+            then print it
    Example:
-        Input:695 Output:692
-        Input:181 Output:182
+            Input:695 Output:692
+            Input:181 Output:182
 */
 #include<stdio.h>
+void get3d_make1s2(void);
 int main()
+{
+    get3d_make1s2();
+}
+void get3d_make1s2(void)
 {
   int number;
   printf("Enter a three digit number: ");
   scanf("%d",&number);
   printf("Result = %d",((number/10)*10)+2);
-  return 0;
 }
