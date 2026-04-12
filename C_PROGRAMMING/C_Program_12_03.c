58446aa79237b431162832aa9e35f783277ec8af
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-12 23:36:36 +0530

    Update C_Program_12_03.c

--- C_PROGRAMMING/C_Program_12_03.c
+++ C_PROGRAMMING/C_Program_12_03.c
@@ -1,56 +1,57 @@
-/* C LEVEL 12 - Problem 03
-        Use Pointers
+/* 
+   C LEVEL 12 - Problem 03
+            Use Pointers
    Question: 
-        Get a check string and substring. 
-        check the substring in the main string and print the position.
+            Get a check string and substring. 
+            check the substring in the main string and print the position.
    Example:
    string: helloetalvislearning
    sub-string: etal
    Answer: 6
 */
 #include<stdio.h>
-char fullString[50], substring[5];
-void getStrSubstr(char *str, char *sstr);
-void print_position(char *str, char *sstr);
+void get_str_sstr(char *str, char *sstr);
+void print_position(char *str, char *sstr);;
 int getlength(char *str);
 int main()
 {
-    getStrSubstr(fullString, substring);
-    print_position(fullString, substring);
+    char s[50], subs[10];
+    get_str_sstr(s, subs);
+    print_position(s, subs);
 }
 void print_position(char *str, char *sstr)
 {
-    int sl, ssl, found;
+    int sl, sbl, found = 0;
     sl = getlength(str);
-    ssl = getlength(sstr);
+    sbl = getlength(sstr);
     for(int i = 0; i<sl; i++)
     {
         found = 1;
-        for(int j = 0; j<ssl; j++)
+        for(int j = 0; j<sbl; j++)
         {
-            if(str[i+j]!=sstr[j])
-                {
-                    found = 0;
-                    break;
-                }
+            if(*(sstr+j)!= *(str+i+j))
+            {
+                found = 0;
+                break;
+            }
         }
         if(found)
-                printf("%d ", i+1);
+                printf("%d ",i+1);
     }
 }
 int getlength(char *str)
 {
-    int l=0;
-    while(str[l]!='\0')
+    int l = 0;
+    while(*(str+l)!='\0')
     {
         l++;
     }
     return l;
 }
-void getStrSubstr(char *str, char *sstr)
+void get_str_sstr(char *str, char *sstr)
 {
     printf("String: ");
     scanf("%s",str);
-    printf("sub-string: ");
+    printf("Sub-string: ");
     scanf("%s",sstr);
 }
