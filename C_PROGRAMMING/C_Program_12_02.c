82471379be81d5557c2f94855d195852cdbb92e0
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-12 22:56:02 +0530

    Update C_Program_12_02.c

--- C_PROGRAMMING/C_Program_12_02.c
+++ C_PROGRAMMING/C_Program_12_02.c
@@ -1,45 +1,48 @@
-/* C LEVEL 12 - Problem 02
-        Use Pointers
+/* 
+   C LEVEL 12 - Problem 02
+            Use Pointers
    Question: 
-        Get a string and a character from the user, 
-        find the all the positions where the character is present, and print it.
+            Get a string and a character from the user, 
+            find the all the positions where the character is present, and 
+            print it.
    Example:
    string: helloetalvislearning
    character: e
    Answer: 2,6,14
 */
 #include<stdio.h>
-char char_string[100], letter;
-void get_string_char(char * str, char *c);
-void print_position(char *str, char c);
+void get_str_char(char *str, char *l);
+void print_lposition(char *str, char *l);
 int main()
 {
-    get_string_char(char_string, &letter);
-    print_position(char_string, letter);
+    char input[51];
+    char letter;
+    get_str_char(input, &letter);
+    print_lposition(input, &letter);
 }
-void print_position(char *str, char c)
+void print_lposition(char *str, char *l)
 {
     int i = 0, found = 0, count = 0;
-    while(str[i]!='\0')
+    while(*(str+i)!='\0')
     {
-        if(str[i] == c)
-            {
-                if(found)
-                        printf(",");
-                found = 0;
-                printf("%d",i+1);
-                found = 1;
-                count++;
-            }
+        if(*(str+i)== *l)
+        {
+            if(found)
+                    printf(",");
+            found = 0;
+            printf("%d",i+1);
+            found = 1;
+            count++;
+        }
         i++;
     }
     if(!count)
-            printf("character not found");
+            printf("Character not found");
 }
-void get_string_char(char *str, char *c)
+void get_str_char(char *str, char *l)
 {
     printf("String: ");
     scanf("%s",str);
-    printf("character: ");
-    scanf(" %c",c);
+    printf("Character: ");
+    scanf(" %c",l);
 }
