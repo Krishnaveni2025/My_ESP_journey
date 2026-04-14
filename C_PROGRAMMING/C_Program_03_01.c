e1d43f6001e11d5651f95e19e235891d35bf0544
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-14 22:01:44 +0530

    Update C_Program_03_01.c

--- C_PROGRAMMING/C_Program_03_01.c
+++ C_PROGRAMMING/C_Program_03_01.c
@@ -1,14 +1,15 @@
-/* C LEVEL 03 Program 01
-      Use int variables
-      Use only Decimals
-      Use "if/else" statements 
+/* 
+   C LEVEL 03 Program 01
+            Use int variables
+            Use only Decimals
+            Use "if/else" statements 
    Question: 
-      Get a number from the user and 
-      check if the number is equals to 50. 
-      If yes then print "Success", otherwise print "Failure".
+            Get a number from the user and 
+            check if the number is equals to 50. 
+            If yes then print "Success", otherwise print "Failure".
    Example:
-      Input:50    Output: Success
-      Input:56789 Output: Failure
+            Input:50    Output: Success
+            Input:56789 Output: Failure
 */
 #include<stdio.h>
 int main()
