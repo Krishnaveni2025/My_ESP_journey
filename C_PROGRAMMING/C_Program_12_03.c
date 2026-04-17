329413881f9e8de1f35f3519081472909595a431
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-17 17:07:03 +0530

    Update C_Program_12_03.c

--- C_PROGRAMMING/C_Program_12_03.c
+++ C_PROGRAMMING/C_Program_12_03.c
@@ -9,49 +9,40 @@
    sub-string: etal
    Answer: 6
 */
-#include<stdio.h>
 void get_str_sstr(char *str, char *sstr);
-void print_position(char *str, char *sstr);;
-int getlength(char *str);
+void print_position(char *str, char *sstr);
+#include<stdio.h>
 int main()
 {
     char s[50], subs[10];
-    get_str_sstr(s, subs);
+    get_str_sstr(s,subs);
     print_position(s, subs);
 }
 void print_position(char *str, char *sstr)
 {
-    int sl, sbl, found = 0;
-    sl = getlength(str);
-    sbl = getlength(sstr);
-    for(int i = 0; i<sl; i++)
+    int i = 0, j = 0,found = 0;
+    while(*(str+i)!='\0')
     {
         found = 1;
-        for(int j = 0; j<sbl; j++)
-        {
-            if(*(sstr+j)!= *(str+i+j))
+        j = 0;
+       while(*(sstr+j)!='\0')
+       {
+           if(*(sstr+j)!=*(str+j+i))
             {
                 found = 0;
                 break;
             }
-        }
-        if(found)
-                printf("%d ",i+1);
+            j++;
+       }
+       if(found)
+            printf("%d ",i+1);
+       i++;
     }
-}
-int getlength(char *str)
-{
-    int l = 0;
-    while(*(str+l)!='\0')
-    {
-        l++;
-    }
-    return l;
 }
 void get_str_sstr(char *str, char *sstr)
 {
     printf("String: ");
     scanf("%s",str);
-    printf("Sub-string: ");
+    printf("sub-string: ");
     scanf("%s",sstr);
 }
