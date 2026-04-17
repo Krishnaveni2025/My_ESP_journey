52a6d3dc549cc81909a735df0d4a020046d682a8
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-17 21:32:41 +0530

    Update C_Program_12_05.c

--- C_PROGRAMMING/C_Program_12_05.c
+++ C_PROGRAMMING/C_Program_12_05.c
@@ -1,74 +1,98 @@
-/* C LEVEL 12 - Problem 05
+/* 
+   C LEVEL 12 - Problem 05
             Use Pointers
    Question: 
             Write a program to multiply upto 50 digit numbers.
-  #include<stdio.h>
-  int main()
-  {
-    getnumbers(number1, number2);
-    multiplynumbers(number1, number2, result);
-    print(result);
-  }
+            #include<stdio.h>
+            int main()
+            {
+                getnumbers(number1, number2);
+                multiplynumbers(number1, number2, result);
+                print(result);
+            }
 */
-#include<stdio.h>
 void getnumbers(char *n1, char *n2);
-void multiplynumbers(char *n1, char *n2, char *r);
 void print(char *str);
-int getlength(char *str);
+int getlen(char *str);
+void strcopy(char *src, char *dst);
+void removeleading0s(char *num);
+void multiplynumbers(char *n1, char *n2, char *r);
+void con_int_to_char_array(int *a,int s, char *str);
 #include<stdio.h>
 int main()
 {
-    char number1[51],number2[51],result[102];
-    getnumbers(number1,number2);
+    char number1[51], number2[51], result[102];
+    getnumbers(number1, number2);
     multiplynumbers(number1, number2, result);
     print(result);
 }
+void con_int_to_char_array(int *a,int s, char *str)
+{
+    int i;
+    for(i = 0; i<s; i++)
+    {
+        *(str+i) = a[i]+'0';
+    }
+    *(str+i) = '\0'; 
+}
 void multiplynumbers(char *n1, char *n2, char *r)
 {
-    int temp_res[101] = {0};
-    int l1,l2,i,j,d1,d2,product;
-    l1 = getlength(n1);
-    l2 = getlength(n2);
-    for(i = l1-1; i>=0; i--)
+    int l1, l2, d1, d2, product;
+    l1 = getlen(n1);
+    l2 = getlen(n2);
+    int temp[102];
+    for(int i = l1-1; i>=0; i--)
     {
-        for(j = l2-1; j>=0; j--)
+        for(int j = l2-1;j>=0; j--)
         {
-            d1 = *(n1+i)-'0';
-            d2 = *(n2+j)-'0';
-            product = (d1*d2)+temp_res[i+j+1];
-            temp_res[i+j+1] = product%10;
-            temp_res[i+j] += (product/10);
+            d1 = n1[i]-'0';
+            d2 = n2[j]-'0';
+            product = (d1*d2)+temp[i+j+1];
+            temp[i+j+1] = (product%10);
+            temp[i+j] += (product/10);
         }
     }
-    int k = 0, start = 0;
-    while((start<(l1+l2)) && (temp_res[start]==0)) start++;
-    if(start==(l1+l2)) r[k++] = '0';
-    else
-    {
-        while(start<(l1+l2))
-        {
-            r[k++] = temp_res[start++]+'0';
-        }
-    }
-    r[k] = '\0';
+    con_int_to_char_array(temp, l1+l2, r);
 }
-int getlength(char *str)
+void removeleading0s(char *num)
 {
-    int length = 0;
-    while(str[length]!='\0')
+    int l, i = 0;
+    l = getlen(num);
+    while(*(num+i)=='0'&&i<l-1)
     {
-        length++;
+            i++;
     }
-    return length;
+    if(i>0)
+    {
+        strcopy(&num[i],num);
+    }
 }
-void getnumbers(char *n1, char *n2)
+void strcopy(char *src, char *dst)
 {
-    printf("Enter First Number(Upto 50 digits)\n");
-    scanf("%s",n1);
-    printf("Enter Second Number(Upto 50 digits)\n");
-    scanf("%s",n2);
+    while(*(src)!='\0')
+    {
+        *(dst++) = *(src++);
+    }
+    *(dst) = '\0';
+}
+int getlen(char *str)
+{
+    int l = 0;
+    while(*(str+l)!='\0')
+    {
+        l++;
+    }
+    return l;
 }
 void print(char *str)
 {
+    removeleading0s(str);
     printf("%s",str);
+}
+void getnumbers(char *n1, char *n2)
+{
+    printf("First Number\n");
+    scanf("%s",n1);
+    printf("Second Number\n");
+    scanf("%s",n2);
 }
