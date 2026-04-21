b6bc9fccdf3a81d25ef4e3ec1d5fd8f8a1214ca8
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-21 23:27:34 +0530

    Update C_Program_13_01.c

--- C_PROGRAMMING/C_Program_13_01.c
+++ C_PROGRAMMING/C_Program_13_01.c
@@ -1,4 +1,4 @@
-/* C LEVEL - Problem 01
+/* C LEVEL 13 - Problem 01
 Create a linked list for the following structure
 and get user inputs id, math mark, science mark.
 once the user inputs the id as -1
