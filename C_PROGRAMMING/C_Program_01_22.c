8c9f14466a4caf6b65239a26c041034f4f8e879c
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 03:33:24 +0530

    Update C_Program_01_22.c

--- C_PROGRAMMING/C_Program_01_22.c
+++ C_PROGRAMMING/C_Program_01_22.c
@@ -1,30 +1,25 @@
-/* C LEVEL 01 Program 22
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +,-,/,*,%
-        Do not use any other operations
+/* 
+   C LEVEL 01 Program 22
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +,-,/,*,%
+            Do not use any other operations
    Question: 
-        Get a number from the user and 
-        subtract 5 from that number 
-        if the number's ten's digit position is odd 
-        then print the result. 
-        Do not use "if"
+            Get a number from the user and 
+            subtract 5 from that number 
+            if the number's ten's digit position is odd 
+            then print the result. 
+            Do not use "if"
    Example:
-        Input:685   Output:685
-        Input:89172 Output:89167
+            Input:685   Output:685
+            Input:89172 Output:89167
 */
 #include<stdio.h>
-void getn_sub5(void);
 int main()
 {
-    getn_sub5();
-}
-void getn_sub5(void)
-{
-  int number;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  printf("Result = %d", (number - 5*(((number/10)%10)%2)));   
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    printf("Output: %d",(number-(5*(((number/10)%10)%2))));
 }
