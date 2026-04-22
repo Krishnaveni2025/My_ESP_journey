aa2903a08eed0fa50d3983b3cff848edd38dbab6
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 03:54:11 +0530

    Update C_Program_01_25.c

--- C_PROGRAMMING/C_Program_01_25.c
+++ C_PROGRAMMING/C_Program_01_25.c
@@ -1,40 +1,44 @@
-/* C LEVEL 01 Program 25
+/* 
+   C LEVEL 01 Program 25
         Use only int variables
         Use only Decimals
         Use the following operations only
         +,-,/,*,%
         Do not use any other operations
    Question: 
-        Get a three digit number from the user. 
-        If the sum of the digits is less than 10, 
-            print the same, 
-        otherwise 
-            add the digits of sum. 
-        If the sum of the digits is less than 10 
-            then print the sum, 
-        otherwise 
-            add the digits of the sum, 
-        and print the sum.
-   Note: The result of the sum should always be single digit only
+            Get a three digit number from the user. 
+            If the sum of the digits is less than 10, 
+                print the same, 
+            otherwise 
+                add the digits of sum. 
+            If the sum of the digits is less than 10 
+                then print the sum, 
+            otherwise 
+                add the digits of the sum, 
+            and print the sum.
+            
+            Note: The result of the sum should always be single digit only
    Example:
-        Input:123 Output:6
-        Input:149 Output:5(14,5)
-        Input:991 Output:1(19;10;1)
+            Input:123 Output:6
+            Input:149 Output:5(14,5)
+            Input:991 Output:1(19;10;1)
 */
 #include<stdio.h>
-void get3d_printsum(void);
+int sumofdigits(int n);
 int main()
 {
-    get3d_printsum();
+    int three_dn;
+    printf("Input: ");
+    scanf("%d",&three_dn);
+    printf("Output: %d",sumofdigits(sumofdigits(sumofdigits(three_dn))));
 }
-void get3d_printsum(void)
+int sumofdigits(int n)
 {
-  int number;
-  printf("Enter a three digit number: ");
-  scanf("%d",&number);
-  //your code here
-  number = ((number/100)+(number%10)+((number/10)%10));
-  number = (number/10 + number%10);
-  number = (number/10 + number%10);
-  printf("%d",number);
+    int sum = 0;
+    while(n)
+    {
+        sum += (n%10);
+        n /= 10;
+    }
+    return sum;
 }
