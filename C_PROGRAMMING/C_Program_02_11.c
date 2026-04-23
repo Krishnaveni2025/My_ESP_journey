a21bdd225f90d65b306baa9da191de0fbb164ba1
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 16:20:48 +0530

    Update C_Program_02_11.c

--- C_PROGRAMMING/C_Program_02_11.c
+++ C_PROGRAMMING/C_Program_02_11.c
@@ -1,25 +1,39 @@
-/* C LEVEL 02 Program 11
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +, -, /, *, %, ==, !=, <, >, <=, >=
-        Do not use if, for, while, arrays, pointers, strings, etc.
+/* 
+   C LEVEL 02 Program 11
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +, -, /, *, %, ==, !=, <, >, <=, >=
+            Do not use if, for, while, arrays, pointers, strings, etc.
    Question: 
-        Get a two digit number from the user and 
-        check if the digit 1 is less than or equal to digit 0. 
-        If yes then print 1, Otherwise print 0.
+            Get a two digit number from the user and 
+            check if the digit 1 is less than or equal to digit 0. 
+            If yes then print 1, Otherwise print 0.
    Example:
-        Input: 66    Output: 1
-        Input: 67    Output: 1
-        Input: 98    Output: 0
+            Input: 66    Output: 1
+            Input: 67    Output: 1
+            Input: 98    Output: 0
   */
 #include<stdio.h>
+int digit0(int n);
+int digit1(int n);
+int isd1lessthanequaltod0(int n);
 int main()
 {
-  int number;
-  printf("Enter a two digit number: ");
-  scanf("%d",&number);
-  //your code here
-  printf("Result = %d",( (number/10) <= (number%10) ));
-  return 0;
+    int twodn;
+    printf("Input: ");
+    scanf("%d",&twodn);
+    printf("Output: %d", isd1lessthanequaltod0(twodn));
+}
+int isd1lessthanequaltod0(int n)
+{
+    return ( (digit1(n)<=(digit0(n))) );
+}
+int digit1(int n)
+{
+    return n/10;
+}
+int digit0(int n)
+{
+    return n%10;
 }
