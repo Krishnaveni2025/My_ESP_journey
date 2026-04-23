7844eb29d44c991616d32a55e986e5b85e1bf24d
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 23:52:21 +0530

    Update C_Program_03_14.c

--- C_PROGRAMMING/C_Program_03_14.c
+++ C_PROGRAMMING/C_Program_03_14.c
@@ -1,28 +1,29 @@
-/* C LEVEL 03 Program 14
-      Use int variables
-      Use only Decimals
-      Use "if/else" statements 
+/* 
+   C LEVEL 03 Program 14
+            Use int variables
+            Use only Decimals
+            Use "if/else" statements 
    Question: 
-      Get a four digit number from the user and 
-      check if the first two digits and last two digits are same. 
-      If yes then print "Success", otherwise "Failure".
+            Get a four digit number from the user and 
+            check if the first two digits and last two digits are same. 
+            If yes then print "Success", otherwise "Failure".
    Example:
-      Input:1919 Output:"Success"
-      Input:6567 Output:"Failure"
+            Input:1919 Output: Success
+            Input:6567 Output: Failure
 */
 #include<stdio.h>
+int is1st2dsequaltolast2ds(int n);
 int main()
 {
-  int number;
-  printf("Enter a three digit number: ");
-  scanf("%d",&number);
-  //your code here
-  if((number/100)==(number%100))
-  {
-    printf("\"Success\"");
-  }
-  else
-  {
-    printf("\"Failure\"");
-  }
+    int four_dn;
+    printf("Input: ");
+    scanf("%d",&four_dn);
+    if(is1st2dsequaltolast2ds(four_dn))
+            printf("Output: Success");
+    else
+            printf("Output: Failure");
+}
+int is1st2dsequaltolast2ds(int n)
+{
+    return ((n%100)==(n/100));
 }
