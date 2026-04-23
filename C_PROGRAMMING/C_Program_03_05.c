2edd79d82ca9ab67b72ac91a80c3d8b4d4080d80
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 21:22:50 +0530

    Update C_Program_03_05.c

--- C_PROGRAMMING/C_Program_03_05.c
+++ C_PROGRAMMING/C_Program_03_05.c
@@ -1,29 +1,30 @@
-/* C LEVEL 03 Program 05
-      Use int variables
-      Use only Decimals
-      Use "if/else" statements 
+/* 
+   C LEVEL 03 Program 05
+            Use int variables
+            Use only Decimals
+            Use "if/else" statements 
    Question: 
-      Get a number from the user and 
-      check if the number is less than or equal to 50. 
-      If yes then print "Success", otherwise print "Failure".
+            Get a number from the user and 
+            check if the number is less than or equal to 50. 
+            If yes then print "Success", otherwise print "Failure".
    Example:
-     Input:56789    Output: Failure
-     Input:45       Output: Success
+            Input:56789    Output: Failure
+            Input:45       Output: Success
 */
 #include<stdio.h>
+int islessthanequalto50(int n);
 int main()
 {
-  int number;
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  //your code here
-  if(number<=50)
-  {
-    printf("Success");
-  }
-  else
-  {
-    printf("Failure");
-  }
-  return 0;
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    if(islessthanequalto50(number))
+            printf("Output: Success");
+    else
+            printf("Output: Failure");
+}
+int islessthanequalto50(int n)
+{
+    if(n<=50)   return 1;
+    return 0;
 }
