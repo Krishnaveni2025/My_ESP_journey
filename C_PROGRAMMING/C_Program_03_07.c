ae64929e0319426275b050017c3ae2df9eba5be2
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 21:31:55 +0530

    Update C_Program_03_07.c

--- C_PROGRAMMING/C_Program_03_07.c
+++ C_PROGRAMMING/C_Program_03_07.c
@@ -1,28 +1,40 @@
-/* C LEVEL 03 Program 07
-      Use int variables
-      Use only Decimals
-      Use "if/else" statements 
+/* 
+   C LEVEL 03 Program 07
+            Use int variables
+            Use only Decimals
+            Use "if/else" statements 
    Question: 
-      Get a two digit number from the user and 
-      check if the digit 0 and digit 1 are identical. 
-      If yes then print "Success", otherwise "Failure".
+            Get a two digit number from the user and 
+            check if the digit 0 and digit 1 are identical. 
+            If yes then print "Success", otherwise "Failure".
    Example:
-      Input:55 Output:"Success"
-      Input:56 Output:"Failure"
+            Input:55 Output:"Success"
+            Input:56 Output:"Failure"
 */
 #include<stdio.h>
+int isd01same(int n);
+int digit0(int n);
+int digit1(int n);
 int main()
 {
-  int number;
-  printf("Enter a two digit number: ");
-  scanf("%d",&number);
-  //your code here
-  if(((number/10)==(number%10)))
-  {
-    printf("\"Success\"");
-  }
-  else
-  {
-    printf("\"Failure\"");
-  }
+    int two_dn;
+    printf("Input: ");
+    scanf("%d",&two_dn);
+    if(isd01same(two_dn))
+            printf("Output: Success");
+    else
+            printf("Output: Failure");
+}
+int isd01same(int n)
+{
+    if(digit1(n)==(digit0(n)))  return 1;
+    return 0;
+}
+int digit1(int n)
+{
+    return n/10;
+}
+int digit0(int n)
+{
+    return n%10;
 }
