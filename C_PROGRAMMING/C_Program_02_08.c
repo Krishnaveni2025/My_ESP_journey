a82e8e8046cf1f9e0ce2cda05971d440f8be3f3c
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 15:59:11 +0530

    Update C_Program_02_08.c

--- C_PROGRAMMING/C_Program_02_08.c
+++ C_PROGRAMMING/C_Program_02_08.c
@@ -1,25 +1,39 @@
-/* C LEVEL 02 Program 08
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +, -, /, *, %, ==, !=, <, >, <=, >=
-        Do not use if, for, while, arrays, pointers, strings, etc.
-   Question: 
-        Get a two digit number from the user and 
-        check if the digit 1 and digit 0 are not identical . 
-        If yes then print 1, 
-        Otherwise print 0.
-   Example:
-        Input: 55    Output: 0
-        Input: 56    Output: 1
+/* 
+    C LEVEL 02 Program 08
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +, -, /, *, %, ==, !=, <, >, <=, >=
+            Do not use if, for, while, arrays, pointers, strings, etc.
+    Question: 
+            Get a two digit number from the user and 
+            check if the digit 1 and digit 0 are not identical . 
+            If yes then print 1, 
+            Otherwise print 0.
+    Example:
+            Input: 55    Output: 0
+            Input: 56    Output: 1
   */
 #include<stdio.h>
+int is1snotequalto10s(int n);
+int ones(int n);
+int tens(int n);
 int main()
 {
-  int number;
-  printf("Enter a two digit number: ");
-  scanf("%d",&number);
-  //your code here
-  printf("Result = %d",((number%10)!=(number/10)));
-  return 0;
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    printf("Output: %d",is1snotequalto10s(number));
+}
+int tens(int n)
+{
+    return n/10;
+}
+int ones(int n)
+{
+    return n%10;
+}
+int is1snotequalto10s(int n)
+{
+    return (ones(n)!=(tens(n)));
 }
