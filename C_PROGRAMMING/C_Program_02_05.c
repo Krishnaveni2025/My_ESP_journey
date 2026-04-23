e45056346d2aab11d977a74bb515d481a3a25c25
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 15:43:54 +0530

    Update C_Program_02_05.c

--- C_PROGRAMMING/C_Program_02_05.c
+++ C_PROGRAMMING/C_Program_02_05.c
@@ -1,25 +1,34 @@
-/* C LEVEL 02 Program 05
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +, -, /, *, %, ==, !=, <, >, <=, >=
-        Do not use if, for, while, arrays, pointers, strings, etc.
-   Question: 
-        Get a number from the user and 
-        check if the number is less than or equal to 50. 
-        If yes then print 1, 
-        Otherwise print 0.
-   Example:
-        Input: 45    Output: 1
-        Input: 56789 Output: 0
+/* 
+    C LEVEL 02 Program 05
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +, -, /, *, %, ==, !=, <, >, <=, >=
+            Do not use if, for, while, arrays, pointers, strings, etc.
+    Question: 
+            Get a number from the user and 
+            check if the number is less than or equal to 50. 
+            If yes then print 1, 
+            Otherwise print 0.
+    Example:
+            Input: 45    Output: 1
+            Input: 56789 Output: 0
   */
 #include<stdio.h>
+int islessthan50(int n);
+int is50(int n);
 int main()
 {
-  int number;
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  //your code here
-  printf("Result = %d",number<=50);
-  return 0;
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    printf("Output: %d",(is50(number)+islessthan50(number)));
+}
+int is50(int n)
+{
+    return n==50;
+}
+int islessthan50(int n)
+{
+    return n<50;
 }
