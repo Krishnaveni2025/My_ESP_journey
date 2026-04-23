1b9176fd142325558895f141c43c386c1ae6bac4
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 21:12:27 +0530

    Update C_Program_03_02.c

--- C_PROGRAMMING/C_Program_03_02.c
+++ C_PROGRAMMING/C_Program_03_02.c
@@ -1,29 +1,31 @@
-/* C LEVEL 03 Program 02
-      Use int variables
-      Use only Decimals
-      Use "if/else" statements 
+/* 
+   C LEVEL 03 Program 02
+            Use int variables
+            Use only Decimals
+            Use "if/else" statements 
    Question: 
-      Get a number from the user and 
-      check if the number is not equals to 50. 
-      If yes then print "Success", otherwise print "Failure".
+            Get a number from the user and 
+            check if the number is not equals to 50. 
+            If yes then print "Success", otherwise print "Failure".
    Example:
-      Input:50    Output: Failure
-      Input:56789 Output: Success
+            Input:50    Output: Failure
+            Input:56789 Output: Success
 */
 #include<stdio.h>
+int not50(int n);
 int main()
 {
-  int number;
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  //your code here
-  if(number!=50)
-  {
-    printf("Success");
-  }
-  else
-  {
-    printf("Failure");
-  }
-  return 0;
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    if(not50(number))
+            printf("Output: Success");
+    else
+            printf("Output: Failure");
+}
+int not50(int n)
+{
+    if(n!=50)
+            return 1;
+    return 0;
 }
