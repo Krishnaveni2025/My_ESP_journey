43f01c7f244365c00a040f000c13efef90ad2fdc
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 21:19:56 +0530

    Update C_Program_03_04.c

--- C_PROGRAMMING/C_Program_03_04.c
+++ C_PROGRAMMING/C_Program_03_04.c
@@ -1,29 +1,30 @@
-/* C LEVEL 03 Program 04
-      Use int variables
-      Use only Decimals
-      Use "if/else" statements 
+/* 
+   C LEVEL 03 Program 04
+            Use int variables
+            Use only Decimals
+            Use "if/else" statements 
    Question: 
-      Get a number from the user and 
-      check if the number is greater than 50. 
-      If yes then print "Success", otherwise print "Failure".
+            Get a number from the user and 
+            check if the number is greater than 50. 
+            If yes then print "Success", otherwise print "Failure".
    Example:
-      Input:45          Output: Failure
-      Input:56789       Output: Success
+            Input:45          Output: Failure
+            Input:56789       Output: Success
 */
 #include<stdio.h>
+int isgreaterthan50(int n);
 int main()
 {
-  int number;
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  //your code here
-  if(number>50)
-  {
-    printf("Success");
-  }
-  else
-  {
-    printf("Failure");
-  }
-  return 0;
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    if(isgreaterthan50(number))
+                printf("Output: Success");
+    else
+                printf("Output: Failure");
+}
+int isgreaterthan50(int n)
+{
+    if(n>50) return 1;
+    return 0;
 }
