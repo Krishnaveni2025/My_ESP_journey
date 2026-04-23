5442ab66735e1544d9a39cebbeaa9e2f32247a28
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 15:31:10 +0530

    Update C_Program_02_02.c

--- C_PROGRAMMING/C_Program_02_02.c
+++ C_PROGRAMMING/C_Program_02_02.c
@@ -1,25 +1,29 @@
-/* C LEVEL 02 Program 02
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +, -, /, *, %, ==, !=, <, >, <=, >=
-        Do not use if, for, while, arrays, pointers, strings, etc.
+/* 
+    C LEVEL 02 Program 02
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +, -, /, *, %, ==, !=, <, >, <=, >=
+            Do not use if, for, while, arrays, pointers, strings, etc.
    Question: 
-        Get a number from the user and 
-        check if the number is not equals to 50. 
-        If yes print 1, 
-        otherwise print 0.
+            Get a number from the user and 
+            check if the number is not equals to 50. 
+            If yes print 1, 
+            otherwise print 0.
    Example:
-        Input:50    Output:0
-        Input:56789 Output:1
+            Input:50    Output:0
+            Input:56789 Output:1
   */
-#include<stdio.h>
-int main()
-{
-  int number;
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  //your code here
-  printf("Result = %d",number!=50);
-  return 0;
-}
+ #include<stdio.h>
+ int not50(int n);
+ int main()
+ {
+     int number;
+     printf("Input: ");
+     scanf("%d",&number);
+     printf("Output: %d",not50(number));
+ }
+ int not50(int n)
+ {
+     return n!=50;
+ }
