2fbdc3c795a1c323748691f1034b6276967cb963
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-23 16:38:37 +0530

    Update C_Program_02_14.c

--- C_PROGRAMMING/C_Program_02_14.c
+++ C_PROGRAMMING/C_Program_02_14.c
@@ -1,24 +1,28 @@
-/* C LEVEL 02 Program 14
-        Use only int variables
-        Use only Decimals
-        Use the following operations only
-        +, -, /, *, %, ==, !=, <, >, <=, >=
-        Do not use if, for, while, arrays, pointers, strings, etc.
-  Question: 
-        Get a four digit number from the user and 
-        check if first two digits and last two digits are same. 
-        If yes then print 1, Otherwise print 0.
-  Example:
-        Input: 1919    Output: 1
-        Input: 6567    Output: 0
+/* 
+   C LEVEL 02 Program 14
+            Use only int variables
+            Use only Decimals
+            Use the following operations only
+            +, -, /, *, %, ==, !=, <, >, <=, >=
+            Do not use if, for, while, arrays, pointers, strings, etc.
+   Question: 
+            Get a four digit number from the user and 
+            check if first two digits and last two digits are same. 
+            If yes then print 1, Otherwise print 0.
+   Example:
+            Input: 1919    Output: 1
+            Input: 6567    Output: 0
   */
 #include<stdio.h>
+int is1st2dsequaltolast2ds(int n);
 int main()
 {
-  int number;
-  printf("Enter a four digit number: ");
-  scanf("%d",&number);
-  //your code here
-  printf("Result = %d",((number%100) == (number/100)));
-  return 0;
+    int four_dn;
+    printf("Input: ");
+    scanf("%d",&four_dn);
+    printf("Output: %d",is1st2dsequaltolast2ds(four_dn));
+}
+int is1st2dsequaltolast2ds(int n)
+{
+    return ((n%100)==(n/100));
 }
