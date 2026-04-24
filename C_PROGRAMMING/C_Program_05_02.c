d9f4988e33aca5cd3f8f96de73a6c2de3eba5f15
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 22:24:12 +0530

    Refactor loop to print numbers from 5 to 1

--- C_PROGRAMMING/C_Program_05_02.c
+++ C_PROGRAMMING/C_Program_05_02.c
@@ -1,33 +1,35 @@
-/* C LEVEL 05 Problem 02
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 02
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a loop program to print 5 to 1 on one by one.
+            Write a loop program to print 5 to 1 on one by one.
    Answer: 
-        5
-        4
-        3
-        2
-        1
+            5
+            4
+            3
+            2
+            1
   */
 #include<stdio.h>
+void print_5to1(void);
 int main()
 {
-  int number;
-  //your code here
-  number = 5;
-  for(;;)
+    print_5to1();
+}
+void print_5to1(void)
+{
+    int n = 5;
+    for(; ;)
     {
-      //your code here
-      loop:
-      if(number>0)
-      {
-        printf("%d\n",number);
-        number--;
-        goto loop;
-      }
-      break;
+        if(n>=1)
+        {
+            printf("%d\n",n);
+            n--;
+        }
+        else
+            break;
     }
 }
