57d667a9857dee89afdbedbac82af40f9cf61c61
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:58:30 +0530

    Refactor reverse number program with function

--- C_PROGRAMMING/C_Program_04_13.c
+++ C_PROGRAMMING/C_Program_04_13.c
@@ -1,29 +1,34 @@
-/* C LEVEL 04 Program 13
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+   C LEVEL 04 Program 13
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a program to get a number from the user and print the reverse of the number
+            Write a program to get a number from the user and print the reverse of the number
    Answer:
-        Input:123456   Output:654321
-        Input:76895439 Output:93459867
-        Input:675      Output:576
+            Input:123456   Output:654321
+            Input:76895439 Output:93459867
+            Input:675      Output:576
   */
 #include<stdio.h>
+int reverse(int n);
 int main()
 {
-  int number;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  int reverse = 0;
-  loop:if(number)
-  {
-    reverse = (reverse*10) + (number%10);
-    number /= 10;
-     goto loop;
-  }
-   printf("Result = %d",reverse);
-  return 0;
+    int number;
+    printf("Number: ");
+    scanf("%d",&number);
+    printf("%d",reverse(number));
+}
+int reverse(int n)
+{
+    int rev = 0;
+    loop:
+        if(n)
+        {
+            rev = (rev*10)+(n%10);
+            n /= 10;
+            goto loop;
+        }
+    return rev;
 }
