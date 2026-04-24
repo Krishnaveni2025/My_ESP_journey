d11ba0d22983ecffb94b062ce34293f435a05963
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:17:35 +0530

    Refactor to use print_odd_1to9 function

--- C_PROGRAMMING/C_Program_04_05.c
+++ C_PROGRAMMING/C_Program_04_05.c
@@ -1,32 +1,35 @@
-/* C LEVEL 04 Program 05
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+   C LEVEL 04 Program 05
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a loop program to print odd number between 1 and 9
+            Write a loop program to print odd number between 1 and 9
    Answer:
-        1
-        3
-        5
-        7
-        9
-        
+            1
+            3
+            5
+            7
+            9
   */
 #include<stdio.h>
+void print_odd_1to9(void);
 int main()
 {
-  int number;
-  //your code here
-  number = 1;
-  loop:if(number<10)
-  {
-    if(number%2){
-    printf("%d\n",number);
-  }
-    number++;
-    goto loop;
-  }
-  return 0;
+    print_odd_1to9();
 }
-
+void print_odd_1to9(void)
+{
+    int n = 1;
+    loop:
+        if(n<10)
+        {
+            if(n%2)
+            {
+                printf("%d\n",n);
+            }
+            n++;
+            goto loop;
+        }
+}
