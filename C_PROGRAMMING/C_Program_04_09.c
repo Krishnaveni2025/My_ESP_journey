c2dc41c7b79753ee0b519bf0d0889c068401039c
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:33:01 +0530

    Refactor program to use a separate function for logic

--- C_PROGRAMMING/C_Program_04_09.c
+++ C_PROGRAMMING/C_Program_04_09.c
@@ -1,32 +1,37 @@
-/* C LEVEL 04 Program 09
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+   C LEVEL 04 Program 09
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a loop program to print two digit even numbers whose sum of digits are 6
+            Write a loop program to print two digit even numbers whose sum of digits are 6
    Answer:
-        24
-        42
-        60
+            24
+            42
+            60
   */
 #include<stdio.h>
+void print_2deven_sofd6(void);
 int main()
 {
-  int number;
-  //your code here
-  number = 11;
-  loop:if(number<100)
-  {
-    if(!(number%2)){
-    if((number%10)+(number/10)==6)
-    {
-      printf("%d\n",number);
-    }
-  }
-    number++;
-    goto loop;
-  }
-  return 0;
+    print_2deven_sofd6();
 }
-
+void print_2deven_sofd6(void)
+{
+    int n = 11, sum;
+    loop:
+        if(n<61)
+        {
+            if(!(n%2))
+            {
+                sum = (n%10)+(n/10);
+                if(sum==6)
+                {
+                    printf("%d\n",n);
+                }
+            }
+            n++;
+            goto loop;
+        }
+}
