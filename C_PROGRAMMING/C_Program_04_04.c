4d0972a2f4b638ea504fd75fc577b02cab3cde7c
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:13:07 +0530

    Refactor sum calculation into separate function

--- C_PROGRAMMING/C_Program_04_04.c
+++ C_PROGRAMMING/C_Program_04_04.c
@@ -1,27 +1,30 @@
-/* C LEVEL 04 Program 04
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+   C LEVEL 04 Program 04
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a loop program to print sum of 6 to 1
+            Write a loop program to print sum of 6 to 1
    Answer:
-        21
+            21
   */
 #include<stdio.h>
+void print_sum_6to1(int n);
 int main()
 {
-  int number,sum;
-  //your code here
-  number = 6;
-  sum = 0;
-  loop:if(number>=0)
-  {
-    sum += number;
-    number--;
-    goto loop;
-  }
-  printf("Sum = %d",sum);
-  return 0;
+    int number = 6;
+    print_sum_6to1(number);
 }
-
+void print_sum_6to1(int n)
+{
+    int sum = 0;
+    loop:
+        if(n>0)
+        {
+            sum += n;
+            n--;
+            goto loop;
+        }
+    printf("%d",sum);
+}
