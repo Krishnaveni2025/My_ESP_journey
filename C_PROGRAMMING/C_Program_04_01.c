8f280035d9a1acf3260f845e69894be7b9d11621
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:03:25 +0530

    Refactor C program to improve formatting and clarity

--- C_PROGRAMMING/C_Program_04_01.c
+++ C_PROGRAMMING/C_Program_04_01.c
@@ -1,28 +1,29 @@
-/* C LEVEL 04 Program 01
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+   C LEVEL 04 Program 01
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a loop program to print 1 to 5 on one by one
+            Write a loop program to print 1 to 5 on one by one
    Answer:
-        1
-        2
-        3
-        4
-        5
+            1
+            2
+            3
+            4
+            5
   */
-#include<stdio.h>
-int main()
-{
-  int number;
-  //your code here
-  number = 1;
-  loop:if(number<6)
+  #include<stdio.h>
+  int main()
   {
-    printf("%d\n",number);
-    number++;
-    goto loop;
+      int number = 1;
+      loop:
+            if(number<6)
+            {
+                printf("%d\n",number);
+                number++;
+                goto loop;
+            }
+        
+      return 0;
   }
-  return 0;
-}
