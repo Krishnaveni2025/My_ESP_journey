8083dbb06bdb6dc8db494e2e81270dcb9a46cde7
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:55:09 +0530

    Refactor digit sum program with a separate function

--- C_PROGRAMMING/C_Program_04_12.c
+++ C_PROGRAMMING/C_Program_04_12.c
@@ -1,29 +1,34 @@
-/* C LEVEL 04 Program 12
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+   C LEVEL 04 Program 12
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a program to get a number from the user and print the sum of all digits 
+            Write a program to get a number from the user and print the sum of all digits 
    Answer:
-        Input:123456   Output:21
-        Input:76895439 Output:51
-        Input:675      Output:18
+            Input:123456   Output:21
+            Input:76895439 Output:51
+            Input:675      Output:18
   */
 #include<stdio.h>
+int sumofdigits(int n);
 int main()
 {
-  int number;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  int sum = 0;
-  loop:if(number)
-  {
-    sum += (number%10);
-    number /= 10;
-     goto loop;
-  }
-   printf("Result = %d",sum);
-  return 0;
+    int number;
+    printf("Number: ");
+    scanf("%d",&number);
+    printf("%d",sumofdigits(number));
+}
+int sumofdigits(int n)
+{
+    int sum = 0;
+    loop:
+        if(n)
+        {
+            sum += (n%10);
+            n /= 10;
+            goto loop;
+        }
+    return sum;
 }
