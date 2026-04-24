35056374ef16267c145548518d4ece913a9940d5
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:38:27 +0530

    Refactor C program to separate logic into function
    Refactor program to use a separate function for summing two-digit odd numbers with ten's digit 7.

--- C_PROGRAMMING/C_Program_04_10.c
+++ C_PROGRAMMING/C_Program_04_10.c
@@ -1,33 +1,35 @@
-/* C LEVEL 04 Program 10
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+   C LEVEL 04 Program 10
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a loop program to print sum of two digit odd numbers whose ten's digit is 7
+            Write a loop program to print sum of two digit odd numbers whose ten's digit is 7
    Answer:
-        375
+            375
   */
 #include<stdio.h>
+void print_sof2doddn_10s7(void);
 int main()
 {
-  int number;
-  //your code here
-  number = 11;
-  int sum = 0;
-  loop:if(number<100)
-  {
-    if(number%2){
-    if((number/10)==7)
-    {
-      sum += number;
-    }
-  }
-    number++;
-    goto loop;
-  }
-  printf("Sum = %d",sum);
-  return 0;
+    print_sof2doddn_10s7();
 }
-
-
+void print_sof2doddn_10s7(void)
+{
+    int n = 71, sum = 0;
+    loop:
+        if(n<80)
+        {
+            if(n%2)
+            {
+                if((n/10)==7)
+                {
+                    sum += n;
+                }
+            }
+            n++;
+            goto loop;
+        }
+    printf("%d",sum);
+}
