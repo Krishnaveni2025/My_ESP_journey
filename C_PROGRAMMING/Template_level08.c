bbc1b07b5377098aba73d08e7f872a553ab3f63b
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 20:57:09 +0530

    Create Template_level08.c with function template
    Add a template for a C program with a function placeholder.

---
+++ C_PROGRAMMING/Template_level08.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int function_name(int);
+int main()
+{
+  int x, y;
+  scanf("%d",&x);
+  y = function_name(x);
+  printf("%d",y);
+}
+int function_name(int y)
+{
+   int a;
+   //your code here
+   return (y);
+}
