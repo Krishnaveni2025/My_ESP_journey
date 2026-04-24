805a5998cf1ffb92843d8a3b90f0bcc2cc921fdd
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:49:40 +0530

    Update C_Program_04_11.c

--- C_PROGRAMMING/C_Program_04_11.c
+++ C_PROGRAMMING/C_Program_04_11.c
@@ -1,32 +1,35 @@
-/* C LEVEL 04 Program 11
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+  C LEVEL 04 Program 11
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a program to get a number from the user and 
-        print the total number of digits in that number 
+            Write a program to get a number from the user and 
+            print the total number of digits in that number 
    Answer:
-        Input:123456   Output:6
-        Input:76895439 Output:8
-        Input:675      Output:3
+            Input:123456   Output:6
+            Input:76895439 Output:8
+            Input:675      Output:3
   */
 #include<stdio.h>
+void print_total_digits(int n);
 int main()
 {
-  int number;
-  //your code here
-  printf("Enter a number: ");
-  scanf("%d",&number);
-  int digits = 0;
-  loop:if(number)
-  {     
-    digits++;
-    number /= 10;
-     goto loop;
-  }
-   printf("Result = %d",digits);
-  return 0;
+    int number;
+    printf("Number: ");
+    scanf("%d",&number);
+    print_total_digits(number);
 }
-
-
+void print_total_digits(int n)
+{
+    int c = 0;
+    loop:
+        if(n)
+        {
+            c++;
+            n /= 10;
+            goto loop;
+        }
+    printf("%d",c);
+}
