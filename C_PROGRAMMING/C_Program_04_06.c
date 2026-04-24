3c46a7d20e98badd2a9c31c197f99f4f141833e7
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-24 16:21:31 +0530

    Extract odd number printing to print_2dodd_below20 function
    Refactor code to move odd number printing logic to a separate function.

--- C_PROGRAMMING/C_Program_04_06.c
+++ C_PROGRAMMING/C_Program_04_06.c
@@ -1,29 +1,36 @@
-/* C LEVEL 04 Program 06
-        Use only int variables
-        Use only Decimals
-        Use only "if/else", goto statements. Don't use  "for/while"
-        Don't use arrays, pointers, strings, etc.
+/* 
+   C LEVEL 04 Program 06
+            Use only int variables
+            Use only Decimals
+            Use only "if/else", goto statements. Don't use  "for/while"
+            Don't use arrays, pointers, strings, etc.
    Question: 
-        Write a loop program to print two digit odd numbers below 20
-        11
-        13
-        15
-        17
-        19
+            Write a loop program to print two digit odd numbers below 20
+   Answer:  
+            11
+            13
+            15
+            17
+            19
   */
 #include<stdio.h>
+void print_2dodd_below20(void);
 int main()
 {
-  int number;
-  //your code here
-  number = 11;
-  loop:if(number<20)
-  {
-    if(number%2){
-    printf("%d\n",number);
-  }
-    number++;
-    goto loop;
-  }
-  return 0;
+    print_2dodd_below20();
+}
+void print_2dodd_below20(void)
+{
+    int n;
+    n = 11;
+    loop:
+        if(n<20)
+        {
+            if(n%2)
+            {
+                printf("%d\n",n);
+            }
+            n++;
+            goto loop;
+        }
 }
