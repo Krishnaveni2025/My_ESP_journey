5abd4c04c5edb1a09591147015439177a28100e1
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 03:27:31 +0530

    Refactor C program to count single digit primes

--- C_PROGRAMMING/C_Program_05_25.c
+++ C_PROGRAMMING/C_Program_05_25.c
@@ -1,75 +1,60 @@
-/* C LEVEL 05 Problem 25
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 25
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a program to get a number from the user and 
-        print the total number of single digit prime numbers in the number
+            Write a program to get a number from the user and 
+            print the total number of single digit prime numbers in the number
    Answer:
-        Input: 163496481 Output: 1
-        Input: 364925    Output: 3
-  */
+            Input: 163496481 Output: 1
+            Input: 364925    Output: 3
+*/
 #include<stdio.h>
-
+int singlePrime_count(int n);
+int prime(int n);
 int main()
 {
-    int number, sprime_count = 0, divider, is_prime, digit;
-    
-    printf("Enter Number: ");
-    scanf("%d", &number);
-
-    while(number > 0)
-    {
-        loop:
-            // 1. Extract the specific digit we want to check
-            digit = number % 10;
-            divider = 2;
-            is_prime = 1;
-
-            // 2. Prime check logic for the single digit
-            if(digit < 2) 
-            {
-                is_prime = 0; // 0 and 1 are not prime
-            }
-            else 
-            {
-                pcheck:
-                    if(divider < digit)
-                    {
-                        if(digit % divider == 0)
-                        {
-                            is_prime = 0;
-                            // We found a factor, no need to check more for this digit
-                        }
-                        else 
-                        {
-                            divider++;
-                            goto pcheck;
-                        }
-                    }
-            }
-
-            // 3. Increment count if the digit was prime
-            if(is_prime)
-            {
-                sprime_count++;
-            }
-
-            // 4. Move to the next digit
-            number /= 10;
-            
-            // If there are more digits, go back to the top
-            if(number > 0)
-            {
-                goto loop;
-            }
-            else
-            {
-                break; 
-            }
-    }
-
-    printf("Output: %d", sprime_count);
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    printf("Output: %d",singlePrime_count(number));
     return 0;
+}
+int singlePrime_count(int n)
+{
+    int count = 0;
+    for(;;)
+    {
+        if(n)
+        {
+            if(prime(n%10))
+                        count++;
+            n /= 10;
+        }
+        else    
+                break;
+    }
+    return count;
+}
+int prime(int n)
+{
+    int i = 2,is_prime = 1;
+    if(n<=1)    return 0;
+    for(;;)
+    {
+        if((i*i)<=n)
+        {
+            if(!(n%i))
+            {
+                is_prime = 0;
+                break;
+            }
+            i++;
+        }
+        else
+                break;
+    }
+    return is_prime;
 }
