735b9bdd9e64d2d8a34ea4ed6077fae9f359b07d
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-25 18:28:34 +0530

    Refactor C program to sum two-digit odd numbers

--- C_PROGRAMMING/C_Program_05_10.c
+++ C_PROGRAMMING/C_Program_05_10.c
@@ -1,35 +1,36 @@
-/* C LEVEL 05 Problem 10
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 10
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a loop program to print sum of two digit odd numbers, who's ten's digit is 7.
+            Write a loop program to print sum of two digit odd numbers, who's ten's digit is 7.
    Answer:
-        375
-  */
+            375
+*/
 #include<stdio.h>
+void print_sof2odn_10s7(void);
 int main()
 {
-  int number;
-  //your code here
-  number  = 11;
-  int sum = 0;
-  for(;;)
+    print_sof2odn_10s7();
+}
+void print_sof2odn_10s7(void)
+{
+    int n = 71, sum = 0;
+    for(;;)
     {
-      //your code here
-      loop:
-      if(number<100)
-      {
-        if(number%2)
+        if(n<80)
         {
-          if((number/10)==7)
-            sum = sum + number;
+            if(n%2)
+            {
+                if((n/10)==7)
+                            sum += n;
+            }
+            n++;
         }
-        number++;
-        goto loop;
-      }
-      break;
+        else
+                break;
     }
-  printf("%d",sum);
+    printf("%d",sum);
 }
