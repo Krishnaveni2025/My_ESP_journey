87c7f96de2217f28b0567aafefd72e8daff45c83
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-25 17:43:58 +0530

    Refactor sum calculation to use for loop
    Refactor the program to use a for loop instead of goto for summing numbers.

--- C_PROGRAMMING/C_Program_05_03.c
+++ C_PROGRAMMING/C_Program_05_03.c
@@ -1,31 +1,32 @@
-/* C LEVEL 05 Problem 03
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 03
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a loop program to print sum of 1 to 5.
+            Write a loop program to print sum of 1 to 5.
    Answer:
-        15
+            15
   */
 #include<stdio.h>
+void print_sum1to5(void);
 int main()
 {
-  int number;
-  //your code here
-  number  = 1;
-  int sum = 0;
-  for(;;)
+    print_sum1to5();
+}
+void print_sum1to5(void)
+{
+    int n = 1, sum = 0;
+    for(;;)
     {
-      //your code here
-      loop:
-      if(number<6)
-      {
-        sum = sum + number;
-        number++;
-        goto loop;
-      }
-      break;
+        if(n<6)
+        {
+            sum += n;
+            n++;
+        }
+        else
+                break;
     }
-  printf("Sum = %d",sum);
+    printf("%d",sum);
 }
