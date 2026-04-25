93f44d6daeabfa386d1c1524973a5c2d955381b4
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-25 18:59:18 +0530

    Refactor C program to swap first and last digits

--- C_PROGRAMMING/C_Program_05_14.c
+++ C_PROGRAMMING/C_Program_05_14.c
@@ -1,39 +1,40 @@
-/* C LEVEL 05 Problem 14
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 14
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a program to get a number from the user and interchange the first and last digits and print the result .
+            Write a program to get a number from the user and interchange the first and last digits and print the result .
    Answer:
-        Input:123456   Output:623451
-        Input:76895439 Output:96895437
-        Input:675      Output:576
-  */
+            Input:123456   Output:623451
+            Input:76895439 Output:96895437
+            Input:675      Output:576
+*/
 #include<stdio.h>
+int swap_1st_last(int n);
 int main()
 {
-  int number;
-  //your code here
-  printf("Enter a Number: ");
-  scanf("%d",&number);
-  int middle, first, last, multiplier = 1, result = 0,temp; 
-  temp = number;
-  for(;;)
+    int number;
+    printf("Number: ");
+    scanf("%d",&number);
+    printf("%d",swap_1st_last(number));
+}
+int swap_1st_last(int n)
+{
+    int last, temp, middle, multiplier = 1;
+    temp = n;
+    for(;;)
     {
-      //your code here
-      loop:
-      if(temp>10)
-      {
-        temp = temp/10;
-        multiplier *= 10;
-        goto loop;
-      }
-      break;
+        if(temp>9)
+        {
+            multiplier *= 10;
+            temp /= 10;
+        }
+        else 
+                break;
     }
     last = temp;
-    middle = (number%multiplier)/10;
-    first = (number%10)*multiplier;
-    result = first+ (middle*10) + last;
-    printf("%d",result);
+    middle = (n%multiplier)/10;
+    return (((n%10)*multiplier)+(middle*10)+last);
 }
