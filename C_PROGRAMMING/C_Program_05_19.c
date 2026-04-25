c69a688abca929e44f8ad96eea09cbb8edc23384
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-25 21:07:48 +0530

    Refactor prime checking for middle digits of number
    Refactor the program to check if the middle two digits of a four-digit number are prime. Updated input/output prompts and removed unnecessary variables.

--- C_PROGRAMMING/C_Program_05_19.c
+++ C_PROGRAMMING/C_Program_05_19.c
@@ -1,40 +1,48 @@
-/* C LEVEL 05 Problem 19
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 19
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a program to get a four digit number from the user and 
-        print whether the middle two digits(10's & 100's) of that number is prime or not.
-  Answer:
-Input: 6359 - Output: Not Prime
-Input: 3537 - Output: Prime
+            Write a program to get a four digit number from the user and 
+            print whether the middle two digits(10's & 100's) of that number is prime or not.
+   Answer:
+            Input: 6359 - Output: Not Prime
+            Input: 3537 - Output: Prime
 */
 #include<stdio.h>
+int prime(int n);
+int middle(int n);
 int main()
 {
-    int number, is_prime = 1, i = 2;
-    printf("Enter 4 - digit Number: ");
-    scanf("%d",&number);
-    number = (number / 10)%100;
+    int four_dn;
+    printf("Input: ");
+    scanf("%d",&four_dn);
+    if(prime(middle(four_dn)))
+            printf("Output: Prime");
+    else
+            printf("Output: Not Prime");
+}
+int middle(int n)
+{
+    return ((n/10)%100);
+}
+int prime(int n)
+{
+    int is_prime =1, i = 2;
     for(;;)
     {
-        loop:
-            if(i<number)
+        if((i*i)<=n)
+        {
+            if(!(n%i))
             {
-                if(!(number%i))
-                {
-                    is_prime = 0;
-                    break;
-                }
-                i++;
-                goto loop;
+                is_prime = 0;
             }
-            break;
+            i++;
+        }
+        else
+                break;
     }
-    if(is_prime)
-            printf("Prime");
-    else
-            printf("Not Prime");
-  return 0;
+    return is_prime;
 }
