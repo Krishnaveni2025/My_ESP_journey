322138ff6a98776163cae16a452c13f0b1b22c0e
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 03:58:29 +0530

    Refactor HCF program to remove goto and improve clarity

--- C_PROGRAMMING/C_Program_05_30.c
+++ C_PROGRAMMING/C_Program_05_30.c
@@ -1,35 +1,38 @@
-/* C LEVEL 05 Problem 30
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
-   Question: Write a program to get two numbers from the user and print the HCF of those numbers
+/* 
+   C LEVEL 05 Problem 30
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
+   Question: 
+            Write a program to get two numbers from the user and print the HCF of those numbers
    Answer:
-        Input: 20 40  Output: 20
-        Input: 15 78  Output: 3
-  */
+            Input: 20 40  Output: 20
+            Input: 15 78  Output: 3
+*/
 #include<stdio.h>
+int hcf(int n1, int n2);
 int main()
 {
-    int number1, number2, hcf;
-    printf("Enter Two Numbers: ");
+    int number1,number2;
+    printf("Input: ");
     scanf("%d %d",&number1, &number2);
+    printf("Output: %d",hcf(number1, number2));
+}
+int hcf(int n1, int n2)
+{
+    int h;
     for(;;)
     {
-        loop:
-        if(number2!=number1)
+        if(n1!=n2)
         {
-            if(number1>number2)
-            {
-                number1 -= number2;
-            }
+            if(n1>n2)
+                    n1 -= n2;
             else
-            {
-                number2 -= number1;
-            }
-            goto loop;
+                    n2 -= n1;
         }
-        break;
+        else
+                break;
     }
-    printf("Output: %d",number1);
+    return n1;
 }
