64b93761431f97f4f02b6c022f69a78772291553
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-25 18:08:43 +0530

    Refactor C program to print two-digit odd numbers

--- C_PROGRAMMING/C_Program_05_06.c
+++ C_PROGRAMMING/C_Program_05_06.c
@@ -1,36 +1,38 @@
-/* C LEVEL 05 Problem 06
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 06
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a loop program to print two digit odd numbers, below 20.
+            Write a loop program to print two digit odd numbers, below 20.
    Answer: 
-        11
-        13
-        15
-        17
-        19
+            11
+            13
+            15
+            17
+            19
   */
 #include<stdio.h>
+void print_odd_below20(void);
 int main()
 {
-  int number;
-  //your code here
-  number  = 11;
-  for(;;)
+    print_odd_below20();
+}
+void print_odd_below20(void)
+{
+    int n = 11;
+    for(; ;)
     {
-      //your code here
-      loop:
-      if(number<20)
-      {
-        if(number%2)
+        if(n<20)
         {
-         printf("%d\n",number);
+            if(n%2)
+            {
+                printf("%d\n",n);
+            }
+            n++;
         }
-        number++;
-        goto loop;
-      }
-      break;
-    }  
+        else
+                break;
+    }
 }
