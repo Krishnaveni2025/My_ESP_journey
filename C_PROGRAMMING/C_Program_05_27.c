4130158f9f3cca5b3d4999642fd7401ff967984f
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 03:42:35 +0530

    Refactor C program to count numbers with digit sum 14

--- C_PROGRAMMING/C_Program_05_27.c
+++ C_PROGRAMMING/C_Program_05_27.c
@@ -1,37 +1,51 @@
-/* C LEVEL 05 Problem 27
-         Use only int variables
-         Use only Decimals
-         Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 27
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to print the total count of numbers 
-         which are less than 100000 and whose sum of digits is 14
+            Write a program to print the total count of numbers 
+            which are less than 100000 and whose sum of digits is 14
    Answer:
-         2710
-  */
+            2710
+*/
 #include<stdio.h>
+void result(void);
+int isSum14(int n);
 int main()
 {
-    int number = 11, count = 0, sum, temp;
+    result();
+}
+int isSum14(int n)
+{
+    int sum = 0;
     for(;;)
     {
-        loop:
-            if(number<100000)
-            {
-                temp = number, sum = 0;
-                sloop:
-                    if(temp)
-                    {
-                        sum += temp%10;
-                        temp /= 10;
-                        goto sloop;
-                    }
-                if(sum==14)
-                        count++;
-                number++;
-                goto loop;
-            }
-        break;
+        if(n)
+        {
+            sum += (n%10);
+            n /= 10;
+        }
+        else
+                break;
     }
-    printf("Output: %d",count);
+    if(sum==14) return 1;
+    return 0;
+}
+void result(void)
+{
+    int n = 100000, count = 0;
+    for(;;)
+    {
+        if(n>58)
+        {
+            if(isSum14(n))
+                    count++;
+            n--;
+        }
+        else
+                break;
+    }
+    printf("%d",count);
 }
