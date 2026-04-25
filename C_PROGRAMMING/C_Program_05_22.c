058bad4a41821fee2335e08a67ef25f66e0c81b3
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 02:56:41 +0530

    Refactor C program to use function for odd count

--- C_PROGRAMMING/C_Program_05_22.c
+++ C_PROGRAMMING/C_Program_05_22.c
@@ -1,34 +1,39 @@
-/* C LEVEL 05 Problem 22
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 22
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a program to get a number from the user and 
-        print the total number of two digits numbers which are odd in that number
+            Write a program to get a number from the user and 
+            print the total number of two digits numbers which are odd in that number
    Answer:
-        Input: 12345678 Output: 3
-        Input: 987531   Output: 4
-  */
+            Input: 12345678 Output: 3
+            Input: 987531   Output: 4
+*/
 #include<stdio.h>
+int twodigit_odd_count(int n);
 int main()
 {
-  int number;
-  printf("Enter Number: ");
-  scanf("%d",&number);
-  int odd_count = 0;
-  for(;;)
-  {
-      loop:
-            if(number>10)
-            {
-                if((number%100)%2)
-                        odd_count++;
-                number/=10;
-                goto loop;
-            }
-        break;
-  }
-  printf("Ouput: %d",odd_count);
-  return 0;
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    printf("Output: %d",twodigit_odd_count(number));
+    return 0;
+}
+int twodigit_odd_count(int n)
+{
+    int count = 0;
+    for(;;)
+    {
+        if(n>10)
+        {
+            if((n%100)%2)
+                    count++;
+            n /= 10;
+        }
+        else
+                break;
+    }
+    return count;
 }
