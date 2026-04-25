e643fd7b0db604b062bc8fd11156cb6424b06c8b
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-25 18:18:03 +0530

    Refactor C program for printing two-digit even numbers
    Refactor code to improve readability and structure.

--- C_PROGRAMMING/C_Program_05_08.c
+++ C_PROGRAMMING/C_Program_05_08.c
@@ -1,35 +1,38 @@
-/* C LEVEL 05 Problem 08
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 08
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a loop program to print two digit even numbers, who's sum of digits are 6.
+            Write a loop program to print two digit even numbers, who's sum of digits are 6.
    Answer:
-        24
-        42
-        60
+            24
+            42
+            60
   */
 #include<stdio.h>
+void print_2deven_sofd6(void);
 int main()
 {
-  int number;
-  //your code here
-  number  = 11;
-  for(;;)
+    print_2deven_sofd6();
+}
+void print_2deven_sofd6(void)
+{
+    int n = 11, sum;
+    for(;;)
     {
-      //your code here
-      loop:
-      if(number<100)
-      {
-        if(!(number%2))
+        if(n<61)
         {
-         if((number/10)+(number%10)==6)
-           printf("%d\n",number);
+            if(!(n%2))
+            {
+                sum = (n%10)+(n/10);
+                if(sum==6)
+                            printf("%d\n",n);
+            }
+            n++;
         }
-        number++;
-        goto loop;
-      }
-      break;
-    }  
+        else
+                break;
+    }
 }
