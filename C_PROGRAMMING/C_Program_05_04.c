312cdb83cde9bd2c1c0c55752f11e9483d7b9383
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-25 18:01:19 +0530

    Refactor sum calculation loop in C program
    Refactor loop to calculate sum from 6 to 1 without using goto.

--- C_PROGRAMMING/C_Program_05_04.c
+++ C_PROGRAMMING/C_Program_05_04.c
@@ -1,31 +1,32 @@
-/* C LEVEL 05 Problem 04
-        Use only int variables
-        Use only Decimals
-        Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
-        Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 05 Problem 04
+            Use only int variables
+            Use only Decimals
+            Use "for" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-        Write a loop program to print sum of 6 to 1.
+            Write a loop program to print sum of 6 to 1.
    Answer:
-        21
+            21
   */
 #include<stdio.h>
+void print_sum6to1(void);
 int main()
 {
-  int number;
-  //your code here
-  number  = 6;
-  int sum = 0;
-  for(;;)
+    print_sum6to1();
+}
+void print_sum6to1(void)
+{
+    int n = 6, sum = 0;
+    for(; ;)
     {
-      //your code here
-      loop:
-      if(number>0)
-      {
-        sum = sum + number;
-        number--;
-        goto loop;
-      }
-      break;
+        if(n>=1)
+        {
+            sum += n;
+            n--;
+        }
+        else
+                break;
     }
-    printf("Sum = %d",sum);
+    printf("%d",sum);
 }
