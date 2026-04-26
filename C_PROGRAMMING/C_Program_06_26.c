0c7f32d1a889432815e2a89a56ed5190bd400e9e
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 02:23:28 +0530

    Refactor C program for largest 4-digit divisible number
    Refactor program to find the largest 4-digit number divisible by 7 and 9 using while loops. Removed unnecessary comments and cleaned up code structure.

--- C_PROGRAMMING/C_Program_06_26.c
+++ C_PROGRAMMING/C_Program_06_26.c
@@ -1,35 +1,35 @@
-/* C LEVEL 06 Problem 26
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 06 Problem 26
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to print the biggest 4-digit number which is divisible by 7 and 9
+            Write a program to print the biggest 4-digit number which is divisible by 7 and 9
    Answer:
-         9954
-  */
+            9954
+*/
 #include<stdio.h>
+void printbig4d_divby7_9(void);
 int main()
 {
-  int number;
-  //your code here
-  number = 1000;
-   int big = 0;
-  while(1)
+    printbig4d_divby7_9();
+}
+void printbig4d_divby7_9(void)
+{
+    int big, n = 1000;
+    while(1)
     {
-      //your code here
-      loop:
-      if(number<10000)
-      {
-        if(!(number%7))
+        if(n<10000)
         {
-            if(!(number%9))
-                    big = number;
+            if(!(n%7))
+            {
+                if(!(n%9))
+                        big = n;
+            }
+            n++;
         }
-        number++;
-        goto loop;
-      }
-      break;
+        else break;
     }
-    printf("%d",big);
+    printf("Output: %d",big);
 }
