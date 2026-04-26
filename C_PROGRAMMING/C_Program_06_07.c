4ac9950a64e857635d71d2a6884d3bf565811f73
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 00:00:48 +0530

    Refactor program to print two-digit odd numbers with sum of 7

--- C_PROGRAMMING/C_Program_06_07.c
+++ C_PROGRAMMING/C_Program_06_07.c
@@ -1,33 +1,46 @@
-/*C LEVEL 06 Problem 07
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/*
+  C LEVEL 06 Problem 07
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
   Question: 
-         Write a loop program to print to two digit odd numbers whose sum of digits are 7
+            Write a loop program to print to two digit odd numbers whose sum of digits are 7
   Answer:
-         25
-         43
-         61
-
+            25
+            43
+            61
 */
 #include<stdio.h>
+void print_2dodd_sofd7(void);
+int soofd7(int a);
+int odd(int a);
 int main()
 {
-    int number = 25;
+    print_2dodd_sofd7();
+}
+int soofd7(int a)
+{
+    return (((a%10)+(a/10))==7);
+}
+int odd(int a)
+{
+    return (a%2);
+}
+void print_2dodd_sofd7(void)
+{
+    int n = 11, sum = 0;
     while(1)
     {
-        loop:
-                if(number<100)
-                {
-                    if(number%2)
-                    {
-                        if(((number%10)+(number/10))==7)
-                                printf("%d\n",number);
-                    }
-                    number++;
-                    goto loop;
-                }
-            break;
+        if(n<70)
+        {
+            if(odd(n))
+            {
+                if(soofd7(n))
+                        printf("%d\n",n);
+            }
+            n++;
+        }
+        else break;
     }
 }
