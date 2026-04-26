afaacef6c1c31129d87c1a1e97e3848c1becba41
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 01:51:02 +0530

    Refactor C program to count odd digits

--- C_PROGRAMMING/C_Program_06_21.c
+++ C_PROGRAMMING/C_Program_06_21.c
@@ -1,33 +1,37 @@
-/* C LEVEL 06 Problem 21
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 06 Problem 21
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user and 
-         print the total number of digits which are odd in that number
+            Write a program to get a number from the user and 
+            print the total number of digits which are odd in that number
    Answer:
-         Input: 12345678 Output: 4
-         Input: 987531   Output:5
-  */
+            Input: 12345678 Output: 4
+            Input: 987531   Output:5
+*/
 #include<stdio.h>
+int single_oddcount(int n);
 int main()
 {
-    int number, odd_count = 0;
-    printf("Enter Number: ");
+    int number;
+    printf("Input: ");
     scanf("%d",&number);
+    printf("Output: %d",single_oddcount(number));
+    return 0;
+}
+int single_oddcount(int n)
+{
+    int count = 0;
     while(1)
     {
-        loop:
-                if(number)
-                {
-                    if((number%10)%2)
-                            odd_count++;
-                    number /= 10;
-                    goto loop;
-                }
-        break;
+        if(n)
+        {
+            if((n%10)%2)
+                    count++;
+            n /= 10;
+        }
+        else return count;
     }
-    printf("Output: %d",odd_count);
-  return 0;
 }
