60643bc3c8efda9fb9389758701ad9fe75275b05
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 16:41:49 +0530

    Remove goto and simplify sum calculation
    Refactor loop to calculate sum from 1 to 5 without goto.

--- C_PROGRAMMING/C_Program_06_03.c
+++ C_PROGRAMMING/C_Program_06_03.c
@@ -7,20 +7,3 @@
   Answer:
   15
 */
-#include<stdio.h>
-int main()
-{
-    int number = 1, sum = 0;
-    while(1)
-    {
-        loop:
-        if(number<6)
-        {
-            sum += number;
-            number++;
-            goto loop;
-        }
-        break;
-    }
-    printf("Answer: %d",sum);
-}
