01d70911c75ccb55280b2b24b33cc7bb74d09d69
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 02:56:04 +0530

    Refactor LCM calculation and improve output format

--- C_PROGRAMMING/C_Program_06_28.c
+++ C_PROGRAMMING/C_Program_06_28.c
@@ -1,39 +1,40 @@
-/* C LEVEL 06 Problem 28
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get ttwo numbers from the user and print the LCM of those numbers
-  Answer:
-  Input: 20 40 Output: 40
-  Input: 15 78 Output:390
+/* 
+   C LEVEL 06 Problem 28
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
+   Question: 
+            Write a program to get ttwo numbers from the user and 
+            print the LCM of those numbers
+   Answer:
+            Input: 20 40 Output: 40
+            Input: 15 78 Output:390
   */
 #include<stdio.h>
+int lcm(int n1, int n2);
 int main()
 {
-    int number1, number2, lcm;
-    printf("Enter two numbers: ");
+    int number1, number2;
+    printf("Input: ");
     scanf("%d %d",&number1, &number2);
-    if(number1<number2)
-    {
-        lcm = number2;
-    }
-    else
-    {
-        lcm = number1;
-    }
+    printf("Output: %d",lcm(number1, number2));
+    return 0;
+}
+int lcm(int n1, int n2)
+{
+    int lcm;
+    if(n1>n2) lcm = n1;
+    else lcm = n2;
     while(1)
     {
-        loop:
-            if(!(lcm%number1))
+        if(!(lcm%n1))
+        {
+            if(!(lcm%n2))
             {
-                if(!(lcm%number2))
-                {
-                        break;
-                }
+                return lcm;
             }
+        }
         lcm++;
-        goto loop;
-     }
-    printf("Output: %d",lcm);
+    }
 }
