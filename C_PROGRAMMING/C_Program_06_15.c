fe4dcc6b91f7de4cb67e2a6da4330b5f7239923f
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 00:49:58 +0530

    Refactor C program for even/odd last digit handling

--- C_PROGRAMMING/C_Program_06_15.c
+++ C_PROGRAMMING/C_Program_06_15.c
@@ -1,40 +1,48 @@
-/* C LEVEL 06 Problem 15
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 06 Problem 15
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user and 
-         if the last digit of the number is even print the same number. 
-         If the last digit of the number is odd subtract 1 from the last digit and 
-         print the number.(Note: Last Digit - MSB)
+            Write a program to get a number from the user and 
+            if the last digit of the number is even print the same number. 
+            If the last digit of the number is odd subtract 1 from the last digit and 
+            print the number.(Note: Last Digit - MSB)
   Answer:
-         Input:654324   Output:654324 (last digit = 6)
-         Input:76895439 Output:66895439 (last digit = 7)
-         Input:675      Output:675 (last digit = 6)
-  */
+            Input:654324   Output:654324 (last digit = 6)
+            Input:76895439 Output:66895439 (last digit = 7)
+            Input:675      Output:675 (last digit = 6)
+*/
 #include<stdio.h>
+int even(int a);
+int result(int a);
 int main()
 {
-    int number, last, multiplier = 1, temp, result;
-    printf("Enter Number: ");
+    int number;
+    printf("Input: ");
     scanf("%d",&number);
-    temp = number;
+    printf("Output: %d",result(number));
+    return 0;
+}
+int result(int a)
+{
+    int multiplier = 1, last;
+    last = a;
     while(1)
     {
-        loop:
-                if(temp>9)
-                {
-                    temp /= 10;
-                    multiplier *= 10;
-                    goto loop;
-                }
-            break;
+        if(last>10)
+        {
+            multiplier *= 10;
+            last /= 10;
+        }
+        else break;
     }
-    last = temp;
-    if(last%2)
-            last = last-1;
-    result = (last*multiplier)+(number%multiplier);
-    printf("Output: %d",result);
-    return 0;
+    if(even(last))
+            return a;
+    return (((last-1)*multiplier)+(a%multiplier));
+}
+int even(int a)
+{
+    return (!(a%2));
 }
