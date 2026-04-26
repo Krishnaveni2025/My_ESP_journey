419b7123a0061d601f0c80c477ea889291e5b5bf
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 00:29:59 +0530

    Refactor sum of digits calculation
    Refactor code to remove goto statement and improve readability.

--- C_PROGRAMMING/C_Program_06_12.c
+++ C_PROGRAMMING/C_Program_06_12.c
@@ -1,31 +1,36 @@
-/* C LEVEL 06 Problem 12
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 06 Problem 12
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Get a number from the user print the sum of all digits.
+            Get a number from the user print the sum of all digits.
    Answer:
-         Input:123456   Output:21
-         Input:76895439 Output:51
-         Input:675      Output:18
-  */
+            Input:123456   Output:21
+            Input:76895439 Output:51
+            Input:675      Output:18
+*/
 #include<stdio.h>
+int sumofalldigits(int n);
 int main()
 {
-    int number, sum = 0;
-    printf("Enter Number: ");
+    int number;
+    printf("Input: ");
     scanf("%d",&number);
+    printf("Output: %d",sumofalldigits(number));
+    return 0;
+}
+int sumofalldigits(int n)
+{
+    int sum = 0;
     while(1)
     {
-        loop:
-                if(number)
-                {
-                    sum += number%10;
-                    number /= 10;
-                    goto loop;
-                }
-            break;
+        if(n)
+        {
+            sum += (n%10);
+            n /= 10;
+        }
+        else return sum;
     }
-    printf("Output: %d",sum);
 }
