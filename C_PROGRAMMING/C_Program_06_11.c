3bdba0dca95fd4eb6380e66cf7e3061f8547dea0
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 00:25:29 +0530

    Refactor digit counting logic in C program

--- C_PROGRAMMING/C_Program_06_11.c
+++ C_PROGRAMMING/C_Program_06_11.c
@@ -1,31 +1,35 @@
-/* C LEVEL 06 Problem 11
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 06 Problem 11
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Get a number from the user print the total number of digits in that number.
+            Get a number from the user print the total number of digits in that number.
    Answer:
-         Input:123456   Output:6
-         Input:76895439 Output:8
-         Input:675      Output:3
-  */
+            Input:123456   Output:6
+            Input:76895439 Output:8
+            Input:675      Output:3
+*/
 #include<stdio.h>
+int total_digits(int n);
 int main()
 {
-    int number, total_digits = 0;
-    printf("Enter Number: ");
+    int number;
+    printf("Input: ");
     scanf("%d",&number);
+    printf("Output: %d",total_digits(number));
+}
+int total_digits(int n)
+{
+    int count = 0;
     while(1)
     {
-        loop:
-                if(number)
-                {
-                    total_digits++;
-                    number /= 10;
-                    goto loop;
-                }
-            break;
+        if(n)
+        {
+            count++;
+            n /= 10;
+        }
+        else return count;
     }
-    printf("Output: %d",total_digits);
 }
