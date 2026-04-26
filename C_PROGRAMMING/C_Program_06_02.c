d884d442ad2112c657d1d58600a04ba4ce544635
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 16:38:58 +0530

    Refactor print_5to1 function to remove goto
    Refactor loop to print numbers from 5 to 1 without using goto.

--- C_PROGRAMMING/C_Program_06_02.c
+++ C_PROGRAMMING/C_Program_06_02.c
@@ -1,30 +1,35 @@
-/*C LEVEL 06 Problem 02
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/*
+  C LEVEL 06 Problem 02
+           Use only int variables
+           Use only Decimals
+           Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+           Don't use Arrays, pointers, strings,etc.
   Question: 
-         Write a loop program to print 5 to 1 on one by one
+           Write a loop program to print 5 to 1 on one by one
   Answer:
-         5
-         4
-         3
-         2
-         1
+           5
+           4
+           3
+           2
+           1
 */
 #include<stdio.h>
+void print_5to1(void);
 int main()
 {
-    int number = 5;
+    print_5to1();
+}
+void print_5to1(void)
+{
+    int n = 5;
     while(1)
     {
-        loop:
-        if(number>0)
+        if(n>=1)
         {
-            printf("%d\n",number);
-            number--;
-            goto loop;
+            printf("%d\n",n);
+            n--;
         }
-        break;
+        else
+                break;
     }
 }
