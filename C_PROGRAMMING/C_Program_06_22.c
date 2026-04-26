1852bfe6d9813576d392a7eb36f0fec3add12b31
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 01:55:19 +0530

    Refactor C program to count two-digit odd numbers

--- C_PROGRAMMING/C_Program_06_22.c
+++ C_PROGRAMMING/C_Program_06_22.c
@@ -1,33 +1,37 @@
-/* C LEVEL 06 Problem 22
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 06 Problem 22
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user and 
-         print the total number of two-digit odd numbers in the number
+            Write a program to get a number from the user and 
+            print the total number of two-digit odd numbers in the number
    Answer:
-         Input: 12345678 Output: 3
-         Input: 987531   Output: 4
-  */
+            Input: 12345678 Output: 3
+            Input: 987531   Output: 4
+*/
 #include<stdio.h>
+int twodigit_oddcount(int n);
 int main()
 {
-    int number, odd2d_count = 0;
-    printf("Enter Number: ");
+    int number;
+    printf("Input: ");
     scanf("%d",&number);
+    printf("Output: %d",twodigit_oddcount(number));
+    return 0;
+}
+int twodigit_oddcount(int n)
+{
+    int count = 0;
     while(1)
     {
-        loop:
-                if(number>9)
-                {
-                    if((number%100)%2)
-                         odd2d_count++;
-                    number/=10;
-                    goto loop;
-                }
-        break;
+        if(n>10)
+        {
+            if((n%100)%2)
+                    count++;
+            n /= 10;
+        }
+        else return count;
     }
-    printf("Output: %d",odd2d_count);
-    return 0;
 }
