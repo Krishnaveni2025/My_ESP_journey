8b80c67e2c70e6ca11f895c2f02ce95edbc21949
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 23:45:12 +0530

    Refactor odd number printing to use a separate function

--- C_PROGRAMMING/C_Program_06_05.c
+++ C_PROGRAMMING/C_Program_06_05.c
@@ -1,34 +1,35 @@
-/*C LEVEL 06 Problem 05
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/*
+  C LEVEL 06 Problem 05
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
   Question: 
-         Write a loop program to print to odd numbers between 1 and 9
+            Write a loop program to print to odd numbers between 1 and 9
   Answer:
-         1
-         3
-         5
-         7
-         9
-
+            1
+            3
+            5
+            7
+            9
 */
 #include<stdio.h>
+void print_odd_1to9(void);
 int main()
 {
-  int number = 1;
-  while(1)
-  {
-      loop:
-            if(number<10)
-            {
-                if(number%2)
-                {
-                    printf("%d\n",number);
-                }
-                number++;
-                goto loop;
-            }
-      break;
-  }
+    print_odd_1to9();
+}
+void print_odd_1to9(void)
+{
+    int i = 1;
+    while(1)
+    {
+        if(i<10)
+        {
+            if(i%2)
+                    printf("%d\n",i);
+            i++;
+        }
+        else break;
+    }
 }
