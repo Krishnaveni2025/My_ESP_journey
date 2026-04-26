42243f7b43661857a3177062c5146f91a17130b2
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 23:36:25 +0530

    Refactor program to print sum from 1 to 5

--- C_PROGRAMMING/C_Program_06_03.c
+++ C_PROGRAMMING/C_Program_06_03.c
@@ -1,9 +1,32 @@
-/*C LEVEL 06 Problem 03
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
-  Question: Write a loop program to print sum from 1 to 5
+/*
+  C LEVEL 06 Problem 03
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
+  Question: 
+            Write a loop program to print sum from 1 to 5
   Answer:
-  15
+            15
 */
+#include<stdio.h>
+void print_sum1to5(void);
+int main()
+{
+    print_sum1to5();
+}
+void print_sum1to5(void)
+{
+    int n = 1, sum = 0;
+    while(1)
+    {
+        if(n<6)
+        {
+            sum += n;
+            n++;
+        }
+        else
+                break;
+    }
+    printf("Output: %d",sum);
+}
