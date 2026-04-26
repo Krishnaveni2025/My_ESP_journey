65386ecac536eb3459442d0ab4ebda4bb096c593
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 00:33:16 +0530

    Refactor number reversal program using functions

--- C_PROGRAMMING/C_Program_06_13.c
+++ C_PROGRAMMING/C_Program_06_13.c
@@ -1,32 +1,37 @@
-/* C LEVEL 06 Problem 13
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 06 Problem 13
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user 
-         print the reverse of the number.
+            Write a program to get a number from the user 
+            print the reverse of the number.
    Answer:
-         Input:123456   Output:654321
-         Input:76895439 Output:93453867
-         Input:675      Output:576
-  */
+            Input:123456   Output:654321
+            Input:76895439 Output:93453867
+            Input:675      Output:576
+*/
 #include<stdio.h>
+int reverse(int n);
 int main()
 {
-  int number, reverse = 0;
-  printf("Enter number: ");
-  scanf("%d",&number);
-  while(1)
-  {
-      loop:
-            if(number)
-            {
-                reverse = (reverse*10)+(number%10);
-                number /= 10;
-                goto loop;
-            }
-        break;
-  }
-  printf("Output: %d",reverse);
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    printf("Output: %d",reverse(number));
+    return 0;
+}
+int reverse(int n)
+{
+    int rev = 0;
+    while(1)
+    {
+        if(n)
+        {
+            rev = (rev*10) + (n%10);
+            n /= 10;
+        }
+        else return rev;
+    }
 }
