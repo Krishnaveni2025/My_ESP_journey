0333f0e80dab775d1f74197bc6a7555121f284d4
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-26 23:41:39 +0530

    Refactor loop to calculate sum from 6 to 1

--- C_PROGRAMMING/C_Program_06_04.c
+++ C_PROGRAMMING/C_Program_06_04.c
@@ -1,28 +1,32 @@
-/*C LEVEL 06 Problem 04
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/*
+  C LEVEL 06 Problem 04
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
   Question: 
-         Write a loop program to print sum from 6 to 1
+            Write a loop program to print sum from 6 to 1
   Answer:
-         21
-
+            21
 */
 #include<stdio.h>
+void print_sum6to1(void);
 int main()
 {
-    int number = 6, sum = 0;
+    print_sum6to1();
+}
+void print_sum6to1(void)
+{
+    int n = 6, sum = 0;
     while(1)
     {
-        loop:
-        if(number>0)
+        if(n>=1)
         {
-            sum += number;
-            number--;
-            goto loop;
+            sum += n;
+            n--;
         }
-        break;
+        else break;
     }
-    printf("Answer: %d",sum);
+    printf("Output: %d",sum);
+    
 }
