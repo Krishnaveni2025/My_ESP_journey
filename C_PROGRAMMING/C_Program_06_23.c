0ade76d332cd78a0965dd0c7a1e3317d00e2c412
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 02:03:52 +0530

    Refactor C program for counting perfect squares
    Refactor code to improve readability and structure, removing unnecessary labels and using more standard control flow.

--- C_PROGRAMMING/C_Program_06_23.c
+++ C_PROGRAMMING/C_Program_06_23.c
@@ -1,40 +1,45 @@
-/* C LEVEL 06 Problem 23
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 06 Problem 23
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user and 
-         print the total number of single digit perfect square numbers in the number
+            Write a program to get a number from the user and 
+            print the total number of single digit perfect square numbers in the number
    Answer:
-         Input: 123456789 Output: 3
-         Input: 987531    Output: 2
-  */
+            Input: 123456789 Output: 3
+            Input: 987531    Output: 2
+*/
 #include<stdio.h>
+int single_perfectCount(int n);
 int main()
 {
-    int number, single_pscount = 0, i=1;
-    printf("Enter Number: ");
+    int number;
+    printf("Input: ");
     scanf("%d",&number);
+    printf("Output: %d",single_perfectCount(number));
+    return 0;
+}
+int single_perfectCount(int n)
+{
+    int i, count = 0;
     while(1)
     {
-        loop:
-                if(number)
+        if(n)
+        {
+            i = 1;
+            while(1)
+            {
+                if(i<4)
                 {
-                    i = 1;
-                    ploop:
-                            if(i<4)
-                            {
-                                if((number%10)==(i*i))
-                                        single_pscount++;
-                                i++;
-                                goto ploop;
-                            }
-                    number /= 10;
-                    goto loop;
+                    if((i*i)==(n%10))
+                            count++;
+                    i++;
                 }
-        break;
+                else break;
+            }
+            n /= 10;
+        }else return count;
     }
-    printf("Output: %d",single_pscount);
-  return 0;
 }
