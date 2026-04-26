d64a5ccfdd8ceb23d44b4ecdd16cda0a4f1b57da
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 03:04:42 +0530

    Refactor HCF program for clarity and structure
    Refactor HCF calculation to use while loop instead of goto and improve input/output prompts.

--- C_PROGRAMMING/C_Program_06_30.c
+++ C_PROGRAMMING/C_Program_06_30.c
@@ -1,35 +1,37 @@
-/* C LEVEL 06 Problem 30
-         Use only int variables
-         Use only Decimals
-         Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
-         Don't use Arrays, pointers, strings,etc.
-  Question: Write a program to get two numbers from the user and print the HCF of those numbers
-  Answer:
-  Input: 20 40  Output: 20
-  Input: 15 78  Output: 3
-  */
+/* 
+   C LEVEL 06 Problem 30
+            Use only int variables
+            Use only Decimals
+            Use "while" statements to solve the problems. "If" also can be used to decision making but not for loop.
+            Don't use Arrays, pointers, strings,etc.
+   Question: 
+            Write a program to get two numbers from the user and 
+            print the HCF of those numbers
+   Answer:
+            Input: 20 40  Output: 20
+            Input: 15 78  Output: 3
+*/
 #include<stdio.h>
+int hcf2(int n1, int n2);
 int main()
 {
-    int number1, number2, hcf;
-    printf("Enter Two Numbers: ");
+    int number1, number2;
+    printf("Input: ");
     scanf("%d %d",&number1, &number2);
-    for(;;)
+    printf("Output: %d",hcf2(number1, number2));
+    return 0;
+}
+int hcf2(int n1, int n2)
+{
+    while(1)
     {
-        loop:
-        if(number2!=number1)
+        if(n1!=n2)
         {
-            if(number1>number2)
-            {
-                number1 -= number2;
-            }
-            else
-            {
-                number2 -= number1;
-            }
-            goto loop;
+            if(n1>n2)
+                    n1-=n2;
+            else    
+                    n2-=n1;
         }
-        break;
+        else return n1;
     }
-    printf("Output: %d",number1);
 }
