dea1255c1d8c96efb5fd551e12e77a24b74b604c
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-27 23:35:55 +0530

    Refactor comments and struct definition in C program
    Updated comments for clarity and fixed syntax errors in the struct definition.

--- C_PROGRAMMING/C_Program_13_01.c
+++ C_PROGRAMMING/C_Program_13_01.c
@@ -1,8 +1,9 @@
-/* C LEVEL 13 - Problem 01
-Create a linked list for the following structure
-and get user inputs id, math mark, science mark.
-once the user inputs the id as -1
-exit the input mmode and display all the entries.
+/* 
+    C LEVEL 13 - Problem 01
+        Create a linked list for the following structure and 
+        get user inputs id, math mark, science mark.
+        once the user inputs the id as -1 exit the input mode and 
+        display all the entries.
 ********************Hint**********************************
 struct student{
     int id;
@@ -11,3 +12,28 @@ struct student{
     struct student *next;
 }
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+struct student{
+    int id;
+    int math;
+    int science,
+    struct student *next;
+};
+int main()
+{
+    struct student *head = NULL;
+    int inputID;
+    while(1)
+    {
+        printf("Enter ID: ");
+        scanf("%d",&inputID);
+        if(inputID == -1)
+        {
+            break;
+        }
+        head = malloc((sizeof(struct student)));
+        
+    }
+}
