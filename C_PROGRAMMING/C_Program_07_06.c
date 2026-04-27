d0ff61c3f99d9870793da1029c97b7d660f874f0
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 01:49:43 +0530

    Format comments and improve readability

--- C_PROGRAMMING/C_Program_07_06.c
+++ C_PROGRAMMING/C_Program_07_06.c
@@ -1,18 +1,19 @@
-/* C LEVEL 07 Problem 06
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 06
+                  Use only int variables
+                  Use only Decimals
+                  Use functions with not return value.
+                  Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a loop program to print two digit odd numbers, below 20
+                  Write a loop program to print two digit odd numbers, below 20
    Function Name: 
-         disp_2digit_odd_below20
+                  disp_2digit_odd_below20
    Answer:
-         11
-         13
-         15
-         17
-         19
+                  11
+                  13
+                  15
+                  17
+                  19
 */
 #include<stdio.h>
 void disp_2digit_odd_below20(int n);
