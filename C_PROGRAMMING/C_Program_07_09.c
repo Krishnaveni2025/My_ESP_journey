1a76133c8561904d847727ca57c00a553a213c26
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 01:52:13 +0530

    Format comments and clean up code style

--- C_PROGRAMMING/C_Program_07_09.c
+++ C_PROGRAMMING/C_Program_07_09.c
@@ -1,14 +1,15 @@
-/* C LEVEL 07 Problem 09
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 09
+                  Use only int variables
+                  Use only Decimals
+                  Use functions with not return value.
+                  Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a loop program to print the sum of two digit numbers whose one's digit is 5
+                  Write a loop program to print the sum of two digit numbers whose one's digit is 5
    Function Name: 
-         disp_2digit_ones5
+                  disp_2digit_ones5
    Answer:
-         495
+                  495
 */
 #include<stdio.h>
 void disp_2digit_ones5(int n);
