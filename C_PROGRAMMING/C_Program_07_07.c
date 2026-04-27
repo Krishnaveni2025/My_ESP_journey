4a2b9b190022735b66ccdb591333e7c5d24e711a
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 01:50:30 +0530

    Refactor comments and formatting in C program

--- C_PROGRAMMING/C_Program_07_07.c
+++ C_PROGRAMMING/C_Program_07_07.c
@@ -1,16 +1,17 @@
-/* C LEVEL 07 Problem 07
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+  C LEVEL 07 Problem 07
+                  Use only int variables
+                  Use only Decimals
+                  Use functions with not return value.
+                  Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a loop program to print the two-digit odd numbers, who's sum of digits are 7
+                  Write a loop program to print the two-digit odd numbers, who's sum of digits are 7
    Function Name: 
-         disp_2digit_odd_sum7
+                  disp_2digit_odd_sum7
    Answer: 
-         25
-         43
-         61
+                  25
+                  43
+                  61
 */
 #include<stdio.h>
 void disp_2digit_odd_sum7(int n);
