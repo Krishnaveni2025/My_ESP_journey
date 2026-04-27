2ebaedaa32d377bd19b479183b4b706aa9760ecc
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 01:55:31 +0530

    Update comments for clarity in C program

--- C_PROGRAMMING/C_Program_07_13.c
+++ C_PROGRAMMING/C_Program_07_13.c
@@ -1,16 +1,17 @@
-/* C LEVEL 07 Problem 13
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 13
+                  Use only int variables
+                  Use only Decimals
+                  Use functions with not return value.
+                  Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user print the reverse of the number.
+                  Write a program to get a number from the user print the reverse of the number.
    Function Name: 
-         disp_reverse_number
+                  disp_reverse_number
    Answer:
-         Input:123456   Output:654321
-         Input:76895439 Output:93453867
-         Input:675      Output:576
+                  Input:123456   Output:654321
+                  Input:76895439 Output:93453867
+                  Input:675      Output:576
   */
 #include<stdio.h>
 void disp_reverse_number(int n);
