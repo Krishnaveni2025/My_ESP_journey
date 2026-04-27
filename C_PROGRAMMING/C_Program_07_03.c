c1ff03c0d8856482c06042986e602ca5c174ea38
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 01:46:21 +0530

    Update comments in C_Program_07_03.c

--- C_PROGRAMMING/C_Program_07_03.c
+++ C_PROGRAMMING/C_Program_07_03.c
@@ -1,14 +1,14 @@
 /* C LEVEL 07 Problem 03
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+                 Use only int variables
+                 Use only Decimals
+                 Use functions with not return value.
+                 Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a loop program to print sum from 1 to 5
+                 Write a loop program to print sum from 1 to 5
    Function Name: 
-         disp_sum
+                 disp_sum
    Answer:
-         15
+                 15
 */
 #include<stdio.h>
 void disp_sum(int n);
