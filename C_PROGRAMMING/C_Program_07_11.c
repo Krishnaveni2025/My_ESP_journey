b96f972120d6afc4cd5f2c8b796951cae9688e19
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 01:54:02 +0530

    Fix formatting and comments in C program

--- C_PROGRAMMING/C_Program_07_11.c
+++ C_PROGRAMMING/C_Program_07_11.c
@@ -1,17 +1,18 @@
-/* C LEVEL 07 Problem 11
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+  C LEVEL 07 Problem 11
+                  Use only int variables
+                  Use only Decimals
+                  Use functions with not return value.
+                  Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a loop program to get a number from the user and 
-         print the total digits in the number
+                  Write a loop program to get a number from the user and 
+                  print the total digits in the number
    Function Name: 
-         count_total_digits
+                  count_total_digits
    Answer:
-         Input:123456   Output:6
-         Input:76895439 Output:8
-         Input:675      Output:3
+                  Input:123456   Output:6
+                  Input:76895439 Output:8
+                  Input:675      Output:3
 */
 #include<stdio.h>
 void count_total_digits(int n);
