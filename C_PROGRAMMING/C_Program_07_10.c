02b164f055b3a869dff35215c429932fe09c5210
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 01:53:13 +0530

    Format comments and improve readability in C program

--- C_PROGRAMMING/C_Program_07_10.c
+++ C_PROGRAMMING/C_Program_07_10.c
@@ -1,14 +1,15 @@
-/* C LEVEL 07 Problem 10
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 10
+                  Use only int variables
+                  Use only Decimals
+                  Use functions with not return value.
+                  Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a loop program to print the sum of two digit odd numbers, whose ten's digit is 7
+                  Write a loop program to print the sum of two digit odd numbers, whose ten's digit is 7
    Function Name: 
-         disp_2digit_odd_sum_tens7
+                  disp_2digit_odd_sum_tens7
    Answer:
-         375
+                  375
 */
 #include<stdio.h>
 void disp_2digit_odd_sum_tens7(int n);
