acde0dd2eeea11d005c942a7dc14c90b109c48f1
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 15:54:06 +0530

    Refactor prime checking function and update output

--- C_PROGRAMMING/C_Program_07_16.c
+++ C_PROGRAMMING/C_Program_07_16.c
@@ -1,40 +1,40 @@
-/* C LEVEL 07 Problem 16
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 16
+                 Use only int variables
+                 Use only Decimals
+                 Use functions with not return value.
+                 Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user and 
-         print whether that number is prime or not.
+                 Write a program to get a number from the user and 
+                 print whether that number is prime or not.
    Function Name: 
-         check_prime
+                 check_prime
    Answer:
-         Input: 31 - Output: Prime
-         Input: 27 - Output: Not prime
+                 Input: 31 - Output: Prime
+                 Input: 27 - Output: Not prime
 */
 #include<stdio.h>
-void check_prime(int n);
+void check_prime(int a);
 int main()
 {
-   int number;
-   printf("Enter Number: ");
-   scanf("%d",&number);
-   check_prime(number);
+    int number;
+    printf("Input: ");
+    scanf("%d",&number);
+    check_prime(number);
 }
-void check_prime(int n)
+void check_prime(int a)
 {
-    int is_prime = 1, divider = 2;
-    while(divider<n)
+    int is_prime = 1, i;
+    for(i = 2; i<a; i++)
     {
-        if(!(n%divider))
+        if(!(a%i))
         {
             is_prime = 0;
             break;
         }
-        divider++;
     }
     if(is_prime)
-            printf("Prime");
-    else    
-            printf("Not Prime");
+            printf("Output: Prime");
+    else
+            printf("Output: Not Prime");
 }
