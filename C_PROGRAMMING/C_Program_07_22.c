578d23ae5c4c96c72cf798c27e0502da461c4c72
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 17:20:33 +0530

    Refactor function and update comments in C program

--- C_PROGRAMMING/C_Program_07_22.c
+++ C_PROGRAMMING/C_Program_07_22.c
@@ -1,36 +1,35 @@
-/* C LEVEL 07 Problem 22
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 22
+                    Use only int variables
+                    Use only Decimals
+                    Use functions with not return value.
+                    Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user and 
-         print the total number of two-digit odd numbers in the number
+                    Write a program to get a number from the user and 
+                    print the total number of two-digit odd numbers in the number
    Function Name: 
-         disp_total_2digit_odd
+                    disp_total_2digit_odd
    Answer:
-         Input: 12345678 Output: 3
-         Input: 987531   Output: 4
-  */
+                    Input: 12345678 Output: 3
+                    Input: 987531   Output: 4
+*/
 #include<stdio.h>
-void disp_total_2digit_odd(int n);
+void disp_total_2digit_odd(int a);
 int main()
 {
     int number;
-    printf("Enter Number: ");
+    printf("Input: ");
     scanf("%d",&number);
     disp_total_2digit_odd(number);
 }
-void disp_total_2digit_odd(int n)
+void disp_total_2digit_odd(int a)
 {
-    int odd2_count = 0;
-    while(n>9)
+    int count = 0;
+    while(a>10)
     {
-        if((n%100)%2)
-        {
-            odd2_count++;
-        }
-        n /= 10;
+        if((a%100)%2)
+                count++;
+        a /= 10;
     }
-    printf("%d",odd2_count);
+    printf("Output: %d",count);
 }
