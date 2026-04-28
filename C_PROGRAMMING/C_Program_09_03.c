efd03a26ac2ffefe06eee13f4f0978d669e981e6
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 21:45:27 +0530

    Refactor to find smallest number using disp_small

--- C_PROGRAMMING/C_Program_09_03.c
+++ C_PROGRAMMING/C_Program_09_03.c
@@ -1,43 +1,40 @@
 /*
   C LEVEL 09 Problem 03
-          Use only int variables
-          Use only decimals
-          Use arrays
+           Use only int variables
+           Use only decimals
+           Use arrays
   Question: 
-          Get 5 numbers from the user and print the smallest number
+           Get 5 numbers from the user and print the smallest number
 */
 #include<stdio.h>
-
+#define size 5
+int disp_small(int a[]);
 void get5numbers(int a[]);
-int findsmall(int a[],int s);
-void print(int s);
-
 int main()
 {
-    int number[5], small;
-    get5numbers(number);
-    small = findsmall(number, small);
-    print(small);
-}
-int findsmall(int a[], int s)
-{
-    s = a[0];
-    for(int j = 1; j<5; j++)
-    {
-        if(s>a[j])
-                s = a[j];
-    }
-    return s;
+    int numbers[size];
+    get5numbers(numbers);
+    printf("Output: %d",disp_small(numbers));
+    return 0;
 }
 void get5numbers(int a[])
 {
-    for(int i = 0; i<5; i++)
+    int i;
+    for(i = 0; i<size; i++)
     {
         printf("Enter %d Number: ",i+1);
         scanf("%d",&a[i]);
     }
 }
-void print(int s)
+int disp_small(int a[])
 {
-    printf("Smallest Number: %d",s);
+    int i, small = a[0];
+    for(i = 1; i<size; i++)
+    {
+        if(small>a[i])
+        {
+            small = a[i];
+        }
+    }
+    return small;
 }
