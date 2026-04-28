c301e602660aeda6e2faee9b7fd5f2105fb54309
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 19:26:58 +0530

    Refactor input/output format in C program

--- C_PROGRAMMING/C_Program_08_20.c
+++ C_PROGRAMMING/C_Program_08_20.c
@@ -1,27 +1,26 @@
-/* C LEVEL 08 Problem 20
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+  C LEVEL 08 Problem 20
+                Use only int variables
+                Use only Decimals
+                Use Functions with a return value
+                Do not use arrays, strings, pointers, etc.
   Question: 
-        Write a program to get two numbers from the user and 
-        print the HCF of those numbers
+                Write a program to get two numbers from the user and 
+                print the HCF of those numbers
   Function Name: 
-        disp_count_HCF2
+                disp_count_HCF2
   Answer:
-        Input: 20 40  Output: 20
-        Input: 15 78  Output: 3
+                Input: 20 40  Output: 20
+                Input: 15 78  Output: 3
   */
 #include<stdio.h>
 int disp_count_HCF2(int n1, int n2);
 int main()
 {
     int number1, number2;
-    printf("Enter Number: ");
-    scanf("%d",&number1);
-    printf("Enter Number: ");
-    scanf("%d",&number2);
-    printf("%d",disp_count_HCF2(number1,number2));
+    printf("Input: ");
+    scanf("%d %d",&number1, &number2);
+    printf("Output: %d",disp_count_HCF2(number1,number2));
 }
 int disp_count_HCF2(int n1, int n2)
 {
