c6ee73eccfd69b2a7f44e19b8f0e720fced7b9f1
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 22:47:34 +0530

    Refactor C program to remove odd numbers from array

--- C_PROGRAMMING/C_Program_09_08.c
+++ C_PROGRAMMING/C_Program_09_08.c
@@ -1,57 +1,62 @@
 /*
   C LEVEL 09 Problem 08
-    Use only int variables
-    Use only decimals
-    Use arrays
-  Question: Get 5 numbers from the user and 
-            remove the odd  numbers and 
-            create a new array and 
-            then  print the same
+             Use only int variables
+             Use only decimals
+             Use arrays
+  Question: 
+             Get 5 numbers from the user and 
+             remove the odd  numbers and 
+             create a new array and 
+             then  print the same
   Example:
-        Input:  15 14 13 12 11
-        Output: 14 12
+             Input:  15 14 13 12 11
+             Output: 14 12
 */
 #include<stdio.h>
-
-void getnumbers(int a[], int s);
-void print_array(int a[], int s);
-int removeOddNumbers(int a[], int b[], int s);
-
+#define size 5
+int remove_odd(int a[], int r[]);
+int odd(int a);
+void print(int a[], int s);
+void getnumbers(int a[]);
 int main()
 {
-    int size = 5, evencount;
-    int numbers[size], evenNumbers[size];
-    getnumbers(numbers,size);
-    print_array(numbers, size);
-    evencount = removeOddNumbers(numbers, evenNumbers, size);
-    print_array(evenNumbers, evencount);
+    int numbers[size], result[size], even;
+    getnumbers(numbers);
+    even = remove_odd(numbers, result);
+    print(result, even);
 }
-int removeOddNumbers(int a[], int b[], int s)
+int remove_odd(int a[], int r[])
 {
-    int ec = 0;
-    for(int i = 0; i<s; i++)
+    int i, count = 0;
+    for(i = 0; i<size; i++)
     {
-        if(!(a[i]%2))
+        if(!(odd(a[i])))
         {
-            b[ec] = a[i];
-            ec++;
-        }  
+            r[count] = a[i];
+            count++;
+        }
     }
-    return ec;
+    return count;
 }
-void print_array(int a[], int s)
+void print(int a[], int s)
 {
-    printf("\n");
-    for(int i = 0; i<s; i++)
+    int i;
+    printf("Output: ");
+    for(i = 0; i<s; i++)
     {
         printf("%d ",a[i]);
     }
 }
-void getnumbers(int a[], int s)
+int odd(int a)
 {
-    for(int i = 0; i<s; i++)
+    return (a%2);
+}
+void getnumbers(int a[])
+{
+    int i;
+    printf("Input: ");
+    for(i = 0; i<size; i++)
     {
-        printf("Enter %d Number: ",i+1);
         scanf("%d",&a[i]);
     }
 }
