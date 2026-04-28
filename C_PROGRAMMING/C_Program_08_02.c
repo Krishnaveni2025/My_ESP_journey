75dc1738186a93ab9e94f54314973c86a880a3a6
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 18:19:34 +0530

    Refactor comments and improve output formatting

--- C_PROGRAMMING/C_Program_08_02.c
+++ C_PROGRAMMING/C_Program_08_02.c
@@ -1,20 +1,23 @@
-/* C LEVEL 08 Problem 02
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 02
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a loop program to print sum of 6 to 1
+                    Write a loop program to print sum of 6 to 1
    Function Name: 
-        disp_rsum
+                    disp_rsum
    Answer:
-        21
+                    21
 */
 #include<stdio.h>
 int disp_rsum(int n);
 int main()
 {
-    printf("%d",disp_rsum(6));
+    int number = 6;
+    printf("Output: %d",disp_rsum(number));
+    return 0;
 }
 int disp_rsum(int n)
 {
