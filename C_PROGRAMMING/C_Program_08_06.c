0eeeb88e03feacc3dd75b811be9c4e98eaf5f3c2
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 18:35:42 +0530

    Refactor input/output prompts in C program

--- C_PROGRAMMING/C_Program_08_06.c
+++ C_PROGRAMMING/C_Program_08_06.c
@@ -1,25 +1,27 @@
-/* C LEVEL 08 Problem 06
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 06
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Get a number from the user print the sum of all digits.
+                    Get a number from the user print the sum of all digits.
    Function Name: 
-        disp_sum_all_digits
+                    disp_sum_all_digits
    Answer:
-        Input:123456   Output:21
-        Input:76895439 Output:51
-        Input:675      Output:18
-  */
+                    Input:123456   Output:21
+                    Input:76895439 Output:51
+                    Input:675      Output:18
+*/
 #include<stdio.h>
 int disp_sum_all_digits(int n);
 int main()
 {
     int number;
-    printf("Enter Number: ");
+    printf("Input: ");
     scanf("%d",&number);
-    printf("%d",disp_sum_all_digits(number));
+    printf("Output: %d",disp_sum_all_digits(number));
+    return 0;
 }
 int disp_sum_all_digits(int n)
 {
