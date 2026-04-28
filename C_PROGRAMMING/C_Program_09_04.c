7774eb19a428e134db547c1948d644f58a3d0554
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 21:53:37 +0530

    Refactor to improve function usage and clarity

--- C_PROGRAMMING/C_Program_09_04.c
+++ C_PROGRAMMING/C_Program_09_04.c
@@ -1,43 +1,41 @@
 /*
   C LEVEL 09 Problem 04
-          Use only int variables
-          Use only decimals
-          Use arrays
+           Use only int variables
+           Use only decimals
+           Use arrays
   Question: 
-          Get 5 numbers from the user and print the biggest number
+           Get 5 numbers from the user and print the biggest number
 */
 #include<stdio.h>
-
+#define size 5
+int disp_biggest(int a[]);
 void get5numbers(int a[]);
-int findbigg(int a[],int b);
-void print(int b);
-
 int main()
 {
-    int number[5], biggest;
-    get5numbers(number);
-    biggest = findbigg(number, biggest);
-    print(biggest);
-}
-int findbigg(int a[], int b)
-{
-    b = a[0];
-    for(int j = 1; j<5; j++)
-    {
-        if(b<a[j])
-                b = a[j];
-    }
-    return b;
+    int numbers[size];
+    get5numbers(numbers);
+    printf("Output: %d",disp_biggest(numbers));
+    return 0;
 }
 void get5numbers(int a[])
 {
-    for(int i = 0; i<5; i++)
+    int i;
+    for(i = 0; i<size; i++)
     {
         printf("Enter %d Number: ",i+1);
         scanf("%d",&a[i]);
     }
 }
-void print(int b)
+int disp_biggest(int a[])
 {
-    printf("Biggest Number: %d",b);
+    int big = a[0];
+    int i;
+    for(i = 1; i<size; i++)
+    {
+        if(big<a[i])
+        {
+            big = a[i];
+        }
+    }
+    return big;
 }
