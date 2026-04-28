3b9ae923aebd4ce4c969e2b9f464af22cff87391
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 19:23:32 +0530

    Refactor input handling for LCM program

--- C_PROGRAMMING/C_Program_08_18.c
+++ C_PROGRAMMING/C_Program_08_18.c
@@ -1,26 +1,25 @@
-/* C LEVEL 08 Problem 18
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 18
+                 Use only int variables
+                 Use only Decimals
+                 Use Functions with a return value
+                 Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a program to get two numbers from the user and 
-        print the LCM of those numbers
+                 Write a program to get two numbers from the user and 
+                 print the LCM of those numbers
    Function Name: 
-        disp_LCM2
+                 disp_LCM2
    Answer:
-        Input: 20 40 Output: 40
-        Input: 15 78 Output:390
-  */
+                 Input: 20 40 Output: 40
+                 Input: 15 78 Output:390
+*/
 #include<stdio.h>
 int disp_LCM2(int n1, int n2);
 int main()
 {
     int number1, number2;
-    printf("Enter Number: ");
-    scanf("%d",&number1);
-    printf("Enter Number: ");
-    scanf("%d",&number2);
+    printf("Input: ");
+    scanf("%d %d",&number1, &number2);
     printf("%d",disp_LCM2(number1, number2));
 }
 int disp_LCM2(int n1, int n2)
