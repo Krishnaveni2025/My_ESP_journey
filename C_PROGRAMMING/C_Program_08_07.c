48979a93374efc8c4769447dd00cc688c2c160f0
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 18:38:57 +0530

    Refactor number reversal program and update output format

--- C_PROGRAMMING/C_Program_08_07.c
+++ C_PROGRAMMING/C_Program_08_07.c
@@ -1,33 +1,35 @@
-/* C LEVEL 08 Problem 07
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 07
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a program to get a number from the user print the reverse of the number.
+                    Write a program to get a number from the user print the reverse of the number.
    Function Name: 
-        disp_reverse_number
+                    disp_reverse_number
    Answer:
-        Input:123456   Output:654321
-        Input:76895439 Output:93453867
-        Input:675      Output:576
-  */
+                    Input:123456   Output:654321
+                    Input:76895439 Output:93453867
+                    Input:675      Output:576
+*/
 #include<stdio.h>
 int disp_reverse_number(int n);
 int main()
 {
     int number;
-    printf("Enter Number: ");
+    printf("Input: ");
     scanf("%d",&number);
-    printf("%d",disp_reverse_number(number));
+    printf("Output: %d",disp_reverse_number(number));
+    return 0;
 }
 int disp_reverse_number(int n)
 {
-    int reverse = 0;
+    int rev = 0;
     while(n)
     {
-        reverse = (reverse*10)+(n%10);
+        rev = (rev*10)+(n%10);
         n /= 10;
     }
-    return reverse;
+    return rev;
 }
