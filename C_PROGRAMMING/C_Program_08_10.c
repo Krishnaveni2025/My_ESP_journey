8c5255190fc5a7af35c7cb202fc807ab2cd79378
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 18:56:37 +0530

    Refactor single_prime_count function and output format

--- C_PROGRAMMING/C_Program_08_10.c
+++ C_PROGRAMMING/C_Program_08_10.c
@@ -1,37 +1,41 @@
-/* C LEVEL 08 Problem 10
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 10
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a program to print total number of single digit prime numbers
+                    Write a program to print total number of single digit prime numbers
    Function Name: 
-        single_prime_count
+                    single_prime_count
    Answer:
-        4
+                    4
 */
 #include<stdio.h>
 int single_prime_count(int n);
 int main()
 {
-    printf("%d",single_prime_count(2));
+    int number = 2;
+    printf("Output: %d",single_prime_count(number));
+    return 0;
 }
 int single_prime_count(int n)
 {
-    int is_prime , divider, count = 0;
-    for(n=2; n<9; n++)
+    int i, count = 0, is_prime = 1;
+    while(n<9)
     {
         is_prime = 1;
-       for(divider = 2; divider<n; divider++)
-         {
-            if(!(n%divider))
-                {
-                    is_prime = 0;
-                    break;
-                }
-         }
-         if(is_prime)
-                count++;
+        for(i = 2; i<n; i++)
+        {
+            if(!(n%i))
+            {
+                is_prime = 0;
+                break;
+            }
+        }
+        if(is_prime)
+                    count++;
+        n++;
     }
     return count;
 }
