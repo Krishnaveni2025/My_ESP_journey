cb492946b978aa6a84101e6eab11f39e0eaa1dd9
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 18:28:43 +0530

    Refactor C program for clarity and output format

--- C_PROGRAMMING/C_Program_08_04.c
+++ C_PROGRAMMING/C_Program_08_04.c
@@ -1,20 +1,23 @@
-/* C LEVEL 08 Problem 04
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 04
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a program to print the sum of the two digit odd numbers whose tens digit is 7
+                    Write a program to print the sum of the two digit odd numbers whose tens digit is 7
    Function Name: 
-        disp_2digit_odd_sum_tens7
+                    disp_2digit_odd_sum_tens7
    Answer:
-        375
+                    375
 */
 #include<stdio.h>
 int disp_2digit_odd_sum_tens7(int n);
 int main()
 {
-    printf("%d",disp_2digit_odd_sum_tens7(70));
+    int number = 70;
+    printf("Output: %d",disp_2digit_odd_sum_tens7(number));
+    return 0;
 }
 int disp_2digit_odd_sum_tens7(int n)
 {
