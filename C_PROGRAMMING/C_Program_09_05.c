2bbf4db230f9059fbeeb27441f73a26b818ada9a
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 22:09:46 +0530

    Refactor C program for sorting numbers

--- C_PROGRAMMING/C_Program_09_05.c
+++ C_PROGRAMMING/C_Program_09_05.c
@@ -1,55 +1,54 @@
 /*
   C LEVEL 09 Problem 05
-        Use only int variables
-        Use only decimals
-        Use arrays
+            Use only int variables
+            Use only decimals
+            Use arrays
   Question: 
-        Get 5 numbers from the user and arrange in ascending order and print the same
+            Get 5 numbers from the user and arrange in ascending order and print the same
 */
 #include<stdio.h>
-
-void printaarray(int a[],int s);
-void getnumbers(int a[], int s);
-void arrange_ascend(int a[], int s);
-
+#define size 5
+void arrange_ascend(int a[]);
+void getnumbers(int a[]);
+void print(int a[]);
 int main()
 {
-    int size = 5;
-    int numbers[size];
-    getnumbers(numbers, size);
-    printaarray(numbers,size);
-    arrange_ascend(numbers,size);
-    printaarray(numbers,size);
+    int numbers[5];
+    getnumbers(numbers);
+    arrange_ascend(numbers);
+    print(numbers);
+    return 0;
 }
-void arrange_ascend(int a[], int s)
+void print(int a[])
 {
-    int temp;
-    for(int i = 0; i<s; i++)
-    {
-        for(int j = i+1; j<s; j++)
-        {
-            if(a[j]<a[i])
-            {
-                temp = a[j];
-                a[j] = a[i];
-                a[i] = temp;
-            }
-        }
-    }
-}
-void printaarray(int a[], int s)
-{
-    printf("\n");
-    for(int i = 0; i<s; i++)
+    int i;
+    for(i = 0; i<size; i++)
     {
         printf("%d ",a[i]);
     }
 }
-void getnumbers(int a[], int s)
+void getnumbers(int a[])
 {
-    for(int i = 0; i<s; i++)
+    int i;
+    for(i = 0; i<size; i++)
     {
         printf("Enter %d Number: ",i+1);
         scanf("%d",&a[i]);
+    }
+}
+void arrange_ascend(int a[])
+{
+    int i, j, temp;
+    for(i = 0; i<size; i++)
+    {
+        for(j = i+1; j<size; j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
     }
 }
