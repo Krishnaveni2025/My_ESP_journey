0e18bc4a621c8ddcd04f76f485b66b8d354bb438
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 17:54:47 +0530

    Refactor C program to improve variable usage

--- C_PROGRAMMING/C_Program_07_27.c
+++ C_PROGRAMMING/C_Program_07_27.c
@@ -1,38 +1,39 @@
-/* C LEVEL 07 Problem 27
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 27
+                    Use only int variables
+                    Use only Decimals
+                    Use functions with not return value.
+                    Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to print the total count of numbers 
-         which are less than 100000 and whose sum of digits is 14
+                    Write a program to print the total count of numbers 
+                    which are less than 100000 and whose sum of digits is 14
    Function Name: 
-         disp_count_sum14
+                    disp_count_sum14
    Answer:
-         2710
-  */
-
+                    2710
+*/
 #include<stdio.h>
-void disp_count_sum14(int n);
+void disp_count_sum14(int a);
 int main()
 {
-    disp_count_sum14(59);
+    int number = 100000;
+    disp_count_sum14(number);
 }
-void disp_count_sum14(int n)
+void disp_count_sum14(int a)
 {
-    int count = 0, sum,temp;
-    while(n<100000)
+    int count = 0, sum, temp;
+    while(a>58)
     {
         sum = 0;
-        temp = n;
+        temp = a;
         while(temp)
         {
             sum += (temp%10);
-            temp/=10;
+            temp /= 10;
         }
         if(sum==14)
                 count++;
-        n++;
+        a--;
     }
-    printf("%d",count);
+    printf("Output: %d",count);
 }
