a367f3ff1b1cc587c864000410ece62d67cd3b35
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-29 03:49:30 +0530

    Refactor number input and result calculation logic

--- C_PROGRAMMING/C_Program_09_14.c
+++ C_PROGRAMMING/C_Program_09_14.c
@@ -8,30 +8,36 @@
             Stop when the number is 0. 
             If total number is odd then print middle number, 
             otherwise print the average of middle two numbers 
+  Example:
+            Input: 1 2 3 4 5  0     Output: 3
+            Input: 10 20 30 40 0    Output: 25
 */
 #include<stdio.h>
-
-void getnumbers(int a[]);
-int total;
+#define MAX 50
+void result(int a[]);
 int main()
 {
-    int numbers[50];
-    getnumbers(numbers);
-    if(total%2)
-            printf("Total number is odd\nMiddle = %d",numbers[total/2]);
-    else
-            printf("Total number is Even\nAverage = %d",((numbers[total/2])+(numbers[(total/2)-1]))/2);
+    int numbers[MAX];
+    result(numbers);
 }
-void getnumbers(int a[])
+void result(int a[])
 {
-    int number;
+    int number, total = 0;
+    printf("Input: ");
     while(1)
     {
-        printf("Enter %d Number: ",total+1);
         scanf("%d",&number);
-        if(!number)
-                break;
+        if(!(number)) break;
         a[total] = number;
         total++;
+    }
+    printf("Output: ");
+    if(total%2)
+    {
+        printf("%d",a[total/2]);
+    }
+    else
+    {
+        printf("%d",((a[total/2])+(a[(total/2)-1]))/2);
     }
 }
