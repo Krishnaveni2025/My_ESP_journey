f3f5f252f1918840a512b072e2d79262d5805801
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 19:11:34 +0530

    Refactor comments and improve output formatting

--- C_PROGRAMMING/C_Program_08_14.c
+++ C_PROGRAMMING/C_Program_08_14.c
@@ -1,34 +1,36 @@
-/* C LEVEL 08 Problem 14
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 14
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a program to get a number from the user and 
-        print the total number of two digit perfect square numbers in the number
+                    Write a program to get a number from the user and 
+                    print the total number of two digit perfect square numbers in the number
    Function Name: 
-        disp_two_digit_square
+                    disp_two_digit_square
    Answer:
-        Input: 163496481 Output: 4
-        Input: 364925    Output: 4
-  */
+                    Input: 163496481 Output: 4
+                    Input: 364925    Output: 4
+*/
 #include<stdio.h>
 int disp_two_digit_square(int n);
 int main()
 {
     int number;
-    printf("Enter Number: ");
+    printf("Input: ");
     scanf("%d",&number);
-    printf("%d",disp_two_digit_square(number));
+    printf("Output: %d",disp_two_digit_square(number));
+    return 0;
 }
 int disp_two_digit_square(int n)
 {
-    int count = 0;
-    while(n>10)
+    int i, count = 0;
+    while(n>15)
     {
-        for(int i=4; i<10; i++)
+        for(i = 4; i<10; i++)
         {
-            if((n%100)==(i*i))
+            if((i*i)==(n%100))
                     count++;
         }
         n /= 10;
