cdb9e88414c105fc9c20d59bd365f85f2135eb1c
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 16:51:16 +0530

    Refactor check_first_2digits_prime function

--- C_PROGRAMMING/C_Program_07_18.c
+++ C_PROGRAMMING/C_Program_07_18.c
@@ -1,43 +1,40 @@
-/* C LEVEL 07 Problem 18
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 18
+                 Use only int variables
+                 Use only Decimals
+                 Use functions with not return value.
+                 Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user and print whether the first two digits(1's & ten's) of that number is prime or not.
+                 Write a program to get a number from the user and print whether the first two digits(1's & ten's) of that number is prime or not.
    Function Name: 
-         check_first_2digits_prime
+                 check_first_2digits_prime
    Answer:
-         Input: 359 - Output: Prime
-         Input: 3577 - Output: Not prime
+                 Input: 359 - Output: Prime
+                 Input: 3577 - Output: Not prime
 */
 #include<stdio.h>
-void check_first_2digits_prime(int n);
+void check_first_2digits_prime(int a);
 int main()
 {
     int number;
+    printf("Input: ");
     scanf("%d",&number);
     check_first_2digits_prime(number);
+    return 0;
 }
-void check_first_2digits_prime(int n)
+void check_first_2digits_prime(int a)
 {
-//your code here
-  n %= 100;
-  int divider = 2, is_prime = 1;
-  for(divider = 2; divider<n; divider++)
+    int is_prime = 1, i;
+    for(i = 2; (i*i)<=(a%100); i++)
     {
-      if((n%divider)==0)
-      {
-        is_prime = 0;
-        break;
-      }
+        if(!((a%100)%i))
+        {
+            is_prime = 0;
+            break;
+        }
     }
-  if(is_prime)
-  {
-    printf("Prime");
-  }
-  else
-  {
-    printf("Not Prime");
-  }
+    if(is_prime)
+                printf("Output: prime");
+    else
+                printf("Output: Not prime");
 }
