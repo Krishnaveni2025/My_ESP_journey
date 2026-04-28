acfe3fb36e96d60b9196c556d050516970bda3f1
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 19:07:41 +0530

    Refactor C program for perfect square counting

--- C_PROGRAMMING/C_Program_08_13.c
+++ C_PROGRAMMING/C_Program_08_13.c
@@ -1,34 +1,36 @@
-/* C LEVEL 08 Problem 13
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 13
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a program to get a number from the user and 
-        print the total number of single digit perfect square numbers in the number
+                    Write a program to get a number from the user and 
+                    print the total number of single digit perfect square numbers in the number
    Function Name:  
-        disp_single_digit_square
-  Answer:
-        Input: 123456789 Output: 3
-        Input: 987531    Output: 2
-  */
+                    disp_single_digit_square
+   Answer:
+                    Input: 123456789 Output: 3
+                    Input: 987531    Output: 2
+*/
 #include<stdio.h>
 int disp_single_digit_square(int n);
 int main()
 {
     int number;
-    printf("Enter Number: ");
+    printf("Input: ");
     scanf("%d",&number);
-    printf("%d",disp_single_digit_square(number));
+    printf("Output: %d",disp_single_digit_square(number));
+    return 0;
 }
 int disp_single_digit_square(int n)
 {
-    int count = 0;
+    int i, count = 0;
     while(n)
     {
-        for(int i = 1; i<4; i++)
+        for(i = 1; i<4; i++)
         {
-            if((n%10)==(i*i))
+            if((i*i)==(n%10))
                     count++;
         }
         n /= 10;
