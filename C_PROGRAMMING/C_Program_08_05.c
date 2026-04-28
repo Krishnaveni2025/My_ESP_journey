0c08fe2c550626ffd433a87375e2c6d62bc49c3d
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 18:32:32 +0530

    Refactor comments and update input/output prompts

--- C_PROGRAMMING/C_Program_08_05.c
+++ C_PROGRAMMING/C_Program_08_05.c
@@ -1,26 +1,28 @@
-/* C LEVEL 08 Problem 05
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 05
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a program to get a number from the user and 
-        print the total number of digits in that number
+                    Write a program to get a number from the user and 
+                    print the total number of digits in that number
    Function Name: 
-        count_total_digits
+                    count_total_digits
    Answer:
-        Input:123456   Output:6
-        Input:76895439 Output:8
-        Input:675      Output:3
+                    Input:123456   Output:6
+                    Input:76895439 Output:8
+                    Input:675      Output:3
 */
 #include<stdio.h>
 int count_total_digits(int n);
 int main()
 {
     int number;
-    printf("Enter Number: ");
+    printf("Input: ");
     scanf("%d",&number);
-    printf("%d",count_total_digits(number));
+    printf("Output: %d",count_total_digits(number));
+    return 0;
 }
 int count_total_digits(int n)
 {
