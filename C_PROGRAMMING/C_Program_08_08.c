bc9e7a8bfdf998a06cec33bcc32b510ea5475702
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 18:45:46 +0530

    Refactor comments and update print statements

--- C_PROGRAMMING/C_Program_08_08.c
+++ C_PROGRAMMING/C_Program_08_08.c
@@ -1,36 +1,37 @@
-/* C LEVEL 08 Problem 08
-        Use only int variables
-        Use only Decimals
-        Use Functions with a return value
-        Do not use arrays, strings, pointers, etc.
+/* 
+   C LEVEL 08 Problem 08
+                    Use only int variables
+                    Use only Decimals
+                    Use Functions with a return value
+                    Do not use arrays, strings, pointers, etc.
    Question: 
-        Write a program to get a number from the user and 
-        interchange the first and last digits and print the result 
+                    Write a program to get a number from the user and 
+                    interchange the first and last digits and print the result 
    Function Name: 
-        disp_interchange_first_last_digit
+                    disp_interchange_first_last_digit
    Answer:
-        Input:123456   Output:623451
-        Input:76895439 Output:96895437
-        Input:675      Output:576
-  */
+                    Input:123456   Output:623451
+                    Input:76895439 Output:96895437
+                    Input:675      Output:576
+*/
 #include<stdio.h>
 int disp_interchange_first_last_digit(int n);
 int main()
 {
     int number;
-    printf("Enter Number: ");
+    printf("Input: ");
     scanf("%d",&number);
-    printf("%d",disp_interchange_first_last_digit(number));
+    printf("Output: %d",disp_interchange_first_last_digit(number));
+    return 0;
 }
 int disp_interchange_first_last_digit(int n)
 {
-    int last, first, multiplier = 1;
-    first = n%10;
+    int last, multiplier = 1;
     last = n;
     while(last>9)
     {
         multiplier *= 10;
         last /= 10;
     }
-    return ((first*multiplier)+(((n%multiplier)/10)*10)+last);
+    return (((n%10)*multiplier)+(((n%multiplier)/10)*10)+last);
 }
