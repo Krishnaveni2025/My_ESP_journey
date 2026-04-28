0e493f8f35823f0fc9f5dde138af26a1069095d5
Krishnaveni <krishnavenipurne@gmail.com>
2026-04-28 17:32:24 +0530

    Refactor function and improve comments in C program

--- C_PROGRAMMING/C_Program_07_24.c
+++ C_PROGRAMMING/C_Program_07_24.c
@@ -1,37 +1,39 @@
-/* C LEVEL 07 Problem 24
-         Use only int variables
-         Use only Decimals
-         Use functions with not return value.
-         Don't use Arrays, pointers, strings,etc.
+/* 
+   C LEVEL 07 Problem 24
+                    Use only int variables
+                    Use only Decimals
+                    Use functions with not return value.
+                    Don't use Arrays, pointers, strings,etc.
    Question: 
-         Write a program to get a number from the user 
-         and print the total number of two digit perfect square numbers in the number
+                    Write a program to get a number from the user and 
+                    print the total number of two digit perfect square numbers in the number
    Function Name: 
-         disp_two_digit_square
+                    disp_two_digit_square
    Answer:
-         Input: 163496481 Output: 4
-         Input: 364925    Output: 4
-  */
+                    Input: 163496481 Output: 4
+                    Input: 364925    Output: 4
+*/
 #include<stdio.h>
-void disp_two_digit_square(int n);
+void disp_two_digit_square(int a);
 int main()
 {
     int number;
-    printf("Enter Number: ");
+    printf("Input: ");
     scanf("%d",&number);
     disp_two_digit_square(number);
+    return 0;
 }
-void disp_two_digit_square(int n)
+void disp_two_digit_square(int a)
 {
-    int tps_count = 0,i;
-    while(n>9)
+    int count = 0, i;
+    while(a>15)
     {
-        for(i=4; i<10; i++)
+        for(i = 4; i<10; i++)
         {
-            if((n%100)==(i*i))
-                    tps_count++;
+            if((i*i)==(a%100))
+                    count++;
         }
-        n/=10;
+        a /= 10;
     }
-    printf("%d",tps_count);
+    printf("Output: %d",count);
 }
