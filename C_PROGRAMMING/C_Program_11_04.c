4a3b31da9e98119dab7e78c2060715e6d9879296
Krishnaveni <krishnavenipurne@gmail.com>
2026-05-10 11:46:59 +0530

    Refactor function to find two-digit odd numbers with sum 7

--- C_PROGRAMMING/C_Program_11_04.c
+++ C_PROGRAMMING/C_Program_11_04.c
@@ -1,4 +1,5 @@
-/* C LEVEL 11 Problem 04
+/* 
+   C LEVEL 11 Problem 04
                  Use Pointers
    Question: 
                  Write a function to find the two digit odd numbers whose sum of digits is 7. 
@@ -11,21 +12,24 @@
                  61
 */
 #include<stdio.h>
-int find_2digit_odd_sum7(int *number);
+int find_2digit_odd_sum7(int *a);
 int main()
 {
-    for(int i = 25; i<62; i++)
+    int i;
+    for(i = 25; i<62; i++)
     {
         if(find_2digit_odd_sum7(&i))
                 printf("%d\n",i);
     }
-}
-int find_2digit_odd_sum7(int *number)
-{
-    if((*number)%2)
-    {
-        if(((*number)%10)+((*number)/10)==7)
-                return 1;
-    }
+            
     return 0;
+}
+int find_2digit_odd_sum7(int *i)
+{
+    if(*i%2)
+        {
+            if((*i/10)+(*i%10) == 7)
+                    return 1;  
+        }  
+    return 0; 
 }
