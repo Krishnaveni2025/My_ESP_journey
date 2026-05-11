61312593d59bbc5fef183aa6159f6910fab213c9
Krishnaveni <krishnavenipurne@gmail.com>
2026-05-11 16:53:17 +0530

    Refactor string handling functions and variables

--- C_PROGRAMMING/C_Program_11_10.c
+++ C_PROGRAMMING/C_Program_11_10.c
@@ -7,41 +7,43 @@
                  strcon(src1, src2, dst)
 */
 #include<stdio.h>
-void getstr(char *source1, char *source2);
-void strcon(char *source1, char *source2, char *destination);
+#define s1 10
+#define s2 10
+#define s s1+s2
+void getTwostrs(char *src1, char *src2);
+void strcon(char *src1, char *src2, char *dst);
 void print(char *str);
 int main()
 {
-    char src1[51], src2[51],dst[102];
-    getstr(src1,src2);
-    print(src1);
-    print(src2);
-    strcon(src1, src2, dst);
-    print(dst);
+    char source1[s1], source2[s2], destination[s];
+    getTwostrs(source1, source2);
+    strcon(source1, source2, destination);
+    print(destination);
+    return 0;
 }
 void print(char *str)
 {
-    printf("\n%s",str);
+    printf("Destination: %s",str);
 }
-void strcon(char *source1, char *source2, char *destination)
+void strcon(char *src1, char *src2, char *dst)
 {
-    int l1 = 0,l2 = 0;
-    while(*(source1+l1)!='\0')
+    int l1, l2;
+    while(*(src1+l1)!='\0')
     {
-        *(destination+l1) = *(source1+l1);
+        *(dst+l1) = *(src1+l1);
         l1++;
     }
-    while(*(source2+l2)!='\0')
+    while(*(src2+l2)!='\0')
     {
-        *(destination+l1+l2) = *(source2+l2);
+        *(dst+l1+l2) = *(src2+l2);
         l2++;
     }
-    *(destination+l1+l2) = '\0';
+    *(dst+l1+l2) = '\0';
 }
-void getstr(char *source1, char *source2)
+void getTwostrs(char *src1, char *src2)
 {
     printf("Source 1: ");
-    scanf("%s",source1);
+    scanf("%s",src1);
     printf("Source 2: ");
-    scanf("%s",source2);
+    scanf("%s",src2);
 }
