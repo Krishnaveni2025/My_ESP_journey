0d872486daf23b5870f74f0b8acae9017e9e72c6
Krishnaveni <krishnavenipurne@gmail.com>
2026-05-11 14:51:04 +0530

    Refactor integer comparison and input functions

--- C_PROGRAMMING/C_Program_11_07.c
+++ C_PROGRAMMING/C_Program_11_07.c
@@ -6,58 +6,40 @@
    Function Name: 
                   memcomp(src,dst,size)
 */
-void print(int *a, int s);
-int memcomp(int *source, int *destination, int length);
-void getint(int *set1, int *set2, int s);
-
 #include<stdio.h>
+#define Max_size 8
+int memcomp(int *src, int *dst, int size);
+void getsetint(int *src, int *dst, int size);
 int main()
 {
-    int size,equal;
-    printf("Enter size of the set: ");
-    scanf("%d",&size);
-    int src[size],dst[size];
-    getint(src,dst,size);
-    equal = memcomp(src, dst,size);
-    print(src,size);
-    print(dst,size);
-    if(equal)
-            printf("\nsuccess");
+    int source[Max_size], destination[Max_size];
+    getsetint(source, destination, Max_size);
+    if(memcomp(source, destination, Max_size))
+                printf("Success");
     else
-            printf("\nfailure");
+                printf("Failure");
     return 0;
-    
 }
-void getint(int *set1, int *set2, int s)
+void getsetint(int *src, int *dst, int size)
 {
-    int i = 0, j = 0;
-    printf("First Set\n");
-    for( i = 0; i<s; i++)
+    int i;
+    printf("Integer 1: ");
+    for(i = 0; i<size; i++)
     {
-        printf("Enter %d Number: ",i+1);
-        scanf("%d",&set1[i]);
+        scanf("%d",&src[i]);
     }
-    printf("Second Set\n");
-    for(j = 0; j<s; j++)
+    printf("Integer 2: ");
+    for(i = 0; i<size; i++)
     {
-        printf("Enter %d Number: ",j+1);
-        scanf("%d",&set2[j]);
+        scanf("%d",&dst[i]);
     }
 }
-int memcomp(int *source, int *destination, int length)
+int memcomp(int *src, int *dst, int size)
 {
-    for(int i = 0; i<length; i++)
+    for(int i = 0; i<size; i++)
     {
-        if(*(source+i)!= *(destination+i))
+        if(src[i]!=dst[i])
                 return 0;
     }
     return 1;
-}
-void print(int *a, int s)
-{
-    printf("\n");
-    for(int i = 0; i<s; i++)
-    {
-        printf("%d ",a[i]);
-    }
 }
