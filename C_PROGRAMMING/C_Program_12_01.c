3140290a97639ce6052ba8e10e2e89c6512326aa
Krishnaveni <krishnavenipurne@gmail.com>
2026-05-14 05:57:23 +0530

    Refactor number handling functions in C program

--- C_PROGRAMMING/C_Program_12_01.c
+++ C_PROGRAMMING/C_Program_12_01.c
@@ -11,69 +11,48 @@
                  print(result);
              }
 */
+#include<stdio.h>
+#define SIZE 50
 void getnumbers(char *n1, char *n2);
-void print(char *str);
-void strcopy(char *src, char *dst);
-void reverse(char *str);
-int getlen(char *str);
 void addnumbers(char *n1, char *n2, char *r);
-void removeleading0s(char *num);
+void removeleading0s(char *n);
+void strcopy(char *src, char *dst);
+void reverse(char *n);
+int getlength(char *n);
+void print(char *n);
 #include<stdio.h>
 int main()
 {
-    char number1[51], number2[51], result[52];
+    char number1[SIZE], number2[SIZE], result[SIZE+1];
     getnumbers(number1, number2);
     addnumbers(number1, number2, result);
     print(result);
 }
-void removeleading0s(char *num)
-{
-    int i = 0;
-    int l = getlen(num);
-    while(num[i]=='0'&& i<l-1)
-    {
-        i++;
-    }
-    if(i>0)
-    {
-        strcopy(&num[i], num);
-    }
-}
 void addnumbers(char *n1, char *n2, char *r)
 {
-    int d1, d2, l1, l2,l=0, sum, carry = 0;
-    l1 = getlen(n1)-1;
-    l2 = getlen(n2)-1;
+    int l1, l2, l = 0, d1, d2, sum, carry = 0;
+    l1 = getlength(n1)-1;
+    l2 = getlength(n2)-1;
     while(l1>=0||l2>=0||carry>0)
     {
         d1 = (l1>=0) ? n1[l1--]-'0': 0;
         d2 = (l2>=0) ? n2[l2--]-'0': 0;
-        sum = d1+d2+carry;
-        carry = sum/10;
+        sum = d1 + d2 + carry;
+        carry = (sum/10);
         r[l++] = (sum%10)+'0';
     }
     r[l] = '\0';
     reverse(r);
 }
-int getlen(char *str)
+void removeleading0s(char *n)
 {
-    int l = 0;
-    while (*(str+l)!='\0')
+    int l, i = 0;
+    l = getlength(n);
+    while(n[i]=='0'&& i<l)
     {
-        l++;
+        i++;
     }
-    return l;
-}
-void reverse(char *str)
-{
-    int l = getlen(str);
-    int temp;
-    for(int i = 0; i<l/2; i++)
-    {
-        temp = str[i];
-        str[i] = str[l-i-1];
-        str[l-i-1] = temp;
-    }
+    strcopy(&n[i], n);
 }
 void strcopy(char *src, char *dst)
 {
@@ -81,17 +60,37 @@ void strcopy(char *src, char *dst)
     {
         *(dst++) = *(src++);
     }
-    *dst = '\0';
+    *dst = '\0';    
 }
-void print(char *str)
+void reverse(char *n)
 {
-    removeleading0s(str);
-    printf("%s",str);
+    int l, i, temp;
+    l = getlength(n);
+    for(i = 0; i<l/2; i++)
+    {
+        temp = n[i];
+        n[i] = n[l-i-1];
+        n[l-i-1] = temp;
+    }
+}
+int getlength(char *n)
+{
+    int l = 0;
+    while(*(n+l)!='\0')
+    {
+        l++;
+    }
+    return l;
 }
 void getnumbers(char *n1, char *n2)
 {
-    printf("First Number\n");
+    printf("Number 1: ");
     scanf("%s",n1);
-    printf("Second Number\n");
+    printf("Number 2: ");
     scanf("%s",n2);
+}
+void print(char *n)
+{
+    removeleading0s(n);
+    printf("Result: %s",n);
 }
