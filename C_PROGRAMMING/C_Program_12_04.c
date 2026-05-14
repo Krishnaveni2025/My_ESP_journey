80e47174fd021d44705e5265a2a047f55f642a66
Krishnaveni <krishnavenipurne@gmail.com>
2026-05-14 16:51:27 +0530

    Refactor word counting logic in C program

--- C_PROGRAMMING/C_Program_12_04.c
+++ C_PROGRAMMING/C_Program_12_04.c
@@ -8,27 +8,24 @@
             Answer: 4
 */
 #include<stdio.h>
-void print_noofwords(char *str);
 int main()
 {
-    char line[51];
-    printf("String: ");
-    gets(line);
-    print_noofwords(line);
-}
-void print_noofwords(char *str)
-{
-    int i = 0, count = 0;
-    while(*(str+i)!='\0')
-    {
-        if(*(str+i)!=' '&& *(str+i)!='\t')
-        {
-            if(i==0 || *(str+i-1)==' ' || *(str+i-1)=='\t')
-            {
-                count++;
-            }
-        }
-        i++;
-    }
-    printf("Answer: %d",count);
+   char str[100];
+   int i = 0, count = 0;
+   printf("String: ");
+   gets(str);
+   while(*(str+i)!= '\0')
+   {
+      if(*(str+i) != ' ')
+      {
+         if(*(str+i) != '\t')
+         {
+            if(i == 0 || *(str+i-1) == ' ' || *(str+i-1) == '\t')
+                        count++;
+         }
+      }
+      i++;
+   }
+   printf("Output: %d",count);
+   return 0;
 }
