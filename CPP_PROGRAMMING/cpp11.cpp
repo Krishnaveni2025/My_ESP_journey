cade4d5250bddc5c74171a44e7211e4fc31ccb33
Krishnaveni <krishnavenipurne@gmail.com>
2026-05-24 20:47:41 +0530

    Create cpp11.cpp

---
+++ CPP_PROGRAMMING/cpp11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+public:
+    // your code here: implement void printCategory() to print "Generic Vehicle"
+    void printCategory(){
+        cout<<"Generic Vehicle\n";
+    }
+};
+
+// your code here: define class Car inheriting publicly from Vehicle
+// implement void printCar() to print "Car Vehicle"
+class Car : public Vehicle{
+    public:
+        void printCar(){
+            cout<<"Car Vehicle\n";
+        }
+};
+int main() {
+    Car c;
+    c.printCategory(); // inherited method
+    c.printCar();      // derived method
+    return 0;
+}
