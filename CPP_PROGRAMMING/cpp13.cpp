9b99b4c0e017e52455395520775752e1049d665f
Krishnaveni <krishnavenipurne@gmail.com>
2026-05-27 22:01:48 +0530

    Add Actuator class with showActuator method
    Defined Actuator class with private inheritance from Device and implemented showActuator() method.

---
+++ CPP_PROGRAMMING/cpp13.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Device {
+public:
+    void showType() {
+        cout << "Generic Device\n";
+    }
+};
+
+// your code here: define class Actuator with private inheritance from Device
+// implement showActuator() that calls showType() and then prints "Actuator Device"
+class Actuator: private Device{
+    public:
+        void showActuator()
+        {
+            showType();
+            cout<<"Actuator Device";
+        }
+};
+int main() {
+    Actuator a;
+    a.showActuator();  // should print both lines
+    return 0;
+}
