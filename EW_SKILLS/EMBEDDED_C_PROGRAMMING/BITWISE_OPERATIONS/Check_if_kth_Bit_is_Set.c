7354935221d4464911c95515465836ad36e9ca7a
Krishnaveni <krishnavenipurne@gmail.com>
2026-08-25 10:21:10 +0530

    Add program to check K-th bit status in integer
    Implement a C program to check if the K-th bit of an integer N is set.

---
+++ EW_SKILLS/EMBEDDED_C_PROGRAMMING/BITWISE_OPERATIONS/Check_if_kth_Bit_is_Set.c
@@ -0,0 +1,20 @@
+/*
+3. Check if K-th Bit is Set
+Write a C program to check if the K-th bit (0-based index) of an integer N is set (1) or not (0).
+
+Input Format
+
+Two integers N and K.
+Output Format
+
+Print 1 if the K-th bit of Integer N is set (1), otherwise print 0.
+ 
+
+Example
+
+Input N= 8 &  K= 3
+
+Here Binary value of 8 is 00001000
+
+So output will be 1
+*/
