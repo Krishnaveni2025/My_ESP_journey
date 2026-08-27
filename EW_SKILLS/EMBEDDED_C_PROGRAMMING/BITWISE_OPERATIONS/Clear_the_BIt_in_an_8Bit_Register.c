09ee456389d5e97f9772da984b7be48595543ecf
Krishnaveni <krishnavenipurne@gmail.com>
2026-08-27 22:18:15 +0530

    Implement clear_bit function to clear a specific bit

--- EW_SKILLS/EMBEDDED_C_PROGRAMMING/BITWISE_OPERATIONS/Clear_the_BIt_in_an_8Bit_Register.c
+++ EW_SKILLS/EMBEDDED_C_PROGRAMMING/BITWISE_OPERATIONS/Clear_the_BIt_in_an_8Bit_Register.c
@@ -15,3 +15,18 @@ Example 2
 Input: reg = 0b00001111, pos = 3 
 Output: 0b00000111
 */
+#include <stdio.h>
+#include <stdint.h>
+
+uint8_t clear_bit(uint8_t reg, uint8_t pos) {
+    // Your code here
+    return (reg & ~(1<<pos));
+}
+
+int main() {
+    uint8_t reg, pos;
+    scanf("%hhu %hhu", &reg, &pos);
+    uint8_t result = clear_bit(reg, pos);
+    printf("%u", result);
+    return 0;
+}
