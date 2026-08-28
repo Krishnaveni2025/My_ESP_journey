e47c8ea92dac5b8e7177a3a9dfc877042645ccd4
Krishnaveni <krishnavenipurne@gmail.com>
2026-08-28 18:47:24 +0530

    Add documentation for Set_Multiple_Bits function
    Added a comment block explaining the function to set multiple bits in an 8-bit register.

---
+++ EW_SKILLS/EMBEDDED_C_PROGRAMMING/BITFIELD_OPERATIONS/Set_Multiple_Bits_in_8Bit_Register.c
@@ -0,0 +1,20 @@
+/*
+18. Set Multiple Bits in 8-bit Register
+You are given an 8-bit register. Set all bits between position start and end (inclusive).
+
+Use 0-based indexing and assume start <= end.
+
+
+Example 1
+
+Input: reg = 0b00000000, start = 1, end = 3 
+Output: 0b00001110
+Example 2
+
+Input: reg = 0b00001000, start = 0, end = 2 
+Output: 0b00001111
+Example 3
+
+Input: reg = 0b00000001, start = 3, end = 5 
+Output: 0b00111001
+  */
