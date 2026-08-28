2f7f70327fd4f8203919734d225eab70c1130677
Krishnaveni <krishnavenipurne@gmail.com>
2026-08-28 18:49:51 +0530

    Add function to clear specific bits in a 32-bit register
    This file contains a function to clear specific bits in a 32-bit register based on given position and length.

---
+++ EW_SKILLS/EMBEDDED_C_PROGRAMMING/BITFIELD_OPERATIONS/Clear_Specific_Bits_in_a_32Bit_Register.c
@@ -0,0 +1,16 @@
+/*
+20. Clear Specific Bits in a 32-bit Register
+You are given a 32-bit control register. Clear a group of bits (set them to 0) starting at a given position and length.
+
+Other bits must stay untouched.
+
+
+Example 1
+
+Input: reg = 0b1111 1111, pos = 4, len = 4 
+Output: 0b0000 1111
+Example 2 
+
+Input: reg = 0b0000 1111, pos = 0, len = 2 
+Output: 0b0000 1100
+*/
