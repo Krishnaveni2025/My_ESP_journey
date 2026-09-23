c02e571e5da7fa959645c053b3dadcaca034037d
Krishnaveni <krishnavenipurne@gmail.com>
2026-09-23 06:27:33 +0530

    Add Reference Alias Update for integer addition
    Implement a C++ reference alias for integer addition.

---
+++ EW_SKILLS/CPP_for_Embedded_Systems/References/Reference_Alias_Update.c
@@ -0,0 +1,26 @@
+/*
+3. Reference Alias Update
+Given two signed 32-bit integers a and b, create a C++ reference that aliases a.
+Update the value of a by adding b only through this reference.
+
+Do not modify a directly after the reference is created.
+
+Print the updated value of a.
+
+Example 
+
+Input:
+
+5 3
+Output:
+
+8
+ 
+
+Constraints:
+
+a and b are signed 32-bit integers (int32_t)
+Modification of a must occur only through a C++ reference
+Do not use pointers
+Assume no integer overflow occurs
+*/
