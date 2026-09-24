c2fe8267d6ec1c4108fcca54b878374f9ca6b125
Krishnaveni <krishnavenipurne@gmail.com>
2026-09-24 22:43:22 +0530

    Implement increment functions for pointers and references
    Added functions to increment values using pointers and references.

--- EW_SKILLS/CPP_for_Embedded_Systems/References/Reference_Alias_Update.cpp
+++ EW_SKILLS/CPP_for_Embedded_Systems/References/Reference_Alias_Update.cpp
@@ -24,3 +24,33 @@ Modification of a must occur only through a C++ reference
 Do not use pointers
 Assume no integer overflow occurs
 */
+#include <iostream>
+
+// Define incrementPtr(int* x)
+// Increment the value pointed to by x
+// Do nothing if x is nullptr
+void incrementPtr(int* x){
+    if(x)
+            (*x)++;
+    else
+            return;
+}
+// Define incrementRef(int& x)
+// Increment the referenced value
+void incrementRef(int& x){
+    x++;
+}
+int main() {
+    int n;
+    std::cin >> n;
+
+    int a = n;
+    incrementPtr(&a);
+    std::cout << "After incrementPtr: " << a << "\n";
+
+    int b = n;
+    incrementRef(b);
+    std::cout << "After incrementRef: " << b;
+
+    return 0;
+}
